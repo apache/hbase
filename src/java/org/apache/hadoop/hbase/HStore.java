6d2b6bd072e8b7c1b8a1514db8310fd6febed97f
Michael Stack <stack@apache.org>
2008-03-31 19:19:53 +0000

    HBASE-550 EOF trying to read reconstruction log stops region deployment M  src/java/org/apache/hadoop/hbase/HStore.java     (Constructor) If an exception out of reconstructionLog method, log it and     keep going.  Presumption is that its result of a lack of HADOOP--1700.     (reconstructionLog): Check for empty log file.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/branches/0.1@643107 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/HStore.java
+++ src/java/org/apache/hadoop/hbase/HStore.java
@@ -40,6 +40,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.filter.RowFilterInterface;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
@@ -740,10 +741,7 @@ public class HStore implements HConstants {
     }
 
     if(LOG.isDebugEnabled()) {
-      LOG.debug("starting " + storeName +
-          ((reconstructionLog == null || !fs.exists(reconstructionLog)) ?
-          " (no reconstruction log)" :
-            " with reconstruction log: " + reconstructionLog.toString()));
+      LOG.debug("starting " + storeName);
     }
 
     // Go through the 'mapdir' and 'infodir' together, make sure that all 
@@ -770,7 +768,16 @@ public class HStore implements HConstants {
           this.maxSeqId);
     }
     
-    doReconstructionLog(reconstructionLog, maxSeqId);
+    try {
+      doReconstructionLog(reconstructionLog, maxSeqId);
+    } catch (IOException e) {
+      // Presume we got here because of some HDFS issue or because of a lack of
+      // HADOOP-1700; for now keep going but this is probably not what we want
+      // long term.  If we got here there has been data-loss
+      LOG.warn("Exception processing reconstruction log " + reconstructionLog +
+        " opening " + this.storeName +
+        " -- continuing.  Probably DATA LOSS!", e);
+    }
 
     // By default, we compact if an HStore has more than
     // MIN_COMMITS_FOR_COMPACTION map files
@@ -830,6 +837,12 @@ public class HStore implements HConstants {
     
     if (reconstructionLog == null || !fs.exists(reconstructionLog)) {
       // Nothing to do.
+      return;
+    }
+    // Check its not empty.
+    FileStatus[] stats = fs.listStatus(reconstructionLog);
+    if (stats == null || stats.length == 0) {
+      LOG.warn("Passed reconstruction log " + reconstructionLog + " is zero-length");
       return;
     }
     long maxSeqIdInLog = -1;
