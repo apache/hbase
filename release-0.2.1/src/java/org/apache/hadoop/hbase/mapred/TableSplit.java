96de179a0df9fca255a324ecb0fbb0f187c2d9f0
Jim Kellerman <jimk@apache.org>
2008-09-13 18:18:30 +0000

    HBase 0.2.1 release.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/tags@694998 13f79535-47bb-0310-9956-ffa450edef68

---
+++ release-0.2.1/src/java/org/apache/hadoop/hbase/mapred/TableSplit.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright 2007 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.mapred;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapred.InputSplit;
+
+/**
+ * A table split corresponds to a key range [low, high)
+ */
+public class TableSplit implements InputSplit {
+  private byte [] m_tableName;
+  private byte [] m_startRow;
+  private byte [] m_endRow;
+
+  /** default constructor */
+  public TableSplit() {
+    this(HConstants.EMPTY_BYTE_ARRAY, HConstants.EMPTY_BYTE_ARRAY,
+      HConstants.EMPTY_BYTE_ARRAY);
+  }
+
+  /**
+   * Constructor
+   * @param tableName
+   * @param startRow
+   * @param endRow
+   */
+  public TableSplit(byte [] tableName, byte [] startRow, byte [] endRow) {
+    m_tableName = tableName;
+    m_startRow = startRow;
+    m_endRow = endRow;
+  }
+
+  /** @return table name */
+  public byte [] getTableName() {
+    return m_tableName;
+  }
+
+  /** @return starting row key */
+  public byte [] getStartRow() {
+    return m_startRow;
+  }
+
+  /** @return end row key */
+  public byte [] getEndRow() {
+    return m_endRow;
+  }
+
+  /** {@inheritDoc} */
+  public long getLength() {
+    // Not clear how to obtain this... seems to be used only for sorting splits
+    return 0;
+  }
+
+  /** {@inheritDoc} */
+  public String[] getLocations() {
+    // Return a random node from the cluster for now
+    return new String[] { };
+  }
+
+  /** {@inheritDoc} */
+  public void readFields(DataInput in) throws IOException {
+    this.m_tableName = Bytes.readByteArray(in);
+    this.m_startRow = Bytes.readByteArray(in);
+    this.m_endRow = Bytes.readByteArray(in);
+  }
+
+  /** {@inheritDoc} */
+  public void write(DataOutput out) throws IOException {
+    Bytes.writeByteArray(out, this.m_tableName);
+    Bytes.writeByteArray(out, this.m_startRow);
+    Bytes.writeByteArray(out, this.m_endRow);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public String toString() {
+    return Bytes.toString(m_tableName) +"," + Bytes.toString(m_startRow) +
+      "," + Bytes.toString(m_endRow);
+  }
+}
---
+++ release-0.2.1/src/java/org/apache/hadoop/hbase/master/MetaScanner.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright 2008 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.master;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.apache.hadoop.hbase.RemoteExceptionHandler;
+
+/**
+ * MetaScanner <code>META</code> table.
+ * 
+ * When a <code>META</code> server comes on line, a MetaRegion object is
+ * queued up by regionServerReport() and this thread wakes up.
+ *
+ * It's important to do this work in a separate thread, or else the blocking 
+ * action would prevent other work from getting done.
+ */
+class MetaScanner extends BaseScanner {
+  /** Initial work for the meta scanner is queued up here */
+  private volatile BlockingQueue<MetaRegion> metaRegionsToScan =
+    new LinkedBlockingQueue<MetaRegion>();
+    
+  private final List<MetaRegion> metaRegionsToRescan =
+    new ArrayList<MetaRegion>();
+    
+  /** Constructor */
+  public MetaScanner(HMaster master, RegionManager regionManager) {
+    super(master, regionManager, false, master.metaRescanInterval, master.closed);
+  }
+
+  // Don't retry if we get an error while scanning. Errors are most often
+  // caused by the server going away. Wait until next rescan interval when
+  // things should be back to normal
+  private boolean scanOneMetaRegion(MetaRegion region) {
+    boolean scanSuccessful = false;
+    while (!master.closed.get() && !regionManager.isInitialRootScanComplete() &&
+      regionManager.getRootRegionLocation() == null) {
+      master.sleeper.sleep();
+    }
+    if (master.closed.get()) {
+      return scanSuccessful;
+    }
+
+    try {
+      // Don't interrupt us while we're working
+      synchronized (scannerLock) {
+        scanRegion(region);
+        regionManager.putMetaRegionOnline(region);
+      }
+      scanSuccessful = true;
+    } catch (IOException e) {
+      e = RemoteExceptionHandler.checkIOException(e);
+      LOG.warn("Scan one META region: " + region.toString(), e);
+      // The region may have moved (TestRegionServerAbort, etc.).  If
+      // so, either it won't be in the onlineMetaRegions list or its host
+      // address has changed and the containsValue will fail. If not
+      // found, best thing to do here is probably return.
+      if (!regionManager.isMetaRegionOnline(region.getStartKey())) {
+        LOG.debug("Scanned region is no longer in map of online " +
+        "regions or its value has changed");
+        return scanSuccessful;
+      }
+      // Make sure the file system is still available
+      master.checkFileSystem();
+    } catch (Exception e) {
+      // If for some reason we get some other kind of exception, 
+      // at least log it rather than go out silently.
+      LOG.error("Unexpected exception", e);
+    }
+    return scanSuccessful;
+  }
+
+  @Override
+  protected boolean initialScan() {
+    MetaRegion region = null;
+    while (!master.closed.get() &&
+        (region == null && metaRegionsToScan.size() > 0) &&
+          !metaRegionsScanned()) {
+      try {
+        region = metaRegionsToScan.poll(master.threadWakeFrequency, 
+          TimeUnit.MILLISECONDS);
+      } catch (InterruptedException e) {
+        // continue
+      }
+      if (region == null && metaRegionsToRescan.size() != 0) {
+        region = metaRegionsToRescan.remove(0);
+      }
+      if (region != null) {
+        if (!scanOneMetaRegion(region)) {
+          metaRegionsToRescan.add(region);
+        }
+      }
+    }
+    initialScanComplete = true;
+    return true;
+  }
+
+  @Override
+  protected void maintenanceScan() {
+    List<MetaRegion> regions = regionManager.getListOfOnlineMetaRegions();
+    for (MetaRegion r: regions) {
+      scanOneMetaRegion(r);
+    }
+    metaRegionsScanned();
+  }
+
+  /**
+   * Called by the meta scanner when it has completed scanning all meta 
+   * regions. This wakes up any threads that were waiting for this to happen.
+   */
+  private synchronized boolean metaRegionsScanned() {
+    if (!regionManager.isInitialRootScanComplete() ||
+      regionManager.numMetaRegions() != regionManager.numOnlineMetaRegions()) {
+      return false;
+    }
+    LOG.info("all meta regions scanned");
+    notifyAll();
+    return true;
+  }
+
+  /**
+   * Other threads call this method to wait until all the meta regions have
+   * been scanned.
+   */
+  synchronized boolean waitForMetaRegionsOrClose() {
+    while (!master.closed.get()) {
+      if (regionManager.isInitialRootScanComplete() &&
+          regionManager.numMetaRegions() ==
+            regionManager.numOnlineMetaRegions()) {
+        break;
+      }
+      try {
+        wait(master.threadWakeFrequency);
+      } catch (InterruptedException e) {
+        // continue
+      }
+    }
+    return master.closed.get();
+  }
+  
+  /**
+   * Add another meta region to scan to the queue.
+   */ 
+  void addMetaRegionToScan(MetaRegion m) {
+    metaRegionsToScan.add(m);
+  }
+}
