820b2bbea53c9193a46d129036391c6efd526bf4
Michael Stack <stack@apache.org>
2009-01-21 19:08:10 +0000

    0.19.0 release
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/tags@736379 13f79535-47bb-0310-9956-ffa450edef68

---
+++ 0.19.0/src/java/org/apache/hadoop/hbase/regionserver/CompactSplitThread.java
@@ -0,0 +1,251 @@
+/**
+ * Copyright 2008 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.regionserver;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.hadoop.util.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.RemoteExceptionHandler;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.io.BatchUpdate;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.Writables;
+
+/** 
+ * Compact region on request and then run split if appropriate
+ *
+ * NOTE: This class extends Thread rather than Chore because the sleep time
+ * can be interrupted when there is something to do, rather than the Chore
+ * sleep time which is invariant.
+ */
+class CompactSplitThread extends Thread implements HConstants {
+  static final Log LOG = LogFactory.getLog(CompactSplitThread.class);
+  
+  private HTable root = null;
+  private HTable meta = null;
+  private final long frequency;
+  private final ReentrantLock lock = new ReentrantLock();
+  
+  private final HRegionServer server;
+  private final HBaseConfiguration conf;
+  
+  private final BlockingQueue<HRegion> compactionQueue =
+    new LinkedBlockingQueue<HRegion>();
+  
+  private final HashSet<HRegion> regionsInQueue = new HashSet<HRegion>();
+
+  private volatile int limit = 1;
+
+  /** @param server */
+  public CompactSplitThread(HRegionServer server) {
+    super();
+    this.server = server;
+    this.conf = server.conf;
+    this.frequency =
+      conf.getLong("hbase.regionserver.thread.splitcompactcheckfrequency",
+      20 * 1000);
+  }
+  
+  @Override
+  public void run() {
+    while (!this.server.isStopRequested() && this.server.isInSafeMode()) {
+      try {
+        Thread.sleep(this.frequency);
+      } catch (InterruptedException ex) {
+        continue;
+      }
+    }
+    int count = 0;
+    while (!this.server.isStopRequested()) {
+      HRegion r = null;
+      try {
+        if ((limit > 0) && (++count > limit)) {
+          try {
+            Thread.sleep(this.frequency);
+          } catch (InterruptedException ex) {
+            continue;
+          }
+          count = 0;
+        }
+        r = compactionQueue.poll(this.frequency, TimeUnit.MILLISECONDS);
+        if (r != null && !this.server.isStopRequested()) {
+          synchronized (regionsInQueue) {
+            regionsInQueue.remove(r);
+          }
+          lock.lock();
+          try {
+            // Don't interrupt us while we are working
+            byte [] midKey = r.compactStores();
+            if (midKey != null && !this.server.isStopRequested()) {
+              split(r, midKey);
+            }
+          } finally {
+            lock.unlock();
+          }
+        }
+      } catch (InterruptedException ex) {
+        continue;
+      } catch (IOException ex) {
+        LOG.error("Compaction/Split failed" +
+            (r != null ? (" for region " + Bytes.toString(r.getRegionName())) : ""),
+            RemoteExceptionHandler.checkIOException(ex));
+        if (!server.checkFileSystem()) {
+          break;
+        }
+      } catch (Exception ex) {
+        LOG.error("Compaction failed" +
+            (r != null ? (" for region " + Bytes.toString(r.getRegionName())) : ""),
+            ex);
+        if (!server.checkFileSystem()) {
+          break;
+        }
+      }
+    }
+    regionsInQueue.clear();
+    compactionQueue.clear();
+    LOG.info(getName() + " exiting");
+  }
+
+  /**
+   * @param r HRegion store belongs to
+   * @param why Why compaction requested -- used in debug messages
+   */
+  public synchronized void compactionRequested(final HRegion r,
+      final String why) {
+    compactionRequested(r, false, why);
+  }
+
+  /**
+   * @param r HRegion store belongs to
+   * @param force Whether next compaction should be major
+   * @param why Why compaction requested -- used in debug messages
+   */
+  public synchronized void compactionRequested(final HRegion r,
+      final boolean force, final String why) {
+    if (this.server.stopRequested.get()) {
+      return;
+    }
+    r.setForceMajorCompaction(force);
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Compaction " + (force? "(major) ": "") +
+        "requested for region " + Bytes.toString(r.getRegionName()) +
+        "/" + r.getRegionInfo().getEncodedName() +
+        (why != null && !why.isEmpty()? " because: " + why: ""));
+    }
+    synchronized (regionsInQueue) {
+      if (!regionsInQueue.contains(r)) {
+        compactionQueue.add(r);
+        regionsInQueue.add(r);
+      }
+    }
+  }
+  
+  private void split(final HRegion region, final byte [] midKey)
+  throws IOException {
+    final HRegionInfo oldRegionInfo = region.getRegionInfo();
+    final long startTime = System.currentTimeMillis();
+    final HRegion[] newRegions = region.splitRegion(midKey);
+    if (newRegions == null) {
+      // Didn't need to be split
+      return;
+    }
+    
+    // When a region is split, the META table needs to updated if we're
+    // splitting a 'normal' region, and the ROOT table needs to be
+    // updated if we are splitting a META region.
+    HTable t = null;
+    if (region.getRegionInfo().isMetaTable()) {
+      // We need to update the root region
+      if (this.root == null) {
+        this.root = new HTable(conf, ROOT_TABLE_NAME);
+      }
+      t = root;
+    } else {
+      // For normal regions we need to update the meta region
+      if (meta == null) {
+        meta = new HTable(conf, META_TABLE_NAME);
+      }
+      t = meta;
+    }
+
+    // Mark old region as offline and split in META.
+    // NOTE: there is no need for retry logic here. HTable does it for us.
+    oldRegionInfo.setOffline(true);
+    oldRegionInfo.setSplit(true);
+    // Inform the HRegionServer that the parent HRegion is no-longer online.
+    this.server.removeFromOnlineRegions(oldRegionInfo);
+    
+    BatchUpdate update = new BatchUpdate(oldRegionInfo.getRegionName());
+    update.put(COL_REGIONINFO, Writables.getBytes(oldRegionInfo));
+    update.put(COL_SPLITA, Writables.getBytes(newRegions[0].getRegionInfo()));
+    update.put(COL_SPLITB, Writables.getBytes(newRegions[1].getRegionInfo()));
+    t.commit(update);
+    
+    // Add new regions to META
+    for (int i = 0; i < newRegions.length; i++) {
+      update = new BatchUpdate(newRegions[i].getRegionName());
+      update.put(COL_REGIONINFO, Writables.getBytes(
+        newRegions[i].getRegionInfo()));
+      t.commit(update);
+    }
+        
+    // Now tell the master about the new regions
+    server.reportSplit(oldRegionInfo, newRegions[0].getRegionInfo(),
+      newRegions[1].getRegionInfo());
+    LOG.info("region split, META updated, and report to master all" +
+      " successful. Old region=" + oldRegionInfo.toString() +
+      ", new regions: " + newRegions[0].toString() + ", " +
+      newRegions[1].toString() + ". Split took " +
+      StringUtils.formatTimeDiff(System.currentTimeMillis(), startTime));
+    
+    // Do not serve the new regions. Let the Master assign them.
+  }
+
+  /**
+   * Sets the number of compactions allowed per cycle.
+   * @param limit the number of compactions allowed, or -1 to unlimit
+   */
+  void setLimit(int limit) {
+    this.limit = limit;
+  }
+
+  int getLimit() {
+    return this.limit;
+  }
+
+  /**
+   * Only interrupt once it's done with a run through the work loop.
+   */ 
+  void interruptIfNecessary() {
+    if (lock.tryLock()) {
+      this.interrupt();
+    }
+  }
+}
