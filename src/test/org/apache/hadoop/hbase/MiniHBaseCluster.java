7a29d460a20b1efb6afee3dd721643a650bc813c
Andrew Kyle Purtell <apurtell@apache.org>
2009-06-12 21:31:13 +0000

    add missing hunk from last commit
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk_on_hadoop-0.18.3@784282 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hadoop/hbase/MiniHBaseCluster.java
+++ src/test/org/apache/hadoop/hbase/MiniHBaseCluster.java
@@ -26,6 +26,7 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.apache.hadoop.hbase.client.HConnectionManager;
 import org.apache.hadoop.hbase.master.HMaster;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
 import org.apache.hadoop.hbase.regionserver.HRegion;
