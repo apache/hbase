c8200872ad31da78f8db8d1aa035fecc9bbeb01b
Andrew Kyle Purtell <apurtell@apache.org>
2009-08-27 19:37:51 +0000

    HBASE-1780 HTable.flushCommits clears write buffer in finally clause
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/branches/0.20@808577 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/client/HTable.java
+++ src/java/org/apache/hadoop/hbase/client/HTable.java
@@ -55,13 +55,15 @@ import org.apache.hadoop.hbase.util.Writables;
 
 /**
  * Used to communicate with a single HBase table
+ * <p>
+ * This class is not MT safe for writing.
  */
 public class HTable {
   private final HConnection connection;
   private final byte [] tableName;
   protected final int scannerTimeout;
   private volatile HBaseConfiguration configuration;
-  private ArrayList<Put> writeBuffer;
+  private final ArrayList<Put> writeBuffer = new ArrayList<Put>();
   private long writeBufferSize;
   private boolean autoFlush;
   private long currentWriteBufferSize;
@@ -121,7 +123,6 @@ public class HTable {
       conf.getInt("hbase.regionserver.lease.period", 60 * 1000);
     this.configuration = conf;
     this.connection.locateRegion(tableName, HConstants.EMPTY_START_ROW);
-    this.writeBuffer = new ArrayList<Put>();
     this.writeBufferSize = conf.getLong("hbase.client.write.buffer", 2097152);
     this.autoFlush = true;
     this.currentWriteBufferSize = 0;
@@ -578,14 +579,18 @@ public class HTable {
    * @throws IOException
    */
   public void flushCommits() throws IOException {
+    int last = 0;
     try {
-      connection.processBatchOfRows(writeBuffer, tableName);
+      last = connection.processBatchOfRows(writeBuffer, tableName);
     } finally {
+      writeBuffer.subList(0, last).clear();
       currentWriteBufferSize = 0;
-      writeBuffer.clear();
+      for (int i = 0; i < writeBuffer.size(); i++) {
+        currentWriteBufferSize += writeBuffer.get(i).heapSize();
+      }
     }
   }
-   
+
   /**
    * Release held resources
    * 
@@ -681,14 +686,6 @@ public class HTable {
     if(currentWriteBufferSize > writeBufferSize) {
       flushCommits();
     }
-  }
-
-  /**
-   * Get the write buffer 
-   * @return the current write buffer
-   */
-  public ArrayList<Put> getWriteBuffer() {
-    return writeBuffer;
   }
 
   // Old API. Pre-hbase-880, hbase-1304.
