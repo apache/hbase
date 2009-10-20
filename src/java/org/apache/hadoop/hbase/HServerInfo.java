6ca9a03ec091fb2d29e1402cae6a4059db76b26a
Jean-Daniel Cryans <jdcryans@apache.org>
2009-10-20 03:52:08 +0000

    HBASE-1918 fix
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/branches/0.20@826943 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/HServerInfo.java
+++ src/java/org/apache/hadoop/hbase/HServerInfo.java
@@ -22,6 +22,8 @@ package org.apache.hadoop.hbase;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.hadoop.io.WritableComparable;
 
@@ -40,6 +42,7 @@ public class HServerInfo implements WritableComparable<HServerInfo> {
   private int infoPort;
   private String serverName = null;
   private String name;
+  private static Map<String,String> dnsCache = new HashMap<String,String>();
 
   /** default constructor - used by Writable */
   public HServerInfo() {
@@ -235,6 +238,11 @@ public class HServerInfo implements WritableComparable<HServerInfo> {
       String host = serverAddress.substring(0, colonIndex);
       int port =
         Integer.valueOf(serverAddress.substring(colonIndex + 1)).intValue();
+      if(!dnsCache.containsKey(host)) {
+        HServerAddress address = new HServerAddress(serverAddress);
+        dnsCache.put(host, address.getHostname());
+      }
+      host = dnsCache.get(host);
       name = getServerName(host, port, startCode);
     }
     return name;
