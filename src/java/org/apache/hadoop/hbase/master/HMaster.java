379d2663a18f358415b5e84a9bfc1191cdad4f97
Michael Stack <stack@apache.org>
2010-01-05 05:49:28 +0000

    HBASE-2021 Add compaction details to master UI
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/branches/0.20@895911 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/master/HMaster.java
+++ src/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -159,6 +159,9 @@ public class HMaster extends Thread implements HConstants, HMasterInterface,
   
   private MasterMetrics metrics;
 
+  private long lastFragmentationQuery = -1L;
+  private Map<String, Integer> fragmentation = null;
+  
   /** 
    * Build the HMaster out of a raw configuration item.
    * @param conf configuration
@@ -1178,6 +1181,17 @@ public class HMaster extends Thread implements HConstants, HMasterInterface,
     }
   }
    
+  public Map<String, Integer> getTableFragmentation() throws IOException {
+    long now = System.currentTimeMillis();
+    // only check every two minutes by default
+    int check = this.conf.getInt("hbase.master.fragmentation.check.frequency", 2 * 60 * 1000);
+    if (lastFragmentationQuery == -1 || now - lastFragmentationQuery > check) {
+      fragmentation = FSUtils.getTableFragmentation(this);
+      lastFragmentationQuery = now;
+    }
+    return fragmentation;
+  }
+  
   /*
    * Main program
    */
