56389b9da01d21ef6710e6df953b6c03440aa753
Andrew Kyle Purtell <apurtell@apache.org>
2010-03-22 18:18:00 +0000

    HBASE-2355 Unsynchronized logWriters map is mutated from several threads in HLog splitting
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/branches/0.20@926235 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/regionserver/HLog.java
+++ src/java/org/apache/hadoop/hbase/regionserver/HLog.java
@@ -1150,7 +1150,8 @@ public class HLog implements HConstants, Syncable {
     final HBaseConfiguration conf)
   throws IOException {
     final Map<byte [], WriterAndPath> logWriters =
-      new TreeMap<byte [], WriterAndPath>(Bytes.BYTES_COMPARATOR);
+      Collections.synchronizedMap(
+        new TreeMap<byte [], WriterAndPath>(Bytes.BYTES_COMPARATOR));
     List<Path> splits = null;
     
     // Number of threads to use when log splitting to rewrite the logs.
