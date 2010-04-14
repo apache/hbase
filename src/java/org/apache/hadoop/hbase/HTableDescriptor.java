e9a28bfaa125bb4278ce15ebb8d02c74132c3cb1
Michael Stack <stack@apache.org>
2010-04-14 22:34:38 +0000

    HBASE-2439 HBase can get stuck if updates to META are blocked
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/branches/0.20@934228 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/HTableDescriptor.java
+++ src/java/org/apache/hadoop/hbase/HTableDescriptor.java
@@ -112,7 +112,6 @@ ISerializable {
     for(HColumnDescriptor descriptor : families) {
       this.families.put(descriptor.getName(), descriptor);
     }
-    setMemStoreFlushSize(16 * 1024);
   }
 
   /**
