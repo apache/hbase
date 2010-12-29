c096d555d212094b0e0638017b5e5be222f800a0
Michael Stack <stack@apache.org>
2010-12-29 00:07:31 +0000

    Tagging 0.90 RC2
    git-svn-id: https://svn.apache.org/repos/asf/hbase/tags@1053485 13f79535-47bb-0310-9956-ffa450edef68

---
+++ 0.90.0RC2/src/main/java/org/apache/hadoop/hbase/HRegionLocation.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2007 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase;
+
+/**
+ * Contains the HRegionInfo for the region and the HServerAddress for the
+ * HRegionServer serving the region
+ */
+public class HRegionLocation implements Comparable<HRegionLocation> {
+  // TODO: Is this class necessary?  Why not just have a Pair?
+  private HRegionInfo regionInfo;
+  private HServerAddress serverAddress;
+
+  /**
+   * Constructor
+   *
+   * @param regionInfo the HRegionInfo for the region
+   * @param serverAddress the HServerAddress for the region server
+   */
+  public HRegionLocation(HRegionInfo regionInfo, HServerAddress serverAddress) {
+    this.regionInfo = regionInfo;
+    this.serverAddress = serverAddress;
+  }
+
+  /**
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "address: " + this.serverAddress.toString() + ", regioninfo: " +
+      this.regionInfo.getRegionNameAsString();
+  }
+
+  /**
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    if (!(o instanceof HRegionLocation)) {
+      return false;
+    }
+    return this.compareTo((HRegionLocation)o) == 0;
+  }
+
+  /**
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    int result = this.regionInfo.hashCode();
+    result ^= this.serverAddress.hashCode();
+    return result;
+  }
+
+  /** @return HRegionInfo */
+  public HRegionInfo getRegionInfo(){
+    return regionInfo;
+  }
+
+  /** @return HServerAddress */
+  public HServerAddress getServerAddress(){
+    return serverAddress;
+  }
+
+  //
+  // Comparable
+  //
+
+  public int compareTo(HRegionLocation o) {
+    int result = this.regionInfo.compareTo(o.regionInfo);
+    if(result == 0) {
+      result = this.serverAddress.compareTo(o.serverAddress);
+    }
+    return result;
+  }
+}
---
+++ 0.90.0RC2/src/main/java/org/apache/hadoop/hbase/master/handler/ClosedRegionHandler.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2010 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.master.handler;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.Server;
+import org.apache.hadoop.hbase.executor.EventHandler;
+import org.apache.hadoop.hbase.master.AssignmentManager;
+
+/**
+ * Handles CLOSED region event on Master.
+ * <p>
+ * If table is being disabled, deletes ZK unassigned node and removes from
+ * regions in transition.
+ * <p>
+ * Otherwise, assigns the region to another server.
+ */
+public class ClosedRegionHandler extends EventHandler implements TotesHRegionInfo {
+  private static final Log LOG = LogFactory.getLog(ClosedRegionHandler.class);
+  private final AssignmentManager assignmentManager;
+  private final HRegionInfo regionInfo;
+  private final ClosedPriority priority;
+
+  private enum ClosedPriority {
+    ROOT (1),
+    META (2),
+    USER (3);
+
+    private final int value;
+    ClosedPriority(int value) {
+      this.value = value;
+    }
+    public int getValue() {
+      return value;
+    }
+  };
+
+  public ClosedRegionHandler(Server server, AssignmentManager assignmentManager,
+      HRegionInfo regionInfo) {
+    super(server, EventType.RS_ZK_REGION_CLOSED);
+    this.assignmentManager = assignmentManager;
+    this.regionInfo = regionInfo;
+    if(regionInfo.isRootRegion()) {
+      priority = ClosedPriority.ROOT;
+    } else if(regionInfo.isMetaRegion()) {
+      priority = ClosedPriority.META;
+    } else {
+      priority = ClosedPriority.USER;
+    }
+  }
+
+  @Override
+  public int getPriority() {
+    return priority.getValue();
+  }
+
+  @Override
+  public HRegionInfo getHRegionInfo() {
+    return this.regionInfo;
+  }
+
+  @Override
+  public void process() {
+    LOG.debug("Handling CLOSED event for " + regionInfo.getEncodedName());
+    // Check if this table is being disabled or not
+    if (this.assignmentManager.getZKTable().
+        isDisablingOrDisabledTable(this.regionInfo.getTableDesc().getNameAsString())) {
+      assignmentManager.offlineDisabledRegion(regionInfo);
+      return;
+    }
+    // ZK Node is in CLOSED state, assign it.
+    assignmentManager.setOffline(regionInfo);
+    assignmentManager.assign(regionInfo, true);
+  }
+}
