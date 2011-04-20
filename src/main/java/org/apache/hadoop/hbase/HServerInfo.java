1a02a0a183c63bce560780d35d9655898e4b176a
Jean-Daniel Cryans <jdcryans@apache.org>
2011-04-20 21:59:08 +0000

    HBASE-3580  Remove RS from DeadServer when new instance checks in
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.90@1095532 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/HServerInfo.java
+++ src/main/java/org/apache/hadoop/hbase/HServerInfo.java
@@ -279,4 +279,20 @@ public class HServerInfo implements WritableComparable<HServerInfo> {
     }
     return false;
   }
+
+  /**
+   * Utility method to excise the start code from a server name
+   * @param inServerName full server name
+   * @return server name less its start code
+   */
+  public static String getServerNameLessStartCode(String inServerName) {
+    if (inServerName != null && inServerName.length() > 0) {
+      int index = inServerName.lastIndexOf(SERVERNAME_SEPARATOR);
+      if (index > 0) {
+        return inServerName.substring(0, index);
+      }
+    }
+    return inServerName;
+  }
+
 }
