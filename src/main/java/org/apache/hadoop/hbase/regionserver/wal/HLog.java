9cd54f14f5b59bb7094a03b3a26f4498b7ddd1c6
Jean-Daniel Cryans <jdcryans@apache.org>
2011-05-05 22:46:33 +0000

    HBASE-3860  HLog shouldn't create a new HBC when rolling
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.90@1099987 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/regionserver/wal/HLog.java
+++ src/main/java/org/apache/hadoop/hbase/regionserver/wal/HLog.java
@@ -482,8 +482,7 @@ public class HLog implements Syncable {
       long currentFilenum = this.filenum;
       this.filenum = System.currentTimeMillis();
       Path newPath = computeFilename();
-      HLog.Writer nextWriter = this.createWriterInstance(fs, newPath,
-          HBaseConfiguration.create(conf));
+      HLog.Writer nextWriter = this.createWriterInstance(fs, newPath, conf);
       int nextInitialReplication = fs.getFileStatus(newPath).getReplication();
       // Can we get at the dfsclient outputstream?  If an instance of
       // SFLW, it'll have done the necessary reflection to get at the
