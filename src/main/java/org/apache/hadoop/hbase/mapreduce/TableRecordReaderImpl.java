f7bf2f12b6c6c8636a15c59bec4ca02612d3967c
Zhihong Yu <tedyu@apache.org>
2011-08-31 09:07:05 +0000

    revert HBASE-4269 to see if 'Not a host:port pair' error still exists
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.90@1163539 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/mapreduce/TableRecordReaderImpl.java
+++ src/main/java/org/apache/hadoop/hbase/mapreduce/TableRecordReaderImpl.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.hbase.DoNotRetryIOException;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
@@ -131,8 +130,6 @@ public class TableRecordReaderImpl {
     if (value == null) value = new Result();
     try {
       value = this.scanner.next();
-    } catch (DoNotRetryIOException e) {
-      throw e;
     } catch (IOException e) {
       LOG.debug("recovered from " + StringUtils.stringifyException(e));
       if (lastSuccessfulRow == null) {
