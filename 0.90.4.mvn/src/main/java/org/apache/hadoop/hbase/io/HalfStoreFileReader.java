9bb982794cb0db9857a026956d907eeed3417263
Michael Stack <stack@apache.org>
2011-09-02 21:49:58 +0000

    Adding 0.90.4 to mvn repo; making copy of 0.90.4 for mvn to mess on
    git-svn-id: https://svn.apache.org/repos/asf/hbase/tags@1164727 13f79535-47bb-0310-9956-ffa450edef68

---
+++ 0.90.4.mvn/src/main/java/org/apache/hadoop/hbase/io/HalfStoreFileReader.java
@@ -0,0 +1,271 @@
+/**
+ * Copyright 2008 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.io;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.io.hfile.BlockCache;
+import org.apache.hadoop.hbase.io.hfile.HFileScanner;
+import org.apache.hadoop.hbase.regionserver.StoreFile;
+import org.apache.hadoop.hbase.util.Bytes;
+
+/**
+ * A facade for a {@link org.apache.hadoop.hbase.io.hfile.HFile.Reader} that serves up
+ * either the top or bottom half of a HFile where 'bottom' is the first half
+ * of the file containing the keys that sort lowest and 'top' is the second half
+ * of the file with keys that sort greater than those of the bottom half.
+ * The top includes the split files midkey, of the key that follows if it does
+ * not exist in the file.
+ *
+ * <p>This type works in tandem with the {@link Reference} type.  This class
+ * is used reading while Reference is used writing.
+ *
+ * <p>This file is not splitable.  Calls to {@link #midkey()} return null.
+ */
+public class HalfStoreFileReader extends StoreFile.Reader {
+  final Log LOG = LogFactory.getLog(HalfStoreFileReader.class);
+  final boolean top;
+  // This is the key we split around.  Its the first possible entry on a row:
+  // i.e. empty column and a timestamp of LATEST_TIMESTAMP.
+  protected final byte [] splitkey;
+
+  /**
+   * @param fs
+   * @param p
+   * @param c
+   * @param r
+   * @throws IOException
+   */
+  public HalfStoreFileReader(final FileSystem fs, final Path p, final BlockCache c,
+    final Reference r)
+  throws IOException {
+    super(fs, p, c, false);
+    // This is not actual midkey for this half-file; its just border
+    // around which we split top and bottom.  Have to look in files to find
+    // actual last and first keys for bottom and top halves.  Half-files don't
+    // have an actual midkey themselves. No midkey is how we indicate file is
+    // not splittable.
+    this.splitkey = r.getSplitKey();
+    // Is it top or bottom half?
+    this.top = Reference.isTopFileRegion(r.getFileRegion());
+  }
+
+  protected boolean isTop() {
+    return this.top;
+  }
+
+  @Override
+  public HFileScanner getScanner(final boolean cacheBlocks, final boolean pread) {
+    final HFileScanner s = super.getScanner(cacheBlocks, pread);
+    return new HFileScanner() {
+      final HFileScanner delegate = s;
+      public boolean atEnd = false;
+
+      public ByteBuffer getKey() {
+        if (atEnd) return null;
+        return delegate.getKey();
+      }
+
+      public String getKeyString() {
+        if (atEnd) return null;
+
+        return delegate.getKeyString();
+      }
+
+      public ByteBuffer getValue() {
+        if (atEnd) return null;
+
+        return delegate.getValue();
+      }
+
+      public String getValueString() {
+        if (atEnd) return null;
+
+        return delegate.getValueString();
+      }
+
+      public KeyValue getKeyValue() {
+        if (atEnd) return null;
+
+        return delegate.getKeyValue();
+      }
+
+      public boolean next() throws IOException {
+        if (atEnd) return false;
+
+        boolean b = delegate.next();
+        if (!b) {
+          return b;
+        }
+        // constrain the bottom.
+        if (!top) {
+          ByteBuffer bb = getKey();
+          if (getComparator().compare(bb.array(), bb.arrayOffset(), bb.limit(),
+              splitkey, 0, splitkey.length) >= 0) {
+            atEnd = true;
+            return false;
+          }
+        }
+        return true;
+      }
+
+      public boolean seekBefore(byte[] key) throws IOException {
+        return seekBefore(key, 0, key.length);
+      }
+
+      public boolean seekBefore(byte [] key, int offset, int length)
+      throws IOException {
+        if (top) {
+          if (getComparator().compare(key, offset, length, splitkey, 0,
+              splitkey.length) < 0) {
+            return false;
+          }
+        } else {
+          if (getComparator().compare(key, offset, length, splitkey, 0,
+              splitkey.length) >= 0) {
+            return seekBefore(splitkey, 0, splitkey.length);
+          }
+        }
+        return this.delegate.seekBefore(key, offset, length);
+      }
+
+      public boolean seekTo() throws IOException {
+        if (top) {
+          int r = this.delegate.seekTo(splitkey);
+          if (r < 0) {
+            // midkey is < first key in file
+            return this.delegate.seekTo();
+          }
+          if (r > 0) {
+            return this.delegate.next();
+          }
+          return true;
+        }
+
+        boolean b = delegate.seekTo();
+        if (!b) {
+          return b;
+        }
+        // Check key.
+        ByteBuffer k = this.delegate.getKey();
+        return this.delegate.getReader().getComparator().
+          compare(k.array(), k.arrayOffset(), k.limit(),
+            splitkey, 0, splitkey.length) < 0;
+      }
+
+      public int seekTo(byte[] key) throws IOException {
+        return seekTo(key, 0, key.length);
+      }
+
+      public int seekTo(byte[] key, int offset, int length) throws IOException {
+        if (top) {
+          if (getComparator().compare(key, offset, length, splitkey, 0,
+              splitkey.length) < 0) {
+            return -1;
+          }
+        } else {
+          if (getComparator().compare(key, offset, length, splitkey, 0,
+              splitkey.length) >= 0) {
+            // we would place the scanner in the second half.
+            // it might be an error to return false here ever...
+            boolean res = delegate.seekBefore(splitkey, 0, splitkey.length);
+            if (!res) {
+              throw new IOException("Seeking for a key in bottom of file, but key exists in top of file, failed on seekBefore(midkey)");
+            }
+            return 1;
+          }
+        }
+        return delegate.seekTo(key, offset, length);
+      }
+
+      @Override
+      public int reseekTo(byte[] key) throws IOException {
+        return reseekTo(key, 0, key.length);
+      }
+
+      @Override
+      public int reseekTo(byte[] key, int offset, int length)
+      throws IOException {
+        //This function is identical to the corresponding seekTo function except
+        //that we call reseekTo (and not seekTo) on the delegate.
+        if (top) {
+          if (getComparator().compare(key, offset, length, splitkey, 0,
+              splitkey.length) < 0) {
+            return -1;
+          }
+        } else {
+          if (getComparator().compare(key, offset, length, splitkey, 0,
+              splitkey.length) >= 0) {
+            // we would place the scanner in the second half.
+            // it might be an error to return false here ever...
+            boolean res = delegate.seekBefore(splitkey, 0, splitkey.length);
+            if (!res) {
+              throw new IOException("Seeking for a key in bottom of file, but" +
+                  " key exists in top of file, failed on seekBefore(midkey)");
+            }
+            return 1;
+          }
+        }
+        if (atEnd) {
+          // skip the 'reseek' and just return 1.
+          return 1;
+        }
+        return delegate.reseekTo(key, offset, length);
+      }
+
+      public org.apache.hadoop.hbase.io.hfile.HFile.Reader getReader() {
+        return this.delegate.getReader();
+      }
+
+      public boolean isSeeked() {
+        return this.delegate.isSeeked();
+      }
+    };
+  }
+
+  @Override
+  public byte[] getLastKey() {
+    if (top) {
+      return super.getLastKey();
+    }
+    // Get a scanner that caches the block and that uses pread.
+    HFileScanner scanner = getScanner(true, true);
+    try {
+      if (scanner.seekBefore(this.splitkey)) {
+        return Bytes.toBytes(scanner.getKey());
+      }
+    } catch (IOException e) {
+      LOG.warn("Failed seekBefore " + Bytes.toStringBinary(this.splitkey), e);
+    }
+    return null;
+  }
+
+  @Override
+  public byte[] midkey() throws IOException {
+    // Returns null to indicate file is not splitable.
+    return null;
+  }
+}
---
+++ 0.90.4.mvn/src/test/java/org/apache/hadoop/hbase/client/TestHTablePool.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright 2009 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.client;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests HTablePool.
+ */
+public class TestHTablePool  {
+  private static HBaseTestingUtility TEST_UTIL   =  new HBaseTestingUtility();
+  private final static byte [] TABLENAME = Bytes.toBytes("TestHTablePool");
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    TEST_UTIL.startMiniCluster(1);
+    TEST_UTIL.createTable(TABLENAME, HConstants.CATALOG_FAMILY);
+  }
+
+  @AfterClass
+  public static void afterClass() throws IOException {
+    TEST_UTIL.shutdownMiniCluster();
+  }
+
+  @Test
+  public void testTableWithStringName() {
+    HTablePool pool =
+      new HTablePool(TEST_UTIL.getConfiguration(), Integer.MAX_VALUE);
+    String tableName = Bytes.toString(TABLENAME);
+
+    // Request a table from an empty pool
+    HTableInterface table = pool.getTable(tableName);
+    Assert.assertNotNull(table);
+
+    // Return the table to the pool
+    pool.putTable(table);
+
+    // Request a table of the same name
+    HTableInterface sameTable = pool.getTable(tableName);
+    Assert.assertSame(table, sameTable);
+  }
+
+  @Test
+  public void testTableWithByteArrayName() throws IOException {
+    HTablePool pool = new HTablePool(TEST_UTIL.getConfiguration(), Integer.MAX_VALUE);
+
+    // Request a table from an empty pool
+    HTableInterface table = pool.getTable(TABLENAME);
+    Assert.assertNotNull(table);
+
+    // Return the table to the pool
+    pool.putTable(table);
+
+    // Request a table of the same name
+    HTableInterface sameTable = pool.getTable(TABLENAME);
+    Assert.assertSame(table, sameTable);
+  }
+
+  @Test
+  public void testTableWithMaxSize() {
+    HTablePool pool = new HTablePool(TEST_UTIL.getConfiguration(), 2);
+
+    // Request tables from an empty pool
+    HTableInterface table1 = pool.getTable(TABLENAME);
+    HTableInterface table2 = pool.getTable(TABLENAME);
+    HTableInterface table3 = pool.getTable(TABLENAME);
+
+    // Return the tables to the pool
+    pool.putTable(table1);
+    pool.putTable(table2);
+    // The pool should reject this one since it is already full
+    pool.putTable(table3);
+
+    // Request tables of the same name
+    HTableInterface sameTable1 = pool.getTable(TABLENAME);
+    HTableInterface sameTable2 = pool.getTable(TABLENAME);
+    HTableInterface sameTable3 = pool.getTable(TABLENAME);
+    Assert.assertSame(table1, sameTable1);
+    Assert.assertSame(table2, sameTable2);
+    Assert.assertNotSame(table3, sameTable3);
+  }
+
+  @Test
+  public void testTablesWithDifferentNames() throws IOException {
+    HTablePool pool = new HTablePool(TEST_UTIL.getConfiguration(), Integer.MAX_VALUE);
+    byte [] otherTable = Bytes.toBytes("OtherTable");
+    TEST_UTIL.createTable(otherTable, HConstants.CATALOG_FAMILY);
+
+    // Request a table from an empty pool
+    HTableInterface table1 = pool.getTable(TABLENAME);
+    HTableInterface table2 = pool.getTable(otherTable);
+    Assert.assertNotNull(table2);
+
+    // Return the tables to the pool
+    pool.putTable(table1);
+    pool.putTable(table2);
+
+    // Request tables of the same names
+    HTableInterface sameTable1 = pool.getTable(TABLENAME);
+    HTableInterface sameTable2 = pool.getTable(otherTable);
+    Assert.assertSame(table1, sameTable1);
+    Assert.assertSame(table2, sameTable2);
+  }
+
+
+  @Test
+  public void testCloseTablePool() throws IOException {
+    HTablePool pool = new HTablePool(TEST_UTIL.getConfiguration(), 4);
+    HBaseAdmin admin = new HBaseAdmin(TEST_UTIL.getConfiguration());
+
+    if (admin.tableExists(TABLENAME)) {
+      admin.disableTable(TABLENAME);
+      admin.deleteTable(TABLENAME);
+    }
+
+    HTableDescriptor tableDescriptor = new HTableDescriptor(TABLENAME);
+    tableDescriptor.addFamily(new HColumnDescriptor("randomFamily"));
+    admin.createTable(tableDescriptor);
+
+
+    // Request tables from an empty pool
+    HTableInterface[] tables = new HTableInterface[4];
+    for (int i = 0; i < 4; ++i ) {
+      tables[i] = pool.getTable(TABLENAME);
+    }
+
+    pool.closeTablePool(TABLENAME);
+
+    for (int i = 0; i < 4; ++i ) {
+      pool.putTable(tables[i]);
+    }
+
+    Assert.assertEquals(4, pool.getCurrentPoolSize(Bytes.toString(TABLENAME)));
+
+    pool.closeTablePool(TABLENAME);
+
+    Assert.assertEquals(0, pool.getCurrentPoolSize(Bytes.toString(TABLENAME)));
+  }
+}
