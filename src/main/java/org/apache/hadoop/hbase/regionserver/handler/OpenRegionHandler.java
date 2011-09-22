c8349aece2b161f92af73809a230579b93346197
Zhihong Yu <tedyu@apache.org>
2011-09-22 23:05:26 +0000

    HBASE-4452  Possibility of RS opening a region though tickleOpening fails due to                znode version mismatch (Ramkrishna)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.90@1174443 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/regionserver/handler/OpenRegionHandler.java
+++ src/main/java/org/apache/hadoop/hbase/regionserver/handler/OpenRegionHandler.java
@@ -154,13 +154,14 @@ public class OpenRegionHandler extends EventHandler {
     // regions-in-transition timeout period.
     long period = Math.max(1, assignmentTimeout/ 3);
     long lastUpdate = now;
+    boolean tickleOpening = true;
     while (!signaller.get() && t.isAlive() && !this.server.isStopped() &&
         !this.rsServices.isStopping() && (endTime > now)) {
       long elapsed = now - lastUpdate;
       if (elapsed > period) {
         // Only tickle OPENING if postOpenDeployTasks is taking some time.
         lastUpdate = now;
-        tickleOpening("post_open_deploy");
+        tickleOpening = tickleOpening("post_open_deploy");
       }
       synchronized (signaller) {
         try {
@@ -188,8 +189,9 @@ public class OpenRegionHandler extends EventHandler {
       }
     }
     // Was there an exception opening the region?  This should trigger on
-    // InterruptedException too.  If so, we failed.
-    return !t.interrupted() && t.getException() == null;
+    // InterruptedException too.  If so, we failed. If tickle opening fails
+    // it is a failure
+    return !t.interrupted() && t.getException() == null && tickleOpening;
   }
 
   /**
