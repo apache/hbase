aa7f30cfca5404efa916568d3190bf95f2cd44fe
Nicolas Spiegelberg <nspiegelberg@apache.org>
2011-10-11 02:22:54 +0000

    Reduce log frequency when writing a compound Bloom filter
    Summary: This one-line change reduces the frequency of log messages generated
    when adding new compound Bloom filter blocks.
    Test Plan: Unit tests. Looking at the output produced by
    TestCompoundBloomFilter.
    Reviewed By: kannan
    Reviewers: kannan, nspiegelberg
    Commenters: nspiegelberg
    CC: hbase@lists, , nspiegelberg, kannan
    Revert Plan: OK
    Differential Revision: 276254
    
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.89@1181586 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/util/CompoundBloomFilterWriter.java
+++ src/main/java/org/apache/hadoop/hbase/util/CompoundBloomFilterWriter.java
@@ -202,10 +202,6 @@ public class CompoundBloomFilterWriter extends CompoundBloomFilterBase
 
     ByteBloomFilter readyChunkBloom = readyChunk.chunk;
     readyChunkBloom.getDataWriter().write(out);
-    if (LOG.isDebugEnabled() && readyChunk.chunkId % 10 == 0) {
-      LOG.debug("Wrote Bloom chunk #" + readyChunk.chunkId + ": "
-          + readyChunkBloom + "; total Bloom " + "keys: " + totalKeyCount);
-    }
   }
 
   @Override
