63fc4fa1f77263c07238f508b77378fb9d658092
Michael Stack <stack@apache.org>
2011-12-19 16:24:30 +0000

    HBASE-5062 Missing logons if security is enabled
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.92@1220827 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/thrift/ThriftServer.java
+++ src/main/java/org/apache/hadoop/hbase/thrift/ThriftServer.java
@@ -58,6 +58,7 @@ import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.filter.PrefixFilter;
 import org.apache.hadoop.hbase.filter.WhileMatchFilter;
 import org.apache.hadoop.hbase.filter.ParseFilter;
+import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.thrift.generated.AlreadyExists;
 import org.apache.hadoop.hbase.thrift.generated.BatchMutation;
 import org.apache.hadoop.hbase.thrift.generated.ColumnDescriptor;
@@ -70,7 +71,9 @@ import org.apache.hadoop.hbase.thrift.generated.TRegionInfo;
 import org.apache.hadoop.hbase.thrift.generated.TRowResult;
 import org.apache.hadoop.hbase.thrift.generated.TScan;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.Strings;
 import org.apache.hadoop.hbase.util.VersionInfo;
+import org.apache.hadoop.net.DNS;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TCompactProtocol;
@@ -1039,6 +1042,16 @@ public class ThriftServer {
       serverArgs.transportFactory(transportFactory);
       LOG.info("starting HBase ThreadPool Thrift server on " + listenAddress + ":" + Integer.toString(listenPort));
       server = new TThreadPoolServer(serverArgs);
+    }
+
+    // login the server principal (if using secure Hadoop)   
+    Configuration conf = handler.conf;
+    if (User.isSecurityEnabled() && User.isHBaseSecurityEnabled(conf)) {
+      String machineName = Strings.domainNamePointerToHostName(
+        DNS.getDefaultHost(conf.get("hbase.thrift.dns.interface", "default"),
+          conf.get("hbase.thrift.dns.nameserver", "default")));
+      User.login(conf, "hbase.thrift.keytab.file", "hbase.thrift.kerberos.principal",
+        machineName);
     }
 
     server.serve();
