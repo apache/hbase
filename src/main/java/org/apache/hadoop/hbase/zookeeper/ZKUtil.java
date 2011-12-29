5b4552bd94488ec5f514e58903b3d616522abb5c
Zhihong Yu <tedyu@apache.org>
2011-12-29 19:07:44 +0000

    HBASE-5103  Fix improper master znode deserialization (Jonathan Hsieh)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.92@1225620 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/zookeeper/ZKUtil.java
+++ src/main/java/org/apache/hadoop/hbase/zookeeper/ZKUtil.java
@@ -34,6 +34,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.executor.RegionTransitionData;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.zookeeper.AsyncCallback;
@@ -1030,7 +1031,7 @@ public class ZKUtil {
     try {
       sb.append("HBase is rooted at ").append(zkw.baseZNode);
       sb.append("\nMaster address: ").append(
-          Bytes.toStringBinary(getData(zkw, zkw.masterAddressZNode)));
+          ServerName.parseVersionedServerName(getData(zkw, zkw.masterAddressZNode)));
       sb.append("\nRegion server holding ROOT: ").append(
           Bytes.toStringBinary(getData(zkw, zkw.rootServerZNode)));
       sb.append("\nRegion servers:");
