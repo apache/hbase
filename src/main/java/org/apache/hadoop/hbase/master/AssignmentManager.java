06636f317f3215a90db86859752dae6199470878
Zhihong Yu <tedyu@apache.org>
2012-01-17 20:40:26 +0000

    HBASE-5176  AssignmentManager#getRegion: logging nit  adds a redundant '+' (Karthik K)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1232567 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
+++ src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
@@ -1651,7 +1651,7 @@ public class AssignmentManager extends ZooKeeperListener {
       if (existingPlan != null && existingPlan.getDestination() != null) {
         LOG.debug("Found an existing plan for " +
             state.getRegion().getRegionNameAsString() +
-       " destination server is + " + existingPlan.getDestination().toString());
+       " destination server is " + existingPlan.getDestination().toString());
       }
 
       if (forceNewPlan
