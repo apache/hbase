865b14133b8a2ed985a774e39c6944ae9f1d1dee
Jonathan Hsieh <jmhsieh@apache.org>
2012-03-21 19:30:54 +0000

    HBASE-5589 Add of the offline call to the Master Interface
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1303519 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -1822,6 +1822,19 @@ Server {
   public double getAverageLoad() {
     return this.assignmentManager.getAverageLoad();
   }
+  
+  /**
+   * Special method, only used by hbck.
+   */
+  @Override
+  public void offline(final byte[] regionName) 
+  throws IOException {
+    Pair<HRegionInfo, ServerName> pair =
+      MetaReader.getRegion(this.catalogTracker, regionName);
+    if (pair == null) throw new UnknownRegionException(Bytes.toStringBinary(regionName));
+    HRegionInfo hri = pair.getFirst();
+    this.assignmentManager.regionOffline(hri);    
+  }
 
   /**
    * Utility for constructing an instance of the passed HMaster class.
