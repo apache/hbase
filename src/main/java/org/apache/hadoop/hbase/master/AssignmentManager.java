3c234f13ba34352b715a1f3c662993ab5e7dd05b
Zhihong Yu <tedyu@apache.org>
2012-03-25 23:22:00 +0000

    HBASE-5615 the master never does balance because of balancing the parent region (Xufeng)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1305171 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
+++ src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
@@ -2464,6 +2464,7 @@ public class AssignmentManager extends ZooKeeperListener {
       ServerName regionLocation = region.getSecond();
       if (regionInfo == null) continue;
       String tableName = regionInfo.getTableNameAsString();
+      if (regionInfo.isOffline() && regionInfo.isSplit()) continue;
       if (regionLocation == null) {
         // regionLocation could be null if createTable didn't finish properly.
         // When createTable is in progress, HMaster restarts.
