986dcb94292d384d8dea1f30586aa0f6f495bf1e
Michael Stack <stack@apache.org>
2012-03-29 15:30:29 +0000

    AggregationClient fails validation for open stoprow scan
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.92@1306918 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/client/coprocessor/AggregationClient.java
+++ src/main/java/org/apache/hadoop/hbase/client/coprocessor/AggregationClient.java
@@ -112,7 +112,8 @@ public class AggregationClient {
     if (scan == null
         || (Bytes.equals(scan.getStartRow(), scan.getStopRow()) && !Bytes
             .equals(scan.getStartRow(), HConstants.EMPTY_START_ROW))
-        || Bytes.compareTo(scan.getStartRow(), scan.getStopRow()) > 0) {
+        || ((Bytes.compareTo(scan.getStartRow(), scan.getStopRow()) > 0) &&
+        	!Bytes.equals(scan.getStopRow(), HConstants.EMPTY_END_ROW))) {
       throw new IOException(
           "Agg client Exception: Startrow should be smaller than Stoprow");
     } else if (scan.getFamilyMap().size() != 1) {
