e0d390b464fb4fc16f7f65969e38c1ced6485d08
mbautin <mbautin@unknown>
2012-04-06 22:12:43 +0000

    [JIRA] [HBASE-5618] SplitLogManager - prevent unnecessary attempts to resubmits
    Summary: update heartbeat time as soon as possible - w/o first waiting to read zk node (1) when zk watch fires (2) when resubmit fails with bad-version
    
    Test Plan: TestSplitLogMangaer passes.
    
    Reviewers: kannan, kranganathan, liyintang, mbautin
    
    Reviewed By: kannan
    
    CC: hbase-eng@
    
    Differential Revision: https://phabricator.fb.com/D441769
    
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.89-fb@1310611 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/master/SplitLogManager.java
+++ src/main/java/org/apache/hadoop/hbase/master/SplitLogManager.java
@@ -534,12 +534,18 @@ public class SplitLogManager implements Watcher {
           version) == false) {
         LOG.debug("failed to resubmit task " + path +
             " version changed");
+        task.heartbeatNoDetails(EnvironmentEdgeManager.currentTimeMillis());
         return false;
       }
     } catch (NoNodeException e) {
       LOG.warn("failed to resubmit because znode doesn't exist " + path +
           " task done (or forced done by removing the znode)");
       getDataSetWatchSuccess(path, null, Integer.MIN_VALUE);
+      return false;
+    } catch (KeeperException.BadVersionException e) {
+      LOG.debug("failed to resubmit task " + path +
+          " version changed");
+      task.heartbeatNoDetails(EnvironmentEdgeManager.currentTimeMillis());
       return false;
     } catch (KeeperException e) {
       tot_mgr_resubmit_failed.incrementAndGet();
@@ -726,7 +732,12 @@ public class SplitLogManager implements Watcher {
   }
 
   private void nodeDataChanged(String path) {
-    if (tasks.get(path) != null || ZKSplitLog.isRescanNode(watcher, path)) {
+    Task task;
+    task = tasks.get(path);
+    if (task != null || ZKSplitLog.isRescanNode(watcher, path)) {
+      if (task != null) {
+        task.heartbeatNoDetails(EnvironmentEdgeManager.currentTimeMillis());
+      }
       getDataSetWatch(path, zkretries);
     }
   }
@@ -821,7 +832,11 @@ public class SplitLogManager implements Watcher {
     }
 
     public boolean isUnassigned() {
-      return (last_update == -1);
+      return (cur_worker_name == null);
+    }
+
+    public void heartbeatNoDetails(long time) {
+      last_update = time;
     }
 
     public void heartbeat(long time, int version, String worker) {
