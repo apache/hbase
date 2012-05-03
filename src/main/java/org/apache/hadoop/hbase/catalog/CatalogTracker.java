e4162334a8301c900558c29aad9c1980564a4f9f
Zhihong Yu <tedyu@apache.org>
2012-05-03 16:45:32 +0000

    HBASE-5883  Backup master is going down due to connection refused exception (Jieshan)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.90@1333545 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/catalog/CatalogTracker.java
+++ src/main/java/org/apache/hadoop/hbase/catalog/CatalogTracker.java
@@ -456,7 +456,9 @@ public class CatalogTracker {
       LOG.debug("Exception connecting to " + address);
     } catch (IOException ioe) {
       Throwable cause = ioe.getCause();
-      if (cause != null && cause instanceof EOFException) {
+      if (ioe instanceof ConnectException) {
+        // Catch. Connect refused.
+      } else if (cause != null && cause instanceof EOFException) {
         // Catch. Other end disconnected us.
       } else if (cause != null && cause.getMessage() != null &&
         cause.getMessage().toLowerCase().contains("connection reset")) {
