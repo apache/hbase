fc69447832285ed46ea7974e1531cc1a12a7675a
Michael Stack <stack@apache.org>
2012-05-30 15:20:11 +0000

    HBASE-6131 Add attribution for code added by HBASE-5533 metrics
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1344304 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/metrics/TestExponentiallyDecayingSample.java
+++ src/test/java/org/apache/hadoop/hbase/metrics/TestExponentiallyDecayingSample.java
@@ -20,8 +20,8 @@ package org.apache.hadoop.hbase.metrics;
 
 import junit.framework.Assert;
 
-import org.apache.hadoop.hbase.metrics.histogram.ExponentiallyDecayingSample;
-import org.apache.hadoop.hbase.metrics.histogram.Snapshot;
+import com.yammer.metrics.stats.ExponentiallyDecayingSample;
+import com.yammer.metrics.stats.Snapshot;
 import org.junit.Test;
 
 public class TestExponentiallyDecayingSample {
