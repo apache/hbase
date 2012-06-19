93a8fad64a66d05b9e2e6f640f9821ef11fa19af
Andrew Kyle Purtell <apurtell@apache.org>
2012-06-19 02:24:43 +0000

    HBASE-6188. Remove the concept of table owner
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1351557 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/HTableDescriptor.java
+++ src/main/java/org/apache/hadoop/hbase/HTableDescriptor.java
@@ -1134,11 +1134,13 @@ public class HTableDescriptor implements WritableComparable<HTableDescriptor> {
               .setScope(HConstants.REPLICATION_SCOPE_LOCAL)
       });
 
+  @Deprecated
   public void setOwner(User owner) {
     setOwnerString(owner != null ? owner.getShortName() : null);
   }
 
   // used by admin.rb:alter(table_name,*args) to update owner.
+  @Deprecated
   public void setOwnerString(String ownerString) {
     if (ownerString != null) {
       setValue(OWNER_KEY, Bytes.toBytes(ownerString));
@@ -1147,12 +1149,14 @@ public class HTableDescriptor implements WritableComparable<HTableDescriptor> {
     }
   }
 
+  @Deprecated
   public String getOwnerString() {
     if (getValue(OWNER_KEY) != null) {
       return Bytes.toString(getValue(OWNER_KEY));
     }
     // Note that every table should have an owner (i.e. should have OWNER_KEY set).
-    // .META. and -ROOT- should return system user as owner, not null (see MasterFileSystem.java:bootstrap()).
+    // .META. and -ROOT- should return system user as owner, not null (see
+    // MasterFileSystem.java:bootstrap()).
     return null;
   }
 }
