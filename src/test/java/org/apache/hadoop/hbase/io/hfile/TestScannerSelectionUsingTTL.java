781c964401a178b41e7cdc90221d30bb97b639b8
Andrew Kyle Purtell <apurtell@apache.org>
2012-06-26 04:25:21 +0000

    HBASE-6267. hbase.store.delete.expired.storefile should be true by default
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1353813 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/io/hfile/TestScannerSelectionUsingTTL.java
+++ src/test/java/org/apache/hadoop/hbase/io/hfile/TestScannerSelectionUsingTTL.java
@@ -27,6 +27,7 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HRegionInfo;
@@ -38,7 +39,6 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.hfile.BlockType.BlockCategory;
 import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.hadoop.hbase.regionserver.InternalScanner;
-import org.apache.hadoop.hbase.regionserver.StoreFile.BloomType;
 import org.apache.hadoop.hbase.regionserver.metrics.SchemaMetrics;
 import org.apache.hadoop.hbase.regionserver.metrics.SchemaMetrics.BlockMetricType;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -98,6 +98,8 @@ public class TestScannerSelectionUsingTTL {
 
   @Test
   public void testScannerSelection() throws IOException {
+    Configuration conf = TEST_UTIL.getConfiguration();
+    conf.setBoolean("hbase.store.delete.expired.storefile", false);
     HColumnDescriptor hcd =
       new HColumnDescriptor(FAMILY_BYTES)
           .setMaxVersions(Integer.MAX_VALUE)
@@ -107,7 +109,7 @@ public class TestScannerSelectionUsingTTL {
     HRegionInfo info = new HRegionInfo(Bytes.toBytes(TABLE));
     HRegion region =
         HRegion.createHRegion(info, TEST_UTIL.getClusterTestDir(),
-            TEST_UTIL.getConfiguration(), htd);
+            conf, htd);
 
     for (int iFile = 0; iFile < totalNumFiles; ++iFile) {
       if (iFile == NUM_EXPIRED_FILES) {
@@ -127,7 +129,7 @@ public class TestScannerSelectionUsingTTL {
 
     Scan scan = new Scan();
     scan.setMaxVersions(Integer.MAX_VALUE);
-    CacheConfig cacheConf = new CacheConfig(TEST_UTIL.getConfiguration());
+    CacheConfig cacheConf = new CacheConfig(conf);
     LruBlockCache cache = (LruBlockCache) cacheConf.getBlockCache();
     cache.clearCache();
     InternalScanner scanner = region.getScanner(scan);
