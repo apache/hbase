bd581b79bce6e4e1c40572cf1927172aa9580f3e
Lars Hofhansl <larsh@apache.org>
2012-07-13 00:38:45 +0000

    HBASE-6375 Master may be using a stale list of region servers for creating assignment plan during startup (Aditya Kishore)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1361018 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
+++ src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
@@ -2247,15 +2247,6 @@ public class AssignmentManager extends ZooKeeperListener {
    * @throws IOException
    */
   public void assignAllUserRegions() throws IOException, InterruptedException {
-    // Get all available servers
-    List<ServerName> servers = serverManager.getOnlineServersList();
-
-    // Remove the deadNotExpired servers from the server list.
-    removeDeadNotExpiredServers(servers);
-
-    // If there are no servers we need not proceed with region assignment.
-    if(servers.isEmpty()) return;
-
     // Skip assignment for regions of tables in DISABLING state also because
     // during clean cluster startup no RS is alive and regions map also doesn't
     // have any information about the regions. See HBASE-6281.
@@ -2265,6 +2256,15 @@ public class AssignmentManager extends ZooKeeperListener {
     Map<HRegionInfo, ServerName> allRegions = MetaReader.fullScan(catalogTracker,
         disablingAndDisabledTables, true);
     if (allRegions == null || allRegions.isEmpty()) return;
+
+    // Get all available servers
+    List<ServerName> servers = serverManager.getOnlineServersList();
+
+    // Remove the deadNotExpired servers from the server list.
+    removeDeadNotExpiredServers(servers);
+
+    // If there are no servers we need not proceed with region assignment.
+    if(servers.isEmpty()) return;
 
     // Determine what type of assignment to do on startup
     boolean retainAssignment = master.getConfiguration().
