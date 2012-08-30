1da663d1e39b310e2fc69d244a4aefde1ac50647
Michael Stack <stack@apache.org>
2012-08-30 23:16:10 +0000

    HBASE-6684 .META. timeout value is incorrect; REAPPLY
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1379217 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -507,7 +507,7 @@ Server {
   private void initializeZKBasedSystemTrackers() throws IOException,
       InterruptedException, KeeperException {
     this.catalogTracker = createCatalogTracker(this.zooKeeper, this.conf,
-        this, conf.getInt("hbase.master.catalog.timeout", Integer.MAX_VALUE));
+        this, conf.getInt("hbase.master.catalog.timeout", 600000));
     this.catalogTracker.start();
 
     this.balancer = LoadBalancerFactory.getLoadBalancer(conf);
