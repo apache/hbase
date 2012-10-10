c2045b11da35db0ca5b2a4a992f14c0ef19d2b12
Zhihong Yu <tedyu@apache.org>
2012-10-10 23:21:08 +0000

    HBASE-5837 hbase shell deleteall to .META. allows insertion of malformed rowkey (Ricky Saltzer)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1396857 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/ruby/hbase/table.rb
+++ hbase-server/src/main/ruby/hbase/table.rb
@@ -140,6 +140,7 @@ EOF
     #----------------------------------------------------------------------------------------------
     # Delete a row
     def _deleteall_internal(row, column = nil, timestamp = org.apache.hadoop.hbase.HConstants::LATEST_TIMESTAMP)
+      raise ArgumentError, "Row Not Found" if _get_internal(row).nil?
       d = org.apache.hadoop.hbase.client.Delete.new(row.to_s.to_java_bytes, timestamp, nil)
       if column
         family, qualifier = parse_column_name(column)
