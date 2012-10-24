2be66139d6d3189cd126ba146ef52a158b935f4b
gchanan <gchanan@unknown>
2012-10-24 23:01:10 +0000

    HBASE-7045 Add some comments to MVCC code
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1401910 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/MultiVersionConsistencyControl.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/MultiVersionConsistencyControl.java
@@ -108,6 +108,11 @@ public class MultiVersionConsistencyControl {
     perThreadReadPoint.set(0L);
   }
 
+  /**
+   * Generate and return a {@link WriteEntry} with a new write number.
+   * To complete the WriteEntry and wait for it to be visible,
+   * call {@link #completeMemstoreInsert(WriteEntry)}.
+   */
   public WriteEntry beginMemstoreInsert() {
     synchronized (writeQueue) {
       long nextWriteNumber = ++memstoreWrite;
@@ -117,11 +122,28 @@ public class MultiVersionConsistencyControl {
     }
   }
 
+  /**
+   * Complete a {@link WriteEntry} that was created by {@link #beginMemstoreInsert()}.
+   *
+   * At the end of this call, the global read point is at least as large as the write point
+   * of the passed in WriteEntry.  Thus, the write is visible to MVCC readers.
+   */
   public void completeMemstoreInsert(WriteEntry e) {
     advanceMemstore(e);
     waitForRead(e);
   }
 
+  /**
+   * Mark the {@link WriteEntry} as complete and advance the read point as
+   * much as possible.
+   *
+   * How much is the read point advanced?
+   * Let S be the set of all write numbers that are completed and where all previous write numbers
+   * are also completed.  Then, the read point is advanced to the supremum of S.
+   *
+   * @param e
+   * @return true if e is visible to MVCC readers (that is, readpoint >= e.writeNumber)
+   */
   boolean advanceMemstore(WriteEntry e) {
     synchronized (writeQueue) {
       e.markCompleted();
