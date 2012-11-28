e6858bdd6b94c6326f5231c3464aa5e86fbc5051
Michael Stack <stack@apache.org>
2012-11-28 20:08:39 +0000

    HBASE-7231 port HBASE-7200 create integration test for balancing regions and killing region servers to 0.94
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1414890 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/IntegrationTestDataIngestWithChaosMonkey.java
+++ src/test/java/org/apache/hadoop/hbase/IntegrationTestDataIngestWithChaosMonkey.java
@@ -22,11 +22,8 @@ import java.io.IOException;
 
 import junit.framework.Assert;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.ChaosMonkey;
-import org.apache.hadoop.hbase.util.LoadTestTool;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -39,95 +36,33 @@ import org.junit.experimental.categories.Category;
  * configuration parameter.
  */
 @Category(IntegrationTests.class)
-public class IntegrationTestDataIngestWithChaosMonkey {
+public class IntegrationTestDataIngestWithChaosMonkey extends IngestIntegrationTestBase {
 
-  private static final String TABLE_NAME = "TestDataIngestWithChaosMonkey";
   private static final int NUM_SLAVES_BASE = 4; //number of slaves for the smallest cluster
 
-  /** A soft limit on how long we should run */
-  private static final String RUN_TIME_KEY = "hbase.IntegrationTestDataIngestWithChaosMonkey.runtime";
-
-  //run for 5 min by default
+  // run for 5 min by default
   private static final long DEFAULT_RUN_TIME = 5 * 60 * 1000;
 
-  private static final Log LOG = LogFactory.getLog(IntegrationTestDataIngestWithChaosMonkey.class);
-  private IntegrationTestingUtility util;
-  private HBaseCluster cluster;
   private ChaosMonkey monkey;
 
   @Before
   public void setUp() throws Exception {
-    util = new IntegrationTestingUtility();
-
-    util.initializeCluster(NUM_SLAVES_BASE);
-
-    cluster = util.getHBaseClusterInterface();
-    deleteTableIfNecessary();
-
+    super.setUp(NUM_SLAVES_BASE);
     monkey = new ChaosMonkey(util, ChaosMonkey.EVERY_MINUTE_RANDOM_ACTION_POLICY);
     monkey.start();
   }
 
   @After
   public void tearDown() throws Exception {
-    monkey.stop("test has finished, that's why");
-    monkey.waitForStop();
-    util.restoreCluster();
-  }
-
-  private void deleteTableIfNecessary() throws IOException {
-    if (util.getHBaseAdmin().tableExists(TABLE_NAME)) {
-      util.deleteTable(Bytes.toBytes(TABLE_NAME));
+    if (monkey != null) {
+      monkey.stop("test has finished, that's why");
+      monkey.waitForStop();
     }
+    super.tearDown();
   }
 
   @Test
   public void testDataIngest() throws Exception {
-    LOG.info("Running testDataIngest");
-    LOG.info("Cluster size:" + util.getHBaseClusterInterface().getClusterStatus().getServersSize());
-
-    LoadTestTool loadTool = new LoadTestTool();
-    loadTool.setConf(util.getConfiguration());
-
-    long start = System.currentTimeMillis();
-    long runtime = util.getConfiguration().getLong(RUN_TIME_KEY, DEFAULT_RUN_TIME);
-    long startKey = 0;
-
-    long numKeys = estimateDataSize();
-    while (System.currentTimeMillis() - start < 0.9 * runtime) {
-      LOG.info("Intended run time: " + (runtime/60000) + " min, left:" +
-          ((runtime - (System.currentTimeMillis() - start))/60000) + " min");
-
-      int ret = loadTool.run(new String[] {
-          "-tn", TABLE_NAME,
-          "-write", "10:100:20",
-          "-start_key", String.valueOf(startKey),
-          "-num_keys", String.valueOf(numKeys)
-      });
-
-      //assert that load was successful
-      Assert.assertEquals(0, ret);
-
-      ret = loadTool.run(new String[] {
-          "-tn", TABLE_NAME,
-          "-read", "100:20",
-          "-start_key", String.valueOf(startKey),
-          "-num_keys", String.valueOf(numKeys)
-      });
-
-      //assert that verify was successful
-      Assert.assertEquals(0, ret);
-      startKey += numKeys;
-    }
-  }
-
-  /** Estimates a data size based on the cluster size */
-  protected long estimateDataSize() throws IOException {
-    //base is a 4 slave node cluster.
-    ClusterStatus status = cluster.getClusterStatus();
-    int numRegionServers = status.getServersSize();
-    int multiplication = Math.max(1, numRegionServers / NUM_SLAVES_BASE);
-
-    return 10000 * multiplication;
+    runIngestTest(DEFAULT_RUN_TIME, 2500, 10, 100, 20);
   }
 }
