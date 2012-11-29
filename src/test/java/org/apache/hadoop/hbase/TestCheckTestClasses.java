e3bbe5cb1f018a4f6ef724e8e6f4fc94722c61eb
Michael Stack <stack@apache.org>
2012-11-29 06:20:14 +0000

    HBASE-7230 port HBASE-7109 integration tests on cluster are not getting picked up from distribution to 0.94
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1415054 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/TestCheckTestClasses.java
+++ src/test/java/org/apache/hadoop/hbase/TestCheckTestClasses.java
@@ -42,20 +42,6 @@ import org.junit.runners.Suite;
  */
 @Category(SmallTests.class)
 public class TestCheckTestClasses {
-
-  private FileFilter TEST_CLASS_FILE_FILTER = new FileFilter() {
-    @Override
-    public boolean accept(File file) {
-      return file.isDirectory() || isTestClassFile(file);
-
-    }
-    private boolean isTestClassFile(File file) {
-      String fileName = file.getName();
-      return fileName.endsWith(".class")
-          && (fileName.startsWith("Test") || fileName.startsWith("IntegrationTest"));
-    }
-  };
-
   /**
    * Throws an assertion if we find a test class without category (small/medium/large/integration).
    * List all the test classes without category in the assertion message.
@@ -63,124 +49,13 @@ public class TestCheckTestClasses {
   @Test
   public void checkClasses() throws Exception {
     List<Class<?>> badClasses = new java.util.ArrayList<Class<?>>();
-
-    for (Class<?> c : findTestClasses()) {
-      if (!existCategoryAnnotation(c, null)) {
+    ClassTestFinder classFinder = new ClassTestFinder();
+    for (Class<?> c : classFinder.findClasses(false)) {
+      if (ClassTestFinder.getCategoryAnnotations(c).length == 0) {
         badClasses.add(c);
       }
     }
-
     assertTrue("There are " + badClasses.size() + " test classes without category: "
       + badClasses, badClasses.isEmpty());
-  }
-
-  /** Returns whether the class has @Category annotation having the xface value.
-   */
-  private boolean existCategoryAnnotation(Class<?> c, Class<?> xface) {
-    Category category = c.getAnnotation(Category.class);
-
-    if (category != null) {
-      if (xface == null) {
-        return true;
-      }
-      for (Class<?> cc : category.value()) {
-        if (cc.equals(xface)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  /*
-  * A class is considered as a test class if:
-   *  - it's not Abstract AND
-   *  - one or more of its methods is annotated with org.junit.Test OR
-   *  - the class is annotated with Suite.SuiteClasses
-  * */
-  private boolean isTestClass(Class<?> c) {
-    if (Modifier.isAbstract(c.getModifiers())) {
-      return false;
-    }
-
-    if (c.getAnnotation(Suite.SuiteClasses.class) != null) {
-      return true;
-    }
-
-    for (Method met : c.getMethods()) {
-      if (met.getAnnotation(Test.class) != null) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  /**
-   * Finds test classes which are annotated with @Category having xface value
-   * @param xface the @Category value
-   */
-  public List<Class<?>> findTestClasses(Class<?> xface) throws ClassNotFoundException, IOException {
-    List<Class<?>> classes = new ArrayList<Class<?>>();
-    for (Class<?> c : findTestClasses()) {
-      if (existCategoryAnnotation(c, xface)) {
-        classes.add(c);
-      }
-    }
-    return classes;
-  }
-
-  private List<Class<?>> findTestClasses() throws ClassNotFoundException, IOException {
-    final String packageName = "org.apache.hadoop.hbase";
-    final String path = packageName.replace('.', '/');
-
-    Enumeration<URL> resources = this.getClass().getClassLoader().getResources(path);
-    List<File> dirs = new ArrayList<File>();
-
-    while (resources.hasMoreElements()) {
-      URL resource = resources.nextElement();
-      dirs.add(new File(resource.getFile()));
-    }
-
-    List<Class<?>> classes = new ArrayList<Class<?>>();
-    for (File directory : dirs) {
-      classes.addAll(findTestClasses(directory, packageName));
-    }
-
-    return classes;
-  }
-
-
-  private List<Class<?>> findTestClasses(File baseDirectory, String packageName)
-    throws ClassNotFoundException {
-    List<Class<?>> classes = new ArrayList<Class<?>>();
-    if (!baseDirectory.exists()) {
-      return classes;
-    }
-
-    File[] files = baseDirectory.listFiles(TEST_CLASS_FILE_FILTER);
-    assertNotNull(files);
-    Pattern p = Pattern.compile("hbase-hadoop\\d?-compat");
-    for (File file : files) {
-      final String fileName = file.getName();
-      if (p.matcher(file.getAbsolutePath()).find()) {
-        continue;
-      }
-
-      if (file.isDirectory()) {
-        classes.addAll(findTestClasses(file, packageName + "." + fileName));
-      } else {
-        Class<?> c = Class.forName(
-          packageName + '.' + fileName.substring(0, fileName.length() - 6),
-          false,
-          this.getClass().getClassLoader());
-
-        if (isTestClass(c)) {
-          classes.add(c);
-        }
-      }
-    }
-
-    return classes;
   }
 }
