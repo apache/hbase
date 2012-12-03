8bb307791da86d5c35eed73c8442abf339628af6
liyin <liyin@unknown>
2012-12-03 23:26:50 +0000

    [HBASE-7099] [89-fb] Fix TestCompactionHook fail
    Author: adela
    
    Summary:
    under jdk7 one of the test cases (the one which is executed
    second) is sometimes failing sometimes
    not. Looks like the update on the configuration didn't take effect on
    time, so there is a KV without updated value.
    This diff separates the two tests in different Java classes, since it
    looks like a method wrapped with @Before annotation will take the same effect on
    all test cases (and here we don't want that).
    
    Test Plan: run the tests
    
    Reviewers: kannan, kranganathan
    
    Reviewed By: kranganathan
    
    CC: hbase-eng@
    
    Differential Revision: https://phabricator.fb.com/D645053
    
    Task ID: 1928247
    
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.89-fb@1416728 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/hadoop/hbase/regionserver/compactionhook/TestLowerToUpperCompactionHook.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.hadoop.hbase.regionserver.compactionhook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.hbase.HBaseTestCase;
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.hfile.Compression;
+import org.apache.hadoop.hbase.regionserver.HRegion;
+import org.apache.hadoop.hbase.regionserver.InternalScanner;
+import org.apache.hadoop.hbase.regionserver.metrics.SchemaMetrics;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+@SuppressWarnings("deprecation")
+public class TestLowerToUpperCompactionHook {
+
+  private static byte[] TABLE = Bytes.toBytes("TestCompactionHook");
+  private static byte[] FAMILY = Bytes.toBytes("family");
+  private static byte[] START_KEY = Bytes.toBytes("aaa");
+  private static byte[] END_KEY = Bytes.toBytes("zzz");
+  private static int BLOCK_SIZE = 70;
+
+  private static HBaseTestingUtility TEST_UTIL = null;
+  private static HTableDescriptor TESTTABLEDESC = null;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    SchemaMetrics.setUseTableNameInTest(true);
+    TEST_UTIL = new HBaseTestingUtility();
+    TESTTABLEDESC = new HTableDescriptor(TABLE);
+
+    TESTTABLEDESC.addFamily(new HColumnDescriptor(FAMILY).setMaxVersions(10)
+        .setBlockCacheEnabled(true).setBlocksize(BLOCK_SIZE)
+        .setCompressionType(Compression.Algorithm.NONE));
+    TEST_UTIL
+        .getConfiguration()
+        .set(
+            HConstants.COMPACTION_HOOK,
+            "org.apache.hadoop.hbase.regionserver.compactionhook.LowerToUpperCompactionHook");
+  }
+
+  @Test
+  public void testLowerToUpperHook() throws Exception {
+    HRegion r = HBaseTestCase.createNewHRegion(TESTTABLEDESC, START_KEY,
+        END_KEY, TEST_UTIL.getConfiguration());
+    Put[] puts = new Put[25];
+    // put some lowercase strings
+    for (int i = 0; i < 25; i++) {
+      byte[] row = Bytes.toBytes("row" + i);
+      Put put = new Put(row);
+      byte[] qualifier = Bytes.toBytes("qual" + i);
+      byte[] value = Bytes.toBytes("ab" + (char) (i + 97));
+      put.add(FAMILY, qualifier, value);
+      puts[i] = put;
+    }
+    r.put(puts);
+    // without calling this, the compaction will not happen
+    r.flushcache();
+    r.compactStores(true);
+    //
+    Scan scan = new Scan();
+    InternalScanner s = r.getScanner(scan);
+    List<KeyValue> results = new ArrayList<KeyValue>();
+    while (s.next(results))
+      ;
+    s.close();
+    // check if the values in results are in upper case
+    // this should return true, we are flushing the HRegion, so we expect the
+    // compaction to be done
+    Assert.assertTrue(checkIfLowerCase(results));
+    // check if we got all 25 results back
+    Assert.assertEquals(25, results.size());
+  }
+
+  public boolean checkIfLowerCase(List<KeyValue> result) {
+    for (KeyValue kv : result) {
+      String currValue = new String(kv.getValue());
+      String uppercase = currValue.toUpperCase();
+      if (!currValue.equals(uppercase)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
