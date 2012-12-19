81d81f8aa28638060b91f0bc75dd2a5b59325828
Gary Helmling <garyh@apache.org>
2012-12-19 18:18:42 +0000

    HBASE-7357  Use hbase.security.authentication for client / server security negotiation
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1423997 13f79535-47bb-0310-9956-ffa450edef68

--- security/src/main/java/org/apache/hadoop/hbase/ipc/SecureClient.java
+++ security/src/main/java/org/apache/hadoop/hbase/ipc/SecureClient.java
@@ -98,7 +98,7 @@ public class SecureClient extends HBaseClient {
 
       User ticket = remoteId.getTicket();
       Class<?> protocol = remoteId.getProtocol();
-      this.useSasl = User.isSecurityEnabled();
+      this.useSasl = User.isHBaseSecurityEnabled(conf);
       if (useSasl && protocol != null) {
         TokenInfo tokenInfo = protocol.getAnnotation(TokenInfo.class);
         if (tokenInfo != null) {
