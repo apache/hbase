bc0371e478153da28a67b3563446994c85c338a9
larsh <larsh@unknown>
2012-12-21 05:59:06 +0000

    HBASE-5778 Reapply, Test failures not caused by this. Sorry for the noise.
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1424810 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestHLogSplit.java
+++ src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestHLogSplit.java
@@ -86,7 +86,7 @@ public class TestHLogSplit {
   private Configuration conf;
   private FileSystem fs;
 
-  private final static HBaseTestingUtility
+  protected final static HBaseTestingUtility
           TEST_UTIL = new HBaseTestingUtility();
 
 
