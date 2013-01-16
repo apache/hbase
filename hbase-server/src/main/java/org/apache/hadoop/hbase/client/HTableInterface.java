db4a453149521a5e225be5f10183176096b15025
Zhihong Yu <tedyu@apache.org>
2013-01-16 02:37:55 +0000

    HBASE-7549 Make HTableInterface#batch() javadoc proper (Anoop)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1433805 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/client/HTableInterface.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/client/HTableInterface.java
@@ -80,12 +80,12 @@ public interface HTableInterface extends Closeable {
   boolean exists(Get get) throws IOException;
 
   /**
-   * Method that does a batch call on Deletes, Gets and Puts. The ordering of
-   * execution of the actions is not defined. Meaning if you do a Put and a
+   * Method that does a batch call on Deletes, Gets, Puts, Increments, Appends and RowMutations.
+   * The ordering of execution of the actions is not defined. Meaning if you do a Put and a
    * Get in the same {@link #batch} call, you will not necessarily be
    * guaranteed that the Get returns what the Put had put.
    *
-   * @param actions list of Get, Put, Delete objects
+   * @param actions list of Get, Put, Delete, Increment, Append, RowMutations objects
    * @param results Empty Object[], same size as actions. Provides access to partial
    *                results, in case an exception is thrown. A null in the result array means that
    *                the call for that action failed, even after retries
@@ -98,7 +98,7 @@ public interface HTableInterface extends Closeable {
    * Same as {@link #batch(List, Object[])}, but returns an array of
    * results instead of using a results parameter reference.
    *
-   * @param actions list of Get, Put, Delete objects
+   * @param actions list of Get, Put, Delete, Increment, Append, RowMutations objects
    * @return the results from the actions. A null in the return array means that
    *         the call for that action failed, even after retries
    * @throws IOException
