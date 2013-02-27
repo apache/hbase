7850fda086ce22daafd37606d38e23789f3d6139
Zhihong Yu <tedyu@apache.org>
2013-02-27 15:00:59 +0000

    HBASE-7878 recoverFileLease does not check return value of recoverLease (Ted Yu)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1450793 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/util/FSHDFSUtils.java
+++ src/main/java/org/apache/hadoop/hbase/util/FSHDFSUtils.java
@@ -50,6 +50,8 @@ public class FSHDFSUtils extends FSUtils{
    * in o.a.h.hdfs.protocol.HdfsConstants cause of HDFS-1620.
    */
   public static final long LEASE_SOFTLIMIT_PERIOD = 60 * 1000;
+  
+  public static final String TEST_TRIGGER_DFS_APPEND = "hbase.test.trigger.dfs.append";
 
   @Override
   public void recoverFileLease(final FileSystem fs, final Path p, Configuration conf)
@@ -68,13 +70,23 @@ public class FSHDFSUtils extends FSUtils{
 
     // Trying recovery
     boolean recovered = false;
+    long recoveryTimeout = conf.getInt("hbase.lease.recovery.timeout", 300000);
+    // conf parameter passed from unit test, indicating whether fs.append() should be triggered
+    boolean triggerAppend = conf.getBoolean(TEST_TRIGGER_DFS_APPEND, false);
+    Exception ex = null;    
     while (!recovered) {
       try {
         try {
           if (fs instanceof DistributedFileSystem) {
             DistributedFileSystem dfs = (DistributedFileSystem)fs;
-            DistributedFileSystem.class.getMethod("recoverLease",
-              new Class[] {Path.class}).invoke(dfs, p);
+            if (triggerAppend) throw new IOException("recoverFileLease");
+            try {
+              recovered = (Boolean) DistributedFileSystem.class.getMethod(
+                "recoverLease", new Class[] { Path.class }).invoke(dfs, p);
+            } catch (InvocationTargetException ite) {
+              // function was properly called, but threw it's own exception
+              throw (IOException) ite.getCause();
+            }
           } else {
             throw new Exception("Not a DistributedFileSystem");
           }
@@ -82,12 +94,20 @@ public class FSHDFSUtils extends FSUtils{
           // function was properly called, but threw it's own exception
           throw (IOException) ite.getCause();
         } catch (Exception e) {
-          LOG.debug("Failed fs.recoverLease invocation, " + e.toString() +
-            ", trying fs.append instead");
+          // hdfs 2.0 may throw RecoveryInProgressException
+          if (!e.getClass().getName().contains("RecoveryInProgressException")) {
+            LOG.debug("Failed fs.recoverLease invocation, " + e.toString() +
+                ", trying fs.append instead");
+            ex = e;
+          }
+        }
+        if (ex != null || System.currentTimeMillis() - startWaiting > recoveryTimeout) {
+          ex = null; // assume the following append() call would succeed
           FSDataOutputStream out = fs.append(p);
           out.close();
+          recovered = true;
         }
-        recovered = true;
+        if (recovered) break;
       } catch (IOException e) {
         e = RemoteExceptionHandler.checkIOException(e);
         if (e instanceof AlreadyBeingCreatedException) {
@@ -111,9 +131,9 @@ public class FSHDFSUtils extends FSUtils{
       }
       try {
         Thread.sleep(1000);
-      } catch (InterruptedException ex) {
+      } catch (InterruptedException ie) {
         InterruptedIOException iioe = new InterruptedIOException();
-        iioe.initCause(ex);
+        iioe.initCause(ie);
         throw iioe;
       }
     }
