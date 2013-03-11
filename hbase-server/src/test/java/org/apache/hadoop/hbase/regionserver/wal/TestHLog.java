20816c9d476a442b1b189335870fdd82db28a5db
mbertozzi <mbertozzi@unknown>
2013-03-11 23:46:12 +0000

    HBASE-8069 TestHLog is dependent on the execution order
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1455363 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestHLog.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestHLog.java
@@ -487,6 +487,9 @@ public class TestHLog  {
     }
     assertEquals(total, count);
     reader.close();
+
+    // Reset the lease period
+    setLeasePeriod.invoke(cluster, new Object[]{new Long(60000), new Long(3600000)});
   }
 
   /**
