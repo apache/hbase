486b290863a66641b0ec30dbf2d9e349733cae44
Zhihong Yu <tedyu@apache.org>
2013-03-14 18:31:55 +0000

    HBASE-8044 Revert, fixed by HBASE-6643
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1456604 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/HBaseAdmin.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/HBaseAdmin.java
@@ -1148,7 +1148,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void closeRegion(final String regionname, final String serverName)
   throws IOException {
-    closeRegion(Bytes.toBytesBinary(regionname), serverName);
+    closeRegion(Bytes.toBytes(regionname), serverName);
   }
 
   /**
@@ -1266,7 +1266,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void flush(final String tableNameOrRegionName)
   throws IOException, InterruptedException {
-    flush(Bytes.toBytesBinary(tableNameOrRegionName));
+    flush(Bytes.toBytes(tableNameOrRegionName));
   }
 
   /**
@@ -1335,7 +1335,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void compact(final String tableNameOrRegionName)
   throws IOException, InterruptedException {
-    compact(Bytes.toBytesBinary(tableNameOrRegionName));
+    compact(Bytes.toBytes(tableNameOrRegionName));
   }
 
   /**
@@ -1362,7 +1362,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void compact(String tableOrRegionName, String columnFamily)
     throws IOException,  InterruptedException {
-    compact(Bytes.toBytesBinary(tableOrRegionName), Bytes.toBytes(columnFamily));
+    compact(Bytes.toBytes(tableOrRegionName), Bytes.toBytes(columnFamily));
   }
 
   /**
@@ -1389,7 +1389,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void majorCompact(final String tableNameOrRegionName)
   throws IOException, InterruptedException {
-    majorCompact(Bytes.toBytesBinary(tableNameOrRegionName));
+    majorCompact(Bytes.toBytes(tableNameOrRegionName));
   }
 
   /**
@@ -1696,7 +1696,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void split(final String tableNameOrRegionName)
   throws IOException, InterruptedException {
-    split(Bytes.toBytesBinary(tableNameOrRegionName));
+    split(Bytes.toBytes(tableNameOrRegionName));
   }
 
   /**
@@ -1714,7 +1714,7 @@ public class HBaseAdmin implements Abortable, Closeable {
 
   public void split(final String tableNameOrRegionName,
     final String splitPoint) throws IOException, InterruptedException {
-    split(Bytes.toBytesBinary(tableNameOrRegionName), Bytes.toBytesBinary(splitPoint));
+    split(Bytes.toBytes(tableNameOrRegionName), Bytes.toBytes(splitPoint));
   }
 
   /**
