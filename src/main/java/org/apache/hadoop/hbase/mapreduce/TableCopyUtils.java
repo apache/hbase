366ca123e94106752fbecfbbed4b60adf5146d20
liyin <liyin@unknown>
2013-03-21 18:52:50 +0000

    [HBASE-7707][89-fb] Improved copy table user interface
    Author: adela
    
    Summary:
    Deleted the functionality of creating a smaller table by scan
    and put. Improved the interface for the users
    
    Test Plan: tested on my machine
    
    Reviewers: liyintang
    
    Reviewed By: liyintang
    
    CC: hbase-eng@
    
    Differential Revision: https://phabricator.fb.com/D693127
    
    Task ID: 2051427
    
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.89-fb@1459455 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/mapreduce/TableCopyUtils.java
+++ src/main/java/org/apache/hadoop/hbase/mapreduce/TableCopyUtils.java
@@ -18,124 +18,68 @@
 package org.apache.hadoop.hbase.mapreduce;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.MasterNotRunningException;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /**
- * Util which can do two things:
- * <ul>
- *  <li>Make a copy of entire table using bulkupload</li>
- *  <li>Make a smaller table out of a bigger one</li>
- * </ul>
+ * Make a copy of entire table using bulkupload</li>
  */
 public class TableCopyUtils {
 
   private Configuration conf;
   private HBaseAdmin admin;
+  private long startTime;
+  private long endTime;
+  private String newTableName;
+  private String oldTableName;
+
 
   public TableCopyUtils() throws MasterNotRunningException {
     this.conf = HBaseConfiguration.create();
     this.admin = new HBaseAdmin(conf);
+    this.startTime = 0;
+    this.endTime = 0;
   }
 
-  /**
-   * Creates a table out of limited number of KVs and regions from an old table
-   *
-   * @param numRegions
-   * @param numKVs
-   * @param oldTable
-   * @param newTableName
-   * @throws IOException
-   */
-  public void createTableFromRegions(int numRegions, int numKVs,
-      String oldTable, String newTableName) throws IOException {
-    HTable tableOld = new HTable(conf, oldTable);
-    HColumnDescriptor[] families = tableOld.getTableDescriptor()
-        .getColumnFamilies();
-    createTable(newTableName, families, tableOld.getStartKeys());
-    HTable tableNew = new HTable(conf, newTableName);
-    Set<HRegionInfo> allRegions = tableOld.getRegionsInfo().keySet();
-    List<Put> puts = new ArrayList<Put>();
-    int i = 0;
-    int currentKvs = 0;
-    for (HRegionInfo info : allRegions) {
-      if (i == numRegions) {
-        break;
-      }
-      ResultScanner scanner = tableOld.getScanner(new Scan(info.getStartKey(),
-          info.getEndKey()));
-      for (Iterator<Result> iterator = scanner.iterator(); iterator.hasNext();) {
-        Result result = (Result) iterator.next();
-        List<KeyValue> kvs = result.list();
-        for (KeyValue kv : kvs) {
-          Put p = new Put(kv.getRow());
-          p.add(kv);
-          puts.add(p);
-          currentKvs++;
-          if (numKVs != -1 && currentKvs == numKVs) {
-            tableNew.put(puts);
-            return;
-          }
-        }
-      }
-      scanner.close();
-      i++;
-    }
-    tableNew.put(puts);
-  }
 
   /**
    * Makes exact copy of another table with the specified name using bulkload
-   *
-   * @param oldTableName the table that we want to copy
-   * @param startRow optional parameter if we want to scan & copy smaller portion of the table
-   * @param stopRow optional parameter if we want to scan & copy smaller portion of the table
-   * @param newTableName name of the new table that will be created
+   * @param args - arguments with which we run the tool
    * @throws IOException
    * @throws ClassNotFoundException
    * @throws InterruptedException
    */
-  public void copyTableWithBulkUpload(String oldTableName, byte[] startRow, byte[] stopRow,
-      String newTableName) throws IOException, ClassNotFoundException,
-      InterruptedException {
+  public void copyTableWithBulkUpload(String[] args) throws IOException,
+      ClassNotFoundException, InterruptedException {
+    if (!doCommandLine(args)) {
+      return;
+    }
     HTable oldTable = new HTable(conf, oldTableName);
     // temp files will be stored here when data is loaded with the MR job
-    Path outputPath = new Path("OUTPUT-" + oldTableName + "-" + newTableName);
+    Path outputPath = new Path("COPYTABLE-" + oldTableName + "-" + newTableName);
     HColumnDescriptor[] families = oldTable.getTableDescriptor()
         .getColumnFamilies();
     Scan scan = new Scan();
-    if (startRow.length != 0) {
-      scan.setStartRow(startRow);
-      if (stopRow.length != 0) {
-        scan.setStopRow(stopRow);
-      }
+    if (this.startTime != 0) {
+      scan.setTimeRange(startTime,
+          this.endTime == 0 ? HConstants.LATEST_TIMESTAMP : endTime);
     }
     for (HColumnDescriptor descriptor : families) {
       scan.addFamily(descriptor.getName());
@@ -168,49 +112,72 @@ public class TableCopyUtils {
     admin.createTable(desc, startKeys);
   }
 
-  public static void main(String[] args) throws IOException, ParseException,
-      ClassNotFoundException, InterruptedException {
-    Options opt = new Options();
-    opt.addOption("bl", "bulkupload", false,
-        "copy table with bulk upload method");
-    opt.addOption("lc", "limited copy", false,
-        "copy just a number of keys and regions from the old table to a new table");
-    CommandLine cmd = new GnuParser().parse(opt, args);
-    TableCopyUtils tcu = new TableCopyUtils();
-    Scanner s = new Scanner(System.in);
-    System.out.print("Name of the old table: ");
-    String oldTableName = s.next();
-    System.out.print("Name of the new table: ");
-    String newTableName = s.next();
-    if (cmd.hasOption("bl")) {
-      byte[] startRow = new byte[] {};
-      byte[] stopRow = new byte[] {};
-      System.out.print("Do you want to set start and stop row for the old table scanner? [y/n] ");
-      if (s.next().trim().toLowerCase().equals("y")) {
-        System.out
-            .print("Insert b (for setting start and stop row) OR s (for setting start row)");
-        String input = s.next().trim().toLowerCase();
-        if (input.equals("b") || input.equals("s")) {
-          System.out.print("Start row: ");
-          startRow = Bytes.toBytes(s.nextLine());
-          if (input.equals("b")) {
-            System.out.print("Stop row: ");
-            stopRow = Bytes.toBytes(s.nextLine());
-          }
+  private static void printUsage(final String errorMsg) {
+    if (errorMsg != null && errorMsg.length() > 0) {
+      System.err.println("ERROR: " + errorMsg);
+    }
+    System.err.println("Usage: TableCopyUtils " +
+        "[--starttime=X] [--endtime=Y] " +
+        "<newtablename> <tablename>");
+    System.err.println();
+    System.err.println("Options:");
+    System.err.println(" starttime    beginning of the time range");
+    System.err.println("              without endtime means from starttime to forever");
+    System.err.println(" endtime      end of the time range");
+    System.err.println();
+    System.err.println("Args:");
+    System.err.println(" newname      Name of the new table");
+    System.err.println(" tablename    Name of the table to copy");
+    System.err.println();
+    System.err.println("Example:");
+    System.err.println(" $ bin/hbase " +
+        "org.apache.hadoop.hbase.mapreduce.TableCopyUtils --starttime=1265875194289 --endtime=1265878794289 " +
+        "NewTable OriginalTable ");
+  }
+
+  private boolean doCommandLine(final String[] args) {
+    if (args.length < 1) {
+      printUsage(null);
+      return false;
+    }
+    try {
+      for (int i = 0; i < args.length; i++) {
+        String cmd = args[i];
+        if (cmd.equals("-h") || cmd.startsWith("--h")) {
+          printUsage(null);
+          return false;
+        }
+        final String startTimeArgKey = "--starttime=";
+        if (cmd.startsWith(startTimeArgKey)) {
+          this.startTime = Long.parseLong(cmd.substring(startTimeArgKey.length()));
+          continue;
+        }
+        final String endTimeArgKey = "--endtime=";
+        if (cmd.startsWith(endTimeArgKey)) {
+          this.endTime = Long.parseLong(cmd.substring(endTimeArgKey.length()));
+          continue;
+        }
+        if (i == args.length - 1) {
+          this.oldTableName = cmd;
+        }
+        if (i == args.length - 2) {
+          this.newTableName = cmd;
         }
       }
-      tcu.copyTableWithBulkUpload(oldTableName, startRow, stopRow, newTableName);
-    } else if (cmd.hasOption("lc")) {
-      System.out
-          .println("Use this method only if you are copying a very small portion of the table..");
-      System.out.print("Number of regions to be copied: ");
-      int numRegions = s.nextInt();
-      System.out
-          .println("Number of KVs to be copied: (if you don't want to limit the number of KVs insert -1)");
-      int numKVs = s.nextInt();
-      tcu.createTableFromRegions(numRegions, numKVs, oldTableName, newTableName);
+      if (this.newTableName == null) {
+        printUsage("New table name must be specified");
+        return false;
+      }
+      if (this.oldTableName == null) {
+        printUsage("Original table must be specified");
+        return false;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      printUsage("Can't start because " + e.getMessage());
+      return false;
     }
-    s.close();
+    return true;
   }
 
   public static class KeyValueMapper extends
@@ -222,5 +189,11 @@ public class TableCopyUtils {
         context.write(key, kv);
       }
     }
+  }
+
+  public static void main(String[] args) throws IOException, ParseException,
+      ClassNotFoundException, InterruptedException {
+    TableCopyUtils tcu = new TableCopyUtils();
+    tcu.copyTableWithBulkUpload(args);
   }
 }
