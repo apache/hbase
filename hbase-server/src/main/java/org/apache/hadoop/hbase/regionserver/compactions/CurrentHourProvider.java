c9d33bef3f74cc771be1574db191666c2bc043d2
sershe <sershe@unknown>
2013-04-22 23:45:39 +0000

    HBASE-7437 Improve CompactSelection (Hiroshi Ikeda)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.95@1470755 13f79535-47bb-0310-9956-ffa450edef68

---
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/compactions/CurrentHourProvider.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.regionserver.compactions;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.apache.hadoop.classification.InterfaceAudience;
+
+@InterfaceAudience.Private
+public class CurrentHourProvider {
+  private CurrentHourProvider() { throw new AssertionError(); }
+
+  private static final class Tick {
+    final int currentHour;
+    final long expirationTimeInMillis;
+
+    Tick(int currentHour, long expirationTimeInMillis) {
+      this.currentHour = currentHour;
+      this.expirationTimeInMillis = expirationTimeInMillis;
+    }
+  }
+
+  private static Tick nextTick() {
+    Calendar calendar = new GregorianCalendar();
+    int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+    moveToNextHour(calendar);
+    return new Tick(currentHour, calendar.getTimeInMillis());
+  }
+
+  private static void moveToNextHour(Calendar calendar) {
+    calendar.add(Calendar.HOUR_OF_DAY, 1);
+    calendar.set(Calendar.MINUTE, 0);
+    calendar.set(Calendar.SECOND, 0);
+    calendar.set(Calendar.MILLISECOND, 0);
+  }
+
+  private static volatile Tick tick = nextTick();
+
+  public static int getCurrentHour() {
+    Tick tick = CurrentHourProvider.tick;
+    if(System.currentTimeMillis() < tick.expirationTimeInMillis) {
+      return tick.currentHour;
+    }
+
+    CurrentHourProvider.tick = tick = nextTick();
+    return tick.currentHour;
+  }
+}
---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/compactions/TestOffPeakHours.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.regionserver.compactions;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.SmallTests;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SmallTests.class)
+public class TestOffPeakHours {
+  private static HBaseTestingUtility testUtil;
+
+  @BeforeClass
+  public static void setUpClass() {
+    testUtil = new HBaseTestingUtility();
+  }
+
+  private int hourOfDay;
+  private int hourPlusOne;
+  private int hourMinusOne;
+  private int hourMinusTwo;
+  private Configuration conf;
+
+  @Before
+  public void setUp() {
+    hourOfDay = 15;
+    hourPlusOne = ((hourOfDay+1)%24);
+    hourMinusOne = ((hourOfDay-1+24)%24);
+    hourMinusTwo = ((hourOfDay-2+24)%24);
+    conf = testUtil.getConfiguration();
+  }
+
+  @Test
+  public void testWithoutSettings() {
+    Configuration conf = testUtil.getConfiguration();
+    OffPeakHours target = OffPeakHours.getInstance(conf);
+    assertFalse(target.isOffPeakHour(hourOfDay));
+  }
+
+  @Test
+  public void testSetPeakHourToTargetTime() {
+    conf.setLong("hbase.offpeak.start.hour", hourMinusOne);
+    conf.setLong("hbase.offpeak.end.hour", hourPlusOne);
+    OffPeakHours target = OffPeakHours.getInstance(conf);
+    assertTrue(target.isOffPeakHour(hourOfDay));
+  }
+
+  @Test
+  public void testSetPeakHourOutsideCurrentSelection() {
+    conf.setLong("hbase.offpeak.start.hour", hourMinusTwo);
+    conf.setLong("hbase.offpeak.end.hour", hourMinusOne);
+    OffPeakHours target = OffPeakHours.getInstance(conf);
+    assertFalse(target.isOffPeakHour(hourOfDay));
+  }
+}
