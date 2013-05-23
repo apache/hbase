1b6f7e3b20b0271cad48fa3ccdea422c9951317d
Michael Stack <stack@apache.org>
2013-05-23 22:18:33 +0000

    HBASE-2231 Compaction events should be written to HLog; LOGGING ADDENDUM
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.95@1485874 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/HLogUtil.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/HLogUtil.java
@@ -39,7 +39,8 @@ import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.protobuf.generated.WALProtos.CompactionDescriptor;
 import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
 import org.apache.hadoop.hbase.util.FSUtils;
-import org.apache.hadoop.hbase.util.Bytes;
+
+import com.google.protobuf.TextFormat;
 
 public class HLogUtil {
   static final Log LOG = LogFactory.getLog(HLogUtil.class);
@@ -262,6 +263,8 @@ public class HLogUtil {
     WALEdit e = WALEdit.createCompaction(c);
     log.append(info, c.getTableName().toByteArray(), e,
         EnvironmentEdgeManager.currentTimeMillis(), htd);
-    LOG.info("Appended compaction marker " + c);
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("Appended compaction marker " + TextFormat.shortDebugString(c));
+    }
   }
 }
