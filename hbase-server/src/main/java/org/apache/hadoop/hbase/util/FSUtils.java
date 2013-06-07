a0ea51a96ee33842ef392dc45379455540906233
Jean-Daniel Cryans <jdcryans@apache.org>
2013-06-07 23:46:22 +0000

    HBASE-8712  HFileLinkCleaner (FSUtils.listStatus) logs too much if             links do not exists (Matteo via JD)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.95@1490869 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/util/FSUtils.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/util/FSUtils.java
@@ -1474,7 +1474,9 @@ public abstract class FSUtils {
       status = filter == null ? fs.listStatus(dir) : fs.listStatus(dir, filter);
     } catch (FileNotFoundException fnfe) {
       // if directory doesn't exist, return null
-      LOG.debug(dir + " doesn't exist");
+      if (LOG.isTraceEnabled()) {
+        LOG.trace(dir + " doesn't exist");
+      }
     }
     if (status == null || status.length < 1) return null;
     return status;
