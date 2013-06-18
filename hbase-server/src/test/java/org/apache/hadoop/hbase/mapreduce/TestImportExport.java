8d7a1605fafdf618ac20395f5905d0614fac2ec7
Michael Stack <stack@apache.org>
2013-06-18 19:34:33 +0000

    HBASE-8453 TestImportExport failing again due to configuration issues
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.95@1494266 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/test/java/org/apache/hadoop/hbase/mapreduce/TestImportExport.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/mapreduce/TestImportExport.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
@@ -43,6 +44,7 @@ import org.apache.hadoop.hbase.filter.PrefixFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.mapred.JobConf;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -103,7 +105,9 @@ public class TestImportExport {
     GenericOptionsParser opts = new GenericOptionsParser(new Configuration(UTIL.getConfiguration()), args);
     Configuration conf = opts.getConfiguration();
     args = opts.getRemainingArgs();
-    Job job = Export.createSubmittableJob(conf, args);
+    JobConf jobConf = UTIL.getMRCluster().createJobConf();
+    HBaseConfiguration.merge(jobConf, conf);
+    Job job = Export.createSubmittableJob(jobConf, args);
     job.waitForCompletion(false);
     return job.isSuccessful();
   }
@@ -121,7 +125,9 @@ public class TestImportExport {
     GenericOptionsParser opts = new GenericOptionsParser(new Configuration(UTIL.getConfiguration()), args);
     Configuration conf = opts.getConfiguration();
     args = opts.getRemainingArgs();
-    Job job = Import.createSubmittableJob(conf, args);
+    JobConf jobConf = UTIL.getMRCluster().createJobConf();
+    HBaseConfiguration.merge(jobConf, conf);
+    Job job = Import.createSubmittableJob(jobConf, args);
     job.waitForCompletion(false);
     return job.isSuccessful();
   }
@@ -173,7 +179,7 @@ public class TestImportExport {
 
   /**
    * Test export .META. table
-   * 
+   *
    * @throws Exception
    */
   @Test
