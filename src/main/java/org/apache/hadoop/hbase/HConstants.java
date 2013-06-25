2fc28939001c25b83952ad5795dc40015a26397f
sershe <sershe@unknown>
2013-06-25 01:08:34 +0000

    HBASE-8776 tweak retry settings some more (on trunk and 0.94)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1496325 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/HConstants.java
+++ src/main/java/org/apache/hadoop/hbase/HConstants.java
@@ -420,7 +420,7 @@ public final class HConstants {
    * This is a retry backoff multiplier table similar to the BSD TCP syn
    * backoff table, a bit more aggressive than simple exponential backoff.
    */
-  public static int RETRY_BACKOFF[] = { 1, 1, 1, 2, 2, 4, 4, 8, 16, 32 };
+  public static int RETRY_BACKOFF[] = { 1, 1, 1, 2, 2, 4, 4, 8, 16, 32, 64 };
 
   public static final String REGION_IMPL = "hbase.hregion.impl";
 
