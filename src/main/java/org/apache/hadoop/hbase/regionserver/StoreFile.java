423de6443e06a9eab64edeb7fb6b4d41afa778c4
Michael Stack <stack@apache.org>
2013-07-05 21:07:45 +0000

    HBASE-8814 Possible NPE in split if a region has empty store files; RESTORE FOR SECOND TIME
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1500136 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/regionserver/StoreFile.java
+++ src/main/java/org/apache/hadoop/hbase/regionserver/StoreFile.java
@@ -953,6 +953,8 @@ public class StoreFile extends SchemaConfigured {
       //check if smaller than first key
       KeyValue splitKey = KeyValue.createLastOnRow(splitRow);
       byte[] firstKey = f.createReader().getFirstKey();
+      // If firstKey is null means storefile is empty.
+      if (firstKey == null) return null;
       if (f.getReader().getComparator().compare(splitKey.getBuffer(), 
           splitKey.getKeyOffset(), splitKey.getKeyLength(), 
           firstKey, 0, firstKey.length) < 0) {
@@ -963,6 +965,8 @@ public class StoreFile extends SchemaConfigured {
       //check if larger than last key.
       KeyValue splitKey = KeyValue.createFirstOnRow(splitRow);
       byte[] lastKey = f.createReader().getLastKey();      
+      // If lastKey is null means storefile is empty.
+      if (lastKey == null) return null;
       if (f.getReader().getComparator().compare(splitKey.getBuffer(), 
           splitKey.getKeyOffset(), splitKey.getKeyLength(), 
           lastKey, 0, lastKey.length) > 0) {
