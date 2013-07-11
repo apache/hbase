ae9b21ccc8e94a32bc9760cfe146a07949c3f12b
Lars George <larsgeorge@apache.org>
2013-07-11 09:10:17 +0000

    HBASE-8695 The HBase thrift service ignores XML configuration
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1502161 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/thrift/ThriftServer.java
+++ src/main/java/org/apache/hadoop/hbase/thrift/ThriftServer.java
@@ -194,10 +194,12 @@ public class ThriftServer {
         conf, TBoundedThreadPoolServer.THREAD_KEEP_ALIVE_TIME_SEC_CONF_KEY);
 
     // Set general thrift server options
-    conf.setBoolean(
-        ThriftServerRunner.COMPACT_CONF_KEY, cmd.hasOption(COMPACT_OPTION));
-    conf.setBoolean(
-        ThriftServerRunner.FRAMED_CONF_KEY, cmd.hasOption(FRAMED_OPTION));
+    boolean compact = cmd.hasOption(COMPACT_OPTION) ||
+      conf.getBoolean(ThriftServerRunner.COMPACT_CONF_KEY, false);
+    conf.setBoolean(ThriftServerRunner.COMPACT_CONF_KEY, compact);
+    boolean framed = cmd.hasOption(FRAMED_OPTION) ||
+      conf.getBoolean(ThriftServerRunner.FRAMED_CONF_KEY, false);
+    conf.setBoolean(ThriftServerRunner.FRAMED_CONF_KEY, framed);
     if (cmd.hasOption(BIND_OPTION)) {
       conf.set(
           ThriftServerRunner.BIND_CONF_KEY, cmd.getOptionValue(BIND_OPTION));
