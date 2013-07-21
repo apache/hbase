c92372810896ebfb0def66b0f30a792197299575
Michael Stack <stack@apache.org>
2013-07-21 04:43:38 +0000

    HBASE-9006 RPC code requires cellBlockCodecClass even though one is defined by default
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1505269 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServer.java
@@ -1534,7 +1534,6 @@ public class RpcServer implements RpcServerInterface {
     private void setupCellBlockCodecs(final ConnectionHeader header)
     throws FatalConnectionException {
       // TODO: Plug in other supported decoders.
-      if (!header.hasCellBlockCodecClass()) throw new FatalConnectionException("No codec");
       String className = header.getCellBlockCodecClass();
       try {
         this.codec = (Codec)Class.forName(className).newInstance();
