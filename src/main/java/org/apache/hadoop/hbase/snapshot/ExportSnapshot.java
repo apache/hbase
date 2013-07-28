b6cfdf8fff284d5e6751e34e808a480bae5f88e6
mbertozzi <mbertozzi@unknown>
2013-07-28 12:17:39 +0000

    HBASE-9060 ExportSnapshot job fails if target path contains percentage character (Jerry He)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1507792 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/snapshot/ExportSnapshot.java
+++ src/main/java/org/apache/hadoop/hbase/snapshot/ExportSnapshot.java
@@ -253,7 +253,7 @@ public final class ExportSnapshot extends Configured implements Tool {
         final Path outputPath, final FSDataOutputStream out,
         final long inputFileSize) {
       final String statusMessage = "copied %s/" + StringUtils.humanReadableInt(inputFileSize) +
-                                   " (%.3f%%) from " + inputPath + " to " + outputPath;
+                                   " (%.3f%%)";
 
       try {
         byte[] buffer = new byte[BUFFER_SIZE];
@@ -270,7 +270,8 @@ public final class ExportSnapshot extends Configured implements Tool {
             context.getCounter(Counter.BYTES_COPIED).increment(reportBytes);
             context.setStatus(String.format(statusMessage,
                               StringUtils.humanReadableInt(totalBytesWritten),
-                              reportBytes/(float)inputFileSize));
+                              totalBytesWritten/(float)inputFileSize) +
+                              " from " + inputPath + " to " + outputPath);
             reportBytes = 0;
           }
         }
@@ -278,7 +279,8 @@ public final class ExportSnapshot extends Configured implements Tool {
         context.getCounter(Counter.BYTES_COPIED).increment(reportBytes);
         context.setStatus(String.format(statusMessage,
                           StringUtils.humanReadableInt(totalBytesWritten),
-                          reportBytes/(float)inputFileSize));
+                          totalBytesWritten/(float)inputFileSize) +
+                          " from " + inputPath + " to " + outputPath);
 
         // Verify that the written size match
         if (totalBytesWritten != inputFileSize) {
