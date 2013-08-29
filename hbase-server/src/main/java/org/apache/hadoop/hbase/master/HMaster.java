d25517e01e17ea1b7582248a569f04f7fa98ffee
Jean-Daniel Cryans <jdcryans@apache.org>
2013-08-29 00:36:22 +0000

    HBASE-9371 Eliminate log spam when tailing files HBASE-9326 ServerName is created using getLocalSocketAddress, breaks binding to the wildcard            address. Revert HBASE-8640
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.95@1518429 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -399,15 +399,22 @@ MasterServices, Server {
     // Disable the block cache on the master
     this.conf.setFloat(HConstants.HFILE_BLOCK_CACHE_SIZE_KEY, 0.0f);
     // Server to handle client requests.
-    String hostname = conf.get("hbase.master.ipc.address",
-      Strings.domainNamePointerToHostName(DNS.getDefaultHost(
-        conf.get("hbase.master.dns.interface", "default"),
-        conf.get("hbase.master.dns.nameserver", "default"))));
+    String hostname = Strings.domainNamePointerToHostName(DNS.getDefaultHost(
+      conf.get("hbase.master.dns.interface", "default"),
+      conf.get("hbase.master.dns.nameserver", "default")));
     int port = conf.getInt(HConstants.MASTER_PORT, HConstants.DEFAULT_MASTER_PORT);
     // Test that the hostname is reachable
     InetSocketAddress initialIsa = new InetSocketAddress(hostname, port);
     if (initialIsa.getAddress() == null) {
       throw new IllegalArgumentException("Failed resolve of hostname " + initialIsa);
+    }
+    // Verify that the bind address is reachable if set
+    String bindAddress = conf.get("hbase.master.ipc.address");
+    if (bindAddress != null) {
+      initialIsa = new InetSocketAddress(bindAddress, port);
+      if (initialIsa.getAddress() == null) {
+        throw new IllegalArgumentException("Failed resolve of bind address " + initialIsa);
+      }
     }
     String name = "master/" + initialIsa.toString();
     // Set how many times to retry talking to another server over HConnection.
@@ -422,8 +429,8 @@ MasterServices, Server {
       0); // this is a DNC w/o high priority handlers
     // Set our address.
     this.isa = this.rpcServer.getListenerAddress();
-    this.serverName =
-        new ServerName(this.isa.getHostName(), this.isa.getPort(), System.currentTimeMillis());
+    // We don't want to pass isa's hostname here since it could be 0.0.0.0
+    this.serverName = new ServerName(hostname, this.isa.getPort(), System.currentTimeMillis());
     this.rsFatals = new MemoryBoundedLogMessageBuffer(
       conf.getLong("hbase.master.buffer.for.rs.fatals", 1*1024*1024));
 
