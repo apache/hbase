c903aa045ededae2ef56b3707d50d47e2f48d373
mbertozzi <mbertozzi@unknown>
2013-09-22 12:22:08 +0000

    HBASE-9607 Data loss after snapshot restore into cloned table
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1525351 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/client/TestRestoreSnapshotFromClient.java
+++ src/test/java/org/apache/hadoop/hbase/client/TestRestoreSnapshotFromClient.java
@@ -220,7 +220,7 @@ public class TestRestoreSnapshotFromClient {
   }
 
   @Test
-  public void testRestoreSnapshotOfCloned() throws IOException, InterruptedException {
+  public void testCloneSnapshotOfCloned() throws IOException, InterruptedException {
     byte[] clonedTableName = Bytes.toBytes("clonedtb-" + System.currentTimeMillis());
     admin.cloneSnapshot(snapshotName0, clonedTableName);
     SnapshotTestingUtils.verifyRowCount(TEST_UTIL, clonedTableName, snapshot0Rows);
@@ -232,6 +232,21 @@ public class TestRestoreSnapshotFromClient {
     admin.cloneSnapshot(snapshotName2, clonedTableName);
     SnapshotTestingUtils.verifyRowCount(TEST_UTIL, clonedTableName, snapshot0Rows);
     TEST_UTIL.deleteTable(clonedTableName);
+  }
+
+  @Test
+  public void testCloneAndRestoreSnapshot() throws IOException, InterruptedException {
+    TEST_UTIL.deleteTable(tableName);
+    waitCleanerRun();
+
+    admin.cloneSnapshot(snapshotName0, tableName);
+    SnapshotTestingUtils.verifyRowCount(TEST_UTIL, tableName, snapshot0Rows);
+    waitCleanerRun();
+
+    admin.disableTable(tableName);
+    admin.restoreSnapshot(snapshotName0);
+    admin.enableTable(tableName);
+    SnapshotTestingUtils.verifyRowCount(TEST_UTIL, tableName, snapshot0Rows);
   }
 
   // ==========================================================================
