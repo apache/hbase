0df4fb83efabf062fcd90e5b111b28360d7ba08c
larsh <larsh@unknown>
2013-10-07 20:57:28 +0000

    HBASE-9711 Improve HBASE-9428 - avoid copying bytes for RegexFilter unless necessary
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.96@1530060 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-client/src/main/java/org/apache/hadoop/hbase/filter/RegexStringComparator.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/filter/RegexStringComparator.java
@@ -109,12 +109,17 @@ public class RegexStringComparator extends ByteArrayComparable {
 
   @Override
   public int compareTo(byte[] value, int offset, int length) {
-    // See HBASE-9428. Make a copy of the relevant part of the byte[],
-    // or the JDK will copy the entire byte[] during String decode
-    byte[] tmp = Arrays.copyOfRange(value, offset, offset+length);
     // Use find() for subsequence match instead of matches() (full sequence
     // match) to adhere to the principle of least surprise.
-    return pattern.matcher(new String(tmp, charset)).find() ? 0 : 1;
+    String tmp;
+    if (length < value.length / 2) {
+      // See HBASE-9428. Make a copy of the relevant part of the byte[],
+      // or the JDK will copy the entire byte[] during String decode
+      tmp = new String(Arrays.copyOfRange(value, offset, offset + length), charset);
+    } else {
+      tmp = new String(value, offset, length, charset);
+    }
+    return pattern.matcher(tmp).find() ? 0 : 1;
   }
 
   /**
