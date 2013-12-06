4d3dbfd740bf4f5abb5cb2369788ef3d00b982bb
ndimiduk <ndimiduk@unknown>
2013-12-06 23:13:32 +0000

    HBASE-10061 TableMapReduceUtil.findOrCreateJar calls updateMap(null, ) resulting in thrown NPE (Amit Sela)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.96@1548749 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/TableMapReduceUtil.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/TableMapReduceUtil.java
@@ -696,7 +696,7 @@ public class TableMapReduceUtil {
     }
 
     if (null == jar || jar.isEmpty()) {
-      throw new IOException("Cannot locate resource for class " + my_class.getName());
+      return null;
     }
 
     LOG.debug(String.format("For class %s, using jar %s", my_class.getName(), jar));
@@ -710,6 +710,9 @@ public class TableMapReduceUtil {
    * @param packagedClasses map[class -> jar]
    */
   private static void updateMap(String jar, Map<String, String> packagedClasses) throws IOException {
+    if (null == jar || jar.isEmpty()) {
+      return;
+    }
     ZipFile zip = null;
     try {
       zip = new ZipFile(jar);
