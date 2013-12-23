13a19f5593ce46d94d95b4cff8e1e9abd1b61acd
larsh <larsh@unknown>
2013-12-23 18:36:02 +0000

    HBASE-10225 Bug in calls to RegionObsever.postScannerFilterRow (Anoop)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1553170 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -4154,7 +4154,8 @@ public class HRegion implements HeapSize { // , Writable{
       resetFilters();
       // Calling the hook in CP which allows it to do a fast forward
       if (this.region.getCoprocessorHost() != null) {
-        return this.region.getCoprocessorHost().postScannerFilterRow(this, currentRow);
+        return this.region.getCoprocessorHost().postScannerFilterRow(this, currentRow, offset,
+            length);
       }
       return true;
     }
