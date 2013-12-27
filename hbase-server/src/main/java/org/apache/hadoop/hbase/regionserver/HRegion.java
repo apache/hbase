8efd986db89621ba36539166970672b413410f14
Zhihong Yu <tedyu@apache.org>
2013-12-27 18:17:41 +0000

    HBASE-7226 HRegion.checkAndMutate uses incorrect comparison result for <, <=, > and >=
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.96@1553712 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -2409,10 +2409,10 @@ public class HRegion implements HeapSize { // , Writable{
               kv.getValueOffset(), kv.getValueLength());
           switch (compareOp) {
           case LESS:
-            matches = compareResult <= 0;
+            matches = compareResult < 0;
             break;
           case LESS_OR_EQUAL:
-            matches = compareResult < 0;
+            matches = compareResult <= 0;
             break;
           case EQUAL:
             matches = compareResult == 0;
@@ -2421,10 +2421,10 @@ public class HRegion implements HeapSize { // , Writable{
             matches = compareResult != 0;
             break;
           case GREATER_OR_EQUAL:
-            matches = compareResult > 0;
+            matches = compareResult >= 0;
             break;
           case GREATER:
-            matches = compareResult >= 0;
+            matches = compareResult > 0;
             break;
           default:
             throw new RuntimeException("Unknown Compare op " + compareOp.name());
