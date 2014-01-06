9e8e5474096c272d362219f4ae3a990689606a56
larsh <larsh@unknown>
2014-01-06 18:25:19 +0000

    HBASE-10284 Build broken with svn 1.8
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1555962 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-common/src/saveVersion.sh
+++ hbase-common/src/saveVersion.sh
@@ -32,7 +32,7 @@ date=`date`
 cwd=`pwd`
 if [ -d .svn ]; then
   revision=`svn info | sed -n -e 's/Last Changed Rev: \(.*\)/\1/p'`
-  url=`svn info | sed -n -e 's/URL: \(.*\)/\1/p'`
+  url=`svn info | sed -n -e 's/^URL: \(.*\)/\1/p'`
 elif [ -d .git ]; then
   revision=`git log -1 --pretty=format:"%H"`
   hostname=`hostname`
