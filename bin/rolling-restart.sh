407e077c8d476b517b0502483cf445beceaf5600
Jonathan Hsieh <jmhsieh@apache.org>
2014-03-12 22:16:28 +0000

    HBASE-10731 Fix environment variables typos in scripts (Albert Chu)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.98@1576957 13f79535-47bb-0310-9956-ffa450edef68

--- bin/rolling-restart.sh
+++ bin/rolling-restart.sh
@@ -28,9 +28,9 @@
 #     Default is ${HADOOP_CONF_DIR}/regionservers
 #   HADOOP_CONF_DIR  Alternate conf dir. Default is ${HADOOP_HOME}/conf.
 #   HBASE_CONF_DIR  Alternate hbase conf dir. Default is ${HBASE_HOME}/conf.
-#   HADOOP_SLAVE_SLEEP Seconds to sleep between spawning remote commands.
-#   HADOOP_SLAVE_TIMEOUT Seconds to wait for timing out a remote command. 
-#   HADOOP_SSH_OPTS Options passed to ssh when running remote commands.
+#   HBASE_SLAVE_SLEEP Seconds to sleep between spawning remote commands.
+#   HBASE_SLAVE_TIMEOUT Seconds to wait for timing out a remote command. 
+#   HBASE_SSH_OPTS Options passed to ssh when running remote commands.
 #
 # Modelled after $HADOOP_HOME/bin/slaves.sh.
 
