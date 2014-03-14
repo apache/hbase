ffabf9ba9b70baa679cca6cf804ef4ec275fdccc
Devaraj Das <ddas@apache.org>
2014-03-14 07:01:04 +0000

    HBASE-10616. Addendum that fixes the case of multiget_batchsize == 1
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/hbase-10070@1577438 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/test/java/org/apache/hadoop/hbase/util/MultiThreadedReader.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/util/MultiThreadedReader.java
@@ -206,7 +206,7 @@ public class MultiThreadedReader extends MultiThreadedAction
           numKeys++;
         } while (numKeys < batchSize);
 
-        if (numKeys > 1) { //meaning there is some key to read
+        if (numKeys > 0) { //meaning there is some key to read
           readKey(keysForThisReader);
           // We have verified some unique key(s).
           numUniqueKeysVerified.getAndAdd(readingRandomKeyStartIndex == -1 ?
