3e054d4f8b78af0734fee6007ae809d100f41466
Michael Stack <stack@apache.org>
2014-03-15 04:30:20 +0000

    HBASE-10751 TestHRegion testWritesWhileScanning occasional fail since HBASE-10514 went in
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1577784 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/regionserver/TestHRegion.java
+++ src/test/java/org/apache/hadoop/hbase/regionserver/TestHRegion.java
@@ -40,6 +40,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.DroppedSnapshotException;
 import org.apache.hadoop.hbase.DoNotRetryIOException;
 import org.apache.hadoop.hbase.DroppedSnapshotException;
 import org.apache.hadoop.hbase.HBaseConfiguration;
