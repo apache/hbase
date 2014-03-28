f1b53f58455318ed94b979db699d4469f622b67b
Michael Stack <stack@apache.org>
2014-03-28 03:29:44 +0000

    HBASE-10426 user_permission in security.rb calls non-existent UserPermission#getTable method
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.96@1582587 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-shell/src/main/ruby/hbase/security.rb
+++ hbase-shell/src/main/ruby/hbase/security.rb
@@ -187,7 +187,7 @@ module Hbase
       count  = 0
       perms.each do |value|
         user_name = String.from_java_bytes(value.getUser)
-        table = (value.getTable != nil) ? value.getTable.toString() : ''
+        table = (value.getTableName != nil) ? value.getTableName.getNameAsString() : ''
         family = (value.getFamily != nil) ? org.apache.hadoop.hbase.util.Bytes::toStringBinary(value.getFamily) : ''
         qualifier = (value.getQualifier != nil) ? org.apache.hadoop.hbase.util.Bytes::toStringBinary(value.getQualifier) : ''
 
