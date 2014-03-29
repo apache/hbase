64dff91895f9df55fe3a07217a333efa4d37d92a
Michael Stack <stack@apache.org>
2014-03-29 03:34:28 +0000

    HBASE-9120 ClassFinder logs errors that are not
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1582944 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-common/src/test/java/org/apache/hadoop/hbase/ClassFinder.java
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/ClassFinder.java
@@ -133,7 +133,7 @@ public class ClassFinder {
     try {
       jarFile = new JarInputStream(new FileInputStream(jarFileName));
     } catch (IOException ioEx) {
-      LOG.error("Failed to look for classes in " + jarFileName + ": " + ioEx);
+      LOG.warn("Failed to look for classes in " + jarFileName + ": " + ioEx);
       throw ioEx;
     }
 
@@ -147,7 +147,7 @@ public class ClassFinder {
           if (!proceedOnExceptions) {
             throw ioEx;
           }
-          LOG.error("Failed to get next entry from " + jarFileName + ": " + ioEx);
+          LOG.warn("Failed to get next entry from " + jarFileName + ": " + ioEx);
           break;
         }
         if (entry == null) {
@@ -172,7 +172,7 @@ public class ClassFinder {
         Class<?> c = makeClass(className, proceedOnExceptions);
         if (c != null) {
           if (!classes.add(c)) {
-            LOG.error("Ignoring duplicate class " + className);
+            LOG.warn("Ignoring duplicate class " + className);
           }
         }
       }
@@ -186,13 +186,13 @@ public class ClassFinder {
       boolean proceedOnExceptions) throws ClassNotFoundException, LinkageError {
     Set<Class<?>> classes = new HashSet<Class<?>>();
     if (!baseDirectory.exists()) {
-      LOG.error("Failed to find " + baseDirectory.getAbsolutePath());
+      LOG.warn("Failed to find " + baseDirectory.getAbsolutePath());
       return classes;
     }
 
     File[] files = baseDirectory.listFiles(this.fileFilter);
     if (files == null) {
-      LOG.error("Failed to get files from " + baseDirectory.getAbsolutePath());
+      LOG.warn("Failed to get files from " + baseDirectory.getAbsolutePath());
       return classes;
     }
 
@@ -207,7 +207,7 @@ public class ClassFinder {
         Class<?> c = makeClass(className, proceedOnExceptions);
         if (c != null) {
           if (!classes.add(c)) {
-            LOG.error("Ignoring duplicate class " + className);
+            LOG.warn("Ignoring duplicate class " + className);
           }
         }
       }
