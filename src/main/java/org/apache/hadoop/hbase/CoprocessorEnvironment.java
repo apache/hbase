16274495ee06a80f7e2e7a7b65feee6e70867e80
liyin <liyin@unknown>
2014-04-02 20:49:16 +0000

    [HBASE-2000] Adding Coprocessor and CoprocessorEnvironment
    Author: adela
    
    Summary:
    two very basic util classes which both David and I are going to use
    (ported from opensource)
    
    Test Plan: N/A
    
    Reviewers: daviddeng
    
    Reviewed By: daviddeng
    
    CC: hbase-eng@, liyintang, manukranthk
    
    Differential Revision: https://phabricator.fb.com/D1222934
    
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.89-fb@1584165 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/hadoop/hbase/CoprocessorEnvironment.java
@@ -0,0 +1,66 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hbase;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.HTableInterface;
+
+/**
+ * Coprocessor environment state.
+ */
+@InterfaceAudience.Private
+public interface CoprocessorEnvironment {
+
+  /** @return the Coprocessor interface version */
+  int getVersion();
+
+  /** @return the HBase version as a string (e.g. "0.21.0") */
+  String getHBaseVersion();
+
+  /** @return the loaded coprocessor instance */
+  Coprocessor getInstance();
+
+  /** @return the priority assigned to the loaded coprocessor */
+  int getPriority();
+
+  /** @return the load sequence number */
+  int getLoadSequence();
+
+  /** @return the configuration */
+  Configuration getConfiguration();
+
+  /**
+   * @return an interface for accessing the given table
+   * @throws IOException
+   */
+  HTableInterface getTable(String tableName) throws IOException;
+
+  /**
+   * @return an interface for accessing the given table using the passed executor to run batch
+   *         operations
+   * @throws IOException
+   */
+  HTableInterface getTable(String tableName, ExecutorService service) throws IOException;
+
+  /**
+   * @return the classloader for the loaded coprocessor instance
+   */
+  ClassLoader getClassLoader();
+}
