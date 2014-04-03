e124054e4e8a505034802f255832d65eb3827156
Zhihong Yu <tedyu@apache.org>
2014-04-03 10:13:32 +0000

    HBASE-10850 essential column family optimization is broken
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1584335 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -3636,7 +3636,7 @@ public class HRegion implements HeapSize { // , Writable{
     // KeyValue indicating that limit is reached when scanning
     private final KeyValue KV_LIMIT = new KeyValue();
     protected final byte[] stopRow;
-    private final Filter filter;
+    private final FilterWrapper filter;
     private int batch;
     protected int isScan;
     private boolean filterClosed = false;
@@ -3916,14 +3916,15 @@ public class HRegion implements HeapSize { // , Writable{
               isStopRow(nextKv.getRowArray(), nextKv.getRowOffset(), nextKv.getRowLength());
           // save that the row was empty before filters applied to it.
           final boolean isEmptyRow = results.isEmpty();
-
+          
           // We have the part of the row necessary for filtering (all of it, usually).
           // First filter with the filterRow(List).
+          FilterWrapper.FilterRowRetCode ret = FilterWrapper.FilterRowRetCode.NOT_CALLED;
           if (filter != null && filter.hasFilterRow()) {
-            filter.filterRowCells(results);
+            ret = filter.filterRowCellsWithRet(results);
           }
           
-          if (isEmptyRow || filterRow()) {
+          if ((isEmptyRow || ret == FilterWrapper.FilterRowRetCode.EXCLUDE) || filterRow()) {
             results.clear();
             boolean moreRows = nextRow(currentRow, offset, length);
             if (!moreRows) return false;
