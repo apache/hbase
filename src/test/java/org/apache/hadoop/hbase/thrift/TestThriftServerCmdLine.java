f476e3d54c0787bed8a74697c57ce5a37c570f82
larsh <larsh@unknown>
2014-04-18 18:48:12 +0000

    HBASE-10988; Addendum: increase wait to 20s.
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.94@1588541 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/thrift/TestThriftServerCmdLine.java
+++ src/test/java/org/apache/hadoop/hbase/thrift/TestThriftServerCmdLine.java
@@ -164,8 +164,8 @@ public class TestThriftServerCmdLine {
 
     thriftServer = new ThriftServer(TEST_UTIL.getConfiguration());
     startCmdLineThread(args.toArray(new String[0]));
-    // wait up to 10s for the server to start
-    for (int i = 0; i < 100
+    // wait up to 20s for the server to start
+    for (int i = 0; i < 200
         && (thriftServer.serverRunner == null || thriftServer.serverRunner.tserver == null); i++) {
       Thread.sleep(100);
     }
