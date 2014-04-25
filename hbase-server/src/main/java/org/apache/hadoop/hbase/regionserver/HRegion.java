15cd44e44367639cb307f8d3952232add13bb65f
ndimiduk <ndimiduk@unknown>
2014-04-25 16:00:29 +0000

    HBASE-11038 Filtered scans can bypass metrics collection
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1590067 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -3773,7 +3773,7 @@ public class HRegion implements HeapSize { // , Writable{
       }
       resetFilters();
       if (isFilterDoneInternal()) {
-        return false;
+        returnResult = false;
       }
       if (region != null && region.metricsRegion != null) {
         long totalSize = 0;
