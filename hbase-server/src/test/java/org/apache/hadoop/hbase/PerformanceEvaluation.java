6b1c419445ddd6e706053e21cab07b787455cbf0
Jean-Daniel Cryans <jdcryans@apache.org>
2014-05-14 16:51:00 +0000

    HBASE-10251 Restore API Compat for PerformanceEvaluation.generateValue() (Dima Spivak via JD)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1594637 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-server/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
@@ -1065,6 +1065,15 @@ public class PerformanceEvaluation extends Configured implements Tool {
     return b;
   }
 
+  /**
+   * @deprecated Use {@link #generateData(java.util.Random, int)} instead.
+   * @return Generated random value to insert into a table cell.
+   */
+  @Deprecated
+  public static byte[] generateValue(final Random r) {
+    return generateData(r, VALUE_LENGTH);
+  }
+
   static byte [] getRandomRow(final Random random, final int totalRows) {
     return format(random.nextInt(Integer.MAX_VALUE) % totalRows);
   }
