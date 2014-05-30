2d0160975c0477f063154aa058eba240577de471
elliott <elliott@e7acf4d4-3532-417f-9e73-7a9ae25a1f51>
2014-05-30 00:05:07 +0000

    [master] Specify url of repo when deploying automatically.
    Summary: Specify the repo url and name on the command line when deploying
    
    Test Plan: http://nexus.vip.facebook.com:8181/nexus/content/repositories/libs-snapshots-local/com/facebook/hbase/hbase/3.0.42560-SNAPSHOT/
    
    Reviewers: adela, oozie, jjmaestro
    
    Reviewed By: jjmaestro
    
    Subscribers: hbase-prodeng@, hbase-eng@
    
    Differential Revision: https://phabricator.fb.com/D1354446
    
    Tasks: 4412611
    
    git-svn-id: svn+ssh://tubbs/svnhive/hadoop/branches/titan/VENDOR.hbase/hbase-trunk@42574 e7acf4d4-3532-417f-9e73-7a9ae25a1f51

--- bin/devSupport/create_release.sh
+++ bin/devSupport/create_release.sh
@@ -6,8 +6,24 @@ MVNCMD=${MVNCMD:-mvn}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HBASE_HOME=${DIR}/../../
 
-VERSION=`${DIR}/generate_version.py --pom ${HBASE_HOME}/pom.xml --svn`
+if [ -d "${HBASE_HOME}/.git" ]; then
+	VERSION=`${DIR}/generate_version.py --pom ${HBASE_HOME}/pom.xml --git_svn`
+else
+	VERSION=`${DIR}/generate_version.py --pom ${HBASE_HOME}/pom.xml --svn`
+fi
+
 echo $VERSION
 
+BASE_REPO=http://nexus.vip.facebook.com:8181/nexus/content/repositories
+
+if [[ $VERSION == *-SNAPSHOT* ]]; then
+    REPO_NAME=libs-snapshots-local
+else
+    REPO_NAME=libs-releases-local
+fi
+
+REPO_URL=${BASE_REPO}/${REPO_NAME}
+
+
 $MVNCMD versions:set -DnewVersion=${VERSION}
-$MVNCMD clean package assembly:single deploy -DskipTests
+$MVNCMD clean package assembly:single deploy -DskipTests -DaltDeploymentRepository=${REPO_NAME}::default::${REPO_URL}
