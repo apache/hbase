cc61cc308168af407a2f21851e9932292af8ca77
Jimmy Xiang <jxiang@cloudera.com>
2014-07-23 11:42:22 -0700

    HBASE-11575 Pseudo distributed mode does not work as documented

--- bin/local-master-backup.sh
+++ bin/local-master-backup.sh
@@ -42,8 +42,9 @@ run_master () {
   DN=$2
   export HBASE_IDENT_STRING="$USER-$DN"
   HBASE_MASTER_ARGS="\
-    -D hbase.master.port=`expr 16000 + $DN` \
     -D hbase.master.info.port=`expr 16010 + $DN` \
+    -D hbase.regionserver.port=`expr 16020 + $DN` \
+    -D hbase.regionserver.info.port=`expr 16030 + $DN` \
     --backup"
   "$bin"/hbase-daemon.sh --config "${HBASE_CONF_DIR}" $1 master $HBASE_MASTER_ARGS
 }
