8fd4ec4628c5c62791da6a030a7cc8b415062c43
Andrew Purtell <apurtell@apache.org>
2014-08-18 14:50:51 -0700

    HBASE-11754 [Shell] Record table property SPLITS_FILE in descriptor (chendihao)

--- hbase-shell/src/main/ruby/hbase/admin.rb
+++ hbase-shell/src/main/ruby/hbase/admin.rb
@@ -238,6 +238,7 @@ module Hbase
           File.foreach(splits_file) do |line|
             arg[SPLITS].push(line.strip())
           end
+          htd.setValue(SPLITS_FILE, arg[SPLITS_FILE])
         end
 
         if arg.has_key?(SPLITS) 
