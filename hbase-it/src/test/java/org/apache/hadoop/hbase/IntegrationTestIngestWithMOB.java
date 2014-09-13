e5d3850776174a63ddc2e0b5ead58409ca7c8706
anoopsjohn <anoopsamjohn@gmail.com>
2014-09-13 19:47:07 +0530

    HBASE-11647 addendum to fix compile issues.

--- hbase-it/src/test/java/org/apache/hadoop/hbase/IntegrationTestIngestWithMOB.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/IntegrationTestIngestWithMOB.java
@@ -60,7 +60,7 @@ public class IntegrationTestIngestWithMOB extends IntegrationTestIngest {
   protected String[] getArgsForLoadTestToolInitTable() {
     List<String> args = new ArrayList<String>();
     args.add("-tn");
-    args.add(getTablename());
+    args.add(getTablename().getNameAsString());
     // pass all remaining args from conf with keys <test class name>.<load test tool arg>
     String clazz = this.getClass().getSimpleName();
     for (String arg : LOAD_TEST_TOOL_MOB_INIT_ARGS) {
@@ -104,7 +104,7 @@ public class IntegrationTestIngestWithMOB extends IntegrationTestIngest {
   protected void initTable() throws IOException {
     super.initTable();
 
-    byte[] tableName = getTablename().getBytes();
+    byte[] tableName = getTablename().getName();
     HBaseAdmin admin = new HBaseAdmin(conf);
     HTableDescriptor tableDesc = admin.getTableDescriptor(tableName);
     LOG.info("Disabling table " + getTablename());
