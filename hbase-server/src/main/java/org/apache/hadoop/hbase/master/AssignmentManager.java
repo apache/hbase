6851bbc83d2c399d7b34df2ec2b114a788d24311
Jimmy Xiang <jxiang@cloudera.com>
2014-09-30 15:51:20 -0700

    HBASE-12124 Closed region could stay closed if master stops at bad time

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
@@ -2918,6 +2918,9 @@ public class AssignmentManager extends ZooKeeperListener {
         State state = regionState.getState();
         LOG.info("Processing " + regionState);
         switch (state) {
+        case CLOSED:
+          invokeAssign(regionState.getRegion());
+          break;
         case PENDING_OPEN:
           retrySendRegionOpen(regionState);
           break;
