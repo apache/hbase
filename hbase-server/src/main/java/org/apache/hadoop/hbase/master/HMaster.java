85a60fec6e062094e3e57b8eb299a20166b86554
Ted Yu <tedyu@apache.org>
2014-10-04 00:41:03 +0000

    HBASE-12137 Alter table add cf doesn't do compression test (Virag Kothari)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -1848,18 +1848,19 @@ MasterServices, Server {
   }
 
   @Override
-  public void addColumn(final TableName tableName, final HColumnDescriptor column)
+  public void addColumn(final TableName tableName, final HColumnDescriptor columnDescriptor)
       throws IOException {
     checkInitialized();
+    checkCompression(columnDescriptor);
     if (cpHost != null) {
-      if (cpHost.preAddColumn(tableName, column)) {
+      if (cpHost.preAddColumn(tableName, columnDescriptor)) {
         return;
       }
     }
     //TODO: we should process this (and some others) in an executor
-    new TableAddFamilyHandler(tableName, column, this, this).prepare().process();
+    new TableAddFamilyHandler(tableName, columnDescriptor, this, this).prepare().process();
     if (cpHost != null) {
-      cpHost.postAddColumn(tableName, column);
+      cpHost.postAddColumn(tableName, columnDescriptor);
     }
   }
 
