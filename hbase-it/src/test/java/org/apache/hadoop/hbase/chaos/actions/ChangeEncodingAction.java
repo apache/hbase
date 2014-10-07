f88487ea643b5c3a43f497e3d78d219515e8a954
Jimmy Xiang <jxiang@cloudera.com>
2014-10-06 19:04:02 -0700

    HBASE-11838 Enable PREFIX_TREE in integration tests

--- hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/ChangeEncodingAction.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/ChangeEncodingAction.java
@@ -60,10 +60,12 @@ public class ChangeEncodingAction extends Action {
 
     LOG.debug("Performing action: Changing encodings on " + tableNameString);
     // possible DataBlockEncoding id's
-    int[] possibleIds = {0, 2, 3, 4/*, 6*/};
+    int[] possibleIds = {0, 2, 3, 4, 6};
     for (HColumnDescriptor descriptor : columnDescriptors) {
       short id = (short) possibleIds[random.nextInt(possibleIds.length)];
       descriptor.setDataBlockEncoding(DataBlockEncoding.getEncodingById(id));
+      LOG.debug("Set encoding of column family " + descriptor.getNameAsString()
+        + " to: " + descriptor.getDataBlockEncoding());
     }
 
     admin.modifyTable(tableName, tableDescriptor);
