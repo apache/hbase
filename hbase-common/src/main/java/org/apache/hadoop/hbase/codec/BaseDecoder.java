e1666cb2c1a828137a5527217d5f13764ce4d6d9
Andrew Purtell <apurtell@apache.org>
2014-11-05 18:47:03 -0800

    HBASE-12419 'Partial cell read caused by EOF' ERRORs on replication source

--- hbase-common/src/main/java/org/apache/hadoop/hbase/codec/BaseDecoder.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/codec/BaseDecoder.java
@@ -63,7 +63,9 @@ public abstract class BaseDecoder implements Codec.Decoder {
       LOG.trace("Error getting available for error message - ignoring", t);
     }
     if (!isEof) throw ioEx;
-    LOG.error("Partial cell read caused by EOF: " + ioEx);
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("Partial cell read caused by EOF", ioEx);
+    }
     EOFException eofEx = new EOFException("Partial cell read");
     eofEx.initCause(ioEx);
     throw eofEx;
