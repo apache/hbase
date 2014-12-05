30c70325c75746b7d7968f578c6a126c76252bf1
Andrew Purtell <apurtell@apache.org>
2014-12-04 16:43:33 -0800

    HBASE-12635 Delete acl notify znode of table after the table is deleted (Liu Shaohui)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/AccessController.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/AccessController.java
@@ -985,6 +985,7 @@ public class AccessController extends BaseMasterAndRegionObserver
         return null;
       }
     });
+    this.authManager.getZKPermissionWatcher().deleteTableACLNode(tableName);
   }
 
   @Override
