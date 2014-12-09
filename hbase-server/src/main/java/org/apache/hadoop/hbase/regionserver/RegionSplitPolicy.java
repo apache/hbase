53493ff5db40dd61eebbcb24a81ac57ff724274f
Rajeshbabu Chintaguntla <rajeshbabu@apache.org>
2014-12-09 23:36:28 +0530

    HBASE-12583 Allow creating reference files even the split row not lies in the storefile range if required - Addendum(Rajeshbabu)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionSplitPolicy.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionSplitPolicy.java
@@ -127,12 +127,12 @@ public abstract class RegionSplitPolicy extends Configured {
   }
 
   /**
-   * In {@link HRegionFileSystem#splitStoreFile(org.apache.hadoop.hbase.HRegionInfo,
-   * String, StoreFile, byte[], boolean)} we are not creating the split reference if split row
-   * not lies in the StoreFile range. But some use cases we may need to create the split reference
-   * even the split row not lies in the range.
-   * This method can be used to whether to skip the the StoreRile range check or not.
-   * @return whether to skip the StoreFile range check or or not
+   * In {@link HRegionFileSystem#splitStoreFile(org.apache.hadoop.hbase.HRegionInfo, String,
+   * StoreFile, byte[], boolean, RegionSplitPolicy)} we are not creating the split reference
+   * if split row not lies in the StoreFile range. But in some use cases we may need to create
+   * the split reference even when the split row not lies in the range. This method can be used
+   * to decide, whether to skip the the StoreFile range check or not.
+   * @return whether to skip the StoreFile range check or not
    */
   protected boolean skipStoreFileRangeCheck() {
     return false;
