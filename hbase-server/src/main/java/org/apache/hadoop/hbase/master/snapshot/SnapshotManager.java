3a652ba41c86aa566ea5377234611888da5a8bd8
Jonathan M Hsieh <jmhsieh@apache.org>
2014-12-15 11:17:59 -0800

    HBASE-12692 NPE from SnapshotManager#stop (Ashish Singhi)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/snapshot/SnapshotManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/snapshot/SnapshotManager.java
@@ -927,7 +927,9 @@ public class SnapshotManager extends MasterProcedureManager implements Stoppable
       restoreHandler.cancel(why);
     }
     try {
-      coordinator.close();
+      if (coordinator != null) {
+        coordinator.close();
+      }
     } catch (IOException e) {
       LOG.error("stop ProcedureCoordinator error", e);
     }
