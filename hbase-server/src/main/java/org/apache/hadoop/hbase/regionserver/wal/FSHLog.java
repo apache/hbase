bc464cc745cd3d021eb9072fc40c3a0543e247f1
Sean Busbey <busbey@apache.org>
2014-12-18 09:43:38 -0600

    HBASE-12719 test provider to remove just the hdfs interactions from fshlog.
    * Adds a provider that takes a config to determine if we actually do syncs, appends, and file rolls.
      - required relaxing some members of FSHLog from private to protected.
    * Fixes DisabledWALProvider so it can be used as hbase.wal.provider in the wal perf eval tool.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/FSHLog.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/FSHLog.java
@@ -201,7 +201,8 @@ public class FSHLog implements WAL {
   /**
    * file system instance
    */
-  private final FileSystem fs;
+  protected final FileSystem fs;
+
   /**
    * WAL directory, where all WAL files would be placed.
    */
@@ -236,7 +237,7 @@ public class FSHLog implements WAL {
   /**
    * conf object
    */
-  private final Configuration conf;
+  protected final Configuration conf;
   /** Listeners that are called on WAL events. */
   private final List<WALActionsListener> listeners = new CopyOnWriteArrayList<WALActionsListener>();
 
