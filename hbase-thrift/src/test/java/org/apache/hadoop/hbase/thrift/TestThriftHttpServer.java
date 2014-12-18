ebf9c9bce70ad3b604d910931dd73b520bcb9579
tedyu <yuzhihong@gmail.com>
2014-12-17 20:00:04 -0800

    HBASE-12640 Addendum fixes TestThriftHttpServer (Srikanth Srungarapu)

--- hbase-thrift/src/test/java/org/apache/hadoop/hbase/thrift/TestThriftHttpServer.java
+++ hbase-thrift/src/test/java/org/apache/hadoop/hbase/thrift/TestThriftHttpServer.java
@@ -65,6 +65,7 @@ public class TestThriftHttpServer {
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
     TEST_UTIL.getConfiguration().setBoolean("hbase.regionserver.thrift.http", true);
+    TEST_UTIL.getConfiguration().setBoolean("hbase.table.sanity.checks", false);
     TEST_UTIL.startMiniCluster();
     //ensure that server time increments every time we do an operation, otherwise
     //successive puts having the same timestamp will override each other
