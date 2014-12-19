a4e60c9b5d24349c3e1cdad8e50dbdc8232ebc85
Andrew Purtell <apurtell@apache.org>
2014-12-19 09:37:03 -0800

    HBASE-12724 Upgrade the interface audience of RegionScanner from Private to LimitedPrivate

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionScanner.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionScanner.java
@@ -22,14 +22,17 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
+import org.apache.hadoop.hbase.classification.InterfaceStability;
 import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.HBaseInterfaceAudience;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.client.Scan;
 
 /**
  * RegionScanner describes iterators over rows in an HRegion.
  */
-@InterfaceAudience.Private
+@InterfaceAudience.LimitedPrivate(HBaseInterfaceAudience.COPROC)
+@InterfaceStability.Stable
 public interface RegionScanner extends InternalScanner {
   /**
    * @return The RegionInfo for this scanner.
