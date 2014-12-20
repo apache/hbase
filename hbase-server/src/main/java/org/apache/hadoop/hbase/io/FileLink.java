15c43815ecf250fe0a7c36a410ce41beba2ef93d
stack <stack@apache.org>
2014-12-19 17:47:11 -0800

    HBASE-12732 Log messages in FileLink#tryOpen are reversed (Tobi Vollebregt)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/FileLink.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/FileLink.java
@@ -290,7 +290,7 @@ public class FileLink {
           if (pos != 0) in.seek(pos);
           assert(in.getPos() == pos) : "Link unable to seek to the right position=" + pos;
           if (LOG.isTraceEnabled()) {
-            if (currentPath != null) {
+            if (currentPath == null) {
               LOG.debug("link open path=" + path);
             } else {
               LOG.trace("link switch from path=" + currentPath + " to path=" + path);
