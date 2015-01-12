54f5129da80d18dcf8b16674f8626537603600d3
Enis Soztutar <enis@apache.org>
2015-01-12 13:59:44 -0800

    HBASE-12832 Describe table from shell no longer shows Table's attributes, only CF attributes (Srikanth Srungarapu)

--- hbase-client/src/main/java/org/apache/hadoop/hbase/HTableDescriptor.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/HTableDescriptor.java
@@ -875,6 +875,13 @@ public class HTableDescriptor implements WritableComparable<HTableDescriptor> {
     return s.toString();
   }
 
+  /**
+   * @return map of all table attributes formatted into string.
+   */
+  public String toStringTableAttributes() {
+   return getValues(true).toString();
+  }
+
   private StringBuilder getValues(boolean printDefaults) {
     StringBuilder s = new StringBuilder();
 
