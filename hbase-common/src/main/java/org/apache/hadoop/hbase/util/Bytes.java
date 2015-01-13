acc284eec6e708088971b495f2ad4d720ee35a40
Andrew Purtell <apurtell@apache.org>
2015-01-12 19:09:02 -0800

    HBASE-12801 Failed to truncate a table while maintaing binary region boundaries (Liu Shaohui)

--- hbase-common/src/main/java/org/apache/hadoop/hbase/util/Bytes.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/util/Bytes.java
@@ -1994,6 +1994,18 @@ public class Bytes implements Comparable<Bytes> {
   }
 
   /**
+   * @param t operands
+   * @return Array of binary byte arrays made from passed array of binary strings
+   */
+  public static byte[][] toBinaryByteArrays(final String[] t) {
+    byte[][] result = new byte[t.length][];
+    for (int i = 0; i < t.length; i++) {
+      result[i] = Bytes.toBytesBinary(t[i]);
+    }
+    return result;
+  }
+
+  /**
    * @param column operand
    * @return A byte array of a byte array where first and only entry is
    * <code>column</code>
