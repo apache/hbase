588b43b06ba9a3434dc2178b5b014283cc959d62
Nick Dimiduk <ndimiduk@apache.org>
2015-01-23 12:01:08 -0800

    HBASE-12728 buffered writes substantially less useful after removal of HTablePool (addendum)

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/HTable.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/HTable.java
@@ -1385,6 +1385,10 @@ public class HTable implements HTableInterface {
    */
   @Override
   public void flushCommits() throws IOException {
+    if (mutator == null) {
+      // nothing to flush if there's no mutator; don't bother creating one.
+      return;
+    }
     getBufferedMutator().flush();
   }
 
