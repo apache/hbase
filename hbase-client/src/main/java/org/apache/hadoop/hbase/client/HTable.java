d2d4e05dd0ce3d2e16560bf561190ed2939345ba
tedyu <yuzhihong@gmail.com>
2015-01-26 11:00:15 -0800

    HBASE-12915 Disallow small scan with batching

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/HTable.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/HTable.java
@@ -796,6 +796,9 @@ public class HTable implements HTableInterface, RegionLocator {
    */
   @Override
   public ResultScanner getScanner(final Scan scan) throws IOException {
+    if (scan.getBatch() > 0 && scan.isSmall()) {
+      throw new IllegalArgumentException("Small scan should not be used with batching");
+    }
     if (scan.getCaching() <= 0) {
       scan.setCaching(getScannerCaching());
     }
