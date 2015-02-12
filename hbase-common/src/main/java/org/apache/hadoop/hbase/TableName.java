38de2d227644eaa2818019ec3a216b9ec10d9b1d
Bhupendra <bhupendra.jain@huawei.com>
2015-02-12 15:36:07 +0530

    HBASE-13026: Wrong error message in case incorrect snapshot name OR Incorrect table name
    Signed-off-by: Matteo Bertozzi <matteo.bertozzi@cloudera.com>

--- hbase-common/src/main/java/org/apache/hadoop/hbase/TableName.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/TableName.java
@@ -180,10 +180,11 @@ public final class TableName implements Comparable<TableName> {
     }
 
     if (qualifierName[start] == '.' || qualifierName[start] == '-') {
-      throw new IllegalArgumentException("Illegal first character <" + qualifierName[0] +
-                                         "> at 0. Namespaces can only start with alphanumeric " +
+      throw new IllegalArgumentException("Illegal first character <" + qualifierName[start] +
+                                         "> at 0. " + (isSnapshot ? "Snapshot" : "User-space table") +
+                                         " qualifiers can only start with 'alphanumeric " +
                                          "characters': i.e. [a-zA-Z_0-9]: " +
-                                         Bytes.toString(qualifierName));
+                                         Bytes.toString(qualifierName, start, end));
     }
     for (int i = start; i < end; i++) {
       if (Character.isLetterOrDigit(qualifierName[i]) ||
@@ -194,7 +195,7 @@ public final class TableName implements Comparable<TableName> {
       }
       throw new IllegalArgumentException("Illegal character code:" + qualifierName[i] +
                                          ", <" + (char) qualifierName[i] + "> at " + i +
-                                         ". " + (isSnapshot ? "snapshot" : "User-space table") +
+                                         ". " + (isSnapshot ? "Snapshot" : "User-space table") +
                                          " qualifiers can only contain " +
                                          "'alphanumeric characters': i.e. [a-zA-Z_0-9-.]: " +
                                          Bytes.toString(qualifierName, start, end));
