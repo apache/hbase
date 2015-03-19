1f7b7781a10fa22f3aa0246822b1f6611e5cf9cc
Matteo Bertozzi <matteo.bertozzi@cloudera.com>
2015-03-19 17:54:46 +0000

    HBASE-13285 Fix flaky getRegions() in TestAccessController.setUp()

--- hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestAccessController.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestAccessController.java
@@ -230,7 +230,7 @@ public class TestAccessController extends SecureTestUtil {
     htd.addFamily(hcd);
     htd.setOwner(USER_OWNER);
     admin.createTable(htd, new byte[][] { Bytes.toBytes("s") });
-    TEST_UTIL.waitTableEnabled(TEST_TABLE.getTableName());
+    TEST_UTIL.waitUntilAllRegionsAssigned(TEST_TABLE.getTableName());
 
     HRegion region = TEST_UTIL.getHBaseCluster().getRegions(TEST_TABLE.getTableName()).get(0);
     RegionCoprocessorHost rcpHost = region.getCoprocessorHost();
@@ -928,7 +928,7 @@ public class TestAccessController extends SecureTestUtil {
       setPermission(loadPath, FsPermission.valueOf("-rwxrwxrwx"));
 
       try (HTable table = (HTable)TEST_UTIL.getConnection().getTable(tableName)) {
-        TEST_UTIL.waitTableEnabled(tableName);
+        TEST_UTIL.waitUntilAllRegionsAssigned(tableName);
         LoadIncrementalHFiles loader = new LoadIncrementalHFiles(conf);
         loader.doBulkLoad(loadPath, table);
       }
@@ -2101,13 +2101,13 @@ public class TestAccessController extends SecureTestUtil {
     String namespace = "testNamespaceUserGrant";
     NamespaceDescriptor desc = NamespaceDescriptor.create(namespace).build();
     TEST_UTIL.getMiniHBaseCluster().getMaster().createNamespace(desc);
-    
+
     // Grant namespace READ to USER_NONE, this should supersede any table permissions
     grantOnNamespace(TEST_UTIL, USER_NONE.getShortName(), namespace, Permission.Action.READ);
 
     // Now USER_NONE should be able to read also
     verifyAllowed(getAction, USER_NONE);
-    
+
     TEST_UTIL.getMiniHBaseCluster().getMaster().deleteNamespace(namespace);
   }
 
@@ -2320,7 +2320,7 @@ public class TestAccessController extends SecureTestUtil {
     // Verify that EXEC permission is checked correctly
     verifyDenied(execEndpointAction, userB);
     verifyAllowed(execEndpointAction, userA);
-    
+
     String namespace = "testCoprocessorExec";
     NamespaceDescriptor desc = NamespaceDescriptor.create(namespace).build();
     TEST_UTIL.getMiniHBaseCluster().getMaster().createNamespace(desc);
@@ -2330,7 +2330,7 @@ public class TestAccessController extends SecureTestUtil {
 
     // User B should now be allowed also
     verifyAllowed(execEndpointAction, userA, userB);
-    
+
     TEST_UTIL.getMiniHBaseCluster().getMaster().deleteNamespace(namespace);
   }
 
