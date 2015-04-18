9abe2d1d1a5e2d26e43b308ee593fe18011fe8e9
Lars Hofhansl <larsh@apache.org>
2015-04-17 22:21:09 -0700

    HBASE-13494 0.98: Remove remove(K, V) from type PoolMap<K,V>.

--- hbase-client/src/main/java/org/apache/hadoop/hbase/util/PoolMap.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/util/PoolMap.java
@@ -93,14 +93,6 @@ public class PoolMap<K, V> implements Map<K, V> {
     return null;
   }
 
-  /**
-   * @deprecated Will be removed for Java 8, use {@link #removeValue} instead
-   */
-  @Deprecated
-  public boolean remove(K key, V value) {
-    return removeValue(key, value);
-  }
-
   public boolean removeValue(K key, V value) {
     Pool<V> pool = pools.get(key);
     boolean res = false;
