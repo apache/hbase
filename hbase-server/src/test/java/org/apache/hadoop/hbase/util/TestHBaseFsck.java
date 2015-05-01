3eb53005aea4944878bf20a575b39fa82694c96e
Josh Elser <elserj@apache.org>
2015-05-01 12:37:49 -0400

    HBASE-13591 Attempt to stabilize TestHBaseFsck

--- hbase-server/src/test/java/org/apache/hadoop/hbase/util/TestHBaseFsck.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/util/TestHBaseFsck.java
@@ -614,7 +614,7 @@ public class TestHBaseFsck {
   }
 
   /**
-   * This test makes sure that with 5 retries both parallel instances
+   * This test makes sure that with 10 retries both parallel instances
    * of hbck will be completed successfully.
    *
    * @throws Exception
@@ -628,7 +628,10 @@ public class TestHBaseFsck {
 
       @Override
       public HBaseFsck call() throws Exception {
-        return doFsck(conf, false);
+        // Increase retry attempts to make sure the non-active hbck doesn't get starved
+        Configuration c = new Configuration(conf);
+        c.setInt("hbase.hbck.lockfile.attempts", 10);
+        return doFsck(c, false);
       }
     }
     service = Executors.newFixedThreadPool(2);
@@ -636,7 +639,7 @@ public class TestHBaseFsck {
     hbck2 = service.submit(new RunHbck());
     service.shutdown();
     //wait for 15 seconds, for both hbck calls finish
-    service.awaitTermination(15, TimeUnit.SECONDS);
+    service.awaitTermination(25, TimeUnit.SECONDS);
     HBaseFsck h1 = hbck1.get();
     HBaseFsck h2 = hbck2.get();
     // Both should be successful
