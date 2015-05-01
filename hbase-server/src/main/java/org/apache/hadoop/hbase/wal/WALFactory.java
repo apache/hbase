62c55a6350ac751969221aead51b0e758617c1b2
Lars Hofhansl <larsh@apache.org>
2015-04-30 17:05:05 -0700

    HBASE-13601 Connection leak during log splitting. (Abhishek Singh Chouhan)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/wal/WALFactory.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/wal/WALFactory.java
@@ -279,6 +279,7 @@ public class WALFactory {
       long startWaiting = EnvironmentEdgeManager.currentTime();
       long openTimeout = timeoutMillis + startWaiting;
       int nbAttempt = 0;
+      FSDataInputStream stream = null;
       while (true) {
         try {
           if (lrClass != ProtobufLogReader.class) {
@@ -287,7 +288,7 @@ public class WALFactory {
             reader.init(fs, path, conf, null);
             return reader;
           } else {
-            FSDataInputStream stream = fs.open(path);
+            stream = fs.open(path);
             // Note that zero-length file will fail to read PB magic, and attempt to create
             // a non-PB reader and fail the same way existing code expects it to. If we get
             // rid of the old reader entirely, we need to handle 0-size files differently from
@@ -301,6 +302,14 @@ public class WALFactory {
             return reader;
           }
         } catch (IOException e) {
+          try {
+            if (stream != null) {
+              stream.close();
+            }
+          } catch (IOException exception) {
+            LOG.warn("Could not close FSDataInputStream" + exception.getMessage());
+            LOG.debug("exception details", exception);
+          }
           String msg = e.getMessage();
           if (msg != null && (msg.contains("Cannot obtain block length")
               || msg.contains("Could not obtain the last block")
