1c4b47f856050796f491db3671349aec3220d237
Andrew Purtell <apurtell@apache.org>
2015-05-01 18:12:36 -0700

    HBASE-12415 Add add(byte[][] arrays) to Bytes (Jean-Marc Spaggiari)

--- hbase-common/src/test/java/org/apache/hadoop/hbase/util/TestBytes.java
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/util/TestBytes.java
@@ -49,6 +49,19 @@ public class TestBytes extends TestCase {
     assertNotNull(ee);
   }
 
+  public void testAdd () throws Exception {
+    byte[] a = {0,0,0,0,0,0,0,0,0,0};
+    byte[] b = {1,1,1,1,1,1,1,1,1,1,1};
+    byte[] c = {2,2,2,2,2,2,2,2,2,2,2,2};
+    byte[] d = {3,3,3,3,3,3,3,3,3,3,3,3,3};
+    byte[] result1 = Bytes.add (a, b, c);
+    byte[] result2 = Bytes.add (new byte[][] {a, b, c});
+    assertEquals(0, Bytes.compareTo(result1, result2));
+    byte[] result4 = Bytes.add (result1, d);
+    byte[] result5 = Bytes.add (new byte[][] {result1, d});
+    assertEquals(0, Bytes.compareTo(result1, result2));
+  }
+
   public void testSplit() throws Exception {
     byte [] lowest = Bytes.toBytes("AAA");
     byte [] middle = Bytes.toBytes("CCC");
