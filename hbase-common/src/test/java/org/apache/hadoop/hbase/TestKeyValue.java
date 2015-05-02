9a4ea44d1ee46f6d0883dd10f173810bc6685c7d
tedyu <yuzhihong@gmail.com>
2015-05-01 18:31:16 -0700

    HBASE-12413 Mismatch in the equals and hashcode methods of KeyValue (Jingcheng Du and Gariel Reid)
    Conflicts:
    	hbase-common/src/main/java/org/apache/hadoop/hbase/CellComparator.java
    	hbase-common/src/test/java/org/apache/hadoop/hbase/TestKeyValue.java

--- hbase-common/src/test/java/org/apache/hadoop/hbase/TestKeyValue.java
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/TestKeyValue.java
@@ -39,6 +39,8 @@ import org.apache.hadoop.hbase.KeyValue.MetaComparator;
 import org.apache.hadoop.hbase.KeyValue.Type;
 import org.apache.hadoop.hbase.util.Bytes;
 
+import static org.junit.Assert.assertNotEquals;
+
 public class TestKeyValue extends TestCase {
   private static final Log LOG = LogFactory.getLog(TestKeyValue.class);
 
@@ -829,4 +831,22 @@ public class TestKeyValue extends TestCase {
       return this.kv.getTagsArray();
     }
   }
+
+  public void testEqualsAndHashCode() throws Exception {
+    KeyValue kvA1 = new KeyValue(Bytes.toBytes("key"), Bytes.toBytes("cf"),
+        Bytes.toBytes("qualA"), Bytes.toBytes("1"));
+    KeyValue kvA2 = new KeyValue(Bytes.toBytes("key"), Bytes.toBytes("cf"),
+        Bytes.toBytes("qualA"), Bytes.toBytes("2"));
+    // We set a different sequence id on kvA2 to demonstrate that the equals and hashCode also
+    // don't take this into account.
+    kvA2.setSequenceId(2);
+    KeyValue kvB = new KeyValue(Bytes.toBytes("key"), Bytes.toBytes("cf"),
+        Bytes.toBytes("qualB"), Bytes.toBytes("1"));
+
+    assertEquals(kvA1, kvA2);
+    assertNotEquals(kvA1, kvB);
+    assertEquals(kvA1.hashCode(), kvA2.hashCode());
+    assertNotEquals(kvA1.hashCode(), kvB.hashCode());
+  }
+
 }
