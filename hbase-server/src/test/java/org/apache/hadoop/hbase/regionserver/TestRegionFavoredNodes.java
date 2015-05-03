a59658c6cf5830566127cd6d9aa257d5aec15359
Sean Busbey <busbey@apache.org>
2015-05-03 01:44:08 -0500

    HBASE-13612 TestRegionFavoredNodes should guard against setup failure.
    Conflicts:
    	hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestRegionFavoredNodes.java

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestRegionFavoredNodes.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestRegionFavoredNodes.java
@@ -78,6 +78,10 @@ public class TestRegionFavoredNodes {
 
   @AfterClass
   public static void tearDownAfterClass() throws Exception {
+    // guard against failure in setup
+    if (table != null) {
+      table.close();
+    }
     if (createWithFavoredNode == null) {
       return;
     }
