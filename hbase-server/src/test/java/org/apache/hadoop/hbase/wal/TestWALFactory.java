b6cd9239667e2596da24e54dda10a56975d088a1
Elliott Clark <eclark@apache.org>
2015-05-06 11:27:33 -0700

    HBASE-13635 Regions stuck in transition because master is incorrectly assumed dead
    Summary:
    Requests to tell master that meta is moved can be blocked behind other requests that are trying to mutate meta.
    This causes a dead lock and the master is assumed dead. However the master stays up the whole time.
    
    This patch adds prioritization onto reporting meta moves. It should allow meta to make progress.
    
    Test Plan: unit tests.
    
    Differential Revision: https://reviews.facebook.net/D38109

--- hbase-server/src/test/java/org/apache/hadoop/hbase/wal/TestWALFactory.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/wal/TestWALFactory.java
@@ -227,6 +227,7 @@ public class TestWALFactory {
         fail("This version of Hadoop supports neither Syncable.sync() " +
             "nor Syncable.hflush().");
       }
+
     }
     syncMethod.invoke(out, new Object[]{});
     FSDataInputStream in = fs.open(p);
