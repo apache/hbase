ef6777d6871008c914047635b8c6d733c433e865
tedyu <yuzhihong@gmail.com>
2015-05-12 09:03:51 -0700

    HBASE-13663 HMaster fails to restart 'HMaster: Failed to become active master'

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
@@ -2949,6 +2949,7 @@ public class AssignmentManager extends ZooKeeperListener {
       HRegionLocation[] locations = rl.getRegionLocations();
       if (locations == null) continue;
       for (HRegionLocation hrl : locations) {
+        if (hrl == null) continue;
         HRegionInfo regionInfo = hrl.getRegionInfo();
         if (regionInfo == null) continue;
         int replicaId = regionInfo.getReplicaId();
