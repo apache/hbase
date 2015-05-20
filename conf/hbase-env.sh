8c8792be70851c0191ba4a406139e65d6b8e5faf
Guorui Wu <guorui_wu@hotmail.com>
2015-05-20 19:36:03 -0400

    Start of GroupLoadBalancer with logging working

--- conf/hbase-env.sh
+++ conf/hbase-env.sh
@@ -134,6 +134,6 @@ export HBASE_REGIONSERVER_OPTS="$HBASE_REGIONSERVER_OPTS -XX:PermSize=128m -XX:M
 # In case one needs to do log rolling on a date change, one should set the environment property
 # HBASE_ROOT_LOGGER to "<DESIRED_LOG LEVEL>,DRFA".
 # For example:
-# HBASE_ROOT_LOGGER=INFO,DRFA
+HBASE_ROOT_LOGGER=INFO,DRFA
 # The reason for changing default to RFA is to avoid the boundary case of filling out disk space as
 # DRFA doesn't put any cap on the log size. Please refer to HBase-5655 for more context.
