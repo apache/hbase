10006312da7c30023479ad2703162b094f7e2334
Nick Dimiduk <ndimiduk@apache.org>
2015-05-20 11:40:25 -0700

    HBASE-13711 Provide an API to set min and max versions in HColumnDescriptor (Stephen Yuan Jiang)

--- hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/ChangeVersionsAction.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/ChangeVersionsAction.java
@@ -61,8 +61,7 @@ public class ChangeVersionsAction extends Action {
 
     int versions =  random.nextInt(3) + 1;
     for(HColumnDescriptor descriptor:columnDescriptors) {
-      descriptor.setMaxVersions(versions);
-      descriptor.setMinVersions(versions);
+      descriptor.setVersions(versions, versions);
     }
     LOG.debug("Performing action: Changing versions on " + tableNameString);
     admin.modifyTable(tableName, tableDescriptor);
