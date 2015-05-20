0ef4a1088224b6fa3a2c85ef1d4efba6b7b48673
Enis Soztutar <enis@apache.org>
2015-05-19 19:03:12 -0700

    HBASE-13717 TestBoundedRegionGroupingProvider#setMembershipDedups need to set HDFS diretory for WAL (Stephen Yuan Jiang)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/wal/TestBoundedRegionGroupingProvider.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/wal/TestBoundedRegionGroupingProvider.java
@@ -38,6 +38,7 @@ import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.testclassification.LargeTests;
 import org.apache.hadoop.hbase.testclassification.RegionServerTests;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.FSUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -159,6 +160,9 @@ public class TestBoundedRegionGroupingProvider {
     WALFactory wals = null;
     try {
       conf.setInt(NUM_REGION_GROUPS, temp*4);
+      // Set HDFS root directory for storing WAL
+      FSUtils.setRootDir(conf, TEST_UTIL.getDataTestDirOnTestFS());
+
       wals = new WALFactory(conf, null, currentTest.getMethodName());
       final Set<WAL> seen = new HashSet<WAL>(temp*4);
       final Random random = new Random();
