b1a04c9535b3256418e47e69bf194ebfe3b63f9c
Guorui Wu <guorui_wu@hotmail.com>
2015-05-22 14:12:25 -0400

    Reading from hbase-site.xml

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/GroupLoadBalancer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/GroupLoadBalancer.java
@@ -16,173 +16,35 @@
 package org.apache.hadoop.hbase.master.balancer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NavigableMap;
-import java.util.Random;
-import java.util.TreeMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.hbase.Server;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HBaseInterfaceAudience;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.master.RegionPlan;
 
-import com.google.common.collect.MinMaxPriorityQueue;
-import org.apache.hadoop.hbase.regionserver.HRegion;
-
-/**
- * Makes decisions about the placement and movement of Regions across
- * RegionServers.
- *
- * <p>Cluster-wide load balancing will occur only when there are no regions in
- * transition and according to a fixed period of a time using {@link #balanceCluster(Map)}.
- *
- * <p>Inline region placement with {@link #immediateAssignment} can be used when
- * the Master needs to handle closed regions that it currently does not have
- * a destination set for.  This can happen during master failover.
- *
- * <p>On cluster startup, bulk assignment can be used to determine
- * locations for all Regions in a cluster.
- *
- * <p>This classes produces plans for the 
- * {@link org.apache.hadoop.hbase.master.AssignmentManager} to execute.
- */
 @InterfaceAudience.LimitedPrivate(HBaseInterfaceAudience.CONFIG) public class GroupLoadBalancer
     extends BaseLoadBalancer {
   private static final Log LOG = LogFactory.getLog(GroupLoadBalancer.class);
-  private static final Random RANDOM = new Random(System.currentTimeMillis());
 
-  private RegionInfoComparator riComparator = new RegionInfoComparator();
-  private RegionPlan.RegionPlanComparator rpComparator = new RegionPlan.RegionPlanComparator();
-
-  /**
-   * Stores additional per-server information about the regions added/removed
-   * during the run of the balancing algorithm.
-   *
-   * For servers that shed regions, we need to track which regions we have already
-   * shed. <b>nextRegionForUnload</b> contains the index in the list of regions on
-   * the server that is the next to be shed.
-   */
-  static class BalanceInfo {
-
-    private final int nextRegionForUnload;
-    private int numRegionsAdded;
-
-    public BalanceInfo(int nextRegionForUnload, int numRegionsAdded) {
-      this.nextRegionForUnload = nextRegionForUnload;
-      this.numRegionsAdded = numRegionsAdded;
-    }
-
-    int getNextRegionForUnload() {
-      return nextRegionForUnload;
-    }
-
-    int getNumRegionsAdded() {
-      return numRegionsAdded;
-    }
-
-    void setNumRegionsAdded(int numAdded) {
-      this.numRegionsAdded = numAdded;
-    }
-  }
-
-  /**
-   * Generate a global load balancing plan according to the specified map of
-   * server information to the most loaded regions of each server.
-   *
-   * The load balancing invariant is that all servers are within 1 region of the
-   * average number of regions per server.  If the average is an integer number,
-   * all servers will be balanced to the average.  Otherwise, all servers will
-   * have either floor(average) or ceiling(average) regions.
-   *
-   * HBASE-3609 Modeled regionsToMove using Guava's MinMaxPriorityQueue so that
-   *   we can fetch from both ends of the queue.
-   * At the beginning, we check whether there was empty region server
-   *   just discovered by Master. If so, we alternately choose new / old
-   *   regions from head / tail of regionsToMove, respectively. This alternation
-   *   avoids clustering young regions on the newly discovered region server.
-   *   Otherwise, we choose new regions from head of regionsToMove.
-   *
-   * Another improvement from HBASE-3609 is that we assign regions from
-   *   regionsToMove to underloaded servers in round-robin fashion.
-   *   Previously one underloaded server would be filled before we move onto
-   *   the next underloaded server, leading to clustering of young regions.
-   *
-   * Finally, we randomly shuffle underloaded servers so that they receive
-   *   offloaded regions relatively evenly across calls to balanceCluster().
-   *
-   * The algorithm is currently implemented as such:
-   *
-   * <ol>
-   * <li>Determine the two valid numbers of regions each server should have,
-   *     <b>MIN</b>=floor(average) and <b>MAX</b>=ceiling(average).
-   *
-   * <li>Iterate down the most loaded servers, shedding regions from each so
-   *     each server hosts exactly <b>MAX</b> regions.  Stop once you reach a
-   *     server that already has &lt;= <b>MAX</b> regions.
-   *     <p>
-   *     Order the regions to move from most recent to least.
-   *
-   * <li>Iterate down the least loaded servers, assigning regions so each server
-   *     has exactly </b>MIN</b> regions.  Stop once you reach a server that
-   *     already has &gt;= <b>MIN</b> regions.
-   *
-   *     Regions being assigned to underloaded servers are those that were shed
-   *     in the previous step.  It is possible that there were not enough
-   *     regions shed to fill each underloaded server to <b>MIN</b>.  If so we
-   *     end up with a number of regions required to do so, <b>neededRegions</b>.
-   *
-   *     It is also possible that we were able to fill each underloaded but ended
-   *     up with regions that were unassigned from overloaded servers but that
-   *     still do not have assignment.
-   *
-   *     If neither of these conditions hold (no regions needed to fill the
-   *     underloaded servers, no regions leftover from overloaded servers),
-   *     we are done and return.  Otherwise we handle these cases below.
-   *
-   * <li>If <b>neededRegions</b> is non-zero (still have underloaded servers),
-   *     we iterate the most loaded servers again, shedding a single server from
-   *     each (this brings them from having <b>MAX</b> regions to having
-   *     <b>MIN</b> regions).
-   *
-   * <li>We now definitely have more regions that need assignment, either from
-   *     the previous step or from the original shedding from overloaded servers.
-   *     Iterate the least loaded servers filling each to <b>MIN</b>.
-   *
-   * <li>If we still have more regions that need assignment, again iterate the
-   *     least loaded servers, this time giving each one (filling them to
-   *     </b>MAX</b>) until we run out.
-   *
-   * <li>All servers will now either host <b>MIN</b> or <b>MAX</b> regions.
-   *
-   *     In addition, any server hosting &gt;= <b>MAX</b> regions is guaranteed
-   *     to end up with <b>MAX</b> regions at the end of the balancing.  This
-   *     ensures the minimal number of regions possible are moved.
-   * </ol>
-   *
-   * TODO: We can at-most reassign the number of regions away from a particular
-   *       server to be how many they report as most loaded.
-   *       Should we just keep all assignment in memory?  Any objections?
-   *       Does this mean we need HeapSize on HMaster?  Or just careful monitor?
-   *       (current thinking is we will hold all assignments in memory)
-   *
-   * @param clusterMap Map of regionservers and their load/region information to
-   *                   a list of their most loaded regions
-   * @return a list of regions to be moved, including source and destination,
-   *         or null if cluster is already balanced
-   */
   @Override public List<RegionPlan> balanceCluster(Map<ServerName, List<HRegionInfo>> clusterMap) {
 
     LOG.info("**************** USING GROUP LOAD BALANCER *******************");
 
-    LOG.debug("**************** Debug enabled");
+    LOG.info("**************** Logging info *******************");
+    LOG.debug("**************** Logging debug *******************");
+    LOG.fatal("**************** Logging fatal *******************");
+    LOG.error("**************** Logging error *******************");
+    LOG.trace("**************** Logging trace *******************");
+    LOG.warn("**************** Logging warn *******************");
 
     LOG.info("**************** masterServerName " + masterServerName);
 
@@ -246,6 +108,10 @@ import org.apache.hadoop.hbase.regionserver.HRegion;
 
 
     LOG.info("**************** regionsToReturn " + regionsToReturn);
+
+    Cluster cluster = new Cluster(clusterMap, null, this.regionFinder, this.rackManager);
+    Configuration configuration = HBaseConfiguration.create();
+    LOG.info("**************** configuration for hbase.master.loadbalancer.class " + configuration.get("hbase.master.loadbalancer.class"));
 
     return regionsToReturn;
   }
