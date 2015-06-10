1652702a863fe83e999ecfe9bdacaef2044992d6
Guorui Wu <guorui_wu@hotmail.com>
2015-06-10 16:57:10 -0400

    Create test for reconcileToGroupConfiguration function in GroupLoadBalancer

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestGroupLoadBalancer.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestGroupLoadBalancer.java
@@ -358,8 +358,6 @@ public class TestGroupLoadBalancer extends BalancerTestBase {
     Map<String, Map<ServerName, List<HRegionInfo>>> groupedClusterMap =
         groupLoadBalancerGroupedClusters.getGroupedClusters();
 
-    LOG.info("groupedClusterMap " + groupedClusterMap);
-
     Map<ServerName, List<HRegionInfo>> group1 = groupedClusterMap.get("group1");
     Map<ServerName, List<HRegionInfo>> group2 = groupedClusterMap.get("group2");
 
@@ -376,6 +374,79 @@ public class TestGroupLoadBalancer extends BalancerTestBase {
       assertTrue(hriList.get(0).getRegionNameAsString().contains("test_table_3"));
       assertTrue(hriList.get(1).getRegionNameAsString().contains("test_table_4"));
     }
+  }
+
+  @Test
+  public void testRegionReconciliation() throws Exception {
+    // Create a configuration
+    conf = HBaseConfiguration.create();
+    conf.set("hbase.master.balancer.grouploadbalancer.groups", "group1;group2");
+    conf.set("hbase.master.balancer.grouploadbalancer.defaultgroup", "group1");
+    conf.set("hbase.master.balancer.grouploadbalancer.servergroups.group1", "10.255.196.145,60020");
+    conf.set("hbase.master.balancer.grouploadbalancer.servergroups.group2", "10.255.196.145,60021");
+    conf.set("hbase.master.balancer.grouploadbalancer.tablegroups.group1",
+        "test_table_1;test_table_2");
+    conf.set("hbase.master.balancer.grouploadbalancer.tablegroups.group2",
+        "test_table_3;test_table_4");
+    loadBalancer.setConf(conf);
+
+    // Create two test region servers
+    Random random = new Random();
+    long randomTimeDelta = (long)random.nextInt(60000);
+    long currentTimeStamp = System.currentTimeMillis();
+    ServerName serverName1 = ServerName.valueOf("10.255.196.145:60020", currentTimeStamp);
+    ServerName serverName2 =
+        ServerName.valueOf("10.255.196.145:60021", currentTimeStamp + randomTimeDelta);
+
+    // Create 4 test tables
+    TableName tableName1 = TableName.valueOf("test_table_1");
+    TableName tableName2 = TableName.valueOf("test_table_2");
+    TableName tableName3 = TableName.valueOf("test_table_3");
+    TableName tableName4 = TableName.valueOf("test_table_4");
+    HRegionInfo hri1 = new HRegionInfo(tableName1);
+    HRegionInfo hri2 = new HRegionInfo(tableName2);
+    HRegionInfo hri3 = new HRegionInfo(tableName3);
+    HRegionInfo hri4 = new HRegionInfo(tableName4);
+
+    // Create a cluster
+    Map<ServerName, List<HRegionInfo>> testCluster = new HashMap<>();
+    List<HRegionInfo> hriList1 = new ArrayList<>();
+    List<HRegionInfo> hriList2 = new ArrayList<>();
+    hriList1.add(hri1);
+    hriList1.add(hri3);
+    hriList2.add(hri2);
+    hriList2.add(hri4);
+    testCluster.put(serverName1, hriList1);
+    testCluster.put(serverName2, hriList2);
+
+    GroupLoadBalancerConfiguration groupLoadBalancerConfiguration =
+        new GroupLoadBalancerConfiguration(conf, testCluster);
+
+    GroupLoadBalancerGroupedClusterFactory groupLoadBalancerGroupedClusters =
+        new GroupLoadBalancerGroupedClusterFactory(groupLoadBalancerConfiguration, testCluster);
+    Map<String, Map<ServerName, List<HRegionInfo>>> groupedClusterMap =
+        groupLoadBalancerGroupedClusters.getGroupedClusters();
+
+    List<RegionPlan> regionsToReconcile =
+        loadBalancer.reconcileToGroupConfiguration(groupedClusterMap, testCluster);
+
+    // Sort so they are in the same order every time so we know which server each region needs to be
+    // moved to
+    Collections.sort(regionsToReconcile, new Comparator<RegionPlan>() {
+      @Override public int compare(RegionPlan o1, RegionPlan o2) {
+        return o1.getRegionInfo().getRegionNameAsString().
+            compareTo(o2.getRegionInfo().getRegionNameAsString());
+      }
+    });
+
+    // Check to make sure that regions are being moved to the right servers
+    assertTrue(regionsToReconcile.get(0).getRegionInfo().getRegionNameAsString().contains("test_table_2"));
+    assertTrue(regionsToReconcile.get(0).getSource().toString().contains("10.255.196.145,60021"));
+    assertTrue(regionsToReconcile.get(0).getDestination().toString().contains("10.255.196.145,60020"));
+
+    assertTrue(regionsToReconcile.get(1).getRegionInfo().getRegionNameAsString().contains("test_table_3"));
+    assertTrue(regionsToReconcile.get(1).getSource().toString().contains("10.255.196.145,60020"));
+    assertTrue(regionsToReconcile.get(1).getDestination().toString().contains("10.255.196.145,60021"));
   }
 
 }
