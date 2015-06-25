c34db5e837454438e80fdcc4c56ff45e44197906
tedyu <yuzhihong@gmail.com>
2015-06-25 16:30:52 -0700

    HBASE-13923 Addendum fixes NPE in HRegionServer#buildServerLoad()
    Conflicts:
    	hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -1165,10 +1165,12 @@ public class HRegionServer implements ClientProtos.ClientService.BlockingInterfa
     RegionLoad.Builder regionLoadBldr = RegionLoad.newBuilder();
     RegionSpecifier.Builder regionSpecifier = RegionSpecifier.newBuilder();
     for (HRegion region : regions) {
-      Set<String> regionCoprocessors = region.getCoprocessorHost().getCoprocessors();
-      Iterator<String> iterator = regionCoprocessors.iterator();
-      while (iterator.hasNext()) {
-        serverLoad.addCoprocessors(coprocessorBuilder.setName(iterator.next()).build());
+      if (region.getCoprocessorHost() != null) {
+        Set<String> regionCoprocessors = region.getCoprocessorHost().getCoprocessors();
+        Iterator<String> iterator = regionCoprocessors.iterator();
+        while (iterator.hasNext()) {
+          serverLoad.addCoprocessors(coprocessorBuilder.setName(iterator.next()).build());
+        }
       }
       serverLoad.addRegionLoads(createRegionLoad(region, regionLoadBldr, regionSpecifier));
       for (String coprocessor : getWAL(region.getRegionInfo()).getCoprocessorHost()
