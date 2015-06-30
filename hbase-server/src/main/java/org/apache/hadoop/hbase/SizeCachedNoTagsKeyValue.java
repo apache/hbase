7e7fbdb84b6173a26169ae995bf55c536f77d9c8
anoopsjohn <anoopsamjohn@gmail.com>
2015-06-30 22:50:38 +0530

    HBASE-14001 Optimize write(OutputStream out, boolean withTags) for SizeCachedNoTagsKeyValue.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/SizeCachedNoTagsKeyValue.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/SizeCachedNoTagsKeyValue.java
@@ -19,7 +19,11 @@
  */
 package org.apache.hadoop.hbase;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
+import org.apache.hadoop.hbase.util.Bytes;
 
 /**
  * This class is an extension to ContentSizeCachedKeyValue where there are no tags in Cell.
@@ -41,5 +45,12 @@ public class SizeCachedNoTagsKeyValue extends SizeCachedKeyValue {
   @Override
   public long heapSizeWithoutTags() {
     return super.heapSize();
+  }
+
+  @Override
+  public int write(OutputStream out, boolean withTags) throws IOException {
+    writeInt(out, this.length);
+    out.write(this.bytes, this.offset, this.length);
+    return this.length + Bytes.SIZEOF_INT;
   }
 }
