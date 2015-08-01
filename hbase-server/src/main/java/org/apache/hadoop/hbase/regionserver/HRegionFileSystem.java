92c7bbf310fce99cc0f37806a8b63ec751d31f6f
Ashish Singhi <ashish.singhi@huawei.com>
2015-07-31 17:03:29 -0700

    HBASE-14154 DFS Replication should be configurable at column family level
    Signed-off-by: Andrew Purtell <apurtell@apache.org>
    
    Conflicts:
    	hbase-client/src/main/java/org/apache/hadoop/hbase/HColumnDescriptor.java
    	hbase-client/src/test/java/org/apache/hadoop/hbase/TestHColumnDescriptor.java
    	hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileWriterImpl.java
    	hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestAdmin1.java

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionFileSystem.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionFileSystem.java
@@ -770,7 +770,7 @@ public class HRegionFileSystem {
     // First check to get the permissions
     FsPermission perms = FSUtils.getFilePermissions(fs, conf, HConstants.DATA_FILE_UMASK_KEY);
     // Write the RegionInfo file content
-    FSDataOutputStream out = FSUtils.create(fs, regionInfoFile, perms, null);
+    FSDataOutputStream out = FSUtils.create(conf, fs, regionInfoFile, perms, null);
     try {
       out.write(content);
     } finally {
