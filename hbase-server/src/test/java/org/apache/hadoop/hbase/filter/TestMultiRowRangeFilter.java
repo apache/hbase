8954dd88f243da79d00a0f0c722238c421b40f55
tedyu <yuzhihong@gmail.com>
2015-08-11 10:05:19 -0700

    HBASE-14206 MultiRowRangeFilter returns records whose rowKeys are out of allowed ranges (Anton Nazaruk)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/filter/TestMultiRowRangeFilter.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/filter/TestMultiRowRangeFilter.java
@@ -70,6 +70,24 @@ public class TestMultiRowRangeFilter {
   }
 
   @Test
+  public void testRanges() throws IOException {
+    byte[] key1Start = new byte[] {-3};
+    byte[] key1End  = new byte[] {-2};
+
+    byte[] key2Start = new byte[] {5};
+    byte[] key2End  = new byte[] {6};
+
+    byte[] badKey = new byte[] {-10};
+
+    MultiRowRangeFilter filter = new MultiRowRangeFilter(Arrays.asList(
+      new MultiRowRangeFilter.RowRange(key1Start, true, key1End, false),
+      new MultiRowRangeFilter.RowRange(key2Start, true, key2End, false)
+        ));
+    filter.filterRowKey(badKey, 0, 1);
+    assertEquals(Filter.ReturnCode.SEEK_NEXT_USING_HINT, filter.filterKeyValue(null));
+  }
+
+  @Test
   public void testOutOfOrderScannerNextException() throws Exception {
     MultiRowRangeFilter filter = new MultiRowRangeFilter(Arrays.asList(
             new MultiRowRangeFilter.RowRange(Bytes.toBytes("b"), true, Bytes.toBytes("c"), true),
