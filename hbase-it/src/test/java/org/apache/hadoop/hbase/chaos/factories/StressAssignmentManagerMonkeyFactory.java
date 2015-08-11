8019009563eff5fbe2cbaf91662d989087aa5f6d
Elliott Clark <eclark@apache.org>
2015-08-11 16:52:54 -0700

    HBASE-14211 Add more rigorous integration tests of splits
    Summary:
    Intgration tests don't currently have a lot of splits going on while there is IO.
    This changes that by changing the split aglorithm and the max region hfile size. That should make things split more.
    
    Additionally this allows ITBLL to start with just one region if hbase.test.pre-split-table is False.
    
    Test Plan: Test on a cluster with ITBLL slow determinitic monkey and stress am monkey.
    
    Differential Revision: https://reviews.facebook.net/D44181

--- hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/factories/StressAssignmentManagerMonkeyFactory.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/factories/StressAssignmentManagerMonkeyFactory.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,22 +17,7 @@
  */
 package org.apache.hadoop.hbase.chaos.factories;
 
-import org.apache.hadoop.hbase.chaos.actions.Action;
-import org.apache.hadoop.hbase.chaos.actions.AddColumnAction;
-import org.apache.hadoop.hbase.chaos.actions.BatchRestartRsAction;
-import org.apache.hadoop.hbase.chaos.actions.CompactRandomRegionOfTableAction;
-import org.apache.hadoop.hbase.chaos.actions.CompactTableAction;
-import org.apache.hadoop.hbase.chaos.actions.DumpClusterStatusAction;
-import org.apache.hadoop.hbase.chaos.actions.FlushRandomRegionOfTableAction;
-import org.apache.hadoop.hbase.chaos.actions.FlushTableAction;
-import org.apache.hadoop.hbase.chaos.actions.MergeRandomAdjacentRegionsOfTableAction;
-import org.apache.hadoop.hbase.chaos.actions.MoveRandomRegionOfTableAction;
-import org.apache.hadoop.hbase.chaos.actions.MoveRegionsOfTableAction;
-import org.apache.hadoop.hbase.chaos.actions.RemoveColumnAction;
-import org.apache.hadoop.hbase.chaos.actions.RestartRandomRsAction;
-import org.apache.hadoop.hbase.chaos.actions.RestartRsHoldingMetaAction;
-import org.apache.hadoop.hbase.chaos.actions.RollingBatchRestartRsAction;
-import org.apache.hadoop.hbase.chaos.actions.SplitRandomRegionOfTableAction;
+import org.apache.hadoop.hbase.chaos.actions.*;
 import org.apache.hadoop.hbase.chaos.monkies.ChaosMonkey;
 import org.apache.hadoop.hbase.chaos.monkies.PolicyBasedChaosMonkey;
 import org.apache.hadoop.hbase.chaos.policies.CompositeSequentialPolicy;
@@ -45,28 +30,35 @@ public class StressAssignmentManagerMonkeyFactory extends MonkeyFactory {
 
     // Actions that could slow down region movement.
     // These could also get regions stuck if there are issues.
-    Action[] actions1 = new Action[] {
+    Action[] actions1 = new Action[]{
         new CompactTableAction(tableName, 0.5f),
         new CompactRandomRegionOfTableAction(tableName, 0.6f),
         new FlushTableAction(tableName),
         new FlushRandomRegionOfTableAction(tableName)
     };
 
-    Action[] actions2 = new Action[] {
+    Action[] actions2 = new Action[]{
         new SplitRandomRegionOfTableAction(tableName),
         new MergeRandomAdjacentRegionsOfTableAction(tableName),
         new AddColumnAction(tableName),
         new RemoveColumnAction(tableName, columnFamilies),
-        new MoveRegionsOfTableAction(800, 1600, tableName),
-        new MoveRandomRegionOfTableAction(800, tableName),
-        new RestartRandomRsAction(60000),
-        new BatchRestartRsAction(5000, 0.5f),
-        new RollingBatchRestartRsAction(5000, 1.0f),
-        new RestartRsHoldingMetaAction(35000)
+        new MoveRegionsOfTableAction(MonkeyConstants.DEFAULT_MOVE_REGIONS_SLEEP_TIME,
+            1600,
+            tableName),
+        new MoveRandomRegionOfTableAction(MonkeyConstants.DEFAULT_MOVE_RANDOM_REGION_SLEEP_TIME,
+            tableName),
+        new RestartRandomRsAction(MonkeyConstants.DEFAULT_RESTART_RANDOM_RS_SLEEP_TIME),
+        new BatchRestartRsAction(MonkeyConstants.DEFAULT_ROLLING_BATCH_RESTART_RS_SLEEP_TIME, 0.5f),
+        new RollingBatchRestartRsAction(MonkeyConstants.DEFAULT_BATCH_RESTART_RS_SLEEP_TIME, 1.0f),
+        new RestartRsHoldingMetaAction(MonkeyConstants.DEFAULT_RESTART_RS_HOLDING_META_SLEEP_TIME),
+        new ChangeSplitPolicyAction(tableName),
+        new SplitAllRegionOfTableAction(tableName),
+        new DecreaseMaxHFileSizeAction(MonkeyConstants.DEFAULT_DECREASE_HFILE_SIZE_SLEEP_TIME,
+            tableName),
     };
 
     // Action to log more info for debugging
-    Action[] actions3 = new Action[] {
+    Action[] actions3 = new Action[]{
         new DumpClusterStatusAction()
     };
 
