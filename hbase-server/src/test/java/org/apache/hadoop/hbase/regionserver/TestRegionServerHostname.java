419d15f834773b37cabd1494dad8e49dbe3082c5
Liu Shaohui <liushaohui@xiaomi.com>
2015-08-24 09:59:32 +0800

    HBASE-14277 TestRegionServerHostname.testRegionServerHostname may fail at host with a case sensitive name

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestRegionServerHostname.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestRegionServerHostname.java
@@ -92,7 +92,7 @@ public class TestRegionServerHostname {
           }
           assertTrue(servers.size() == NUM_RS);
           for (String server : servers) {
-            assertTrue(server.startsWith(hostName+","));
+            assertTrue(server.startsWith(hostName.toLowerCase()+","));
           }
           zkw.close();
         } finally {
