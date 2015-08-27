60402b52b1c0cf9c3d7043037084c3e3b0b7983a
Elliott Clark <eclark@apache.org>
2015-08-26 22:28:38 -0700

    HBASE-14313 After a Connection sees ConnectionClosingException it never recovers -- ADDENDUM

--- hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/RpcClientImpl.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/RpcClientImpl.java
@@ -933,7 +933,9 @@ public class RpcClientImpl extends AbstractRpcClient {
         } catch (IOException e) {
           // We set the value inside the synchronized block, this way the next in line
           //  won't even try to write
-          markClosed(e);
+          if (markClosed(e)) {
+            close();
+          }
           writeException = e;
           interrupt();
         }
