58b3cfec0e4461334d5c5ebf1184fd05cd3cd810
Nick Dimiduk <ndimiduk@apache.org>
2015-08-27 11:08:17 -0700

    HBASE-14318 make_rc.sh should purge/re-resolve dependencies from local repository

--- dev-support/make_rc.sh
+++ dev-support/make_rc.sh
@@ -16,10 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Script that assembles all you need to make an RC.
-# Does build of the tar.gzs which it stashes into a dir above $HBASE_HOME
-# named for the script with a timestamp suffix.
-# Deploys builds to maven.
+# Script that assembles all you need to make an RC. Does build of the tar.gzs
+# which it stashes into a dir above $(pwd) named for the script with a
+# timestamp suffix. Deploys builds to maven.
 #
 # To finish, check what was build.  If good copy to people.apache.org and
 # close the maven repos.  Call a vote. 
@@ -31,7 +30,6 @@ set -e
 
 devsupport=`dirname "$0"`
 devsupport=`cd "$devsupport">/dev/null; pwd`
-HBASE_HOME=`cd ${devsupport}/.. > /dev/null; pwd`
 
 # Set mvn and mvnopts
 mvn=mvn
@@ -45,33 +43,41 @@ fi
 
 # Make a dir to save tgzs in.
 d=`date -u +"%Y%m%dT%H%M%SZ"`
-archivedir="${HBASE_HOME}/../`basename $0`.$d"
+archivedir="$(pwd)/../`basename $0`.$d"
 echo "Archive dir ${archivedir}"
-mkdir -p "${archivedir}" 
+mkdir -p "${archivedir}"
 
 function tgz_mover {
-  mv "${HBASE_HOME}"/hbase-assembly/target/hbase-*.tar.gz "${archivedir}"
+  mv ./hbase-assembly/target/hbase-*.tar.gz "${archivedir}"
 }
 
 function deploy {
-  MAVEN_OPTS="${mvnopts}" ${mvn} clean install -DskipTests -Prelease
-  MAVEN_OPTS="${mvnopts}" ${mvn} install -DskipTests post-site assembly:single -Prelease
+  MAVEN_OPTS="${mvnopts}" ${mvn} clean install -DskipTests -Prelease \
+    -Dmaven.repo.local=${archivedir}/repository
+  MAVEN_OPTS="${mvnopts}" ${mvn} install -DskipTests post-site assembly:single -Prelease \
+    -Dmaven.repo.local=${archivedir}/repository
   tgz_mover
-  MAVEN_OPTS="${mvnopts}" ${mvn} deploy -DskipTests -Papache-release -Prelease
+  MAVEN_OPTS="${mvnopts}" ${mvn} deploy -DskipTests -Papache-release -Prelease \
+    -Dmaven.repo.local=${archivedir}/repository
 }
 
 # Build src tarball
-MAVEN_OPTS="${mvnopts}" ${mvn} clean install -DskipTests assembly:single -Dassembly.file="${HBASE_HOME}/hbase-assembly/src/main/assembly/src.xml" -Prelease
+# run clean separate from assembly:single because it fails to clean shaded modules correctly
+MAVEN_OPTS="${mvnopts}" ${mvn} clean
+MAVEN_OPTS="${mvnopts}" ${mvn} install -DskipTests assembly:single \
+  -Dassembly.file="$(pwd)/hbase-assembly/src/main/assembly/src.xml" \
+  -Prelease -Dmaven.repo.local=${archivedir}/repository
+
 tgz_mover
 
 # Now do the two builds,  one for hadoop1, then hadoop2
 deploy
 
 echo "DONE"
-echo "Check the content of ${archivedir}.  If good, sign and push to people.apache.org"
+echo "Check the content of ${archivedir}.  If good, sign and push to dist.apache.org"
 echo " cd ${archivedir}"
 echo ' for i in *.tar.gz; do echo $i; gpg --print-mds $i > $i.mds ; done'
 echo ' for i in *.tar.gz; do echo $i; gpg --armor --output $i.asc --detach-sig $i  ; done'
-echo ' rsync -av ${archivedir} people.apache.org:public_html/hbase-VERSION'
+echo ' rsync -av ${archivedir}/*.gz ${archivedir}/*.mds ${archivedir}/*.asc ~/repos/dist-dev/hbase-VERSION/'
 echo "Check the content deployed to maven.  If good, close the repo and record links of temporary staging repo"
 echo "If all good tag the RC"
