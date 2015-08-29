5532af9727be3dd163056292948a0deddb56ccac
tedyu <yuzhihong@gmail.com>
2015-08-29 02:14:25 -0700

    HBASE-14258 Addendum fixes case sensitivity in 'load' (Vladimir Rodionov)

--- bin/region_mover.rb
+++ bin/region_mover.rb
@@ -210,9 +210,11 @@ end
 # Return servername that matches passed hostname and port
 def getServerName(servers, hostname, port)
   servername = nil
+  upperCaseHostname = hostname.upcase;
   for server in servers
     hostFromServerName, portFromServerName = getHostPortFromServerName(server)
-    if hostFromServerName == hostname and portFromServerName == port
+    hostFromServerName = hostFromServerName.upcase
+    if hostFromServerName == upperCaseHostname and portFromServerName == port
       servername = server
       break
     end
