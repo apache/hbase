fe2c4f630d3b5f3346c9ee9f95c256186c9e6907
Apekshit(Appy) Sharma <appy@cloudera.com>
2015-09-10 12:32:24 -0700

    HBASE-14400 Fix HBase RPC protection documentation
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/security/SaslUtil.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/security/SaslUtil.java
@@ -24,10 +24,13 @@ import java.util.TreeMap;
 import javax.security.sasl.Sasl;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
 
 @InterfaceAudience.Private
 public class SaslUtil {
+  private static final Log log = LogFactory.getLog(SaslUtil.class);
   public static final String SASL_DEFAULT_REALM = "default";
   public static final Map<String, String> SASL_PROPS =
       new TreeMap<String, String>();
@@ -66,16 +69,41 @@ public class SaslUtil {
     return new String(Base64.encodeBase64(password)).toCharArray();
   }
 
-  static void initSaslProperties(String rpcProtection) {
-    QualityOfProtection saslQOP = QualityOfProtection.AUTHENTICATION;
-    if (QualityOfProtection.INTEGRITY.name().toLowerCase()
-        .equals(rpcProtection)) {
-      saslQOP = QualityOfProtection.INTEGRITY;
-    } else if (QualityOfProtection.PRIVACY.name().toLowerCase().equals(
-        rpcProtection)) {
-      saslQOP = QualityOfProtection.PRIVACY;
+  /**
+   * Returns {@link org.apache.hadoop.hbase.security.SaslUtil.QualityOfProtection}
+   * corresponding to the given {@code stringQop} value. Returns null if value is
+   * invalid.
+   */
+  public static QualityOfProtection getQop(String stringQop) {
+    QualityOfProtection qop = null;
+    if (QualityOfProtection.AUTHENTICATION.name().toLowerCase().equals(stringQop)
+        || QualityOfProtection.AUTHENTICATION.saslQop.equals(stringQop)) {
+      qop = QualityOfProtection.AUTHENTICATION;
+    } else if (QualityOfProtection.INTEGRITY.name().toLowerCase().equals(stringQop)
+        || QualityOfProtection.INTEGRITY.saslQop.equals(stringQop)) {
+      qop = QualityOfProtection.INTEGRITY;
+    } else if (QualityOfProtection.PRIVACY.name().toLowerCase().equals(stringQop)
+        || QualityOfProtection.PRIVACY.saslQop.equals(stringQop)) {
+      qop = QualityOfProtection.PRIVACY;
     }
+    if (qop == null) {
+      throw new IllegalArgumentException("Invalid qop: " +  stringQop
+          + ". It must be one of 'authentication', 'integrity', 'privacy'.");
+    }
+    if (QualityOfProtection.AUTHENTICATION.saslQop.equals(stringQop)
+        || QualityOfProtection.INTEGRITY.saslQop.equals(stringQop)
+        || QualityOfProtection.PRIVACY.saslQop.equals(stringQop)) {
+      log.warn("Use authentication/integrity/privacy as value for rpc protection "
+          + "configurations instead of auth/auth-int/auth-conf.");
+    }
+    return qop;
+  }
 
+  static void initSaslProperties(String rpcProtection) {
+    QualityOfProtection saslQOP = getQop(rpcProtection);
+    if (saslQOP == null) {
+      saslQOP = QualityOfProtection.AUTHENTICATION;
+    }
     SaslUtil.SASL_PROPS.put(Sasl.QOP, saslQOP.getSaslQop());
     SaslUtil.SASL_PROPS.put(Sasl.SERVER_AUTH, "true");
   }
