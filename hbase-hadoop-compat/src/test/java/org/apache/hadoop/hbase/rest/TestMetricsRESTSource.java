a5cd676e724f3b703501ff1b94624c59e8699af6
Sean Busbey <busbey@apache.org>
2015-09-29 20:09:48 -0500

    HBASE-14516 categorize hadoop-compat tests
    * make sure the test classifications are in test scope for their use in the hadoop-compat modules
    * added a test category for 'metrics related' since that's what all these tests are for
    * categorized tests as small,metrics
    
    Conflicts:
    	hbase-hadoop2-compat/src/test/java/org/apache/hadoop/hbase/master/TestMetricsMasterSourceImpl.java

--- hbase-hadoop-compat/src/test/java/org/apache/hadoop/hbase/rest/TestMetricsRESTSource.java
+++ hbase-hadoop-compat/src/test/java/org/apache/hadoop/hbase/rest/TestMetricsRESTSource.java
@@ -20,11 +20,15 @@ package org.apache.hadoop.hbase.rest;
 
 import org.apache.hadoop.hbase.CompatibilitySingletonFactory;
 import org.apache.hadoop.hbase.rest.MetricsRESTSource;
+import org.apache.hadoop.hbase.testclassification.SmallTests;
+import org.apache.hadoop.hbase.testclassification.MetricsTests;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 /**
  *   Test of Rest Metrics Source interface.
  */
+@Category({MetricsTests.class, SmallTests.class})
 public class TestMetricsRESTSource {
 
 
