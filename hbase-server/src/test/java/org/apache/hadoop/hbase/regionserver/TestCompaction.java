7654fcd6fa65ac184299875e20adb3c3d14a7d42
Andrew Purtell <apurtell@apache.org>
2015-10-01 12:09:13 -0700

    HBASE-14475 Region split requests are always audited with hbase user rather than request user (Ted Yu)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestCompaction.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestCompaction.java
@@ -290,7 +290,7 @@ public class TestCompaction {
 
     CountDownLatch latch = new CountDownLatch(1);
     TrackableCompactionRequest request = new TrackableCompactionRequest(latch);
-    thread.requestCompaction(r, store, "test custom comapction", Store.PRIORITY_USER, request);
+    thread.requestCompaction(r, store, "test custom comapction", Store.PRIORITY_USER, request,null);
     // wait for the latch to complete.
     latch.await();
 
@@ -326,7 +326,7 @@ public class TestCompaction {
     }
 
     thread.requestCompaction(r, "test mulitple custom comapctions", Store.PRIORITY_USER,
-      Collections.unmodifiableList(requests));
+      Collections.unmodifiableList(requests), null);
 
     // wait for the latch to complete.
     latch.await();
