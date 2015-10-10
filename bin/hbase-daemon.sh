1b66ea71b69e8b24ba7ecb9ba6548d775777f417
tedyu <yuzhihong@gmail.com>
2015-10-10 01:59:57 -0700

    HBASE-14581 Znode cleanup throws auth exception in secure mode

--- bin/hbase-daemon.sh
+++ bin/hbase-daemon.sh
@@ -81,11 +81,11 @@ cleanAfterRun() {
 
   if [ -f ${HBASE_ZNODE_FILE} ]; then
     if [ "$command" = "master" ]; then
-      $bin/hbase master clear > /dev/null 2>&1
+      HBASE_OPTS="$HBASE_OPTS $HBASE_MASTER_OPTS" $bin/hbase master clear > /dev/null 2>&1
     else
       #call ZK to delete the node
       ZNODE=`cat ${HBASE_ZNODE_FILE}`
-      $bin/hbase zkcli delete ${ZNODE} > /dev/null 2>&1
+      HBASE_OPTS="$HBASE_OPTS $HBASE_REGIONSERVER_OPTS" $bin/hbase zkcli delete ${ZNODE} > /dev/null 2>&1
     fi
     rm ${HBASE_ZNODE_FILE}
   fi
