cab158173d34e93c5cb3dee1f42126b37aa7ac7b
stack <stack@apache.org>
2015-10-10 13:19:52 -0700

    Revert "HBASE-14583 Enabled client-side metrics by default" Caused TestShell to hang. Reverting for now.
    This reverts commit dced02ad9ff647118a830e10b3628a4687754a72.

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionManager.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionManager.java
@@ -673,7 +673,7 @@ class ConnectionManager {
       this.rpcCallerFactory = RpcRetryingCallerFactory.instantiate(conf, interceptor, this.stats);
       this.backoffPolicy = ClientBackoffPolicyFactory.create(conf);
       this.asyncProcess = createAsyncProcess(this.conf);
-      if (conf.getBoolean(CLIENT_SIDE_METRICS_ENABLED_KEY, true)) {
+      if (conf.getBoolean(CLIENT_SIDE_METRICS_ENABLED_KEY, false)) {
         this.metrics = new MetricsConnection(this);
       } else {
         this.metrics = null;
