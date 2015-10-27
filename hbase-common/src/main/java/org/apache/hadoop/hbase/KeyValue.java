61d913dd0de04683f021be94a5b6f15bc903a77f
Andrew Purtell <apurtell@apache.org>
2015-10-27 14:39:03 -0700

    HBASE-14705 Javadoc for KeyValue constructor is not correct (Jean-Marc Spaggiari)

--- hbase-common/src/main/java/org/apache/hadoop/hbase/KeyValue.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/KeyValue.java
@@ -392,7 +392,8 @@ public class KeyValue implements Cell, HeapSize, Cloneable, SettableSequenceId, 
   }
 
   /**
-   * Constructs KeyValue structure filled with null value.
+   * Constructs KeyValue structure as a put filled with specified values and
+   * LATEST_TIMESTAMP.
    * @param row - row key (arbitrary byte array)
    * @param family family name
    * @param qualifier column qualifier
