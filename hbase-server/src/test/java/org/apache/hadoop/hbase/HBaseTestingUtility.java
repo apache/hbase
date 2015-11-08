520c02b70f4bfdf223aae73dab6b0aa3bf58a52c
tedyu <yuzhihong@gmail.com>
2015-11-08 08:53:10 -0800

    HBASE-14784 Port conflict is not resolved in HBaseTestingUtility.randomFreePort() (Youngjoon Kim)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
@@ -3682,6 +3682,7 @@ public class HBaseTestingUtility extends HBaseCommonTestingUtility {
     do {
       port = randomPort();
       if (takenRandomPorts.contains(port)) {
+        port = 0;
         continue;
       }
       takenRandomPorts.add(port);
