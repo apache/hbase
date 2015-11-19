a403429952c2bd3d0e951a3382d5708e7c8affee
Enis Soztutar <enis@apache.org>
2015-11-18 19:22:03 -0800

    HBASE-14468 Compaction improvements: FIFO compaction policy (Vladimir Rodionov)

---
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/util/TimeOffsetEnvironmentEdge.java
@@ -0,0 +1,49 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hbase.util;
+
+import org.apache.hadoop.hbase.classification.InterfaceAudience;
+
+@InterfaceAudience.Private
+public class TimeOffsetEnvironmentEdge implements EnvironmentEdge {
+  private long offset;
+  
+  public TimeOffsetEnvironmentEdge(){}
+  
+  public void setTimeOffset(long off){
+    this.offset = off;
+  }
+  
+  public long getTimeOffset()
+  {
+    return offset;
+  }
+  
+  public void increment(long incr)
+  {
+    this.offset += incr;
+  }
+  
+  @Override
+  public long currentTime() {
+    return System.currentTimeMillis() + offset;
+  }
+
+}
