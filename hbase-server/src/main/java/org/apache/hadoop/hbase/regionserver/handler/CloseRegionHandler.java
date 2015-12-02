2566b6d4891fcb3666351154cabfd60c9985071e
stack <stack@apache.org>
2015-12-01 16:03:46 -0800

    Revert "HBASE-14223 Meta WALs are not cleared if meta region was closed and RS aborts" TestRegionRebalancing is failing fairly regularly... reverting for now.
    This reverts commit 89aa2f1a800469adb3f43177bdb68e3159cbaeca.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/handler/CloseRegionHandler.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/handler/CloseRegionHandler.java
@@ -46,9 +46,8 @@ public class CloseRegionHandler extends EventHandler {
   // have a running queue of user regions to close?
   private static final Log LOG = LogFactory.getLog(CloseRegionHandler.class);
 
-  protected final RegionServerServices rsServices;
-  protected final HRegionInfo regionInfo;
-  protected HRegion region;
+  private final RegionServerServices rsServices;
+  private final HRegionInfo regionInfo;
 
   // If true, the hosting server is aborting.  Region close process is different
   // when we are aborting.
@@ -120,7 +119,7 @@ public class CloseRegionHandler extends EventHandler {
       LOG.debug("Processing close of " + name);
       String encodedRegionName = regionInfo.getEncodedName();
       // Check that this region is being served here
-      region = (HRegion)rsServices.getFromOnlineRegions(encodedRegionName);
+      HRegion region = (HRegion)rsServices.getFromOnlineRegions(encodedRegionName);
       if (region == null) {
         LOG.warn("Received CLOSE for region " + name + " but currently not serving - ignoring");
         // TODO: do better than a simple warning
@@ -143,10 +142,6 @@ public class CloseRegionHandler extends EventHandler {
           LOG.warn("Can't close region: was already closed during close(): " +
             regionInfo.getRegionNameAsString());
           return;
-        }
-
-        if (!abort) {
-          releaseWALIfNeeded();
         }
       } catch (IOException ioe) {
         // An IOException here indicates that we couldn't successfully flush the
@@ -171,9 +166,5 @@ public class CloseRegionHandler extends EventHandler {
       this.rsServices.getRegionsInTransitionInRS().
           remove(this.regionInfo.getEncodedNameAsBytes());
     }
-  }
-
-  protected void releaseWALIfNeeded() throws IOException {
-    // release the WAL if needed. Only meta does this for now.
   }
 }
