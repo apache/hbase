ac6a57b6b0723f2cdc29fad785d61bb0e6b862b4
stack <stack@apache.org>
2015-12-10 21:25:21 -0800

    HBASE-14946 Don't allow multi's to over run the max result size.

--- hbase-client/src/main/java/org/apache/hadoop/hbase/RetryImmediatelyException.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/RetryImmediatelyException.java
@@ -19,7 +19,11 @@
 package org.apache.hadoop.hbase;
 
 import java.io.IOException;
+import org.apache.hadoop.hbase.classification.InterfaceAudience;
+import org.apache.hadoop.hbase.classification.InterfaceStability;
 
+@InterfaceAudience.Public
+@InterfaceStability.Evolving
 public class RetryImmediatelyException extends IOException {
   public RetryImmediatelyException(String s) {
     super(s);
