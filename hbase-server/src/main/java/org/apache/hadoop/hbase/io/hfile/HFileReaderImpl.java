c8d133186b85a4e7298bab3376ad96899860b1c7
Ashish Singhi <ashish.singhi@huawei.com>
2016-01-28 14:23:26 +0530

    HBASE-9393 Hbase does not closing a closed socket resulting in many CLOSE_WAIT
    Signed-off-by: stack <stack@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileReaderImpl.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileReaderImpl.java
@@ -575,6 +575,10 @@ public class HFileReaderImpl implements HFile.Reader, Configurable {
 
     @Override
     public void close() {
+      if (!pread) {
+        // For seek + pread stream socket should be closed when the scanner is closed. HBASE-9393
+        reader.unbufferStream();
+      }
       this.returnBlocks(true);
     }
 
@@ -1897,5 +1901,10 @@ public class HFileReaderImpl implements HFile.Reader, Configurable {
 
   public int getMajorVersion() {
     return 3;
+  }
+
+  @Override
+  public void unbufferStream() {
+    fsBlockReader.unbufferStream();
   }
 }
