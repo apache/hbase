b80325fb1b6ad4735c8b2cb259b1430eddefd7b7
Andrew Purtell <apurtell@apache.org>
2016-02-04 16:17:41 -0800

    Amend HBASE-15200 ZooKeeper znode ACL checks should only compare the shortname
    Fixes for newly introduced FindBugs warnings

--- hbase-client/src/main/java/org/apache/hadoop/hbase/zookeeper/ZooKeeperWatcher.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/zookeeper/ZooKeeperWatcher.java
@@ -293,7 +293,7 @@ public class ZooKeeperWatcher implements Watcher, Abortable, Closeable {
       if (Ids.ANYONE_ID_UNSAFE.equals(id)) {
         if (perms != Perms.READ) {
           if (LOG.isDebugEnabled()) {
-            LOG.debug(String.format("permissions for '%s' are not correct: have %0x, want %0x",
+            LOG.debug(String.format("permissions for '%s' are not correct: have 0x%x, want 0x%x",
               id, perms, Perms.READ));
           }
           return false;
@@ -301,7 +301,7 @@ public class ZooKeeperWatcher implements Watcher, Abortable, Closeable {
       } else if (superUsers != null && isSuperUserId(superUsers, id)) {
         if (perms != Perms.ALL) {
           if (LOG.isDebugEnabled()) {
-            LOG.debug(String.format("permissions for '%s' are not correct: have %0x, want %0x",
+            LOG.debug(String.format("permissions for '%s' are not correct: have 0x%x, want 0x%x",
               id, perms, Perms.ALL));
           }
           return false;
@@ -316,8 +316,8 @@ public class ZooKeeperWatcher implements Watcher, Abortable, Closeable {
         if (name.equals(hbaseUser)) {
           if (perms != Perms.ALL) {
             if (LOG.isDebugEnabled()) {
-              LOG.debug(String.format("permissions for '%s' are not correct: have %0x, want %0x",
-                id.toString(), perms, Perms.ALL));
+              LOG.debug(String.format("permissions for '%s' are not correct: have 0x%x, want 0x%x",
+                id, perms, Perms.ALL));
             }
             return false;
           }
@@ -352,7 +352,7 @@ public class ZooKeeperWatcher implements Watcher, Abortable, Closeable {
             } else {
               if (LOG.isDebugEnabled()) {
                 LOG.debug(String.format(
-                  "superuser '%s' does not have correct permissions: have %0x, want %0x",
+                  "superuser '%s' does not have correct permissions: have 0x%x, want 0x%x",
                   acl.getId().getId(), acl.getPerms(), Perms.ALL));
               }
             }
