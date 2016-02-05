18eff3c1c337003b2a419490e621f931d16936fb
stack <stack@apache.org>
2016-02-04 16:09:26 -0800

    HBASE-15210 Undo aggressive load balancer logging at tens of lines per millisecond

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/BaseLoadBalancer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/BaseLoadBalancer.java
@@ -816,9 +816,11 @@ public abstract class BaseLoadBalancer implements LoadBalancer {
           i++;
           lowestLocalityServerIndex = serverIndicesSortedByLocality[i];
         }
-        LOG.debug("Lowest locality region server with non zero regions is "
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("Lowest locality region server with non zero regions is "
             + servers[lowestLocalityServerIndex].getHostname() + " with locality "
             + localityPerServer[lowestLocalityServerIndex]);
+        }
         return lowestLocalityServerIndex;
       }
     }
@@ -841,9 +843,11 @@ public abstract class BaseLoadBalancer implements LoadBalancer {
             lowestLocalityRegionIndex = j;
           }
         }
-        LOG.debug(" Lowest locality region index is " + lowestLocalityRegionIndex
+        if (LOG.isTraceEnabled()) {
+          LOG.debug(" Lowest locality region index is " + lowestLocalityRegionIndex
             + " and its region server contains " + regionsPerServer[serverIndex].length
             + " regions");
+        }
         return regionsPerServer[serverIndex][lowestLocalityRegionIndex];
       } else {
         return -1;
