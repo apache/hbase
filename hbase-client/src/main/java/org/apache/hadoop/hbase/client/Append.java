a1efda29e9f89f6ded192213e90164c7a221f041
cuijianwei <cuijianwei@xiaomi.com>
2016-04-10 14:31:31 +0800

    HBASE-15497 Incorrect javadoc for atomicity guarantee of Increment and Append
    Signed-off-by: Chia-Ping Tsai <chia7712@gmail.com>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/Append.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/Append.java
@@ -34,10 +34,9 @@ import org.apache.hadoop.hbase.util.Bytes;
 /**
  * Performs Append operations on a single row.
  * <p>
- * Note that this operation does not appear atomic to readers. Appends are done
- * under a single row lock, so write operations to a row are synchronized, but
- * readers do not take row locks so get and scan operations can see this
- * operation partially completed.
+ * This operation ensures atomicty to readers. Appends are done
+ * under a single row lock, so write operations to a row are synchronized, and
+ * readers are guaranteed to see this operation fully completed.
  * <p>
  * To append to a set of columns of a row, instantiate an Append object with the
  * row to append to. At least one column to append must be specified using the
