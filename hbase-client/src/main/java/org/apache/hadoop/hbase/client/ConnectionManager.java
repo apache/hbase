3af736a95ddca817dedb12aaf7e7d9f6a4e97746
chenheng <chenheng@apache.org>
2016-04-21 14:16:06 +0800

    HBASE-15674 HRegionLocator#getAllRegionLocations should put the results in cache
    Conflicts:
    	hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionImplementation.java
    	hbase-client/src/main/java/org/apache/hadoop/hbase/client/HRegionLocator.java

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionManager.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionManager.java
@@ -1371,7 +1371,8 @@ class ConnectionManager {
      * @param tableName The table name.
      * @param location the new location
      */
-    private void cacheLocation(final TableName tableName, final RegionLocations location) {
+    @Override
+    public void cacheLocation(final TableName tableName, final RegionLocations location) {
       metaCache.cacheLocation(tableName, location);
     }
 
