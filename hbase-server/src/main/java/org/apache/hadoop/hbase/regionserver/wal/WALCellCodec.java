a7b31f74dbc4f6a6d0c1f6134ecbf809ab691dd4
tedyu <yuzhihong@gmail.com>
2016-05-03 07:01:08 -0700

    HBASE-15752 ClassNotFoundException is encountered when custom WAL codec is not found in WALPlayer job

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/WALCellCodec.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/WALCellCodec.java
@@ -79,7 +79,7 @@ public class WALCellCodec implements Codec {
     this.compression = compression;
   }
 
-  static String getWALCellCodecClass(Configuration conf) {
+  public static String getWALCellCodecClass(Configuration conf) {
     return conf.get(WAL_CELL_CODEC_CLASS_KEY, WALCellCodec.class.getName());
   }
 
