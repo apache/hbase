34ca6f506950d0c64a8c30a6115529f773018967
tedyu <yuzhihong@gmail.com>
2016-05-22 19:13:13 -0700

    HBASE-15873 ACL for snapshot restore / clone is not enforced

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/snapshot/SnapshotManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/snapshot/SnapshotManager.java
@@ -723,7 +723,7 @@ public class SnapshotManager extends MasterProcedureManager implements Stoppable
 
       // call coproc pre hook
       if (cpHost != null) {
-        cpHost.preRestoreSnapshot(reqSnapshot, snapshotTableDesc);
+        cpHost.preRestoreSnapshot(snapshot, snapshotTableDesc);
       }
 
       int tableRegionCount = -1;
@@ -767,12 +767,12 @@ public class SnapshotManager extends MasterProcedureManager implements Stoppable
       LOG.info("Restore snapshot=" + snapshot.getName() + " as table=" + tableName);
 
       if (cpHost != null) {
-        cpHost.postRestoreSnapshot(reqSnapshot, snapshotTableDesc);
+        cpHost.postRestoreSnapshot(snapshot, snapshotTableDesc);
       }
     } else {
       HTableDescriptor htd = new HTableDescriptor(tableName, snapshotTableDesc);
       if (cpHost != null) {
-        cpHost.preCloneSnapshot(reqSnapshot, htd);
+        cpHost.preCloneSnapshot(snapshot, htd);
       }
       try {
         checkAndUpdateNamespaceQuota(manifest, tableName);
@@ -786,7 +786,7 @@ public class SnapshotManager extends MasterProcedureManager implements Stoppable
       LOG.info("Clone snapshot=" + snapshot.getName() + " as table=" + tableName);
 
       if (cpHost != null) {
-        cpHost.postCloneSnapshot(reqSnapshot, htd);
+        cpHost.postCloneSnapshot(snapshot, htd);
       }
     }
   }
