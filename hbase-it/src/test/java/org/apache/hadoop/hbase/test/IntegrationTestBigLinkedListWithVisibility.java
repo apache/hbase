33845b912b3416f2ed74eb6ce1270e74553917f0
Joseph Hwang <jzh@fb.com>
2016-06-02 18:00:08 -0700

    HBASE-15935 Set up a concurrent walker that walks flushed circular linked lists as a Loop Mode
    Signed-off-by: Elliott Clark <eclark@apache.org>

--- hbase-it/src/test/java/org/apache/hadoop/hbase/test/IntegrationTestBigLinkedListWithVisibility.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/test/IntegrationTestBigLinkedListWithVisibility.java
@@ -480,21 +480,22 @@ public class IntegrationTestBigLinkedListWithVisibility extends IntegrationTestB
 
     @Override
     protected void runGenerator(int numMappers, long numNodes, String outputDir, Integer width,
-        Integer wrapMuplitplier) throws Exception {
+        Integer wrapMultiplier, Integer numWalkers) throws Exception {
       Path outputPath = new Path(outputDir);
       UUID uuid = UUID.randomUUID(); // create a random UUID.
       Path generatorOutput = new Path(outputPath, uuid.toString());
 
       Generator generator = new VisibilityGenerator();
       generator.setConf(getConf());
-      int retCode = generator.run(numMappers, numNodes, generatorOutput, width, wrapMuplitplier);
+      int retCode = generator.run(numMappers, numNodes, generatorOutput, width, wrapMultiplier,
+          numWalkers);
       if (retCode > 0) {
         throw new RuntimeException("Generator failed with return code: " + retCode);
       }
     }
 
     protected void runDelete(int numMappers, long numNodes, String outputDir, Integer width,
-        Integer wrapMuplitplier, int tableIndex) throws Exception {
+        Integer wrapMultiplier, int tableIndex) throws Exception {
       LOG.info("Running copier on table "+IntegrationTestBigLinkedListWithVisibility.getTableName(tableIndex));
       Copier copier = new Copier(
           IntegrationTestBigLinkedListWithVisibility.getTableName(tableIndex), tableIndex, true);
@@ -595,8 +596,7 @@ public class IntegrationTestBigLinkedListWithVisibility extends IntegrationTestB
       String outputDir = args[3];
       int numReducers = Integer.parseInt(args[4]);
       Integer width = (args.length < 6) ? null : Integer.parseInt(args[5]);
-      Integer wrapMuplitplier = (args.length < 7) ? null : Integer.parseInt(args[6]);
-
+      Integer wrapMultiplier = (args.length < 7) ? null : Integer.parseInt(args[6]);
       long expectedNumNodes = 0;
 
       if (numIterations < 0) {
@@ -606,7 +606,8 @@ public class IntegrationTestBigLinkedListWithVisibility extends IntegrationTestB
       for (int i = 0; i < numIterations; i++) {
         LOG.info("Starting iteration = " + i);
         LOG.info("Generating data");
-        runGenerator(numMappers, numNodes, outputDir, width, wrapMuplitplier);
+        // By default run no concurrent walkers for test with visibility
+        runGenerator(numMappers, numNodes, outputDir, width, wrapMultiplier, 0);
         expectedNumNodes += numMappers * numNodes;
         // Copying wont work because expressions are not returned back to the
         // client
@@ -619,7 +620,7 @@ public class IntegrationTestBigLinkedListWithVisibility extends IntegrationTestB
         sleep(SLEEP_IN_MS);
         for (int j = 0; j < DEFAULT_TABLES_COUNT; j++) {
           LOG.info("Deleting data on table with index: "+j);
-          runDelete(numMappers, numNodes, outputDir, width, wrapMuplitplier, j);
+          runDelete(numMappers, numNodes, outputDir, width, wrapMultiplier, j);
           sleep(SLEEP_IN_MS);
           LOG.info("Verifying common table after deleting");
           runVerify(outputDir, numReducers, expectedNumNodes, j);
