600d10a8b831afe0c881bc109d53f53cee9e3a8a
Sergey Soldatov <ssa@apache.org>
2016-06-05 23:46:03 -0700

    HBASE-15957 RpcClientImpl.close never ends in some circumstances
    Signed-off-by: Enis Soztutar <enis@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/RpcClientImpl.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/RpcClientImpl.java
@@ -1181,9 +1181,8 @@ public class RpcClientImpl extends AbstractRpcClient {
     }
     if (connsToClose != null) {
       for (Connection conn : connsToClose) {
-        if (conn.markClosed(new InterruptedIOException("RpcClient is closing"))) {
-          conn.close();
-        }
+        conn.markClosed(new InterruptedIOException("RpcClient is closing"));
+        conn.close();
       }
     }
     // wait until all connections are closed
