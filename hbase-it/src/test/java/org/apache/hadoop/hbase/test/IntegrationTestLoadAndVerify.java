f23985145455c486c84c7dd4e1b072f45f02a735
Reid <reidchan@qq.com>
2016-07-12 11:00:16 +0800

    Revert "fix HBASE-14345"
    This reverts commit 0f30024a9df70683fbfe24b82bf75805980cfe37.

--- hbase-it/src/test/java/org/apache/hadoop/hbase/test/IntegrationTestLoadAndVerify.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/test/IntegrationTestLoadAndVerify.java
@@ -549,8 +549,7 @@ public void cleanUpCluster() throws Exception {
     getTestingUtil(getConf()).deleteTable(htd.getTableName());
   }
 
-  @Override
-  public void printUsage() {
+  public void usage() {
     System.err.println(this.getClass().getSimpleName()
       + " [-Doptions] <load|verify|loadAndVerify|search>");
     System.err.println("  Loads a table with row dependencies and verifies the dependency chains");
@@ -572,8 +571,8 @@ public void cleanUpCluster() throws Exception {
 
     String[] args = cmd.getArgs();
     if (args == null || args.length < 1) {
-      printUsage("bin/hbase " + getClass().getName() + " <options>", "Options:", "");
-      System.exit(EXIT_FAILURE);
+      usage();
+      throw new RuntimeException("Incorrect Number of args.");
     }
     toRun = args[0];
     if (toRun.equalsIgnoreCase("search")) {
@@ -609,7 +608,7 @@ public void cleanUpCluster() throws Exception {
       }
     } else {
       System.err.println("Invalid argument " + toRun);
-      printUsage();
+      usage();
       return 1;
     }
 
