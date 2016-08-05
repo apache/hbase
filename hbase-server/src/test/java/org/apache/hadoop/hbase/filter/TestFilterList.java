19c609fa5e2624e537a445d9204f06e56ae782c2
Ramkrishna <ramkrishna.s.vasudevan@intel.com>
2016-08-05 10:58:02 +0530

    HBASE-16303 FilterList with MUST_PASS_ONE optimization (Ram)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/filter/TestFilterList.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/filter/TestFilterList.java
@@ -262,6 +262,7 @@ public class TestFilterList {
    * we expect to get the same result as the 'prefix' only result.
    * @throws Exception
    */
+  @Test
   public void testFilterListTwoFiltersMustPassOne() throws Exception {
     byte[] r1 = Bytes.toBytes("Row1");
     byte[] r11 = Bytes.toBytes("Row11");
@@ -294,6 +295,7 @@ public class TestFilterList {
    * we expect to get the same result as the inclusive stop result.
    * @throws Exception
    */
+  @Test
   public void testFilterListWithInclusiveStopFilteMustPassOne() throws Exception {
     byte[] r1 = Bytes.toBytes("Row1");
     byte[] r11 = Bytes.toBytes("Row11");
@@ -353,6 +355,7 @@ public class TestFilterList {
    * Test filterKeyValue logic.
    * @throws Exception
    */
+  @Test
   public void testFilterKeyValue() throws Exception {
     Filter includeFilter = new FilterBase() {
       @Override
