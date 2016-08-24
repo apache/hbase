440079951ecf8368d377ed9d936d60fed4aa4465
tedyu <yuzhihong@gmail.com>
2016-08-24 13:40:08 -0700

    HBASE-16495 When accessed via Thrift, all column families have timeToLive equal to -1 (Tigran Saluev)

--- hbase-thrift/src/main/java/org/apache/hadoop/hbase/thrift/ThriftUtilities.java
+++ hbase-thrift/src/main/java/org/apache/hadoop/hbase/thrift/ThriftUtilities.java
@@ -94,6 +94,7 @@ public class ThriftUtilities {
     col.inMemory = in.isInMemory();
     col.blockCacheEnabled = in.isBlockCacheEnabled();
     col.bloomFilterType = in.getBloomFilterType().toString();
+    col.timeToLive = in.getTimeToLive();
     return col;
   }
 
