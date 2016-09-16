2597217ae5aa057e1931c772139ce8cc7a2b3efb
tedyu <yuzhihong@gmail.com>
2016-09-16 02:32:03 -0700

    HBASE-16349 TestClusterId may hang during cluster shutdown

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestClusterId.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestClusterId.java
@@ -56,6 +56,7 @@ public class TestClusterId {
 
   @Before
   public void setUp() throws Exception {
+    TEST_UTIL.getConfiguration().setBoolean(ShutdownHook.RUN_SHUTDOWN_HOOK, false);
   }
 
   @After
