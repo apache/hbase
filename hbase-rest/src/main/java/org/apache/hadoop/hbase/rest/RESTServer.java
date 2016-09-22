97ce640f5d71cc10828a7895298e2bbb482b1068
Ben Lau <benlau@yahoo-inc.com>
2016-09-22 19:16:51 +0000

    HBASE-16662 Fix open POODLE vulnerabilities
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

--- hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RESTServer.java
+++ hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RESTServer.java
@@ -36,6 +36,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HBaseInterfaceAudience;
 import org.apache.hadoop.hbase.http.InfoServer;
+import org.apache.hadoop.hbase.jetty.SslSelectChannelConnectorSecure;
 import org.apache.hadoop.hbase.rest.filter.AuthFilter;
 import org.apache.hadoop.hbase.security.UserProvider;
 import org.apache.hadoop.hbase.util.DNS;
@@ -45,7 +46,6 @@ import org.apache.hadoop.hbase.util.VersionInfo;
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.security.SslSelectChannelConnector;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.FilterHolder;
 import org.mortbay.jetty.servlet.ServletHolder;
@@ -194,7 +194,7 @@ public class RESTServer implements Constants {
 
     Connector connector = new SelectChannelConnector();
     if(conf.getBoolean(REST_SSL_ENABLED, false)) {
-      SslSelectChannelConnector sslConnector = new SslSelectChannelConnector();
+      SslSelectChannelConnectorSecure sslConnector = new SslSelectChannelConnectorSecure();
       String keystore = conf.get(REST_SSL_KEYSTORE_STORE);
       String password = HBaseConfiguration.getPassword(conf,
         REST_SSL_KEYSTORE_PASSWORD, null);
---
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/SslRMIServerSocketFactorySecure.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable
+ * law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the License.
+ */
+package org.apache.hadoop.hbase;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.rmi.ssl.SslRMIServerSocketFactory;
+
+/**
+ * Avoid SSL V3.0 "Poodle" Vulnerability - CVE-2014-3566
+ */
+public class SslRMIServerSocketFactorySecure extends SslRMIServerSocketFactory {
+  // If you add more constructors, you may have to change the rest of this implementation,
+  // which assumes an empty constructor, i.e. there are no specially enabled protocols or
+  // cipher suites on this RMI factory nor a provided SSLContext
+  public SslRMIServerSocketFactorySecure() {
+    super();
+  }
+
+  @Override
+  public ServerSocket createServerSocket(int port) throws IOException {
+    return new ServerSocket(port) {
+      public Socket accept() throws IOException {
+        Socket socket = super.accept();
+        SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket sslSocket =
+            (SSLSocket) sslSocketFactory.createSocket(socket,
+              socket.getInetAddress().getHostName(), socket.getPort(), true);
+        sslSocket.setUseClientMode(false);
+        sslSocket.setNeedClientAuth(false);
+
+        ArrayList<String> secureProtocols = new ArrayList<String>();
+        for (String p : sslSocket.getEnabledProtocols()) {
+          if (!p.contains("SSLv3")) {
+            secureProtocols.add(p);
+          }
+        }
+        sslSocket.setEnabledProtocols(secureProtocols.toArray(new String[secureProtocols.size()]));
+
+        return sslSocket;
+      }
+    };
+  }
+}
