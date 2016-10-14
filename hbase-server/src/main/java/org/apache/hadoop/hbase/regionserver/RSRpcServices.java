25ec2f3e4f554bf55642f78552503ca8e3a577d9
tedyu <yuzhihong@gmail.com>
2016-10-13 18:57:09 -0700

    HBASE-16830 RSRpcServices#openRegion() should handle the case where table descriptor is null

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
@@ -1673,7 +1673,8 @@ public class RSRpcServices implements HBaseRPCErrorHandler,
           } else {
             regionServer.updateRegionFavoredNodesMapping(region.getEncodedName(),
               regionOpenInfo.getFavoredNodesList());
-            if (htd.getPriority() >= HConstants.ADMIN_QOS || region.getTable().isSystemTable()) {
+            if ((htd != null && htd.getPriority() >= HConstants.ADMIN_QOS) ||
+                (region.getTable() != null && region.getTable().isSystemTable())) {
               regionServer.service.submit(new OpenPriorityRegionHandler(
                 regionServer, regionServer, region, htd, masterSystemTime, coordination, ord));
             } else {
