79073cd40c3b9a32d4509381c9d2763be42045ba
anoopsamjohn <anoopsamjohn@gmail.com>
2016-10-31 11:05:36 +0530

    HBASE-16747 Track memstore data size and heap overhead separately - Addendum to fix TestIOFencing.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/TestIOFencing.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/TestIOFencing.java
@@ -241,7 +241,7 @@ public class TestIOFencing {
     // Insert our custom region
     c.setClass(HConstants.REGION_IMPL, regionClass, HRegion.class);
     // Encourage plenty of flushes
-    c.setLong("hbase.hregion.memstore.flush.size", 100000);
+    c.setLong("hbase.hregion.memstore.flush.size", 25000);
     c.set(HConstants.HBASE_REGION_SPLIT_POLICY_KEY, ConstantSizeRegionSplitPolicy.class.getName());
     // Only run compaction when we tell it to
     c.setInt("hbase.hstore.compaction.min",1);
