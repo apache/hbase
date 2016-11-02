996b4847fa3867e9b69e6f35727732836354f7a3
Yu Li <liyu@apache.org>
2016-11-02 14:33:44 +0800

    HBASE-16972 Log more details for Scan#next request when responseTooSlow

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
@@ -984,6 +984,7 @@ public class RSRpcServices implements HBaseRPCErrorHandler,
           bindAddress, // use final bindAddress for this server.
           rs.conf,
           rpcSchedulerFactory.create(rs.conf, this, rs));
+      rpcServer.setRsRpcServices(this);
     } catch (BindException be) {
       String configName = (this instanceof MasterRpcServices) ? HConstants.MASTER_PORT :
           HConstants.REGIONSERVER_PORT;
@@ -1047,6 +1048,17 @@ public class RSRpcServices implements HBaseRPCErrorHandler,
       return scannerHolder.s;
     }
     return null;
+  }
+
+  public String getScanDetailsWithId(long scannerId) {
+    RegionScanner scanner = getScanner(scannerId);
+    if (scanner == null) {
+      return null;
+    }
+    StringBuilder builder = new StringBuilder();
+    builder.append("table: ").append(scanner.getRegionInfo().getTable().getNameAsString());
+    builder.append(" region: ").append(scanner.getRegionInfo().getRegionNameAsString());
+    return builder.toString();
   }
 
   /**
