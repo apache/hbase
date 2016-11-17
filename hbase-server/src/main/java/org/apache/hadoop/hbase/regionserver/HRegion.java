c6f1b6e62456a02ffe367c1574946a10262d5a03
tedyu <yuzhihong@gmail.com>
2016-11-17 09:04:39 -0800

    HBASE-17112 Prevent setting timestamp of delta operations the same as previous value's (Phil Yang)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -7496,7 +7496,7 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
               if (idx < results.size()
                   && CellUtil.matchingQualifier(results.get(idx), cell)) {
                 oldCell = results.get(idx);
-                long ts = Math.max(now, oldCell.getTimestamp());
+                long ts = Math.max(now, oldCell.getTimestamp() + 1);
 
                 // Process cell tags
                 // Make a union of the set of tags in the old and new KVs
@@ -7906,7 +7906,7 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
       long ts = now;
       if (idx < currentValues.size() && CellUtil.matchingQualifier(currentValues.get(idx), inc)) {
         currentValue = currentValues.get(idx);
-        ts = Math.max(now, currentValue.getTimestamp());
+        ts = Math.max(now, currentValue.getTimestamp() + 1);
         incrementAmount += getLongValue(currentValue);
         // Carry forward all tags
         tags = Tag.carryForwardTags(tags, currentValue);
