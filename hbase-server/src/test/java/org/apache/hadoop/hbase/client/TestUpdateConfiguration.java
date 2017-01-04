aec558188518f95456e2732ce454ea87619d16c6
Jan Hentschel <jan.hentschel@ultratendency.com>
2017-01-04 22:13:39 +0100

    HBASE-17425 Fixed deprecated calls in TestUpdateConfiguration

--- hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestUpdateConfiguration.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestUpdateConfiguration.java
@@ -49,7 +49,7 @@ public class TestUpdateConfiguration {
   @Test
   public void testOnlineConfigChange() throws IOException {
     LOG.debug("Starting the test");
-    Admin admin = TEST_UTIL.getHBaseAdmin();
+    Admin admin = TEST_UTIL.getAdmin();
     ServerName server = TEST_UTIL.getHBaseCluster().getRegionServer(0).getServerName();
     admin.updateConfiguration(server);
   }
@@ -65,7 +65,7 @@ public class TestUpdateConfiguration {
     // update hbase-site.xml by overwriting it
     Files.copy(cnf2Path, cnfPath, StandardCopyOption.REPLACE_EXISTING);
 
-    Admin admin = TEST_UTIL.getHBaseAdmin();
+    Admin admin = TEST_UTIL.getAdmin();
     ServerName server = TEST_UTIL.getHBaseCluster().getMaster().getServerName();
     admin.updateConfiguration(server);
     Configuration conf = TEST_UTIL.getMiniHBaseCluster().getMaster().getConfiguration();
