50bcb9f9fb81922507896cd57f9abadd87ae71fe
Sudeep Sunthankar <sudeep.sunthankar@hashmapinc.com>
2017-01-05 18:17:52 +1100

    HBASE-17395 [C++] Use custom line wrapping in formatting - ADDENDUM to remove an extra '=' sign.
    Signed-off-by: Enis Soztutar <enis@apache.org>

--- hbase-native-client/bin/format-code.sh
+++ hbase-native-client/bin/format-code.sh
@@ -19,5 +19,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-find core connection serde utils test-util security -name "*.h" -or -name "*.cc" | xargs -P8 clang-format -i --style=='{BasedOnStyle: Google, ColumnLimit: 100}'
+find core connection serde utils test-util security -name "*.h" -or -name "*.cc" | xargs -P8 clang-format -i --style='{BasedOnStyle: Google, ColumnLimit: 100}'
 find core connection serde utils third-party security -name "BUCK" | xargs -P8 yapf -i --style=google
