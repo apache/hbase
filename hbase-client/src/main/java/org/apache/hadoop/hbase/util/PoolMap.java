e371b1e72377f1a9cf481cc64294c202d86036ef
tedyu <yuzhihong@gmail.com>
2017-01-06 12:13:18 -0800

    HBASE-17431 Incorrect precheck condition in RoundRobinPool#get() - revert due to test failure

--- hbase-client/src/main/java/org/apache/hadoop/hbase/util/PoolMap.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/util/PoolMap.java
@@ -360,7 +360,7 @@ public class PoolMap<K, V> implements Map<K, V> {
 
     @Override
     public R get() {
-      if (super.size() <= 0) {
+      if (super.size() < maxSize) {
         return null;
       }
       nextResource %= super.size();
