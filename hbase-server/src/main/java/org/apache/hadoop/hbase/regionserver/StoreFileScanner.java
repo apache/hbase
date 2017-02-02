e39e0e634a2252a352ad799bc2957c72e8d2d2e9
Apekshit Sharma <appy@apache.org>
2017-02-01 23:23:46 -0800

    HBASE-17582 Fix broken drop page cache hint (broken by HBASE-15236).
    Change-Id: I2947ab979979f977db7b0c282c4aaf4eb1f26482

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFileScanner.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFileScanner.java
@@ -129,7 +129,7 @@ public class StoreFileScanner implements KeyValueScanner {
     List<StoreFile> sorted_files = new ArrayList<>(files);
     Collections.sort(sorted_files, StoreFile.Comparators.SEQ_ID);
     for (int i = 0; i < sorted_files.size(); i++) {
-      StoreFileReader r = sorted_files.get(i).createReader();
+      StoreFileReader r = sorted_files.get(i).createReader(canUseDrop);
       r.setReplicaStoreFile(isPrimaryReplica);
       StoreFileScanner scanner = r.getStoreFileScanner(cacheBlocks, usePread, isCompaction, readPt,
         i, matcher != null ? !matcher.hasNullColumnInQuery() : false);
