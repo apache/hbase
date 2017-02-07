5c77a7dcd455f7a6e0ba3f289266032be687dc4f
zhangduo <zhangduo@apache.org>
2017-02-07 11:16:07 +0800

    HBASE-17606 Fix failing TestRpcControllerFactory introduced by HBASE-17508

--- hbase-endpoint/src/test/java/org/apache/hadoop/hbase/client/TestRpcControllerFactory.java
+++ hbase-endpoint/src/test/java/org/apache/hadoop/hbase/client/TestRpcControllerFactory.java
@@ -170,7 +170,7 @@ public class TestRpcControllerFactory {
     ResultScanner scan = table.getScanner(fam1);
     scan.next();
     scan.close();
-    counter = verifyCount(counter + 2);
+    counter = verifyCount(counter + 1);
 
     Get g2 = new Get(row);
     table.get(Lists.newArrayList(g, g2));
@@ -189,7 +189,7 @@ public class TestRpcControllerFactory {
 
     // reversed, regular
     scanInfo.setSmall(false);
-    counter = doScan(table, scanInfo, counter + 2);
+    counter = doScan(table, scanInfo, counter + 1);
 
     table.close();
     connection.close();
