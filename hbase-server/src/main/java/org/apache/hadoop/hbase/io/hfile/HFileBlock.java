834488d435fb59d5cb2b0ed7f09b8b1e70d7e327
James Moore <jcmoore@hubspot.com>
2017-02-24 10:26:12 -0500

    guard against NPE while reading FileTrailer and HFileBlock
    guard against NPE from FSInputStream#seek
    
    Signed-off-by: Michael Stack <stack@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileBlock.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileBlock.java
@@ -1443,7 +1443,7 @@ public class HFileBlock implements Cacheable {
       if (!pread && streamLock.tryLock()) {
         // Seek + read. Better for scanning.
         try {
-          istream.seek(fileOffset);
+          HFileUtil.seekOnMultipleSources(istream, fileOffset);
 
           long realOffset = istream.getPos();
           if (realOffset != fileOffset) {
