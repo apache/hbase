17ef9fb15cca5f4075e90b78fdbcee7d0f80e238
Andrew Purtell <apurtell@apache.org>
2017-03-14 13:23:11 -0700

    HBASE-17780 BoundedByteBufferPool "At capacity" messages are not actionable

--- hbase-common/src/main/java/org/apache/hadoop/hbase/io/BoundedByteBufferPool.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/io/BoundedByteBufferPool.java
@@ -121,7 +121,9 @@ public class BoundedByteBufferPool {
       lock.unlock();
     }
     if (!success) {
-      LOG.warn("At capacity: " + this.buffers.size());
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("At capacity: " + this.buffers.size());
+      }
     } else {
       if (average > this.runningAverage && average < this.maxByteBufferSizeToCache) {
         this.runningAverage = average;
