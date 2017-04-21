68e48c456dc018775df792507087bf275bf3304f
tedyu <yuzhihong@gmail.com>
2017-04-21 06:54:44 -0700

    HBASE-17946 Shell command compact_rs don't work (Guangxu Cheng)

--- hbase-shell/src/main/ruby/shell/commands/compact_rs.rb
+++ hbase-shell/src/main/ruby/shell/commands/compact_rs.rb
@@ -34,7 +34,7 @@ module Shell
       end
 
       def command(regionserver, major = false)
-        admin.compactRegionserver(regionserver, major)
+        admin.compact_regionserver(regionserver, major)
       end
     end
   end
