4249a1f3d92a3b1cc7485e7aaaa289adb149688b
Guanghao Zhang <zghao@apache.org>
2017-05-22 10:51:09 +0800

    HBASE-18069 Fix flaky test TestReplicationAdminWithClusters#testDisableAndEnableReplication

--- hbase-server/src/test/java/org/apache/hadoop/hbase/client/replication/TestReplicationAdminWithClusters.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/client/replication/TestReplicationAdminWithClusters.java
@@ -149,10 +149,6 @@ public class TestReplicationAdminWithClusters extends TestReplicationBase {
     for (HColumnDescriptor fam : table.getColumnFamilies()) {
       assertEquals(fam.getScope(), HConstants.REPLICATION_SCOPE_LOCAL);
     }
-    table = admin2.getTableDescriptor(tableName);
-    for (HColumnDescriptor fam : table.getColumnFamilies()) {
-      assertEquals(fam.getScope(), HConstants.REPLICATION_SCOPE_LOCAL);
-    }
     adminExt.enableTableRep(tableName);
     table = admin1.getTableDescriptor(tableName);
     for (HColumnDescriptor fam : table.getColumnFamilies()) {
