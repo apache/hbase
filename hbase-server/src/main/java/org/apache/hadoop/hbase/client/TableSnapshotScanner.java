13efd41188fb722f8ff0bcb9af637c9a4f99c1d3
huzheng <openinx@gmail.com>
2017-05-24 20:31:57 +0800

    HBASE-16011 TableSnapshotScanner and TableSnapshotInputFormat can produce duplicate rows if split table.
    Signed-off-by: tedyu <yuzhihong@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/client/TableSnapshotScanner.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/client/TableSnapshotScanner.java
@@ -129,6 +129,9 @@ public class TableSnapshotScanner extends AbstractClientScanner {
     htd = meta.getTableDescriptor();
     regions = new ArrayList<HRegionInfo>(restoredRegions.size());
     for (HRegionInfo hri: restoredRegions) {
+      if (hri.isOffline() && (hri.isSplit() || hri.isSplitParent())) {
+        continue;
+      }
       if (CellUtil.overlappingKeys(scan.getStartRow(), scan.getStopRow(),
           hri.getStartKey(), hri.getEndKey())) {
         regions.add(hri);
