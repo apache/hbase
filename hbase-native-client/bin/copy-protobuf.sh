d6c720b8f3938330da815a8e0b013fb15d027811
Enis Soztutar <enis@apache.org>
2017-07-26 14:01:01 -0700

    HBASE-18338 [C++] Implement RpcTestServer - ADDENDUM to fix rsync for new proto files

--- hbase-native-client/bin/copy-protobuf.sh
+++ hbase-native-client/bin/copy-protobuf.sh
@@ -21,4 +21,4 @@ IFS=$'\n\t'
 BIN_DIR=$(dirname "$0")
 PB_SOURCE_DIR="${BIN_DIR}/../../hbase-protocol/src/main/protobuf/"
 PB_DEST_DIR="${BIN_DIR}/../if/"
-rsync -r --delete --exclude BUCK ${PB_SOURCE_DIR} ${PB_DEST_DIR}
+rsync -r --exclude BUCK ${PB_SOURCE_DIR} ${PB_DEST_DIR}
