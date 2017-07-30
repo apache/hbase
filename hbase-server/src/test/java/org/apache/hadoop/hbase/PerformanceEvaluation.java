d666ffc7ad014f21b32cd5d5f7666506aede30b7
Guanghao Zhang <zghao@apache.org>
2017-07-30 18:58:47 +0800

    HBASE-18481 The autoFlush flag was not used in PE tool

--- hbase-server/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
@@ -1393,6 +1393,9 @@ public class PerformanceEvaluation extends Configured implements Tool {
       }
       put.setDurability(opts.writeToWAL ? Durability.SYNC_WAL : Durability.SKIP_WAL);
       mutator.mutate(put);
+      if (opts.autoFlush) {
+        mutator.flush();
+      }
     }
   }
 
@@ -1590,6 +1593,9 @@ public class PerformanceEvaluation extends Configured implements Tool {
       }
       put.setDurability(opts.writeToWAL ? Durability.SYNC_WAL : Durability.SKIP_WAL);
       mutator.mutate(put);
+      if (opts.autoFlush) {
+        mutator.flush();
+      }
     }
   }
 
