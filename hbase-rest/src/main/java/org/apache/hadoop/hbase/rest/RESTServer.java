4376233e2a7456da752ed1a88ad091163b7dc536
Chia-Ping Tsai <chia7712@gmail.com>
2017-08-08 10:47:36 +0800

    HBASE-18315 Eliminate the findbugs warnings for hbase-rest

--- hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RESTServer.java
+++ hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RESTServer.java
@@ -125,9 +125,6 @@ public class RESTServer implements Constants {
   static void addCSRFFilter(ServletContextHandler ctxHandler, Configuration conf) {
     restCSRFEnabled = conf.getBoolean(REST_CSRF_ENABLED_KEY, REST_CSRF_ENABLED_DEFAULT);
     if (restCSRFEnabled) {
-      Set<String> restCsrfMethodsToIgnore = new HashSet<>();
-      restCsrfMethodsToIgnore.addAll(getTrimmedStringList(conf,
-          REST_CSRF_METHODS_TO_IGNORE_KEY, REST_CSRF_METHODS_TO_IGNORE_DEFAULT));
       Map<String, String> restCsrfParams = RestCsrfPreventionFilter
           .getFilterParams(conf, "hbase.rest-csrf.");
       FilterHolder holder = new FilterHolder();
