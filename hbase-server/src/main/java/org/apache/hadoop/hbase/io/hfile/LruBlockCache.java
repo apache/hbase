28909b8f13a73a0c318a33e8b4641ecdffc71692
Biju Nair <gs.biju@gmail.com>
2017-08-18 16:13:13 -0400

    HBASE-18614 Setting BUCKET_CACHE_COMBINED_KEY to false disables stats on RS UI
    Signed-off-by: tedyu <yuzhihong@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/LruBlockCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/LruBlockCache.java
@@ -45,6 +45,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.ClassSize;
 import org.apache.hadoop.hbase.util.HasThread;
 import org.apache.hadoop.util.StringUtils;
+import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 import org.apache.hadoop.hbase.shaded.com.google.common.annotations.VisibleForTesting;
@@ -1161,7 +1162,10 @@ public class LruBlockCache implements ResizableBlockCache, HeapSize {
   }
 
   @Override
+  @JsonIgnore
   public BlockCache[] getBlockCaches() {
+    if (victimHandler != null)
+      return new BlockCache[] {this, this.victimHandler};
     return null;
   }
 
