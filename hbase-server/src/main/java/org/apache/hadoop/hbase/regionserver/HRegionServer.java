338232f81f0c95b8c0ca5ab0091260b62a0b3b3e
Michael Stack <stack@apache.org>
2017-08-22 08:31:55 -0700

    Revert "HBASE-14498 Master stuck in infinite loop when all Zookeeper servers are unreachable" Lots of tests timing out. Revert for now.
    This reverts commit 7a96682643ebfe1477a6c628359aba3e57ef1c9e.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -606,8 +606,8 @@ public class HRegionServer extends HasThread implements
     // Some unit tests don't need a cluster, so no zookeeper at all
     if (!conf.getBoolean("hbase.testing.nocluster", false)) {
       // Open connection to zookeeper and set primary watcher
-      zooKeeper = new ZooKeeperWatcher(conf, getProcessName() + ":" + rpcServices.isa.getPort(),
-          this, canCreateBaseZNode(), true);
+      zooKeeper = new ZooKeeperWatcher(conf, getProcessName() + ":" +
+        rpcServices.isa.getPort(), this, canCreateBaseZNode());
 
       this.csm = (BaseCoordinatedStateManager) csm;
       this.csm.initialize(this);
