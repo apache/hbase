778f507ada6824e6f388f1500dd56787183b1e23
Peter Somogyi <psomogyi@cloudera.com>
2017-08-25 13:34:03 +0200

    HBASE-18683 Upgrade hbase to commons-math 3
    Upgrade commons-math:2.2 to commons-math3:3.6.1
    Remove commons-math 2 specific content from LICENSE.vm
    Add missing jersey-client dependency to hbase-it module
    
    Signed-off-by: Michael Stack <stack@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/HFilePerformanceEvaluation.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/HFilePerformanceEvaluation.java
@@ -24,8 +24,8 @@ import java.util.Random;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.math.random.RandomData;
-import org.apache.commons.math.random.RandomDataImpl;
+import org.apache.commons.math3.random.RandomData;
+import org.apache.commons.math3.random.RandomDataImpl;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
