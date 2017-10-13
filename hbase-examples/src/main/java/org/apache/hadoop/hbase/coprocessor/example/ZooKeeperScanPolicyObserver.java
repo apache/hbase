104595137eec5cac9756eb12ed56759a001ea337
zhangduo <zhangduo@apache.org>
2017-10-13 18:18:39 +0800

    HBASE-18747 Introduce new example and helper classes to tell CP users how to do filtering on scanners

--- hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/ZooKeeperScanPolicyObserver.java
+++ hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/ZooKeeperScanPolicyObserver.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright The Apache Software Foundation
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -20,54 +20,40 @@ package org.apache.hadoop.hbase.coprocessor.example;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.NavigableSet;
 import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.OptionalLong;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.retry.RetryForever;
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CoprocessorEnvironment;
-import org.apache.hadoop.hbase.HConstants;
-import org.apache.hadoop.hbase.client.IsolationLevel;
-import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessor;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.RegionObserver;
-import org.apache.hadoop.hbase.regionserver.HStore;
 import org.apache.hadoop.hbase.regionserver.InternalScanner;
-import org.apache.hadoop.hbase.regionserver.KeyValueScanner;
-import org.apache.hadoop.hbase.regionserver.ScanInfo;
 import org.apache.hadoop.hbase.regionserver.ScanType;
+import org.apache.hadoop.hbase.regionserver.ScannerContext;
 import org.apache.hadoop.hbase.regionserver.Store;
-import org.apache.hadoop.hbase.regionserver.StoreScanner;
 import org.apache.hadoop.hbase.regionserver.compactions.CompactionLifeCycleTracker;
 import org.apache.hadoop.hbase.regionserver.compactions.CompactionRequest;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
 
 /**
- * This is an example showing how a RegionObserver could configured
- * via ZooKeeper in order to control a Region compaction, flush, and scan policy.
- *
- * This also demonstrated the use of shared
- * {@link org.apache.hadoop.hbase.coprocessor.RegionObserver} state.
- * See {@link RegionCoprocessorEnvironment#getSharedData()}.
- *
- * This would be useful for an incremental backup tool, which would indicate the last
- * time of a successful backup via ZK and instruct HBase to not delete data that was
- * inserted since (based on wall clock time).
- *
- * This implements org.apache.zookeeper.Watcher directly instead of using
- * {@link org.apache.hadoop.hbase.zookeeper.ZooKeeperWatcher},
- * because RegionObservers come and go and currently
- * listeners registered with ZooKeeperWatcher cannot be removed.
+ * This is an example showing how a RegionObserver could configured via ZooKeeper in order to
+ * control a Region compaction, flush, and scan policy. This also demonstrated the use of shared
+ * {@link org.apache.hadoop.hbase.coprocessor.RegionObserver} state. See
+ * {@link RegionCoprocessorEnvironment#getSharedData()}.
+ * <p>
+ * This would be useful for an incremental backup tool, which would indicate the last time of a
+ * successful backup via ZK and instruct HBase that to safely delete the data which has already been
+ * backup.
  */
 public class ZooKeeperScanPolicyObserver implements RegionCoprocessor, RegionObserver {
+
   @Override
   public Optional<RegionObserver> getRegionObserver() {
     return Optional.of(this);
@@ -78,178 +64,138 @@ public class ZooKeeperScanPolicyObserver implements RegionCoprocessor, RegionObs
   public static final String ZK_SESSION_TIMEOUT_KEY =
       "ZooKeeperScanPolicyObserver.zookeeper.session.timeout";
   public static final int ZK_SESSION_TIMEOUT_DEFAULT = 30 * 1000; // 30 secs
-  public static final String node = "/backup/example/lastbackup";
-  public static final String zkkey = "ZK";
-  private static final Log LOG = LogFactory.getLog(ZooKeeperScanPolicyObserver.class);
+  public static final String NODE = "/backup/example/lastbackup";
+  private static final String ZKKEY = "ZK";
 
-  private ZooKeeper zk = null;
+  private NodeCache cache;
 
   /**
    * Internal watcher that keep "data" up to date asynchronously.
    */
-  private static class ZKWatcher implements Watcher {
-    private byte[] data = null;
-    private ZooKeeper zk;
-    private volatile boolean needSetup = true;
-    private volatile long lastSetupTry = 0;
+  private static final class ZKDataHolder {
 
-    public ZKWatcher(ZooKeeper zk) {
-      this.zk = zk;
-      // trigger the listening
-      getData();
+    private final String ensemble;
+
+    private final int sessionTimeout;
+
+    private CuratorFramework client;
+
+    private NodeCache cache;
+
+    private int ref;
+
+    public ZKDataHolder(String ensemble, int sessionTimeout) {
+      this.ensemble = ensemble;
+      this.sessionTimeout = sessionTimeout;
     }
 
-    /**
-     * Get the maintained data. In case of any ZK exceptions this will retry
-     * establishing the connection (but not more than twice/minute).
-     *
-     * getData is on the critical path, so make sure it is fast unless there is
-     * a problem (network partion, ZK ensemble down, etc)
-     * Make sure at most one (unlucky) thread retries and other threads don't pile up
-     * while that threads tries to recreate the connection.
-     *
-     * @return the last know version of the data
-     */
-    @edu.umd.cs.findbugs.annotations.SuppressWarnings(value="REC_CATCH_EXCEPTION")
-    public byte[] getData() {
-      // try at most twice/minute
-      if (needSetup && EnvironmentEdgeManager.currentTime() > lastSetupTry + 30000) {
-        synchronized (this) {
-          // make sure only one thread tries to reconnect
-          if (needSetup) {
-            needSetup = false;
-          } else {
-            return data;
-          }
-        }
-        // do this without the lock held to avoid threads piling up on this lock,
-        // as it can take a while
+    private void create() throws Exception {
+      client =
+          CuratorFrameworkFactory.builder().connectString(ensemble).sessionTimeoutMs(sessionTimeout)
+              .retryPolicy(new RetryForever(1000)).canBeReadOnly(true).build();
+      client.start();
+      cache = new NodeCache(client, NODE);
+      cache.start(true);
+    }
+
+    private void close() {
+      if (cache != null) {
         try {
-          LOG.debug("Connecting to ZK");
-          // record this attempt
-          lastSetupTry = EnvironmentEdgeManager.currentTime();
-          if (zk.exists(node, false) != null) {
-            data = zk.getData(node, this, null);
-            LOG.debug("Read synchronously: "+(data == null ? "null" : Bytes.toLong(data)));
-          } else {
-            zk.exists(node, this);
-          }
-        } catch (Exception x) {
-          // try again if this fails
-          needSetup = true;
+          cache.close();
+        } catch (IOException e) {
+          // should not happen
+          throw new AssertionError(e);
+        }
+        cache = null;
+      }
+      if (client != null) {
+        client.close();
+        client = null;
+      }
+    }
+
+    public synchronized NodeCache acquire() throws Exception {
+      if (ref == 0) {
+        try {
+          create();
+        } catch (Exception e) {
+          close();
+          throw e;
         }
       }
-      return data;
+      ref++;
+      return cache;
     }
 
-    @Override
-    public void process(WatchedEvent event) {
-      switch (event.getType()) {
-        case NodeDataChanged:
-        case NodeCreated:
-          try {
-            // get data and re-watch
-            data = zk.getData(node, this, null);
-            LOG.debug("Read asynchronously: " + (data == null ? "null" : Bytes.toLong(data)));
-          } catch (InterruptedException ix) {
-          } catch (KeeperException kx) {
-            needSetup = true;
-          }
-          break;
-
-        case NodeDeleted:
-          try {
-            // just re-watch
-            zk.exists(node, this);
-            data = null;
-          } catch (InterruptedException ix) {
-          } catch (KeeperException kx) {
-            needSetup = true;
-          }
-          break;
-
-        default:
-          // ignore
+    public synchronized void release() {
+      ref--;
+      if (ref == 0) {
+        close();
       }
     }
   }
 
   @Override
-  public void start(CoprocessorEnvironment e) throws IOException {
-    RegionCoprocessorEnvironment re = (RegionCoprocessorEnvironment) e;
-    if (!re.getSharedData().containsKey(zkkey)) {
-      // there is a short race here
-      // in the worst case we create a watcher that will be notified once
-      String ensemble = re.getConfiguration().get(ZK_ENSEMBLE_KEY);
-      int sessionTimeout = re.getConfiguration().getInt(ZK_SESSION_TIMEOUT_KEY,
-          ZK_SESSION_TIMEOUT_DEFAULT);
-      this.zk = new ZooKeeper(ensemble, sessionTimeout, null);
-      re.getSharedData().putIfAbsent(zkkey, new ZKWatcher(zk));
+  public void start(CoprocessorEnvironment env) throws IOException {
+    RegionCoprocessorEnvironment renv = (RegionCoprocessorEnvironment) env;
+    try {
+      this.cache = ((ZKDataHolder) renv.getSharedData().computeIfAbsent(ZKKEY, k -> {
+        String ensemble = renv.getConfiguration().get(ZK_ENSEMBLE_KEY);
+        int sessionTimeout =
+            renv.getConfiguration().getInt(ZK_SESSION_TIMEOUT_KEY, ZK_SESSION_TIMEOUT_DEFAULT);
+        return new ZKDataHolder(ensemble, sessionTimeout);
+      })).acquire();
+    } catch (Exception e) {
+      throw new IOException(e);
     }
   }
 
   @Override
   public void stop(CoprocessorEnvironment env) throws IOException {
-    if (this.zk != null) {
-      try {
-        this.zk.close();
-      } catch (InterruptedException e) {
-        LOG.error("Excepion while closing the ZK connection!", e);
-      }
-    }
+    RegionCoprocessorEnvironment renv = (RegionCoprocessorEnvironment) env;
+    this.cache = null;
+    ((ZKDataHolder) renv.getSharedData().get(ZKKEY)).release();
   }
 
-  protected ScanInfo getScanInfo(Store store, RegionCoprocessorEnvironment e) {
-    byte[] data = ((ZKWatcher) e.getSharedData().get(zkkey)).getData();
+  private OptionalLong getExpireBefore() {
+    ChildData data = cache.getCurrentData();
     if (data == null) {
-      return null;
+      return OptionalLong.empty();
     }
-    ScanInfo oldSI = ((HStore) store).getScanInfo();
-    if (oldSI.getTtl() == Long.MAX_VALUE) {
-      return null;
+    byte[] bytes = data.getData();
+    if (bytes == null || bytes.length != Long.BYTES) {
+      return OptionalLong.empty();
     }
-    long ttl = Math.max(EnvironmentEdgeManager.currentTime() - Bytes.toLong(data), oldSI.getTtl());
-    return new ScanInfo(oldSI.getConfiguration(), store.getColumnFamilyDescriptor(), ttl,
-        oldSI.getTimeToPurgeDeletes(), oldSI.getComparator());
+    return OptionalLong.of(Bytes.toLong(bytes));
+  }
+
+  private InternalScanner wrap(InternalScanner scanner) {
+    OptionalLong optExpireBefore = getExpireBefore();
+    if (!optExpireBefore.isPresent()) {
+      return scanner;
+    }
+    long expireBefore = optExpireBefore.getAsLong();
+    return new DelegatingInternalScanner(scanner) {
+
+      @Override
+      public boolean next(List<Cell> result, ScannerContext scannerContext) throws IOException {
+        boolean moreRows = scanner.next(result, scannerContext);
+        result.removeIf(c -> c.getTimestamp() < expireBefore);
+        return moreRows;
+      }
+    };
   }
 
   @Override
-  public InternalScanner preFlushScannerOpen(ObserverContext<RegionCoprocessorEnvironment> c,
-      Store store, List<KeyValueScanner> scanners, InternalScanner s, long readPoint)
-      throws IOException {
-    ScanInfo scanInfo = getScanInfo(store, c.getEnvironment());
-    if (scanInfo == null) {
-      // take default action
-      return null;
-    }
-    return new StoreScanner((HStore) store, scanInfo, OptionalInt.empty(), scanners,
-        ScanType.COMPACT_RETAIN_DELETES, store.getSmallestReadPoint(), HConstants.OLDEST_TIMESTAMP);
+  public InternalScanner preFlush(ObserverContext<RegionCoprocessorEnvironment> c, Store store,
+      InternalScanner scanner) throws IOException {
+    return wrap(scanner);
   }
 
   @Override
-  public InternalScanner preCompactScannerOpen(ObserverContext<RegionCoprocessorEnvironment> c,
-      Store store, List<? extends KeyValueScanner> scanners, ScanType scanType, long earliestPutTs,
-      InternalScanner s, CompactionLifeCycleTracker tracker, CompactionRequest request,
-      long readPoint) throws IOException {
-    ScanInfo scanInfo = getScanInfo(store, c.getEnvironment());
-    if (scanInfo == null) {
-      // take default action
-      return null;
-    }
-    return new StoreScanner((HStore) store, scanInfo, OptionalInt.empty(), scanners, scanType,
-        store.getSmallestReadPoint(), earliestPutTs);
-  }
-
-  @Override
-  public KeyValueScanner preStoreScannerOpen(ObserverContext<RegionCoprocessorEnvironment> c,
-      Store store, Scan scan, NavigableSet<byte[]> targetCols, KeyValueScanner s, long readPoint)
-      throws IOException {
-    ScanInfo scanInfo = getScanInfo(store, c.getEnvironment());
-    if (scanInfo == null) {
-      // take default action
-      return null;
-    }
-    return new StoreScanner((HStore) store, scanInfo, scan, targetCols,
-        ((HStore) store).getHRegion().getReadPoint(IsolationLevel.READ_COMMITTED));
+  public InternalScanner preCompact(ObserverContext<RegionCoprocessorEnvironment> c, Store store,
+      InternalScanner scanner, ScanType scanType, CompactionLifeCycleTracker tracker,
+      CompactionRequest request) throws IOException {
+    return wrap(scanner);
   }
 }
