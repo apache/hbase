ddb9c49324da71c7ec1dfbc65a23356f4fc82a30
Chia-Ping Tsai <chia7712@gmail.com>
2017-10-23 10:43:18 +0800

    HBASE-18690 Replace o.a.h.c.InterfaceAudience by o.a.h.h.c.InterfaceAudience

--- hbase-hadoop2-compat/src/main/java/org/apache/hadoop/hbase/metrics/MetricsInfoImpl.java
+++ hbase-hadoop2-compat/src/main/java/org/apache/hadoop/hbase/metrics/MetricsInfoImpl.java
@@ -21,7 +21,7 @@ package org.apache.hadoop.hbase.metrics;
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 
-import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.hbase.classification.InterfaceAudience;
 import org.apache.hadoop.metrics2.MetricsInfo;
 
 /**
