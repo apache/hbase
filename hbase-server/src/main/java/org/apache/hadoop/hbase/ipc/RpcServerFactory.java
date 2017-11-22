e048b2cbdb6586340b4bf855f1a1bd4963e33f3a
Michael Stack <stack@apache.org>
2017-11-21 20:16:55 -0800

    HBASE-19323 Make netty engine default in hbase2

--- hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServerFactory.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServerFactory.java
@@ -48,7 +48,7 @@ public class RpcServerFactory {
       final InetSocketAddress bindAddress, Configuration conf,
       RpcScheduler scheduler) throws IOException {
     String rpcServerClass = conf.get(CUSTOM_RPC_SERVER_IMPL_CONF_KEY,
-        SimpleRpcServer.class.getName());
+        NettyRpcServer.class.getName());
     StringBuffer servicesList = new StringBuffer();
     for (BlockingServiceAndInterface s: services) {
       ServiceDescriptor sd = s.getBlockingService().getDescriptorForType();
