b4a4be65ea658f02fdaa2e99532c9b8fecd521cc
Guangxu Cheng <guangxucheng@gmail.com>
2017-11-29 14:43:17 +0800

    HBASE-19362 Remove unused imports from hbase-thrift module
    Signed-off-by: Jan Hentschel <jan.hentschel@ultratendency.com>

--- hbase-thrift/src/test/java/org/apache/hadoop/hbase/thrift/TestCallQueue.java
+++ hbase-thrift/src/test/java/org/apache/hadoop/hbase/thrift/TestCallQueue.java
@@ -30,7 +30,6 @@ import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.test.MetricsAssertHelper;
 import org.apache.hadoop.hbase.testclassification.ClientTests;
 import org.apache.hadoop.hbase.testclassification.SmallTests;
-import org.apache.hadoop.hbase.thrift.CallQueue.Call;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
