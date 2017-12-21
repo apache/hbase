bcaf2fd20c542137c1d11dd32f2702af93d5ce70
Michael Stack <stack@apache.org>
2017-12-21 14:13:23 -0800

    HBASE-19148 Reevaluate default values of configurations; minor ADDENDUM

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -1399,7 +1399,7 @@ public class HMaster extends HRegionServer implements MasterServices {
         }
       }
 
-      boolean isByTable = getConfiguration().getBoolean("hbase.master.loadbalance.bytable", true);
+      boolean isByTable = getConfiguration().getBoolean("hbase.master.loadbalance.bytable", false);
       Map<TableName, Map<ServerName, List<RegionInfo>>> assignmentsByTable =
         this.assignmentManager.getRegionStates().getAssignmentsByTable(!isByTable);
 
