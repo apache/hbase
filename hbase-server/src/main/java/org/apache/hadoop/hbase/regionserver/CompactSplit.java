0e7fff58292122512a4aa444a53c71608e9a5bce
Chia-Ping Tsai <chia7712@gmail.com>
2017-12-27 09:33:53 +0800

    HBASE-19624 TestIOFencing hangs

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CompactSplit.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CompactSplit.java
@@ -397,6 +397,7 @@ public class CompactSplit implements CompactionRequester, PropagatingConfigurati
         }
       } catch (InterruptedException ie) {
         LOG.warn("Interrupted waiting for " + name + " to finish...");
+        t.shutdownNow();
       }
     }
   }
