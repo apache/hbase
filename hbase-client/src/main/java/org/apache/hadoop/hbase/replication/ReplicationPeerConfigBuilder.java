8118d5de4e842ff02e38b66643d07518ff48e38d
Josh Elser <elserj@apache.org>
2018-01-08 16:47:18 -0500

    HBASE-19734 Fix IntegrationTestReplication and related impl changes
    Adds (client-side) validation to ReplicationPeerConfigBuilder and javadoc
    to builder methods in addition to the test fix.
    
    Signed-off-by: Guanghao Zhang <zghao@apache.org>
    Signed-off-by: Michael Stack <stack@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/replication/ReplicationPeerConfigBuilder.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/replication/ReplicationPeerConfigBuilder.java
@@ -43,32 +43,103 @@ public interface ReplicationPeerConfigBuilder {
    */
   ReplicationPeerConfigBuilder setReplicationEndpointImpl(String replicationEndpointImpl);
 
+  /**
+   * Sets a "raw" configuration property for this replication peer. For experts only.
+   * @param key Configuration property key
+   * @param value Configuration property value
+   * @return {@code this}
+   */
+  @InterfaceAudience.Private
   ReplicationPeerConfigBuilder putConfiguration(String key, String value);
 
+  /**
+   * Adds all of the provided "raw" configuration entries to {@code this}.
+   * @param configuration A collection of raw configuration entries
+   * @return {@code this}
+   */
+  @InterfaceAudience.Private
   default ReplicationPeerConfigBuilder putAllConfiguration(Map<String, String> configuration) {
     configuration.forEach(this::putConfiguration);
     return this;
   }
 
+  /**
+   * Sets the serialized peer configuration data
+   * @return {@code this}
+   */
+  @InterfaceAudience.Private
   ReplicationPeerConfigBuilder putPeerData(byte[] key, byte[] value);
 
+  /**
+   * Sets all of the provided serialized peer configuration data.
+   * @return {@code this}
+   */
+  @InterfaceAudience.Private
   default ReplicationPeerConfigBuilder putAllPeerData(Map<byte[], byte[]> peerData) {
     peerData.forEach(this::putPeerData);
     return this;
   }
 
+  /**
+   * Sets an explicit map of tables and column families in those tables that should be replicated
+   * to the given peer. Use {@link #setReplicateAllUserTables(boolean)} to replicate all tables
+   * to a peer.
+   *
+   * @param tableCFsMap A map from tableName to column family names. An empty collection can be
+   *    passed to indicate replicating all column families.
+   * @return {@code this}
+   * @see #setReplicateAllUserTables(boolean)
+   */
   ReplicationPeerConfigBuilder
       setTableCFsMap(Map<TableName, List<String>> tableCFsMap);
 
+  /**
+   * Sets a unique collection of HBase namespaces that should be replicated to this peer.
+   * @param namespaces A set of namespaces to be replicated to this peer.
+   * @return {@code this}
+   */
   ReplicationPeerConfigBuilder setNamespaces(Set<String> namespaces);
 
+  /**
+   * Sets the speed, in bytes per second, for any one RegionServer to replicate data to the peer.
+   * @param bandwidth Bytes per second
+   * @return {@code this}.
+   */
   ReplicationPeerConfigBuilder setBandwidth(long bandwidth);
 
+  /**
+   * Configures HBase to replicate all user tables (not system tables) to the peer. Default is
+   * {@code true}.
+   * @param replicateAllUserTables True if all user tables should be replicated, else false.
+   * @return {@code this}
+   */
   ReplicationPeerConfigBuilder setReplicateAllUserTables(boolean replicateAllUserTables);
 
+  /**
+   * Sets the mapping of table name to column families which should not be replicated. This
+   * method sets state which is mutually exclusive to {@link #setTableCFsMap(Map)}. Invoking this
+   * method is only relevant when all user tables are being replicated.
+   *
+   * @param tableCFsMap A mapping of table names to column families which should not be
+   *    replicated. An empty list of column families implies all families for the table.
+   * @return {@code this}.
+   */
   ReplicationPeerConfigBuilder setExcludeTableCFsMap(Map<TableName, List<String>> tableCFsMap);
 
+  /**
+   * Sets the collection of namespaces which should not be replicated when all user tables are
+   * configured to be replicated. This method sets state which is mutually exclusive to
+   * {@link #setNamespaces(Set)}. Invoking this method is only relevant when all user tables are
+   * being replicated.
+   *
+   * @param namespaces A set of namespaces whose tables should not be replicated.
+   * @return {@code this}
+   */
   ReplicationPeerConfigBuilder setExcludeNamespaces(Set<String> namespaces);
 
+  /**
+   * Builds the configuration object from the current state of {@code this}.
+   * @return A {@link ReplicationPeerConfig} instance.
+   */
   ReplicationPeerConfig build();
 }
