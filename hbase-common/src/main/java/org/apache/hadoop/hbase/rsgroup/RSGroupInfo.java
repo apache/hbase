ee4f0c506ad583578bd76e7919f2264d7485b48d
Xiang Li <waterlx@gmail.com>
2018-01-09 00:42:52 +0800

    HBASE-19702 Improve RSGroupInfo constructors
    Signed-off-by: Chia-Ping Tsai <chia7712@gmail.com>

--- hbase-common/src/main/java/org/apache/hadoop/hbase/rsgroup/RSGroupInfo.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/rsgroup/RSGroupInfo.java
@@ -47,13 +47,12 @@ public class RSGroupInfo {
 
   RSGroupInfo(String name, SortedSet<Address> servers, SortedSet<TableName> tables) {
     this.name = name;
-    this.servers = servers == null? new TreeSet<>(): servers;
-    this.servers.addAll(servers);
-    this.tables = new TreeSet<>(tables);
+    this.servers = (servers == null) ? new TreeSet<>() : new TreeSet<>(servers);
+    this.tables  = (tables  == null) ? new TreeSet<>() : new TreeSet<>(tables);
   }
 
   public RSGroupInfo(RSGroupInfo src) {
-    this(src.getName(), src.servers, src.tables);
+    this(src.name, src.servers, src.tables);
   }
 
   /**
