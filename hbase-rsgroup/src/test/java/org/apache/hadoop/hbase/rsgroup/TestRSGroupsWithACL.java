f0d0501d556efb8796138d4aa92ce4f276c93c3b
Guangxu Cheng <guangxucheng@gmail.com>
2018-01-10 15:26:48 +0800

    HBASE-19483 Add proper privilege check for rsgroup commands addendum fixes unit test
    Signed-off-by: tedyu <yuzhihong@gmail.com>

--- hbase-rsgroup/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsWithACL.java
+++ hbase-rsgroup/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsWithACL.java
@@ -166,9 +166,9 @@ public class TestRSGroupsWithACL extends SecureTestUtil{
     grantGlobal(TEST_UTIL, toGroupEntry(GROUP_READ), Permission.Action.READ);
     grantGlobal(TEST_UTIL, toGroupEntry(GROUP_WRITE), Permission.Action.WRITE);
 
-    assertEquals(5, AccessControlLists.getTablePermissions(conf, TEST_TABLE).size());
+    assertEquals(4, AccessControlLists.getTablePermissions(conf, TEST_TABLE).size());
     try {
-      assertEquals(5, AccessControlClient.getUserPermissions(systemUserConnection,
+      assertEquals(4, AccessControlClient.getUserPermissions(systemUserConnection,
           TEST_TABLE.toString()).size());
     } catch (Throwable e) {
       LOG.error("error during call of AccessControlClient.getUserPermissions. ", e);
