7224546b1ed99faf41ecef82043971c7d44a5836
Josh Elser <elserj@apache.org>
2018-01-11 14:47:13 -0500

    HBASE-19770 Separate command return values from interactive shells
    Uses a new option to the shell to specify that return values are
    unwanted instead of overloading the interactive option. Enable
    return_values when the shell is non-interactive.
    
    Signed-off-by: Apekshit Sharma <appy@apache.org>

--- bin/hirb.rb
+++ bin/hirb.rb
@@ -59,12 +59,15 @@ Usage: shell [OPTIONS] [SCRIPTFILE [ARGUMENTS]]
  -n | --noninteractive          Do not run within an IRB session
                                 and exit with non-zero status on
                                 first error.
+ -r | --return-values           Include return values from commands
+                                executed in the shell.
 HERE
 found = []
 script2run = nil
 log_level = org.apache.log4j.Level::ERROR
 @shell_debug = false
 interactive = true
+return_values = false
 for arg in ARGV
   if arg == '-h' || arg == '--help'
     puts cmdline_help
@@ -77,6 +80,9 @@ for arg in ARGV
     puts 'Setting DEBUG log level...'
   elsif arg == '-n' || arg == '--noninteractive'
     interactive = false
+    found.push(arg)
+  elsif arg == '-r' || arg == '--return-values'
+    return_values = true
     found.push(arg)
   else
     # Presume it a script. Save it off for running later below
@@ -110,7 +116,7 @@ require 'shell/formatter'
 @hbase = Hbase::Hbase.new
 
 # Setup console
-@shell = Shell::Shell.new(@hbase, interactive)
+@shell = Shell::Shell.new(@hbase, interactive, return_values)
 @shell.debug = @shell_debug
 
 # Add commands to this namespace
