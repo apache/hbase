83bbad8bd3be703e6697b0d2fa1155da1b0c419f
Josh Elser <elserj@apache.org>
2018-01-11 14:47:13 -0500

    HBASE-19770 Separate command return values from interactive shells
    Uses a new option to the shell to specify that return values are
    unwanted instead of overloading the interactive option. Enable
    return_values when the shell is non-interactive.
    
    Includes addendum "Replace `if not` with `unless`"
    
    Signed-off-by: Apekshit Sharma <appy@apache.org>
    Signed-off-by: Mike Drob <mdrob@apache.org>

--- hbase-shell/src/test/ruby/shell/noninteractive_test.rb
+++ hbase-shell/src/test/ruby/shell/noninteractive_test.rb
@@ -20,7 +20,7 @@ require 'shell'
 class NonInteractiveTest < Test::Unit::TestCase
   def setup
     @hbase = ::Hbase::Hbase.new($TEST_CLUSTER.getConfiguration)
-    @shell = Shell::Shell.new(@hbase, false)
+    @shell = Shell::Shell.new(@hbase, false, true)
   end
 
   define_test "Shell::Shell noninteractive mode should throw" do
