b20b45e4e82ea8a04ffb15a039b0da49d9f775b2
Peter Somogyi <psomogyi@cloudera.com>
2018-01-24 11:23:29 +0100

    HBASE-19845 Fix findbugs and error-prone warnings in hbase-rsgroup
    Signed-off-by: tedyu <yuzhihong@gmail.com>

--- hbase-rsgroup/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsWithACL.java
+++ hbase-rsgroup/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsWithACL.java
@@ -22,6 +22,7 @@ package org.apache.hadoop.hbase.rsgroup;
 import static org.apache.hadoop.hbase.AuthUtil.toGroupEntry;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
@@ -172,6 +173,7 @@ public class TestRSGroupsWithACL extends SecureTestUtil{
           TEST_TABLE.toString()).size());
     } catch (Throwable e) {
       LOG.error("error during call of AccessControlClient.getUserPermissions. ", e);
+      fail("error during call of AccessControlClient.getUserPermissions.");
     }
   }
 
