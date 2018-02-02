cb7bfc21da4e2658602a9a2b2080c4d883287dd1
Michael Stack <stack@apache.org>
2018-02-01 20:26:46 -0800

    HBASE-19901 HBASE-19901 Up yetus proclimit on nightlies; REAPPLY TO TEST

--- dev-support/hbase_nightly_yetus.sh
+++ dev-support/hbase_nightly_yetus.sh
@@ -67,6 +67,8 @@ YETUS_ARGS=("--whitespace-tabs-ignore-list=${WHITESPACE_IGNORE_LIST}" "${YETUS_A
 YETUS_ARGS=("--sentinel" "${YETUS_ARGS[@]}")
 YETUS_ARGS=("--branch=${BRANCH_NAME}" "${YETUS_ARGS[@]}")
 YETUS_ARGS=("--tests-filter=${TESTS_FILTER}" "${YETUS_ARGS[@]}")
+YETUS_ARGS=("--proclimit=${PROCLIMIT}" "${YETUS_ARGS[@]}")
+YETUS_ARGS=("--dockermemlimit=${DOCKERMEMLIMIT}" "${YETUS_ARGS[@]}")
 
 # Currently, flaky list is calculated only for master branch.
 UNDERSCORED_BRANCH_NAME=$(echo ${BRANCH_NAME} | tr '.-' '_')
