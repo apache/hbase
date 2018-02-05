6d04aa179485f331deff328f511f9b494bdb4d43
Umesh Agashe <uagashe@cloudera.com>
2018-02-05 14:53:29 -0800

    HBASE-19915 (addendum): Fixed a typo because of which only daughterA was getting stored with a CLOSED state and not daughterB

--- hbase-client/src/main/java/org/apache/hadoop/hbase/MetaTableAccessor.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/MetaTableAccessor.java
@@ -1759,7 +1759,7 @@ public class MetaTableAccessor {
       // master tries to assign these offline regions. This is followed by re-assignments of the
       // daughter regions from resumed {@link SplitTableRegionProcedure}
       addRegionStateToPut(putA, RegionState.State.CLOSED);
-      addRegionStateToPut(putA, RegionState.State.CLOSED);
+      addRegionStateToPut(putB, RegionState.State.CLOSED);
 
       addSequenceNum(putA, 1, -1, splitA.getReplicaId()); //new regions, openSeqNum = 1 is fine.
       addSequenceNum(putB, 1, -1, splitB.getReplicaId());
