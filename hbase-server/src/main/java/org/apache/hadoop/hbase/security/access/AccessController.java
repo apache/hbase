6b8439366134b6749895b6019bc068d169865b48
Apekshit Sharma <appy@apache.org>
2018-02-13 12:33:43 -0800

    HBASE-19400 Add missing security checks in MasterRpcServices
    - Added ADMIN permission check for following rpc calls:
      normalize, setNormalizerRunning, runCatalogScan, enableCatalogJanitor, runCleanerChore,
      setCleanerChoreRunning, execMasterService, execProcedure, execProcedureWithRet
    - Moved authorizationEnabled check to start of AccessChecker's functions. Currently, and IDK why,
      we call authManager.authorize() first and then discard its result if authorizationEnabled is false. Weird.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/AccessController.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/AccessController.java
@@ -795,13 +795,14 @@ public class AccessController implements MasterCoprocessor, RegionCoprocessor,
 
     // set the user-provider.
     this.userProvider = UserProvider.instantiate(env.getConfiguration());
+    // Throws RuntimeException if fails to load TableAuthManager so that coprocessor is unloaded.
     accessChecker = new AccessChecker(env.getConfiguration(), zk);
     tableAcls = new MapMaker().weakValues().makeMap();
   }
 
   @Override
   public void stop(CoprocessorEnvironment env) {
-    TableAuthManager.release(getAuthManager());
+    accessChecker.stop();
   }
 
   /*********************************** Observer/Service Getters ***********************************/
@@ -2456,7 +2457,7 @@ public class AccessController implements MasterCoprocessor, RegionCoprocessor,
       throws IOException {
     requirePermission(ctx, "replicateLogEntries", Action.WRITE);
   }
-  
+
   @Override
   public void  preClearCompactionQueues(ObserverContext<RegionServerCoprocessorEnvironment> ctx)
           throws IOException {
