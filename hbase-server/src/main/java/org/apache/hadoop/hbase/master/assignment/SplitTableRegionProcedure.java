07bdd492f443fddba61707e2af24650b01d5e76e
Michael Stack <stack@apache.org>
2018-02-16 23:21:56 -0800

    Revert "HBASE-20015 TestMergeTableRegionsProcedure and TestRegionMergeTransactionOnCluster flakey"
    This reverts commit 72193650dde2225e85d7bdb74b24ad39367145da.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/SplitTableRegionProcedure.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/SplitTableRegionProcedure.java
@@ -270,7 +270,6 @@ public class SplitTableRegionProcedure
       } else {
         LOG.error(msg, e);
         setFailure(e);
-        setFailure("master-split-regions", e);
       }
     }
     // if split fails,  need to call ((HRegion)parent).clearSplit() when it is a force split
