a2bf2ad71eb58bd6708f62c6defbe82d17fb1166
Chia-Ping Tsai <chia7712@gmail.com>
2018-03-01 23:43:40 +0800

    HBASE-20093 (addendum) remove unused import of ServerLoad
    Signed-off-by: tedyu <yuzhihong@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -70,7 +70,6 @@ import org.apache.hadoop.hbase.MasterNotRunningException;
 import org.apache.hadoop.hbase.MetaTableAccessor;
 import org.apache.hadoop.hbase.NamespaceDescriptor;
 import org.apache.hadoop.hbase.PleaseHoldException;
-import org.apache.hadoop.hbase.ServerLoad;
 import org.apache.hadoop.hbase.ServerMetricsBuilder;
 import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableDescriptors;
@@ -1038,7 +1037,7 @@ public class HMaster extends HRegionServer implements MasterServices {
     for (ServerName sn: this.regionServerTracker.getOnlineServers()) {
       // The isServerOnline check is opportunistic, correctness is handled inside
       if (!this.serverManager.isServerOnline(sn) &&
-          serverManager.checkAndRecordNewServer(sn, new ServerLoad(ServerMetricsBuilder.of(sn)))) {
+          serverManager.checkAndRecordNewServer(sn, ServerMetricsBuilder.of(sn))) {
         LOG.info("Registered server found up in zk but who has not yet reported in: " + sn);
       }
     }
