118c1a1f2fce9318bfbff88ae911f4919373aac1
Gabor Bota <gabor.bota@cloudera.com>
2018-04-09 14:28:11 +0200

    HBASE-20253. Error message is missing for restore_snapshot
    Amending Author: Peter Somogyi <psomogyi@apache.org>

--- hbase-shell/src/main/ruby/shell/commands.rb
+++ hbase-shell/src/main/ruby/shell/commands.rb
@@ -119,6 +119,9 @@ module Shell
         if cause.is_a?(org.apache.hadoop.hbase.TableNotEnabledException)
           raise "Table #{args.first} is disabled!"
         end
+        if cause.is_a?(org.apache.hadoop.hbase.TableNotDisabledException)
+          raise "Table #{cause.message} should be disabled!"
+        end
         if cause.is_a?(org.apache.hadoop.hbase.UnknownRegionException)
           raise "Unknown region #{args.first}!"
         end
