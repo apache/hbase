a50d9f43512a5321a00e141d929072300cb41490
Mike Drob <mdrob@apache.org>
2018-04-11 21:25:04 -0500

    HBASE-20356 Make skipping protoc possible

--- dev-support/hbase-personality.sh
+++ dev-support/hbase-personality.sh
@@ -554,7 +554,7 @@ function hbaseprotoc_rebuild
   # Need to run 'install' instead of 'compile' because shading plugin
   # is hooked-up to 'install'; else hbase-protocol-shaded is left with
   # half of its process done.
-  modules_workers patch hbaseprotoc install -DskipTests -Pcompile-protobuf -X -DHBasePatchProcess
+  modules_workers patch hbaseprotoc install -DskipTests -X -DHBasePatchProcess
 
   # shellcheck disable=SC2153
   until [[ $i -eq "${#MODULE[@]}" ]]; do
