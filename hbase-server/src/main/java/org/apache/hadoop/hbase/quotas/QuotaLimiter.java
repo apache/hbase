f6dd195d031ff8bf3e9b9ea5fbdccc911a23a1b7
Huaxiang Sun <hsun@cloudera.com>
2018-04-26 10:43:59 -0700

    HBASE-19924 hbase rpc throttling does not work for multi() with request count rater.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/QuotaLimiter.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/QuotaLimiter.java
@@ -31,11 +31,13 @@ public interface QuotaLimiter {
   /**
    * Checks if it is possible to execute the specified operation.
    *
+   * @param writeReqs the write requests that will be checked against the available quota
    * @param estimateWriteSize the write size that will be checked against the available quota
+   * @param readReqs the read requests that will be checked against the available quota
    * @param estimateReadSize the read size that will be checked against the available quota
    * @throws ThrottlingException thrown if not enough avialable resources to perform operation.
    */
-  void checkQuota(long estimateWriteSize, long estimateReadSize)
+  void checkQuota(long writeReqs, long estimateWriteSize, long readReqs, long estimateReadSize)
     throws ThrottlingException;
 
   /**
@@ -43,10 +45,12 @@ public interface QuotaLimiter {
    * At this point the write and read amount will be an estimate,
    * that will be later adjusted with a consumeWrite()/consumeRead() call.
    *
+   * @param writeReqs the write requests that will be removed from the current quota
    * @param writeSize the write size that will be removed from the current quota
+   * @param readReqs the read requests that will be removed from the current quota
    * @param readSize the read size that will be removed from the current quota
    */
-  void grabQuota(long writeSize, long readSize);
+  void grabQuota(long writeReqs, long writeSize, long readReqs, long readSize);
 
   /**
    * Removes or add back some write amount to the quota.
--- hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/TimeBasedLimiter.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/TimeBasedLimiter.java
@@ -110,47 +110,50 @@ public class TimeBasedLimiter implements QuotaLimiter {
   }
 
   @Override
-  public void checkQuota(long writeSize, long readSize) throws ThrottlingException {
-    if (!reqsLimiter.canExecute()) {
+  public void checkQuota(long writeReqs, long estimateWriteSize, long readReqs,
+      long estimateReadSize) throws ThrottlingException {
+    if (!reqsLimiter.canExecute(writeReqs + readReqs)) {
       ThrottlingException.throwNumRequestsExceeded(reqsLimiter.waitInterval());
     }
-    if (!reqSizeLimiter.canExecute(writeSize + readSize)) {
-      ThrottlingException.throwRequestSizeExceeded(reqSizeLimiter
-          .waitInterval(writeSize + readSize));
+    if (!reqSizeLimiter.canExecute(estimateWriteSize + estimateReadSize)) {
+      ThrottlingException.throwRequestSizeExceeded(
+          reqSizeLimiter.waitInterval(estimateWriteSize + estimateReadSize));
     }
 
-    if (writeSize > 0) {
-      if (!writeReqsLimiter.canExecute()) {
+    if (estimateWriteSize > 0) {
+      if (!writeReqsLimiter.canExecute(writeReqs)) {
         ThrottlingException.throwNumWriteRequestsExceeded(writeReqsLimiter.waitInterval());
       }
-      if (!writeSizeLimiter.canExecute(writeSize)) {
-        ThrottlingException.throwWriteSizeExceeded(writeSizeLimiter.waitInterval(writeSize));
+      if (!writeSizeLimiter.canExecute(estimateWriteSize)) {
+        ThrottlingException.throwWriteSizeExceeded(
+            writeSizeLimiter.waitInterval(estimateWriteSize));
       }
     }
 
-    if (readSize > 0) {
-      if (!readReqsLimiter.canExecute()) {
+    if (estimateReadSize > 0) {
+      if (!readReqsLimiter.canExecute(readReqs)) {
         ThrottlingException.throwNumReadRequestsExceeded(readReqsLimiter.waitInterval());
       }
-      if (!readSizeLimiter.canExecute(readSize)) {
-        ThrottlingException.throwReadSizeExceeded(readSizeLimiter.waitInterval(readSize));
+      if (!readSizeLimiter.canExecute(estimateReadSize)) {
+        ThrottlingException.throwReadSizeExceeded(
+            readSizeLimiter.waitInterval(estimateReadSize));
       }
     }
   }
 
   @Override
-  public void grabQuota(long writeSize, long readSize) {
+  public void grabQuota(long writeReqs, long writeSize, long readReqs, long readSize) {
     assert writeSize != 0 || readSize != 0;
 
-    reqsLimiter.consume(1);
+    reqsLimiter.consume(writeReqs + readReqs);
     reqSizeLimiter.consume(writeSize + readSize);
 
     if (writeSize > 0) {
-      writeReqsLimiter.consume(1);
+      writeReqsLimiter.consume(writeReqs);
       writeSizeLimiter.consume(writeSize);
     }
     if (readSize > 0) {
-      readReqsLimiter.consume(1);
+      readReqsLimiter.consume(readReqs);
       readSizeLimiter.consume(readSize);
     }
   }
