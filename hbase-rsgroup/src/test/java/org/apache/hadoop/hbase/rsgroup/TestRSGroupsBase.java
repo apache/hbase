102f0bf9c565f70afd272cd5f2cb39305c3783f8
Yechao Chen <chenyechao@gmail.com>
2018-05-08 14:04:59 +0800

    HBASE-20500 [rsgroup] should keep at least one server in default group
    Signed-off-by: tedyu <yuzhihong@gmail.com>

--- hbase-rsgroup/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsBase.java
+++ hbase-rsgroup/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsBase.java
@@ -340,6 +340,53 @@ public abstract class TestRSGroupsBase {
   }
 
   @Test
+  public void testMoveServersFromDefaultGroup() throws Exception {
+    //create groups and assign servers
+    rsGroupAdmin.addRSGroup("foo");
+
+    RSGroupInfo fooGroup = rsGroupAdmin.getRSGroupInfo("foo");
+    assertEquals(0, fooGroup.getServers().size());
+    RSGroupInfo defaultGroup = rsGroupAdmin.getRSGroupInfo(RSGroupInfo.DEFAULT_GROUP);
+
+    //test remove all servers from default
+    try {
+      rsGroupAdmin.moveServers(defaultGroup.getServers(), fooGroup.getName());
+      fail(RSGroupAdminServer.KEEP_ONE_SERVER_IN_DEFAULT_ERROR_MESSAGE);
+    } catch (ConstraintException ex) {
+      assertTrue(ex.getMessage().contains(RSGroupAdminServer
+              .KEEP_ONE_SERVER_IN_DEFAULT_ERROR_MESSAGE));
+    }
+
+    //test success case, remove one server from default ,keep at least one server
+    if (defaultGroup.getServers().size() > 1) {
+      Address serverInDefaultGroup = defaultGroup.getServers().iterator().next();
+      LOG.info("moving server " + serverInDefaultGroup + " from group default to group " +
+              fooGroup.getName());
+      rsGroupAdmin.moveServers(Sets.newHashSet(serverInDefaultGroup), fooGroup.getName());
+    }
+
+    fooGroup = rsGroupAdmin.getRSGroupInfo("foo");
+    LOG.info("moving servers " + fooGroup.getServers() + " to group default");
+    rsGroupAdmin.moveServers(fooGroup.getServers(), RSGroupInfo.DEFAULT_GROUP);
+
+    TEST_UTIL.waitFor(WAIT_TIMEOUT, new Waiter.Predicate<Exception>() {
+      @Override
+      public boolean evaluate() throws Exception {
+        return getNumServers() ==
+                rsGroupAdmin.getRSGroupInfo(RSGroupInfo.DEFAULT_GROUP).getServers().size();
+      }
+    });
+
+    fooGroup = rsGroupAdmin.getRSGroupInfo("foo");
+    assertEquals(0, fooGroup.getServers().size());
+
+    //test group removal
+    LOG.info("Remove group " + fooGroup.getName());
+    rsGroupAdmin.removeRSGroup(fooGroup.getName());
+    Assert.assertEquals(null, rsGroupAdmin.getRSGroupInfo(fooGroup.getName()));
+  }
+
+  @Test
   public void testTableMoveTruncateAndDrop() throws Exception {
     final byte[] familyNameBytes = Bytes.toBytes("f");
     String newGroupName = getGroupName(name.getMethodName());
