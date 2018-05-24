d1cbd561dfda875acbc6cf5de96cd414d3fed90c
Thiruvel Thirumoolan <thiruvel@oath.com>
2018-05-24 01:01:54 -0700

    HBASE-20548 Master fails to startup on large clusters, refreshing block distribution
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

--- hbase-rsgroup/src/main/java/org/apache/hadoop/hbase/rsgroup/RSGroupBasedLoadBalancer.java
+++ hbase-rsgroup/src/main/java/org/apache/hadoop/hbase/rsgroup/RSGroupBasedLoadBalancer.java
@@ -468,4 +468,9 @@ public class RSGroupBasedLoadBalancer implements RSGroupableBalancer {
   public void setRsGroupInfoManager(RSGroupInfoManager rsGroupInfoManager) {
     this.rsGroupInfoManager = rsGroupInfoManager;
   }
+
+  @Override
+  public void postMasterStartupInitialize() {
+    this.internalBalancer.postMasterStartupInitialize();
+  }
 }
