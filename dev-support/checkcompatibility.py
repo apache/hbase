ec5a463a8548859ce8ca1223dacd5e1ad10ef19c
Mike Drob <mdrob@apache.org>
2018-06-12 13:23:13 -0500

    HBASE-19377 Update Java API CC version
    Compatibility checker complaining about hash collisions, newer versions
    use longer id strings.

--- dev-support/checkcompatibility.py
+++ dev-support/checkcompatibility.py
@@ -156,7 +156,7 @@ def checkout_java_acc(force):
 
     logging.info("Downloading Java ACC...")
 
-    url = "https://github.com/lvc/japi-compliance-checker/archive/2.1.tar.gz"
+    url = "https://github.com/lvc/japi-compliance-checker/archive/2.4.tar.gz"
     scratch_dir = get_scratch_dir()
     path = os.path.join(scratch_dir, os.path.basename(url))
     jacc = urllib2.urlopen(url)
@@ -166,7 +166,7 @@ def checkout_java_acc(force):
     subprocess.check_call(["tar", "xzf", path],
                           cwd=scratch_dir)
 
-    shutil.move(os.path.join(scratch_dir, "japi-compliance-checker-2.1"),
+    shutil.move(os.path.join(scratch_dir, "japi-compliance-checker-2.4"),
                 os.path.join(acc_dir))
 
 
@@ -266,6 +266,12 @@ def process_java_acc_output(output):
             return_value[line[:6]] = values
     return return_value
 
+def log_java_acc_version():
+    java_acc_path = os.path.join(
+        get_java_acc_dir(), "japi-compliance-checker.pl")
+
+    args = ["perl", java_acc_path, "-dumpversion"]
+    logging.info("Java ACC version: " + check_output(args))
 
 def run_java_acc(src_name, src_jars, dst_name, dst_jars, annotations, skip_annotations, name):
     """ Run the compliance checker to compare 'src' and 'dst'. """
@@ -479,6 +485,7 @@ def main():
 
     # Download deps.
     checkout_java_acc(args.force_download)
+    log_java_acc_version()
 
     # Set up the build.
     scratch_dir = get_scratch_dir()
