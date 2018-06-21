7b716c964bc7c692c1222812974ae75428d50e65
zhangduo <zhangduo@apache.org>
2018-06-21 10:14:57 +0800

    HBASE-20752 Make sure the regions are truly reopened after ReopenTableRegionsProcedure

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/procedure/ReopenTableRegionsProcedure.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/procedure/ReopenTableRegionsProcedure.java
@@ -18,7 +18,14 @@
 package org.apache.hadoop.hbase.master.procedure;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.hadoop.hbase.HBaseIOException;
+import org.apache.hadoop.hbase.HRegionLocation;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.master.RegionPlan;
+import org.apache.hadoop.hbase.master.assignment.MoveRegionProcedure;
 import org.apache.hadoop.hbase.procedure2.ProcedureStateSerializer;
 import org.apache.hadoop.hbase.procedure2.ProcedureSuspendedException;
 import org.apache.hadoop.hbase.procedure2.ProcedureYieldException;
@@ -31,8 +38,9 @@ import org.apache.hadoop.hbase.shaded.protobuf.generated.MasterProcedureProtos.R
 import org.apache.hadoop.hbase.shaded.protobuf.generated.MasterProcedureProtos.ReopenTableRegionsStateData;
 
 /**
- * Used for non table procedures to reopen the regions for a table. For example,
- * {@link org.apache.hadoop.hbase.master.replication.ModifyPeerProcedure}.
+ * Used for reopening the regions for a table.
+ * <p/>
+ * Currently we use {@link MoveRegionProcedure} to reopen regions.
  */
 @InterfaceAudience.Private
 public class ReopenTableRegionsProcedure
@@ -41,6 +49,8 @@ public class ReopenTableRegionsProcedure
   private static final Logger LOG = LoggerFactory.getLogger(ReopenTableRegionsProcedure.class);
 
   private TableName tableName;
+
+  private List<HRegionLocation> regions = Collections.emptyList();
 
   public ReopenTableRegionsProcedure() {
   }
@@ -59,19 +69,53 @@ public class ReopenTableRegionsProcedure
     return TableOperationType.REGION_EDIT;
   }
 
+  private MoveRegionProcedure createReopenProcedure(MasterProcedureEnv env, HRegionLocation loc) {
+    try {
+      return new MoveRegionProcedure(env,
+        new RegionPlan(loc.getRegion(), loc.getServerName(), loc.getServerName()), false);
+    } catch (HBaseIOException e) {
+      // we skip the checks so this should not happen
+      throw new AssertionError(e);
+    }
+  }
+
   @Override
   protected Flow executeFromState(MasterProcedureEnv env, ReopenTableRegionsState state)
       throws ProcedureSuspendedException, ProcedureYieldException, InterruptedException {
     switch (state) {
-      case REOPEN_TABLE_REGIONS_REOPEN_ALL_REGIONS:
-        try {
-          addChildProcedure(env.getAssignmentManager().createReopenProcedures(
-            env.getAssignmentManager().getRegionStates().getRegionsOfTable(tableName)));
-        } catch (IOException e) {
-          LOG.warn("Failed to schedule reopen procedures for {}", tableName, e);
-          throw new ProcedureSuspendedException();
+      case REOPEN_TABLE_REGIONS_GET_REGIONS:
+        if (!env.getAssignmentManager().isTableEnabled(tableName)) {
+          LOG.info("Table {} is disabled, give up reopening its regions");
+          return Flow.NO_MORE_STATE;
         }
-        return Flow.NO_MORE_STATE;
+        regions =
+          env.getAssignmentManager().getRegionStates().getRegionsOfTableForReopen(tableName);
+        setNextState(ReopenTableRegionsState.REOPEN_TABLE_REGIONS_REOPEN_REGIONS);
+        return Flow.HAS_MORE_STATE;
+      case REOPEN_TABLE_REGIONS_REOPEN_REGIONS:
+        addChildProcedure(regions.stream().filter(l -> l.getSeqNum() >= 0)
+          .map(l -> createReopenProcedure(env, l)).toArray(MoveRegionProcedure[]::new));
+        setNextState(ReopenTableRegionsState.REOPEN_TABLE_REGIONS_CONFIRM_REOPENED);
+        return Flow.HAS_MORE_STATE;
+      case REOPEN_TABLE_REGIONS_CONFIRM_REOPENED:
+        regions = regions.stream().map(env.getAssignmentManager().getRegionStates()::checkReopened)
+          .filter(l -> l != null).collect(Collectors.toList());
+        if (regions.isEmpty()) {
+          return Flow.NO_MORE_STATE;
+        }
+        if (regions.stream().anyMatch(l -> l.getSeqNum() >= 0)) {
+          setNextState(ReopenTableRegionsState.REOPEN_TABLE_REGIONS_REOPEN_REGIONS);
+          return Flow.HAS_MORE_STATE;
+        }
+        LOG.info("There are still {} region(s) which need to be reopened for table {} are in " +
+          "OPENING state, try again later", regions.size(), tableName);
+        // All the regions need to reopen are in OPENING state which means we can not schedule any
+        // MRPs. Then sleep for one second, and yield the procedure to let other procedures run
+        // first and hope next time we can get some regions in other state to make progress.
+        // TODO: add a delay for ProcedureYieldException so that we do not need to sleep here which
+        // blocks a procedure worker.
+        Thread.sleep(1000);
+        throw new ProcedureYieldException();
       default:
         throw new UnsupportedOperationException("unhandled state=" + state);
     }
@@ -95,20 +139,24 @@ public class ReopenTableRegionsProcedure
 
   @Override
   protected ReopenTableRegionsState getInitialState() {
-    return ReopenTableRegionsState.REOPEN_TABLE_REGIONS_REOPEN_ALL_REGIONS;
+    return ReopenTableRegionsState.REOPEN_TABLE_REGIONS_GET_REGIONS;
   }
 
   @Override
   protected void serializeStateData(ProcedureStateSerializer serializer) throws IOException {
     super.serializeStateData(serializer);
-    serializer.serialize(ReopenTableRegionsStateData.newBuilder()
-      .setTableName(ProtobufUtil.toProtoTableName(tableName)).build());
+    ReopenTableRegionsStateData.Builder builder = ReopenTableRegionsStateData.newBuilder()
+      .setTableName(ProtobufUtil.toProtoTableName(tableName));
+    regions.stream().map(ProtobufUtil::toRegionLocation).forEachOrdered(builder::addRegion);
+    serializer.serialize(builder.build());
   }
 
   @Override
   protected void deserializeStateData(ProcedureStateSerializer serializer) throws IOException {
     super.deserializeStateData(serializer);
-    tableName = ProtobufUtil
-      .toTableName(serializer.deserialize(ReopenTableRegionsStateData.class).getTableName());
+    ReopenTableRegionsStateData data = serializer.deserialize(ReopenTableRegionsStateData.class);
+    tableName = ProtobufUtil.toTableName(data.getTableName());
+    regions = data.getRegionList().stream().map(ProtobufUtil::toRegionLocation)
+      .collect(Collectors.toList());
   }
 }
