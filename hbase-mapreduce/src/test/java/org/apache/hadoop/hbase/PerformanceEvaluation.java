ace41d5b8093b38042132f7297acf85dba6a1b13
Michael Stack <stack@apache.org>
2018-06-25 12:04:47 -0700

    HBASE-20785 NPE getting metrics in PE testing scans

--- hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
+++ hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
@@ -1138,6 +1138,7 @@ public class PerformanceEvaluation extends Configured implements Tool {
     }
 
     void updateScanMetrics(final ScanMetrics metrics) {
+      if (metrics == null) return;
       Map<String,Long> metricsMap = metrics.getMetricsMap();
       Long rpcCalls = metricsMap.get(ScanMetrics.RPC_CALLS_METRIC_NAME);
       if (rpcCalls != null) {
