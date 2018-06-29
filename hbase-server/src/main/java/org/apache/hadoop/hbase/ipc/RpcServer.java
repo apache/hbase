154b77307fd702f4794213d1713a80516cd3c9bd
Josh Elser <elserj@apache.org>
2018-06-29 15:19:59 -0400

    HBASE-20826 Truncate really long RpcServer warnings unless TRACE is on
    Signed-off-by: zhangduo <zhangduo@apache.org>
    Signed-off-by: Ted Yu <tyu@apache.org>
    Signed-off-by: Michael Stack <stack@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServer.java
@@ -2441,7 +2441,14 @@ public class RpcServer implements RpcServerInterface, ConfigurationObserver {
     responseInfo.put("class", server == null? "": server.getClass().getSimpleName());
     responseInfo.put("method", methodName);
     responseInfo.put("call", call);
-    responseInfo.put("param", ProtobufUtil.getShortTextFormat(param));
+    // The params could be really big, make sure they don't kill us at WARN
+    String stringifiedParam = ProtobufUtil.getShortTextFormat(param);
+    if (stringifiedParam.length() > 150) {
+      // Truncate to 1000 chars if TRACE is on, else to 150 chars
+      stringifiedParam = stringifiedParam.subSequence(
+          0, LOG.isTraceEnabled() ? 1000 : 150) + " <TRUNCATED>";
+    }
+    responseInfo.put("param", stringifiedParam);
     if (param instanceof ClientProtos.ScanRequest && rsRpcServices != null) {
       ClientProtos.ScanRequest request = ((ClientProtos.ScanRequest) param);
       if (request.hasScannerId()) {
