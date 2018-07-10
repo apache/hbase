2355586d92cc33612790fcda94f333155007c26b
Zach York <zyork@apache.org>
2018-07-09 22:36:30 -0700

    HBASE-20868 Fix TestCheckTestClasses on HBASE-18477
    Signed-off-by: Ted Yu <tedyu@apache.org>

--- hbase-common/src/test/java/org/apache/hadoop/hbase/util/TestReadReplicaClustersTableNameUtil.java
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/util/TestReadReplicaClustersTableNameUtil.java
@@ -17,13 +17,23 @@
  */
 package org.apache.hadoop.hbase.util;
 
-import org.apache.hadoop.hbase.TableName;
-import org.junit.Test;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.hadoop.hbase.HBaseClassTestRule;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.testclassification.MiscTests;
+import org.apache.hadoop.hbase.testclassification.SmallTests;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category({MiscTests.class, SmallTests.class})
 public class TestReadReplicaClustersTableNameUtil {
+
+  @ClassRule
+  public static final HBaseClassTestRule CLASS_RULE =
+      HBaseClassTestRule.forClass(TestReadReplicaClustersTableNameUtil.class);
 
   @Test
   public void testIsMetaTableNameWithoutSuffix() {
