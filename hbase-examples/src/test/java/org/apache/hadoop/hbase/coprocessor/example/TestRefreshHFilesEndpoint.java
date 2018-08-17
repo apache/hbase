040c215bfd958d6329473af8b2a9bc4e11952de1
TAK LON WU <wutaklon@amazon.com>
2018-08-17 15:55:10 -0700

    HBASE-21318 Make RefreshHFilesClient runnable
    Other than when user enables hbase.coprocessor.region.classes with
    RefreshHFilesEndPoint, user can also run this client as tool runner
    class/CLI and calls refresh HFiles directly.
    
    Signed-off-by: tedyu <yuzhihong@gmail.com>

--- hbase-examples/src/test/java/org/apache/hadoop/hbase/coprocessor/example/TestRefreshHFilesEndpoint.java
+++ hbase-examples/src/test/java/org/apache/hadoop/hbase/coprocessor/example/TestRefreshHFilesEndpoint.java
@@ -27,92 +27,32 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
-import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.HConstants;
-import org.apache.hadoop.hbase.MiniHBaseCluster;
-import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptor;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptorBuilder;
 import org.apache.hadoop.hbase.client.RegionInfo;
 import org.apache.hadoop.hbase.client.RetriesExhaustedException;
-import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.apache.hadoop.hbase.client.example.RefreshHFilesClient;
-import org.apache.hadoop.hbase.coprocessor.CoprocessorHost;
-import org.apache.hadoop.hbase.master.MasterFileSystem;
 import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.hadoop.hbase.regionserver.HStore;
-import org.apache.hadoop.hbase.regionserver.Region;
 import org.apache.hadoop.hbase.regionserver.RegionServerServices;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
-import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.hbase.util.FSUtils;
-import org.apache.hadoop.hbase.util.HFileTestUtil;
 import org.apache.hadoop.hbase.wal.WAL;
-import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Category(MediumTests.class)
-public class TestRefreshHFilesEndpoint {
+public class TestRefreshHFilesEndpoint extends TestRefreshHFilesBase {
 
   @ClassRule
   public static final HBaseClassTestRule CLASS_RULE =
       HBaseClassTestRule.forClass(TestRefreshHFilesEndpoint.class);
 
-  private static final Logger LOG = LoggerFactory.getLogger(TestRefreshHFilesEndpoint.class);
-  private static final HBaseTestingUtility HTU = new HBaseTestingUtility();
-  private static final int NUM_RS = 2;
-  private static final TableName TABLE_NAME = TableName.valueOf("testRefreshRegionHFilesEP");
-  private static final byte[] FAMILY = Bytes.toBytes("family");
-  private static final byte[] QUALIFIER = Bytes.toBytes("qualifier");
-  private static final byte[][] SPLIT_KEY = new byte[][] { Bytes.toBytes("30") };
-  private static final int NUM_ROWS = 5;
-  private static final String HFILE_NAME = "123abcdef";
-
-  private static Configuration CONF = HTU.getConfiguration();
-  private static MiniHBaseCluster cluster;
-  private static Table table;
-
-  public static void setUp(String regionImpl) {
-    try {
-      CONF.set(HConstants.REGION_IMPL, regionImpl);
-      CONF.setInt(HConstants.HBASE_CLIENT_RETRIES_NUMBER, 2);
-
-      CONF.setStrings(CoprocessorHost.REGION_COPROCESSOR_CONF_KEY, RefreshHFilesEndpoint.class.getName());
-      cluster = HTU.startMiniCluster(NUM_RS);
-
-      // Create table
-      table = HTU.createTable(TABLE_NAME, FAMILY, SPLIT_KEY);
-
-      // this will create 2 regions spread across slaves
-      HTU.loadNumericRows(table, FAMILY, 1, 20);
-      HTU.flush(TABLE_NAME);
-    } catch (Exception ex) {
-      LOG.error("Couldn't finish setup", ex);
-    }
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    HTU.shutdownMiniCluster();
-  }
-
   @Test
   public void testRefreshRegionHFilesEndpoint() throws Exception {
     setUp(HRegion.class.getName());
-    MasterFileSystem mfs = HTU.getMiniHBaseCluster().getMaster().getMasterFileSystem();
-    Path tableDir = FSUtils.getTableDir(mfs.getRootDir(), TABLE_NAME);
-    for (Region region : cluster.getRegions(TABLE_NAME)) {
-      Path regionDir = new Path(tableDir, region.getRegionInfo().getEncodedName());
-      Path familyDir = new Path(regionDir, Bytes.toString(FAMILY));
-      HFileTestUtil
-        .createHFile(HTU.getConfiguration(), HTU.getTestFileSystem(), new Path(familyDir, HFILE_NAME), FAMILY,
-                     QUALIFIER, Bytes.toBytes("50"), Bytes.toBytes("60"), NUM_ROWS);
-    }
+    addHFilesToRegions();
     assertEquals(2, HTU.getNumHFiles(TABLE_NAME, FAMILY));
     callRefreshRegionHFilesEndPoint();
     assertEquals(4, HTU.getNumHFiles(TABLE_NAME, FAMILY));
