bd76de7e910555991b17d791f9e9aae23c3a125d
Toshihiro Suzuki <brfrn169@gmail.com>
2018-08-31 01:05:40 +0900

    HBASE-21104 client.TestRestoreSnapshotFromClientWithRegionReplicas failing on branch-1.3, branch-1.2

--- hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestRestoreSnapshotFromClientWithRegionReplicas.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestRestoreSnapshotFromClientWithRegionReplicas.java
@@ -17,7 +17,14 @@
  */
 package org.apache.hadoop.hbase.client;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.snapshot.SnapshotTestingUtils;
 import org.apache.hadoop.hbase.testclassification.LargeTests;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(LargeTests.class)
@@ -26,5 +33,24 @@ public class TestRestoreSnapshotFromClientWithRegionReplicas extends
   @Override
   protected int getNumReplicas() {
     return 3;
+  }
+
+  @Test
+  public void testOnlineSnapshotAfterSplittingRegions() throws IOException, InterruptedException {
+    List<HRegionInfo> regionInfos = admin.getTableRegions(tableName);
+    RegionReplicaUtil.removeNonDefaultRegions(regionInfos);
+
+    // Split a region
+    splitRegion(regionInfos.get(0));
+
+    // Take a online snapshot
+    admin.snapshot(snapshotName1, tableName);
+
+    // Clone the snapshot to another table
+    TableName clonedTableName = TableName.valueOf("testOnlineSnapshotAfterSplittingRegions-" +
+      System.currentTimeMillis());
+    admin.cloneSnapshot(snapshotName1, clonedTableName);
+
+    SnapshotTestingUtils.verifyRowCount(TEST_UTIL, clonedTableName, snapshot1Rows);
   }
 }
