79f4297373c144f018b90394337e49722e714290
zzr1000 <zhangzhongren2008@126.com>
2018-09-02 23:20:52 +0800

    minor programme mistake

--- conf/hbase-env.sh
+++ conf/hbase-env.sh
@@ -121,7 +121,7 @@ export HBASE_OPTS="-XX:+UseConcMarkSweepGC"
 # otherwise arrive faster than the master can service them.
 # export HBASE_SLAVE_SLEEP=0.1
 
-# Tell HBase whether it should manage it's own instance of ZooKeeper or not.
+# Tell HBase whether it should manage its own instance of ZooKeeper or not.
 # export HBASE_MANAGES_ZK=true
 
 # The default log rolling policy is RFA, where the log file is rolled as per the size defined for the 
