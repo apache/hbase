c8067aaa64fbdf176523b7701a913a4d752f01ff
xcang <xcang@salesforce.com>
2018-09-06 00:55:57 -0700

    HBASE-15728 (addendum) Add remaining per-table region / store / flush / compaction related metrics
    Signed-off-by: Guanghao Zhang <zghao@apache.org>

--- hbase-hadoop2-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsTableSourceImpl.java
+++ hbase-hadoop2-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsTableSourceImpl.java
@@ -367,19 +367,19 @@ public class MetricsTableSourceImpl implements MetricsTableSource {
   }
 
   @Override
-  public void updateFlushMemstoreSize(long bytes) {
+  public synchronized void updateFlushMemstoreSize(long bytes) {
     flushMemstoreSizeHisto.add(bytes);
     flushedMemstoreBytes.incr(bytes);
   }
 
   @Override
-  public void updateFlushOutputSize(long bytes) {
+  public synchronized void updateFlushOutputSize(long bytes) {
     flushOutputSizeHisto.add(bytes);
     flushedOutputBytes.incr(bytes);
   }
 
   @Override
-  public void updateCompactionTime(boolean isMajor, long t) {
+  public synchronized void updateCompactionTime(boolean isMajor, long t) {
     compactionTimeHisto.add(t);
     if (isMajor) {
       majorCompactionTimeHisto.add(t);
@@ -387,7 +387,7 @@ public class MetricsTableSourceImpl implements MetricsTableSource {
   }
 
   @Override
-  public void updateCompactionInputFileCount(boolean isMajor, long c) {
+  public synchronized void updateCompactionInputFileCount(boolean isMajor, long c) {
     compactionInputFileCountHisto.add(c);
     if (isMajor) {
       majorCompactionInputFileCountHisto.add(c);
@@ -395,7 +395,7 @@ public class MetricsTableSourceImpl implements MetricsTableSource {
   }
 
   @Override
-  public void updateCompactionInputSize(boolean isMajor, long bytes) {
+  public synchronized void updateCompactionInputSize(boolean isMajor, long bytes) {
     compactionInputSizeHisto.add(bytes);
     compactedInputBytes.incr(bytes);
     if (isMajor) {
@@ -405,7 +405,7 @@ public class MetricsTableSourceImpl implements MetricsTableSource {
   }
 
   @Override
-  public void updateCompactionOutputFileCount(boolean isMajor, long c) {
+  public synchronized void updateCompactionOutputFileCount(boolean isMajor, long c) {
     compactionOutputFileCountHisto.add(c);
     if (isMajor) {
       majorCompactionOutputFileCountHisto.add(c);
@@ -413,7 +413,7 @@ public class MetricsTableSourceImpl implements MetricsTableSource {
   }
 
   @Override
-  public void updateCompactionOutputSize(boolean isMajor, long bytes) {
+  public synchronized void updateCompactionOutputSize(boolean isMajor, long bytes) {
     compactionOutputSizeHisto.add(bytes);
     compactedOutputBytes.incr(bytes);
     if (isMajor) {
