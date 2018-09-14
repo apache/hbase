d0cd0168523f800e82c2cc83b08cded1810e8497
Nihal Jain <nihaljain.cs@gmail.com>
2018-09-15 00:19:04 +0530

    HBASE-21196 HTableMultiplexer clears the meta cache after every put operation
    Signed-off-by: Andrew Purtell <apurtell@apache.org>
    
    Conflicts:
    	hbase-client/src/main/java/org/apache/hadoop/hbase/client/AsyncRequestFutureImpl.java
    
    Amending-Author: Andrew Purtell <apurtell@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/AsyncProcess.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/AsyncProcess.java
@@ -1345,7 +1345,8 @@ class AsyncProcess {
       for (Map.Entry<byte[], List<Action<Row>>> regionEntry : multiAction.actions.entrySet()) {
         byte[] regionName = regionEntry.getKey();
         Throwable regionException = responses.getExceptions().get(regionName);
-        if (tableName == null && ClientExceptionsUtil.isMetaClearingException(regionException)) {
+        if (tableName == null && regionException != null &&
+              ClientExceptionsUtil.isMetaClearingException(regionException)) {
           // For multi-actions, we don't have a table name, but we want to make sure to clear the
           // cache in case there were location-related exceptions. We don't to clear the cache
           // for every possible exception that comes through, however.
