ba8a252167ea3f671446ab2d8c718368e6ff19b7
Allan Yang <allan163@apache.org>
2018-09-26 19:11:23 +0800

    HBASE-21212 Wrong flush time when update flush metric

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -2787,7 +2787,7 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
     status.setStatus(msg);
 
     if (rsServices != null && rsServices.getMetrics() != null) {
-      rsServices.getMetrics().updateFlush(time - startTime,
+      rsServices.getMetrics().updateFlush(time,
           mss.getDataSize(), flushedOutputFileSize);
     }
 
