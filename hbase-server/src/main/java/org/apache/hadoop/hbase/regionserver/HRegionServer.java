29d6eeb6e83d64c0bfe43d3a5c7dfa93a4e28c5c
Michael Stack <stack@apache.org>
2018-10-31 10:15:04 -0700

    HBASE-21322 Add a scheduleServerCrashProcedure() API to HbckService ADDENDUM

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -2859,7 +2859,7 @@ public class HRegionServer extends HasThread implements
   /**
    * @return Return the walRootDir.
    */
-  protected Path getWALRootDir() {
+  public Path getWALRootDir() {
     return walRootDir;
   }
 
