54c724f5808049f8282c393a1b1b8ba94cbab398
zhangduo <zhangduo@apache.org>
2019-01-15 08:31:26 +0800

    HBASE-21707 Addendum remove debug logs

--- hbase-rsgroup/src/main/java/org/apache/hadoop/hbase/rsgroup/RSGroupBasedLoadBalancer.java
+++ hbase-rsgroup/src/main/java/org/apache/hadoop/hbase/rsgroup/RSGroupBasedLoadBalancer.java
@@ -176,7 +176,6 @@ public class RSGroupBasedLoadBalancer implements RSGroupableBalancer {
     ListMultimap<String,RegionInfo> regionMap = ArrayListMultimap.create();
     ListMultimap<String,ServerName> serverMap = ArrayListMultimap.create();
     generateGroupMaps(regions, servers, regionMap, serverMap);
-    LOG.info("=======" + regionMap + ", " + serverMap);
     for(String groupKey : regionMap.keySet()) {
       if (regionMap.get(groupKey).size() > 0) {
         Map<ServerName, List<RegionInfo>> result =
@@ -194,7 +193,6 @@ public class RSGroupBasedLoadBalancer implements RSGroupableBalancer {
         }
       }
     }
-    LOG.info("=======" + assignments);
     return assignments;
   }
 
