6951f202f5d25b34d21bbaf8762bd3b9b32e875e
zhangduo <zhangduo@apache.org>
2019-02-07 08:44:21 +0800

    HBASE-21843 RegionGroupingProvider breaks the meta wal file name pattern which may cause data loss for meta region

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -2889,7 +2889,7 @@ public class HRegionServer extends HasThread implements
   /**
    * @return Return the walFs.
    */
-  protected FileSystem getWALFileSystem() {
+  public FileSystem getWALFileSystem() {
     return walFs;
   }
 
