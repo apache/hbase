7a2561122c510b5651f593c7d7757e4cfdda7196
Sergey Shelukhin <sershe@apache.org>
2019-02-12 12:53:17 -0800

    HBASE-28652 Backport HBASE-21785 master reports open regions as RITs and also messes up rit age metric
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Change-Id: I6894e28e270a3e59a7b66815deec9d81f0981f0b

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStateNode.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStateNode.java
@@ -178,7 +178,11 @@ public class RegionStateNode implements Comparable<RegionStateNode> {
 
   public long getLastUpdate() {
     TransitRegionStateProcedure proc = this.procedure;
-    return proc != null ? proc.getLastUpdate() : lastUpdate;
+    if (proc != null) {
+      long lastUpdate = proc.getLastUpdate();
+      return lastUpdate != 0 ? lastUpdate : proc.getSubmittedTime();
+    }
+    return lastUpdate;
   }
 
   public void setLastHost(final ServerName serverName) {
