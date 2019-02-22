19b6fa6e36bb31568cfce4bea448d63b61999dce
Josh Elser <elserj@apache.org>
2019-02-22 15:53:24 -0500

    HBASE-21960 Ensure RESTServletContainer used by RESTServer
    Signed-off-by: stack <stack@apache.org>

--- hbase-rest/src/test/java/org/apache/hadoop/hbase/rest/HBaseRESTTestingUtility.java
+++ hbase-rest/src/test/java/org/apache/hadoop/hbase/rest/HBaseRESTTestingUtility.java
@@ -18,90 +18,37 @@
  */
 package org.apache.hadoop.hbase.rest;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.security.UserProvider;
-import org.apache.hadoop.hbase.http.HttpServerUtil;
 import org.apache.hadoop.util.StringUtils;
 
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.servlet.DispatcherType;
-import java.util.Arrays;
-import java.util.EnumSet;
 
 public class HBaseRESTTestingUtility {
 
   private static final Logger LOG = LoggerFactory.getLogger(HBaseRESTTestingUtility.class);
 
-  private int testServletPort;
-  private Server server;
+  private RESTServer server;
 
   public int getServletPort() {
-    return testServletPort;
+    return server.getPort();
   }
 
   public void startServletContainer(Configuration conf) throws Exception {
     if (server != null) {
-      LOG.error("ServletContainer already running");
+      LOG.error("RESTServer already running");
       return;
     }
 
-    // Inject the conf for the test by being first to make singleton
-    RESTServlet.getInstance(conf, UserProvider.instantiate(conf));
+    conf.setInt("hbase.rest.port", 0);
+    conf.setInt("hbase.rest.info.port", -1);
+    conf.setBoolean(RESTServer.SKIP_LOGIN_KEY, true);
 
-    // set up the Jersey servlet container for Jetty
-    ResourceConfig app = new ResourceConfig().
-        packages("org.apache.hadoop.hbase.rest").register(JacksonJaxbJsonProvider.class);
-    ServletHolder sh = new ServletHolder(new ServletContainer(app));
-
-    // set up Jetty and run the embedded server
-    server = new Server(0);
-    LOG.info("configured " + ServletContainer.class.getName());
-
-    HttpConfiguration httpConfig = new HttpConfiguration();
-    httpConfig.setSendDateHeader(false);
-    httpConfig.setSendServerVersion(false);
-    ServerConnector serverConnector = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
-    serverConnector.setPort(testServletPort);
-
-    server.addConnector(serverConnector);
-
-    // set up context
-    ServletContextHandler ctxHandler = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
-    ctxHandler.addServlet(sh, "/*");
-    // Load filters specified from configuration.
-    String[] filterClasses = conf.getStrings(Constants.FILTER_CLASSES,
-        ArrayUtils.EMPTY_STRING_ARRAY);
-    for (String filter : filterClasses) {
-      filter = filter.trim();
-      ctxHandler.addFilter(filter, "/*", EnumSet.of(DispatcherType.REQUEST));
-    }
-    LOG.info("Loaded filter classes :" + Arrays.toString(filterClasses));
-
-    conf.set(RESTServer.REST_CSRF_BROWSER_USERAGENTS_REGEX_KEY, ".*");
-    RESTServer.addCSRFFilter(ctxHandler, conf);
-
-    HttpServerUtil.constrainHttpMethods(ctxHandler, false);
-
-    // start the server
-    server.start();
-    // get the port
-    testServletPort = ((ServerConnector)server.getConnectors()[0]).getLocalPort();
+    server = new RESTServer(conf);
+    server.run();
 
     LOG.info("started " + server.getClass().getName() + " on port " +
-      testServletPort);
+      server.getPort());
   }
 
   public void shutdownServletContainer() {
