1e12e884925f09abbc8c1231c7e000e875e961c6
zhangduo <zhangduo@apache.org>
2019-03-02 15:57:35 +0800

    HBASE-21978 Should close AsyncRegistry if we fail to get cluster id when creating AsyncConnection
    Signed-off-by: Guanghao Zhang <zghao@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionFactory.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionFactory.java
@@ -277,7 +277,6 @@ public class ConnectionFactory {
    * @param conf configuration
    * @param user the user the asynchronous connection is for
    * @return AsyncConnection object wrapped by CompletableFuture
-   * @throws IOException
    */
   public static CompletableFuture<AsyncConnection> createAsyncConnection(Configuration conf,
       final User user) {
@@ -285,10 +284,12 @@ public class ConnectionFactory {
     AsyncRegistry registry = AsyncRegistryFactory.getRegistry(conf);
     addListener(registry.getClusterId(), (clusterId, error) -> {
       if (error != null) {
+        registry.close();
         future.completeExceptionally(error);
         return;
       }
       if (clusterId == null) {
+        registry.close();
         future.completeExceptionally(new IOException("clusterid came back null"));
         return;
       }
@@ -299,6 +300,7 @@ public class ConnectionFactory {
           user.runAs((PrivilegedExceptionAction<? extends AsyncConnection>) () -> ReflectionUtils
             .newInstance(clazz, conf, registry, clusterId, user)));
       } catch (Exception e) {
+        registry.close();
         future.completeExceptionally(e);
       }
     });
