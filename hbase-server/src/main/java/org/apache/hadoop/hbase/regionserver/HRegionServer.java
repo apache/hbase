76d067e9d88dd40095a9cb83fe7ee87c9135a8cd
Sean Busbey <busbey@apache.org>
2019-03-07 08:54:33 -0600

    Revert "SE-22006 Fix branch-2.1 findbugs warning; causes nightly show as failed."
    This reverts commit f4acc47e2debb3d3d87c05436d940ef2fdfe0be3.
    
    bad commit message

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -1588,8 +1588,9 @@ public class HRegionServer extends HasThread implements
           MemStoreLAB.POOL_INITIAL_SIZE_DEFAULT);
       int chunkSize = conf.getInt(MemStoreLAB.CHUNK_SIZE_KEY, MemStoreLAB.CHUNK_SIZE_DEFAULT);
       // init the chunkCreator
-      ChunkCreator.initialize(chunkSize, offheap, globalMemStoreSize, poolSizePercentage,
-        initialCountPercentage, this.hMemManager);
+      ChunkCreator chunkCreator =
+          ChunkCreator.initialize(chunkSize, offheap, globalMemStoreSize, poolSizePercentage,
+      initialCountPercentage, this.hMemManager);
     }
   }
 
