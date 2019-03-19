f5b20451dbcd982dbdc8a7a24d703553b5bf106d
xcang <xcang@salesforce.com>
2019-03-19 13:42:09 -0700

    HBASE-22067 Fix log in StochasticLoadBalancer when balancer is ill-fit for cluster size

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/StochasticLoadBalancer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/StochasticLoadBalancer.java
@@ -387,7 +387,7 @@ public class StochasticLoadBalancer extends BaseLoadBalancer {
             + "maxSteps:{}. Hence load balancing may not work well. Setting parameter "
             + "\"hbase.master.balancer.stochastic.runMaxSteps\" to true can overcome this issue."
             + "(This config change does not require service restart)", calculatedMaxSteps,
-            maxRunningTime);
+            maxSteps);
       }
     }
     LOG.info("start StochasticLoadBalancer.balancer, initCost=" + currentCost + ", functionCost="
