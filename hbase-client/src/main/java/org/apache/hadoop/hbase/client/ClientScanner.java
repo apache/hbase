a31347fcccb27b2163d9f42133e818fa68c920ba
Igor Rudenko <iostream64@gmail.com>
2019-04-23 06:26:33 -0700

    HBASE-22047 LeaseException in Scan should be retried Signed-off-by: stack <stack@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ClientScanner.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ClientScanner.java
@@ -38,6 +38,7 @@ import org.apache.hadoop.hbase.exceptions.ScannerResetException;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.ipc.RpcControllerFactory;
+import org.apache.hadoop.hbase.regionserver.LeaseException;
 import org.apache.hadoop.hbase.NotServingRegionException;
 import org.apache.hadoop.hbase.regionserver.RegionServerStoppedException;
 import org.apache.hadoop.hbase.TableName;
@@ -366,7 +367,7 @@ public abstract class ClientScanner extends AbstractClientScanner {
     if ((cause != null && cause instanceof NotServingRegionException) ||
         (cause != null && cause instanceof RegionServerStoppedException) ||
         e instanceof OutOfOrderScannerNextException || e instanceof UnknownScannerException ||
-        e instanceof ScannerResetException) {
+        e instanceof ScannerResetException || e instanceof LeaseException) {
       // Pass. It is easier writing the if loop test as list of what is allowed rather than
       // as a list of what is not allowed... so if in here, it means we do not throw.
       if (retriesLeft <= 0) {
