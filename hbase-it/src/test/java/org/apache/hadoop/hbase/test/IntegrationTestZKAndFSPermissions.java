1bd5b5cf7bacb75fb3d51bddd0a4f1141f4ae002
李小保 <lixiaobao@mininglamp.com>
2019-04-24 08:16:32 +0800

    HBASE-22250 The same constants used in many places should be placed in constant classes
    Signed-off-by: stack <stack@apache.org>

--- hbase-it/src/test/java/org/apache/hadoop/hbase/test/IntegrationTestZKAndFSPermissions.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/test/IntegrationTestZKAndFSPermissions.java
@@ -31,6 +31,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.IntegrationTestingUtility;
+import org.apache.hadoop.hbase.security.SecurityConstants;
 import org.apache.hadoop.hbase.testclassification.IntegrationTests;
 import org.apache.hadoop.hbase.util.AbstractHBaseTool;
 import org.apache.hadoop.hbase.util.FSUtils;
@@ -104,7 +105,7 @@ public class IntegrationTestZKAndFSPermissions extends AbstractHBaseTool {
   @Override
   protected void processOptions(CommandLine cmd) {
     isForce = cmd.hasOption(FORCE_CHECK_ARG);
-    masterPrincipal = getShortUserName(conf.get("hbase.master.kerberos.principal"));
+    masterPrincipal = getShortUserName(conf.get(SecurityConstants.MASTER_KRB_PRINCIPAL));
     superUser = cmd.getOptionValue(SUPERUSER_ARG, conf.get("hbase.superuser"));
     masterPrincipal = cmd.getOptionValue(PRINCIPAL_ARG, masterPrincipal);
     fsPerms = cmd.getOptionValue(FS_PERMS, "700");
