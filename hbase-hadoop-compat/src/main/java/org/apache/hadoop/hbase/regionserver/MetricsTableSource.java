32d6ef8d35529911316ffc259111ee3fb158993e
Jan Hentschel <jan.hentschel@ultratendency.com>
2019-04-29 16:24:09 +0200

    HBASE-22327 Fixed remaining Checkstyle issue in hbase-hadoop-compat

--- hbase-hadoop-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsTableSource.java
+++ hbase-hadoop-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsTableSource.java
@@ -15,12 +15,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.hadoop.hbase.regionserver;
 
-import org.apache.yetus.audience.InterfaceAudience;
-
 import java.io.Closeable;
+
+import org.apache.yetus.audience.InterfaceAudience;
 
 /**
  * This interface will be implemented to allow region server to push table metrics into
