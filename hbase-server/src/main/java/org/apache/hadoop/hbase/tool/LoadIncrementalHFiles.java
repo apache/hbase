420fbba6aefa58bb6ff252ff0c33e9b4ccae64b0
Artem Ervits <generic13@gmail.com>
2019-05-09 16:50:02 -0400

    HBASE-21536 Fix completebulkload usage instructions
    Signed-off-by: Peter Somogyi <psomogyi@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/tool/LoadIncrementalHFiles.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/tool/LoadIncrementalHFiles.java
@@ -182,13 +182,16 @@ public class LoadIncrementalHFiles extends Configured implements Tool {
   }
 
   private void usage() {
-    System.err.println("usage: " + NAME + " /path/to/hfileoutputformat-output tablename -loadTable"
-        + "\n -D" + CREATE_TABLE_CONF_KEY + "=no - can be used to avoid creation of table by "
-        + "this tool\n  Note: if you set this to 'no', then the target table must already exist "
-        + "in HBase\n -loadTable implies your baseDirectory to store file has a depth of 3 ,you"
-        + " must have an existing table\n-D" + IGNORE_UNMATCHED_CF_CONF_KEY + "=yes - can be used "
-        + "to ignore unmatched column families\n" +
-        "\n");
+    System.err.println("usage: " + "bin/hbase completebulkload <-Dargs> "
+      + "</path/to/hfileoutputformat-output> <tablename>\n"
+      + "\t-D" + CREATE_TABLE_CONF_KEY + "=no can be used to avoid creation "
+      + "of a table by this tool.\n"
+      + "\t Note: if you set this to 'no', then target table must already exist.\n"
+      + "\t-D" + IGNORE_UNMATCHED_CF_CONF_KEY + "=yes can be used to ignore "
+      + "unmatched column families.\n"
+      + "\t-loadTable switch implies your baseDirectory to store file has a "
+      + "depth of 3, table must exist\n"
+      + "\t and -loadTable switch is the last option on the command line.\n\n");
   }
 
   /**
