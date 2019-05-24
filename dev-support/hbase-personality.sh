c00cc4d9d600aa3612204fbc072ba238e4c28d84
Guanghao Zhang <zghao@apache.org>
2019-05-24 09:23:20 +0800

    HBASE-22462 (addendum) Should run a 'mvn install' at the end of hadoop check in pre commit job

--- dev-support/hbase-personality.sh
+++ dev-support/hbase-personality.sh
@@ -590,8 +590,9 @@ function hadoopcheck_rebuild
 
   logfile="${PATCH_DIR}/patch-install-after-hadoopcheck.txt"
   echo_and_redirect "${logfile}" \
-    "$(maven_executor)" clean install \
+    $(maven_executor) clean install \
       -DskipTests -DHBasePatchProcess
+
   return 0
 }
 
