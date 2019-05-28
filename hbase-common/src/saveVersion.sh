3a3857690afd341784aac277cf6b9a6a2655701a
Artem Ervits <generic13@gmail.com>
2019-05-28 14:52:40 -0400

    HBASE-22469 replace md5 checksum in saveVersion script with sha512
    Signed-off-by: Josh Elser <elserj@apache.org>

--- hbase-common/src/saveVersion.sh
+++ hbase-common/src/saveVersion.sh
@@ -49,14 +49,14 @@ if [ -z "${revision}" ]; then
   revision="Unknown"
   url="file://$cwd"
 fi
-if ! [  -x "$(command -v md5sum)" ]; then
-  if ! [ -x "$(command -v md5)" ]; then
+if ! [  -x "$(command -v openssl)" ]; then
+  if ! [ -x "$(command -v gpg)" ]; then
     srcChecksum="Unknown"
   else
-    srcChecksum=`find hbase-*/src/main/ | grep -e "\.java" -e "\.proto" | LC_ALL=C sort | xargs md5 | md5 | cut -d ' ' -f 1`
+    srcChecksum=`find hbase-*/src/main/ | grep -e "\.java" -e "\.proto" | LC_ALL=C sort | xargs gpg --print-md sha512 | gpg --print-md sha512 | cut -d ' ' -f 1`
   fi
 else
-  srcChecksum=`find hbase-*/src/main/ | grep -e "\.java" -e "\.proto" | LC_ALL=C sort | xargs md5sum | md5sum | cut -d ' ' -f 1`
+  srcChecksum=`find hbase-*/src/main/ | grep -e "\.java" -e "\.proto" | LC_ALL=C sort | xargs openssl dgst -sha512 | openssl dgst -sha512 | cut -d ' ' -f 1`
 fi
 popd
 
