84af2a46109da2b47d3b935d847b78149587af5f
Josh Elser <elserj@apache.org>
2019-06-12 19:15:14 -0400

    HBASE-22562 Remove dead code: skipControl
    Signed-off-by: Reid Chan <reidchan@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/throttle/PressureAwareCompactionThroughputController.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/throttle/PressureAwareCompactionThroughputController.java
@@ -146,13 +146,4 @@ public class PressureAwareCompactionThroughputController extends PressureAwareTh
         + throughputDesc(getMaxThroughput()) + ", activeCompactions=" + activeOperations.size()
         + "]";
   }
-
-  @Override
-  protected boolean skipControl(long deltaSize, long controlSize) {
-    if (deltaSize < controlSize) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
