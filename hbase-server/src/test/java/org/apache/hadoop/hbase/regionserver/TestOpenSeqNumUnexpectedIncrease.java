d0be5827c035a49acd2e9ad59aa7f8f7684806cb
Bing Xiao <buptxyzadluffy@gmail.com>
2019-07-01 13:13:19 -0700

    HBASE-22169 Open region failed cause memory leak Signed-off-by: stack <stack@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestOpenSeqNumUnexpectedIncrease.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestOpenSeqNumUnexpectedIncrease.java
@@ -20,6 +20,8 @@ package org.apache.hadoop.hbase.regionserver;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -73,6 +75,11 @@ public class TestOpenSeqNumUnexpectedIncrease {
         rsServices.abort("for testing", new Exception("Inject error for testing"));
         throw new IOException("Inject error for testing");
       }
+    }
+
+    public Map<byte[], List<HStoreFile>> close() throws IOException {
+      //skip close
+      return null;
     }
   }
 
