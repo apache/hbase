c820d885dda031a05c87e5fac4200e12081181e6
Guanghao Zhang <zghao@apache.org>
2019-07-11 15:20:34 +0800

    HBASE-22527 [hbck2] Add a master web ui to show the problematic regions

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/AssignmentManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/AssignmentManager.java
@@ -158,6 +158,8 @@ public class AssignmentManager {
   private final RegionStates regionStates = new RegionStates();
   private final RegionStateStore regionStateStore;
 
+  private final Map<ServerName, Set<byte[]>> rsReports = new HashMap<>();
+
   private final boolean shouldAssignRegionsWithFavoredNodes;
   private final int assignDispatchWaitQueueMaxSize;
   private final int assignDispatchWaitMillis;
@@ -1065,13 +1067,18 @@ public class AssignmentManager {
     }
 
     ServerStateNode serverNode = regionStates.getOrCreateServer(serverName);
-
     synchronized (serverNode) {
       if (!serverNode.isInState(ServerState.ONLINE)) {
         LOG.warn("Got a report from a server result in state " + serverNode.getState());
         return;
       }
     }
+
+    // Track the regionserver reported online regions in memory.
+    synchronized (rsReports) {
+      rsReports.put(serverName, regionNames);
+    }
+
     if (regionNames.isEmpty()) {
       // nothing to do if we don't have regions
       LOG.trace("no online region found on {}", serverName);
@@ -2027,5 +2034,54 @@ public class AssignmentManager {
   @VisibleForTesting
   MasterServices getMaster() {
     return master;
+  }
+
+  /**
+   * Found the potentially problematic opened regions. There are three case:
+   * case 1. Master thought this region opened, but no regionserver reported it.
+   * case 2. Master thought this region opened on Server1, but regionserver reported Server2
+   * case 3. More than one regionservers reported opened this region
+   *
+   * @return the map of potentially problematic opened regions. The key is the region name. The
+   * value is a pair of location in meta and the regionservers which reported opened this region.
+   */
+  public Map<String, Pair<ServerName, Set<ServerName>>> getProblematicRegions() {
+    Map<String, Set<ServerName>> reportedOnlineRegions = new HashMap<>();
+    synchronized (rsReports) {
+      for (Map.Entry<ServerName, Set<byte[]>> entry : rsReports.entrySet()) {
+        for (byte[] regionName : entry.getValue()) {
+          reportedOnlineRegions
+              .computeIfAbsent(RegionInfo.getRegionNameAsString(regionName), r -> new HashSet<>())
+              .add(entry.getKey());
+        }
+      }
+    }
+
+    Map<String, Pair<ServerName, Set<ServerName>>> problematicRegions = new HashMap<>();
+    List<RegionState> rits = regionStates.getRegionsStateInTransition();
+    for (RegionState regionState : regionStates.getRegionStates()) {
+      // Only consider the opened region and not in transition
+      if (!rits.contains(regionState) && regionState.isOpened()) {
+        String regionName = regionState.getRegion().getRegionNameAsString();
+        ServerName serverName = regionState.getServerName();
+        if (reportedOnlineRegions.containsKey(regionName)) {
+          Set<ServerName> reportedServers = reportedOnlineRegions.get(regionName);
+          if (reportedServers.contains(serverName)) {
+            if (reportedServers.size() > 1) {
+              // More than one regionserver reported opened this region
+              problematicRegions.put(regionName, new Pair<>(serverName, reportedServers));
+            }
+          } else {
+            // Master thought this region opened on Server1, but regionserver reported Server2
+            problematicRegions.put(regionName, new Pair<>(serverName, reportedServers));
+          }
+        } else {
+          // Master thought this region opened, but no regionserver reported it.
+          problematicRegions.put(regionName, new Pair<>(serverName, new HashSet<>()));
+        }
+      }
+    }
+
+    return problematicRegions;
   }
 }
