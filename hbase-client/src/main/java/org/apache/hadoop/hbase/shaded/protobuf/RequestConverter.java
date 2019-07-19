2449d9c05d92767b3bcc4e98d38b323a85db949c
Guanghao <zghao@apache.org>
2019-07-19 08:03:06 +0800

    HBASE-22673 Avoid to expose protobuf stuff in Hbck interface
    Signed-off-by: stack <stack@apache.org>
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/shaded/protobuf/RequestConverter.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/shaded/protobuf/RequestConverter.java
@@ -1898,10 +1898,11 @@ public final class RequestConverter {
   }
 
   public static MasterProtos.ScheduleServerCrashProcedureRequest
-      toScheduleServerCrashProcedureRequest(List<HBaseProtos.ServerName> serverNames) {
-    MasterProtos.ScheduleServerCrashProcedureRequest.Builder b =
+      toScheduleServerCrashProcedureRequest(List<ServerName> serverNames) {
+    MasterProtos.ScheduleServerCrashProcedureRequest.Builder builder =
         MasterProtos.ScheduleServerCrashProcedureRequest.newBuilder();
-    return b.addAllServerName(serverNames).build();
+    serverNames.stream().map(ProtobufUtil::toServerName).forEach(sn -> builder.addServerName(sn));
+    return builder.build();
   }
 
   private static List<RegionSpecifier> toEncodedRegionNameRegionSpecifiers(
