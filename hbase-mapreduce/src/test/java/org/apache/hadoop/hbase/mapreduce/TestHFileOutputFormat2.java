6a46e071c5bd20c247a3591a818735bb916cf43b
syedmurtazahassan <syedmurtazahasan@gmail.com>
2019-07-23 14:14:12 +0200

    HBASE-22603 Javadoc Warnings related to @link tag
    Signed-off-by: Jan Hentschel <jan.hentschel@ultratendency.com>

--- hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/mapreduce/TestHFileOutputFormat2.java
+++ hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/mapreduce/TestHFileOutputFormat2.java
@@ -787,9 +787,8 @@ public class TestHFileOutputFormat2  {
   }
 
   /**
-   * Test for {@link HFileOutputFormat2#configureCompression(Configuration, HTableDescriptor)} and
-   * {@link HFileOutputFormat2#createFamilyCompressionMap(Configuration)}.
-   * Tests that the compression map is correctly serialized into
+   * Test for {@link HFileOutputFormat2#createFamilyCompressionMap(Configuration)}.
+   * Tests that the family compression map is correctly serialized into
    * and deserialized from configuration
    *
    * @throws IOException
@@ -859,9 +858,8 @@ public class TestHFileOutputFormat2  {
 
 
   /**
-   * Test for {@link HFileOutputFormat2#configureBloomType(HTableDescriptor, Configuration)} and
-   * {@link HFileOutputFormat2#createFamilyBloomTypeMap(Configuration)}.
-   * Tests that the compression map is correctly serialized into
+   * Test for {@link HFileOutputFormat2#createFamilyBloomTypeMap(Configuration)}.
+   * Tests that the family bloom type map is correctly serialized into
    * and deserialized from configuration
    *
    * @throws IOException
@@ -930,9 +928,8 @@ public class TestHFileOutputFormat2  {
   }
 
   /**
-   * Test for {@link HFileOutputFormat2#configureBlockSize(HTableDescriptor, Configuration)} and
-   * {@link HFileOutputFormat2#createFamilyBlockSizeMap(Configuration)}.
-   * Tests that the compression map is correctly serialized into
+   * Test for {@link HFileOutputFormat2#createFamilyBlockSizeMap(Configuration)}.
+   * Tests that the family block size map is correctly serialized into
    * and deserialized from configuration
    *
    * @throws IOException
@@ -1007,9 +1004,8 @@ public class TestHFileOutputFormat2  {
   }
 
   /**
-   * Test for {@link HFileOutputFormat2#configureDataBlockEncoding(HTableDescriptor, Configuration)}
-   * and {@link HFileOutputFormat2#createFamilyDataBlockEncodingMap(Configuration)}.
-   * Tests that the compression map is correctly serialized into
+   * Test for {@link HFileOutputFormat2#createFamilyDataBlockEncodingMap(Configuration)}.
+   * Tests that the family data block encoding map is correctly serialized into
    * and deserialized from configuration
    *
    * @throws IOException
