c0d4420a6a05513eaa5d087995ca8cc8ba3fa87e
Peter Somogyi <psomogyi@apache.org>
2019-07-30 10:19:09 +0200

    Revert "HBASE-22723 Have CatalogJanitor report holes and overlaps; i.e. problems it sees when doing its regular scan of hbase:meta"
    This reverts commit 4d5dd1a1d3d6eeb4765e89894983884fd6acb9fa.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterRpcServices.java
@@ -1416,8 +1416,7 @@ public class MasterRpcServices extends RSRpcServices
       RunCatalogScanRequest req) throws ServiceException {
     rpcPreCheck("runCatalogScan");
     try {
-      return ResponseConverter.buildRunCatalogScanResponse(
-          this.master.catalogJanitorChore.scan());
+      return ResponseConverter.buildRunCatalogScanResponse(master.catalogJanitorChore.scan());
     } catch (IOException ioe) {
       throw new ServiceException(ioe);
     }
