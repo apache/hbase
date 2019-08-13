f69d15b50033e58b6a42f539aa688c8bc3390da5
Andrew Purtell <apurtell@apache.org>
2019-08-13 15:42:41 -0700

    HBASE-22828 Log a region close journal (#471)
    Signed-off-by: Michael Stack <stack@apache.org>
    Signed-off-by: Reid Chan <reidchan@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -1533,13 +1533,16 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
     MonitoredTask status = TaskMonitor.get().createStatus(
         "Closing region " + this.getRegionInfo().getEncodedName() +
         (abort ? " due to abort" : ""));
-
+    status.enableStatusJournal(false);
     status.setStatus("Waiting for close lock");
     try {
       synchronized (closeLock) {
         return doClose(abort, status);
       }
     } finally {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Region close journal:\n" + status.prettyPrintJournal());
+      }
       status.cleanup();
     }
   }
