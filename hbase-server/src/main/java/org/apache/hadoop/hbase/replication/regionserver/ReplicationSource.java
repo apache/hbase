bb87e3a7bf161c87e838ed4d3eb176f588383bb1
shahrs87 <shahrs87@gmail.com>
2019-08-22 13:04:23 -0700

    [HBASE-22601] Misconfigured addition of peers leads to cluster shutdown. (#490)
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
@@ -310,6 +310,10 @@ public class ReplicationSource extends Thread implements ReplicationSourceInterf
       }
     }
 
+    if (!this.isSourceActive()) {
+      return;
+    }
+
     // In rare case, zookeeper setting may be messed up. That leads to the incorrect
     // peerClusterId value, which is the same as the source clusterId
     if (clusterId.equals(peerClusterId) && !replicationEndpoint.canReplicateToSameCluster()) {
