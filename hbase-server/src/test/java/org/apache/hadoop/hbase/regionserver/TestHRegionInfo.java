bb3fcef95ea5a147914f8e8e2a1c41c72a55b40d
stack <stack@apache.org>
2019-09-03 14:48:04 -0700

    HBASE-22970 split parents show as overlaps in the HBCK Report
    Check if overlap is split parent.
    
    Cleaned up the HBCK Report page too with some notes that it is made of
    two reports; have the two sections display the same.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestHRegionInfo.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestHRegionInfo.java
@@ -33,6 +33,7 @@ import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.RegionInfo;
 import org.apache.hadoop.hbase.client.RegionInfoBuilder;
 import org.apache.hadoop.hbase.exceptions.DeserializationException;
 import org.apache.hadoop.hbase.master.RegionState;
@@ -127,6 +128,12 @@ public class TestHRegionInfo {
     assertTrue(abri.isOverlap(adri));
     assertFalse(dri.isOverlap(ari));
     assertTrue(abri.isOverlap(adri));
+    assertTrue(adri.isOverlap(abri));
+    // Check that splitParent is not reported as an overlap.
+    RegionInfo splitParent = RegionInfoBuilder.newBuilder(adri.getTable()).
+        setStartKey(adri.getStartKey()).setEndKey(adri.getEndKey()).setOffline(true).
+        setSplit(true).build();
+    assertFalse(splitParent.isOverlap(abri));
   }
 
   @Test
