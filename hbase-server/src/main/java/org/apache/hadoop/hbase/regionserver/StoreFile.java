4161008f5d1f1c29ca869189ff2eff79d60d3fd2
sreenivasulureddy <sreenivasulu.y@huawei.com>
2019-09-05 11:42:31 +0530

    HBASE-22649: Encode StoreFile path URLs in the UI to handle scenarios where CF contains special characters (like # etc.)
    Signed-off-by: Sakthi<sakthi@apache.org>
    (cherry picked from commit dd761b1e69cde2120b0ce4a6ebaf7d901717c14f)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFile.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFile.java
@@ -20,7 +20,9 @@ package org.apache.hadoop.hbase.regionserver;
 
 import java.io.DataInput;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
+import java.net.URLEncoder;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Collection;
@@ -286,6 +288,17 @@ public class StoreFile {
   }
 
   /**
+   * @return Encoded Path if this StoreFile was made with a Stream.
+   */
+  public Path getEncodedPath() {
+    try {
+      return new Path(URLEncoder.encode(fileInfo.getPath().toString(), HConstants.UTF8_ENCODING));
+    } catch (UnsupportedEncodingException ex) {
+      throw new RuntimeException("URLEncoder doesn't support UTF-8", ex);
+    }
+  }
+
+  /**
    * @return Returns the qualified path of this StoreFile
    */
   public Path getQualifiedPath() {
@@ -380,7 +393,7 @@ public class StoreFile {
   /**
    * Check if this storefile was created by bulk load.
    * When a hfile is bulk loaded into HBase, we append
-   * '_SeqId_<id-when-loaded>' to the hfile name, unless
+   * '_SeqId_&lt;id-when-loaded&gt;' to the hfile name, unless
    * "hbase.mapreduce.bulkload.assign.sequenceNumbers" is
    * explicitly turned off.
    * If "hbase.mapreduce.bulkload.assign.sequenceNumbers"
