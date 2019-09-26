cd9367512a8b0ab87d14dee152c51faf33d1cd5b
BukrosSzabolcs <45901354+BukrosSzabolcs@users.noreply.github.com>
2019-09-26 10:07:38 +0200

    HBASE-22982: region server suspend/resume and graceful rolling restart actions (#592)
    * Add chaos monkey action for suspend/resume region servers
    * Add chaos monkey action for graceful rolling restart
    * Add these to relevant chaos monkeys
    
    Signed-off-by: Balazs Meszaros <meszibalu@apache.org>
    Signed-off-by: Peter Somogyi <psomogyi@apache.org>

--- hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/RestartActionBaseAction.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/RestartActionBaseAction.java
@@ -45,9 +45,30 @@ public class RestartActionBaseAction extends Action {
       return;
     }
 
+    LOG.info("Killing master: {}", server);
     killMaster(server);
     sleep(sleepTime);
+    LOG.info("Starting master: {}", server);
     startMaster(server);
+  }
+
+  /**
+   * Stop and then restart the region server instead of killing it.
+   * @param server hostname to restart the regionserver on
+   * @param sleepTime number of milliseconds between stop and restart
+   * @throws IOException if something goes wrong
+   */
+  void gracefulRestartRs(ServerName server, long sleepTime) throws IOException {
+    sleepTime = Math.max(sleepTime, 1000);
+    // Don't try the stop if we're stopping already
+    if (context.isStopping()) {
+      return;
+    }
+    LOG.info("Stopping region server: {}", server);
+    stopRs(server);
+    sleep(sleepTime);
+    LOG.info("Starting region server: {}", server);
+    startRs(server);
   }
 
   void restartRs(ServerName server, long sleepTime) throws IOException {
@@ -56,8 +77,10 @@ public class RestartActionBaseAction extends Action {
     if (context.isStopping()) {
       return;
     }
+    LOG.info("Killing region server: {}", server);
     killRs(server);
     sleep(sleepTime);
+    LOG.info("Starting region server: {}", server);
     startRs(server);
   }
 
@@ -67,8 +90,10 @@ public class RestartActionBaseAction extends Action {
     if (context.isStopping()) {
       return;
     }
+    LOG.info("Killing zookeeper node: {}", server);
     killZKNode(server);
     sleep(sleepTime);
+    LOG.info("Starting zookeeper node: {}", server);
     startZKNode(server);
   }
 
@@ -78,8 +103,10 @@ public class RestartActionBaseAction extends Action {
     if (context.isStopping()) {
       return;
     }
+    LOG.info("Killing data node: {}", server);
     killDataNode(server);
     sleep(sleepTime);
+    LOG.info("Starting data node: {}", server);
     startDataNode(server);
   }
 
@@ -89,8 +116,10 @@ public class RestartActionBaseAction extends Action {
     if (context.isStopping()) {
       return;
     }
+    LOG.info("Killing name node: {}", server);
     killNameNode(server);
     sleep(sleepTime);
+    LOG.info("Starting name node: {}", server);
     startNameNode(server);
   }
 
