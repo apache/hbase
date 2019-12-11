6fd9ac83a952ed5f12679be6f02281d2dad69aca
Bharath Vissapragada <bharathv@apache.org>
2019-12-10 17:22:00 -0800

    Fix test type annotation.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestClientMetaServiceRPCs.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestClientMetaServiceRPCs.java
@@ -38,7 +38,7 @@ import org.apache.hadoop.hbase.ipc.RpcClientFactory;
 import org.apache.hadoop.hbase.ipc.RpcControllerFactory;
 import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.testclassification.MasterTests;
-import org.apache.hadoop.hbase.testclassification.SmallTests;
+import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.util.JVMClusterUtil;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -54,7 +54,7 @@ import org.apache.hadoop.hbase.shaded.protobuf.generated.MasterProtos.GetCluster
 import org.apache.hadoop.hbase.shaded.protobuf.generated.MasterProtos.GetMetaRegionLocationsRequest;
 import org.apache.hadoop.hbase.shaded.protobuf.generated.MasterProtos.GetMetaRegionLocationsResponse;
 
-@Category({SmallTests.class, MasterTests.class})
+@Category({MediumTests.class, MasterTests.class})
 public class TestClientMetaServiceRPCs {
 
   @ClassRule
