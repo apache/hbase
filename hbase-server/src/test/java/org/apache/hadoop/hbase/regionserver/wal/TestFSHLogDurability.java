f316b011d3349802734f65e4576901cf0a8cd50e
Duo Zhang <zhangduo@apache.org>
2019-12-18 08:16:41 +0800

    HBASE-20461 Implement fsync for AsyncFSWAL

---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestFSHLogDurability.java
@@ -0,0 +1,83 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.regionserver.wal;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseClassTestRule;
+import org.apache.hadoop.hbase.regionserver.RegionServerServices;
+import org.apache.hadoop.hbase.testclassification.MediumTests;
+import org.junit.ClassRule;
+import org.junit.experimental.categories.Category;
+
+@Category({ RegionServerServices.class, MediumTests.class })
+public class TestFSHLogDurability extends WALDurabilityTestBase<CustomFSHLog> {
+
+  @ClassRule
+  public static final HBaseClassTestRule CLASS_RULE =
+    HBaseClassTestRule.forClass(TestFSHLogDurability.class);
+
+  @Override
+  protected CustomFSHLog getWAL(FileSystem fs, Path root, String logDir, Configuration conf)
+    throws IOException {
+    CustomFSHLog wal = new CustomFSHLog(fs, root, logDir, conf);
+    wal.init();
+    return wal;
+  }
+
+  @Override
+  protected void resetSyncFlag(CustomFSHLog wal) {
+    wal.resetSyncFlag();
+  }
+
+  @Override
+  protected Boolean getSyncFlag(CustomFSHLog wal) {
+    return wal.getSyncFlag();
+  }
+}
+
+class CustomFSHLog extends FSHLog {
+  private Boolean syncFlag;
+
+  public CustomFSHLog(FileSystem fs, Path root, String logDir, Configuration conf)
+    throws IOException {
+    super(fs, root, logDir, conf);
+  }
+
+  @Override
+  public void sync(boolean forceSync) throws IOException {
+    syncFlag = forceSync;
+    super.sync(forceSync);
+  }
+
+  @Override
+  public void sync(long txid, boolean forceSync) throws IOException {
+    syncFlag = forceSync;
+    super.sync(txid, forceSync);
+  }
+
+  void resetSyncFlag() {
+    this.syncFlag = null;
+  }
+
+  Boolean getSyncFlag() {
+    return syncFlag;
+  }
+}
