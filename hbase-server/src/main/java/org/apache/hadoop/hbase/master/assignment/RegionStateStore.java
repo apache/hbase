189a3b1cf6ce832ce7aa832fffa25c7179f5683e
stack <stack@apache.org>
2019-12-18 23:08:24 -0800

    HBASE-23596 HBCKServerCrashProcedure can double assign
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Lijin Bin <binlijin@apache.org>
    Signed-off-by: Viraj Jasani <vjasani@apache.org>
    
    Change its behavior so it will only look in hbase:meta
    if the call to the super class turns up zero references.
    Only then will it search hbase:meta for references to
    'Unknown Servers'. Normal operation where we read Master
    context is usual and sufficient. The scan of hbase:meta
    is only for case where Master state has been corrupted
    and we need to clear out 'Unknown Servers'.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStateStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStateStore.java
@@ -51,7 +51,6 @@ import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.hbase.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.hbase.thirdparty.com.google.common.base.Preconditions;
 
 /**
@@ -115,7 +114,7 @@ public class RegionStateStore {
       if (regionInfo == null) continue;
 
       final int replicaId = regionInfo.getReplicaId();
-      final State state = getRegionState(result, replicaId, regionInfo);
+      final State state = getRegionState(result, regionInfo);
 
       final ServerName lastHost = hrl.getServerName();
       final ServerName regionLocation = getRegionServer(result, replicaId);
@@ -326,12 +325,11 @@ public class RegionStateStore {
 
   /**
    * Pull the region state from a catalog table {@link Result}.
-   * @param r Result to pull the region state from
    * @return the region state, or null if unknown.
    */
-  @VisibleForTesting
-  public static State getRegionState(final Result r, int replicaId, RegionInfo regionInfo) {
-    Cell cell = r.getColumnLatestCell(HConstants.CATALOG_FAMILY, getStateColumn(replicaId));
+  public static State getRegionState(final Result r, RegionInfo regionInfo) {
+    Cell cell = r.getColumnLatestCell(HConstants.CATALOG_FAMILY,
+        getStateColumn(regionInfo.getReplicaId()));
     if (cell == null || cell.getValueLength() == 0) {
       return null;
     }
