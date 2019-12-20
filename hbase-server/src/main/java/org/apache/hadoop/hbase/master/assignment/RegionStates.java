5ec99b4f10213184eaecbe7901648a12f1a22608
binlijin <binlijin@gmail.com>
2019-12-20 15:00:27 +0800

    HBASE-23564 RegionStates may has some expired serverinfo and make regions do not balance. (#930)
    Signed-off-by: stack <stack@apache.org>
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStates.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStates.java
@@ -540,7 +540,7 @@ public class RegionStates {
    * @return A clone of current assignments.
    */
   public Map<TableName, Map<ServerName, List<RegionInfo>>> getAssignmentsForBalancer(
-      TableStateManager tableStateManager, boolean isByTable) {
+    TableStateManager tableStateManager, List<ServerName> onlineServers, boolean isByTable) {
     final Map<TableName, Map<ServerName, List<RegionInfo>>> result = new HashMap<>();
     if (isByTable) {
       for (RegionStateNode node : regionsMap.values()) {
@@ -560,16 +560,21 @@ public class RegionStates {
       }
       // Add online servers with no assignment for the table.
       for (Map<ServerName, List<RegionInfo>> table : result.values()) {
-        for (ServerName serverName : serverMap.keySet()) {
+        for (ServerName serverName : onlineServers) {
           table.putIfAbsent(serverName, new ArrayList<>());
         }
       }
     } else {
       final HashMap<ServerName, List<RegionInfo>> ensemble = new HashMap<>(serverMap.size());
-      for (ServerStateNode serverNode : serverMap.values()) {
-        ensemble.put(serverNode.getServerName(), serverNode.getRegionInfoList().stream()
-          .filter(region -> !isTableDisabled(tableStateManager, region.getTable()))
-          .collect(Collectors.toList()));
+      for (ServerName serverName : onlineServers) {
+        ServerStateNode serverNode = serverMap.get(serverName);
+        if (serverNode != null) {
+          ensemble.put(serverNode.getServerName(), serverNode.getRegionInfoList().stream()
+            .filter(region -> !isTableDisabled(tableStateManager, region.getTable()))
+            .collect(Collectors.toList()));
+        } else {
+          ensemble.put(serverName, new ArrayList<>());
+        }
       }
       // Use a fake table name to represent the whole cluster's assignments
       result.put(HConstants.ENSEMBLE_TABLE_NAME, ensemble);
