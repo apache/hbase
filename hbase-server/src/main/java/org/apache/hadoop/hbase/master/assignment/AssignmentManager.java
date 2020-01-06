33f45d441385c37d5cb00a40a29d0aaa0caac7db
binlijin <binlijin@gmail.com>
2020-01-06 15:19:10 +0800

    HBASE-23636 Disable table may hang when regionserver stop or abort. (#982)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: virajjasani <34790606+virajjasani@users.noreply.github.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/AssignmentManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/AssignmentManager.java
@@ -1764,7 +1764,7 @@ public class AssignmentManager {
 
   // should be called under the RegionStateNode lock
   // for SCP
-  void regionClosedAbnormally(RegionStateNode regionNode) throws IOException {
+  public void regionClosedAbnormally(RegionStateNode regionNode) throws IOException {
     RegionState.State state = regionNode.getState();
     ServerName regionLocation = regionNode.getRegionLocation();
     regionNode.transitionState(State.ABNORMALLY_CLOSED);
