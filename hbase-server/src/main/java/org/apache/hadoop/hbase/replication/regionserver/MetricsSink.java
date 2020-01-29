60b80b6298ef94166208a09452a2cbe5dfaf3f95
Jan Hentschel <janh@apache.org>
2020-01-29 17:02:09 +0100

    HBASE-23736 Removed deprecated getTimeStampOfLastAppliedOp from MetricsSink
    Signed-off-by: Viraj Jasani <vjasani@apache.org>
    Signed-off-by: Xu Cang <xucang@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/MetricsSink.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/MetricsSink.java
@@ -91,18 +91,6 @@ public class MetricsSink {
   }
 
   /**
-   * Get the TimeStampOfLastAppliedOp. If no replication Op applied yet, the value is the timestamp
-   * at which hbase instance starts
-   * @return timeStampsOfLastAppliedOp;
-   * @deprecated Since 2.0.0. Will be removed in 3.0.0.
-   * @see #getTimestampOfLastAppliedOp()
-   */
-  @Deprecated
-  public long getTimeStampOfLastAppliedOp() {
-    return getTimestampOfLastAppliedOp();
-  }
-
-  /**
    * Get the TimestampOfLastAppliedOp. If no replication Op applied yet, the value is the timestamp
    * at which hbase instance starts
    * @return timeStampsOfLastAppliedOp;
