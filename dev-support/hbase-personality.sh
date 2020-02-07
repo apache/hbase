d554689cc4602ae93cf7dacf250edec66056e490
stack <stack@apache.org>
2020-02-07 15:53:15 -0800

    HBASE-23779 Up the default fork count; make count relative to CPU count (#1108)  FINAL ADDENDUM. Removes changes to dev-support/hbase-personality  leaving it as it was. All else about HBASE-23779 change remains  changing surefire fork counts to be dependent on cpu count.

--- dev-support/hbase-personality.sh
+++ dev-support/hbase-personality.sh
@@ -140,11 +140,7 @@ function personality_modules
 
   clear_personality_queue
 
-  # Pass maven a -T argument. Should make it run faster. Pass conservative value.
-  # Default is one thread. 0.5C on an apache box of 24 cores and 2 executors should
-  # make for 6 threads? Lets see. See below for more on -T:
-  # https://cwiki.apache.org/confluence/display/MAVEN/Parallel+builds+in+Maven+3
-  extra="-T0.5C -DHBasePatchProcess"
+  extra="-DHBasePatchProcess"
   if [[ "${PATCH_BRANCH}" = branch-1* ]]; then
     extra="${extra} -Dhttps.protocols=TLSv1.2"
   fi
