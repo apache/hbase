1d47f02737caa3f368ef75a665af1eaad2fb794c
Duo Zhang <zhangduo@apache.org>
2020-02-08 17:10:00 +0800

    HBASE-23809 The RSGroup shell test is missing (#1142)
    Signed-off-by: Guanghao Zhang <zghao@apache.org>

--- hbase-shell/src/test/java/org/apache/hadoop/hbase/client/AbstractTestShell.java
+++ hbase-shell/src/test/java/org/apache/hadoop/hbase/client/AbstractTestShell.java
@@ -17,9 +17,10 @@
  */
 package org.apache.hadoop.hbase.client;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.coprocessor.CoprocessorHost;
@@ -34,26 +35,25 @@ public abstract class AbstractTestShell {
   protected final static HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
   protected final static ScriptingContainer jruby = new ScriptingContainer();
 
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-    // Start mini cluster
-    TEST_UTIL.getConfiguration().setInt("hbase.regionserver.msginterval", 100);
-    TEST_UTIL.getConfiguration().setInt("hbase.client.pause", 250);
-    TEST_UTIL.getConfiguration().setBoolean("hbase.quota.enabled", true);
-    TEST_UTIL.getConfiguration().setInt(HConstants.HBASE_CLIENT_RETRIES_NUMBER, 6);
-    TEST_UTIL.getConfiguration().setBoolean(CoprocessorHost.ABORT_ON_ERROR_KEY, false);
-    TEST_UTIL.getConfiguration().setInt("hfile.format.version", 3);
+  protected static void setUpConfig() throws IOException {
+    Configuration conf = TEST_UTIL.getConfiguration();
+    conf.setInt("hbase.regionserver.msginterval", 100);
+    conf.setInt("hbase.client.pause", 250);
+    conf.setBoolean("hbase.quota.enabled", true);
+    conf.setInt(HConstants.HBASE_CLIENT_RETRIES_NUMBER, 6);
+    conf.setBoolean(CoprocessorHost.ABORT_ON_ERROR_KEY, false);
+    conf.setInt("hfile.format.version", 3);
 
     // Below settings are necessary for task monitor test.
-    TEST_UTIL.getConfiguration().setInt(HConstants.MASTER_INFO_PORT, 0);
-    TEST_UTIL.getConfiguration().setInt(HConstants.REGIONSERVER_INFO_PORT, 0);
-    TEST_UTIL.getConfiguration().setBoolean(HConstants.REGIONSERVER_INFO_PORT_AUTO, true);
+    conf.setInt(HConstants.MASTER_INFO_PORT, 0);
+    conf.setInt(HConstants.REGIONSERVER_INFO_PORT, 0);
+    conf.setBoolean(HConstants.REGIONSERVER_INFO_PORT_AUTO, true);
     // Security setup configuration
-    SecureTestUtil.enableSecurity(TEST_UTIL.getConfiguration());
-    VisibilityTestUtil.enableVisiblityLabels(TEST_UTIL.getConfiguration());
+    SecureTestUtil.enableSecurity(conf);
+    VisibilityTestUtil.enableVisiblityLabels(conf);
+  }
 
-    TEST_UTIL.startMiniCluster();
-
+  protected static void setUpJRubyRuntime() {
     // Configure jruby runtime
     List<String> loadPaths = new ArrayList<>(2);
     loadPaths.add("src/main/ruby");
@@ -63,6 +63,16 @@ public abstract class AbstractTestShell {
     System.setProperty("jruby.jit.logging.verbose", "true");
     System.setProperty("jruby.jit.logging", "true");
     System.setProperty("jruby.native.verbose", "true");
+  }
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    setUpConfig();
+
+    // Start mini cluster
+    TEST_UTIL.startMiniCluster(1);
+
+    setUpJRubyRuntime();
   }
 
   @AfterClass
