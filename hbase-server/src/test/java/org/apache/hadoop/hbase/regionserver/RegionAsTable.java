b515947edbb09a7fffa76516eb2f03df95897850
Toshihiro Suzuki <brfrn169@gmail.com>
2020-02-26 08:09:04 +0900

    HBASE-23146 Support CheckAndMutate with multiple conditions
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/RegionAsTable.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/RegionAsTable.java
@@ -50,6 +50,7 @@ import org.apache.hadoop.hbase.client.coprocessor.Batch.Call;
 import org.apache.hadoop.hbase.client.coprocessor.Batch.Callback;
 import org.apache.hadoop.hbase.client.metrics.ScanMetrics;
 import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
+import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.ipc.CoprocessorRpcChannel;
 
 /**
@@ -270,6 +271,11 @@ public class RegionAsTable implements Table {
 
   @Override
   public CheckAndMutateBuilder checkAndMutate(byte[] row, byte[] family) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public CheckAndMutateWithFilterBuilder checkAndMutate(byte[] row, Filter filter) {
     throw new UnsupportedOperationException();
   }
 
