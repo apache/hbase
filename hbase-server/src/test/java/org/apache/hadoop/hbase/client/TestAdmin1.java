e15525fcec3e6012b4c32b31f80238cedc689875
Sakthi <sakthi@apache.org>
2020-02-27 11:08:55 -0800

    HBASE-23878 : Backport HBASE-22040 to branch-2.1 (#1203)
    HBASE-22040 - Add mergeRegionsAsync with a List of region names method in AsyncAdmin
    
    Signed-off-by: Sean Busbey <busbey@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestAdmin1.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestAdmin1.java
@@ -29,6 +29,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
@@ -1433,6 +1434,41 @@ public class TestAdmin1 {
     } finally {
       this.admin.disableTable(tableName);
       this.admin.deleteTable(tableName);
+    }
+  }
+
+  @Test
+  public void testMergeRegionsInvalidRegionCount()
+      throws IOException, InterruptedException, ExecutionException {
+    TableName tableName = TableName.valueOf(name.getMethodName());
+    TableDescriptor td = TableDescriptorBuilder.newBuilder(tableName)
+      .setColumnFamily(ColumnFamilyDescriptorBuilder.of("d")).build();
+    byte[][] splitRows = new byte[2][];
+    splitRows[0] = new byte[] { (byte) '3' };
+    splitRows[1] = new byte[] { (byte) '6' };
+    try {
+      TEST_UTIL.createTable(td, splitRows);
+      TEST_UTIL.waitTableAvailable(tableName);
+
+      List<RegionInfo> tableRegions = admin.getRegions(tableName);
+      // 0
+      try {
+        admin.mergeRegionsAsync(new byte[0][0], false).get();
+        fail();
+      } catch (IllegalArgumentException e) {
+        // expected
+      }
+      // 1
+      try {
+        admin.mergeRegionsAsync(new byte[][] { tableRegions.get(0).getEncodedNameAsBytes() }, false)
+          .get();
+        fail();
+      } catch (IllegalArgumentException e) {
+        // expected
+      }
+    } finally {
+      admin.disableTable(tableName);
+      admin.deleteTable(tableName);
     }
   }
 
