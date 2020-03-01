1733efa17182e1a1cd9cf766666b211cf26138dd
Viraj Jasani <vjasani@apache.org>
2020-03-01 17:46:50 +0530

    HBASE-23914 : Set hbase.client.retries.number for TestThriftHBaseServiceHandler (#1227)
    Signed-off-by: Jan Hentschel <janh@apache.org>

--- hbase-thrift/src/test/java/org/apache/hadoop/hbase/thrift2/TestThriftHBaseServiceHandler.java
+++ hbase-thrift/src/test/java/org/apache/hadoop/hbase/thrift2/TestThriftHBaseServiceHandler.java
@@ -188,6 +188,7 @@ public class TestThriftHBaseServiceHandler {
 
   @BeforeClass
   public static void beforeClass() throws Exception {
+    UTIL.getConfiguration().set("hbase.client.retries.number", "3");
     UTIL.startMiniCluster();
     HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(tableAname));
     for (HColumnDescriptor family : families) {
