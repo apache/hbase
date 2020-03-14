559bc91bc3e3bb5e6612bacbe5b89c34da0edca8
stack <stack@apache.org>
2020-03-14 15:17:20 -0700

    HBASE-23992 Fix TestAdminShell and TestQuotasShell mistakenly broken by parent commit

--- hbase-shell/src/test/ruby/hbase/admin_test.rb
+++ hbase-shell/src/test/ruby/hbase/admin_test.rb
@@ -311,7 +311,7 @@ module Hbase
       assert(output.include?('2 row(s)'))
 
       assert(output.include?('QUOTAS'))
-      assert(output.include?('LIMIT => 1G'))
+      assert(output.include?('LIMIT => 1.00G'))
       assert(output.include?('VIOLATION_POLICY => NO_INSERTS'))
       assert(output.include?('TYPE => SPACE'))
       assert(output.include?('1 row(s)'))
@@ -333,12 +333,13 @@ module Hbase
               POLICY => NO_INSERTS,
               NAMESPACE => ns)
       output = capture_stdout { command(:describe_namespace, ns) }
+      puts output
 
       assert(output.include?('DESCRIPTION'))
       assert(output.include?("NAME => '#{ns}'"))
 
       assert(output.include?('QUOTAS'))
-      assert(output.include?('LIMIT => 1G'))
+      assert(output.include?('LIMIT => 1.00G'))
       assert(output.include?('VIOLATION_POLICY => NO_INSERTS'))
       assert(output.include?('TYPE => SPACE'))
       assert(output.include?('1 row(s)'))
