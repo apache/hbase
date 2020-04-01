dc16ed1a08fb35c8b7cc2da1c4d1682b05dee9d4
申胜利 <48829688+shenshengli@users.noreply.github.com>
2020-04-01 14:36:43 +0800

    Modify the UT

--- hbase-server/src/test/java/org/apache/hadoop/hbase/io/TestFSDataInputStreamWrapper.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/io/TestFSDataInputStreamWrapper.java
@@ -1,18 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.io;
 
 import org.apache.hadoop.fs.CanUnbuffer;
+import org.apache.hadoop.hbase.HBaseClassTestRule;
+import org.apache.hadoop.hbase.testclassification.IOTests;
+import org.apache.hadoop.hbase.testclassification.SmallTests;
 import org.junit.Assert;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
 
+@Category({IOTests.class, SmallTests.class})
 public class TestFSDataInputStreamWrapper {
     private Method unbuffer = null;
 
+    @ClassRule
+    public static final HBaseClassTestRule CLASS_RULE =
+            HBaseClassTestRule.forClass(TestFSDataInputStreamWrapper.class);
+
+
     @Test
-    public void unbuffer() {
+    public void TestUnbuffer() {
         InputStream wrappedStream = new SonStream();
         final Class<? extends InputStream> streamClass = wrappedStream.getClass();
         Class<?>[] streamInterfaces = streamClass.getInterfaces();
