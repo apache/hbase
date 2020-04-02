80fccb18242fca39824b35ff5ee79ec001fe446f
申胜利 <48829688+shenshengli@users.noreply.github.com>
2020-04-02 16:58:03 +0800

    Modify the UT

--- hbase-server/src/test/java/org/apache/hadoop/hbase/io/TestFSDataInputStreamWrapper.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/io/TestFSDataInputStreamWrapper.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -50,7 +51,10 @@ public class TestFSDataInputStreamWrapper {
 
     @Test
     public void TestUnbuffer() throws IOException {
-        SonStream stream = new SonStream(fs.open(new Path("/p")));
+        String testDir = "/tmp/";
+        String testFile = "test.txt";
+        createFile(testDir, testFile);
+        SonStream stream = new SonStream(fs.open(new Path(testDir + testFile)));
         FSDataInputStreamWrapper fsDISW = new FSDataInputStreamWrapper(stream);
         fsDISW.unbuffer();
         Assert.assertEquals(stream.buffer, null);
@@ -71,6 +75,21 @@ public class TestFSDataInputStreamWrapper {
         @Override
         public void unbuffer() {
             buffer = null;
+        }
+    }
+
+    public static void createFile(String directory, String filename) {
+        File file = new File(directory);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        File file2 = new File(directory, filename);
+        if (!file2.exists()) {
+            try {
+                file2.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
