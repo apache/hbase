137006f2adf4005da5db859752aec427388572e1
Wei-Chiu Chuang <weichiu@apache.org>
2020-04-08 11:31:51 -0700

    HBASE-23833. The relocated hadoop-thirdparty protobuf breaks HBase asyncwal	 (#1301)
    * Use Reflection to access shaded Hadoop protobuf classes.
    
    (cherry picked from commit a321e536989083ca3620bf2c53f12c07740bf5b0)
    
    * Update to improve the code:
    
    1. Added license.
    2. Added more comments.
    3. Wrap byte array instead of copy to make a ByteString.
    4. Moved all reflection instantiation to static class loading time.
    
    * Use LiteralByteString to wrap byte array instead of copying it.
    
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: stack <stack@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/asyncfs/FanOutOneBlockAsyncDFSOutput.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/asyncfs/FanOutOneBlockAsyncDFSOutput.java
@@ -68,7 +68,6 @@ import org.apache.hbase.thirdparty.io.netty.channel.ChannelHandler.Sharable;
 import org.apache.hbase.thirdparty.io.netty.channel.ChannelHandlerContext;
 import org.apache.hbase.thirdparty.io.netty.channel.ChannelId;
 import org.apache.hbase.thirdparty.io.netty.channel.SimpleChannelInboundHandler;
-import org.apache.hbase.thirdparty.io.netty.handler.codec.protobuf.ProtobufDecoder;
 import org.apache.hbase.thirdparty.io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import org.apache.hbase.thirdparty.io.netty.handler.timeout.IdleStateEvent;
 import org.apache.hbase.thirdparty.io.netty.handler.timeout.IdleStateHandler;
