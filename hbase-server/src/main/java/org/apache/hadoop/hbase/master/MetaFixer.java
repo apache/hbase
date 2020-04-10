c5fa7d70bcddd84b6f04de77fd15b49fe998137d
gentlewang <gentlewang@gentlewangdeMacBook-Pro.local>
2020-04-10 10:31:27 +0800

    The max merge count of metafixer should be configurable in MetaFixer

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/MetaFixer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/MetaFixer.java
@@ -51,8 +51,6 @@ import org.apache.hbase.thirdparty.com.google.common.annotations.VisibleForTesti
 @InterfaceAudience.Private
 class MetaFixer {
   private static final Logger LOG = LoggerFactory.getLogger(MetaFixer.class);
-  private static final String MAX_MERGE_COUNT_KEY = "hbase.master.metafixer.max.merge.count";
-  private static final int MAX_MERGE_COUNT_DEFAULT = 10;
 
   private final MasterServices masterServices;
   /**
@@ -63,7 +61,7 @@ class MetaFixer {
   MetaFixer(MasterServices masterServices) {
     this.masterServices = masterServices;
     this.maxMergeCount = this.masterServices.getConfiguration().
-        getInt(MAX_MERGE_COUNT_KEY, MAX_MERGE_COUNT_DEFAULT);
+        getInt("hbase.master.metafixer.max.merge.count", 10);
   }
 
   void fix() throws IOException {
