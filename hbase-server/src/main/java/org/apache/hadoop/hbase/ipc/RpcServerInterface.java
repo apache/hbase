6e6f3aaff67ff85e58129ba310fa3bad30818ec7
Reid Chan <reidchan@apache.org>
2020-04-17 10:32:09 +0800

    HBASE-24174 Fix findbugs warning on ServiceAuthorizationManager

--- hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServerInterface.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/RpcServerInterface.java
@@ -22,6 +22,7 @@ package org.apache.hadoop.hbase.ipc;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
 import org.apache.hadoop.hbase.classification.InterfaceStability;
 import org.apache.hadoop.hbase.CellScanner;
@@ -78,11 +79,12 @@ public interface RpcServerInterface {
 
   /**
    * Refresh authentication manager policy.
+   * @param conf configuration for refresh
    * @param pp
    */
   @VisibleForTesting
-  void refreshAuthManager(PolicyProvider pp);
-  
+  void refreshAuthManager(Configuration conf, PolicyProvider pp);
+
   RpcScheduler getScheduler();
 
   void setRsRpcServices(RSRpcServices rsRpcServices);
