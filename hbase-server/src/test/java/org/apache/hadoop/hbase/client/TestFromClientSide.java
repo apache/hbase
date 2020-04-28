8aee34daf4dbc14e840affc333544c54c81906b6
Duo Zhang <zhangduo@apache.org>
2020-04-28 12:14:55 +0800

    HBASE-24265 Remove hedged rpc call support, implement the logic in MaterRegistry directly

--- hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestFromClientSide.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestFromClientSide.java
@@ -24,6 +24,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
