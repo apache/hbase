f6ee3163bddd787a8d0c44ffe28adb32d1fc84db
Nick Dimiduk <ndimiduk@apache.org>
2020-04-30 15:00:57 -0700

    HBASE-24295 [Chaos Monkey] abstract logging through the class hierarchy
    Adds `protected abstract Logger getLogger()` to `Action` so that
    implementation's names are logged when actions are performed.
    
    Signed-off-by: stack <stack@apache.org>
    Signed-off-by: Jan Hentschel <jan.hentschel@ultratendency.com>

--- hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/FlushRandomRegionOfTableAction.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/FlushRandomRegionOfTableAction.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -19,12 +19,11 @@
 package org.apache.hadoop.hbase.chaos.actions;
 
 import java.util.List;
-
 import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.chaos.monkies.PolicyBasedChaosMonkey;
 import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.RegionInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,25 +45,29 @@ public class FlushRandomRegionOfTableAction extends Action {
     this.tableName = tableName;
   }
 
+  @Override protected Logger getLogger() {
+    return LOG;
+  }
+
   @Override
   public void perform() throws Exception {
     HBaseTestingUtility util = context.getHBaseIntegrationTestingUtility();
     Admin admin = util.getAdmin();
 
-    LOG.info("Performing action: Flush random region of table " + tableName);
-    List<HRegionInfo> regions = admin.getTableRegions(tableName);
+    getLogger().info("Performing action: Flush random region of table " + tableName);
+    List<RegionInfo> regions = admin.getRegions(tableName);
     if (regions == null || regions.isEmpty()) {
-      LOG.info("Table " + tableName + " doesn't have regions to flush");
+      getLogger().info("Table " + tableName + " doesn't have regions to flush");
       return;
     }
 
-    HRegionInfo region = PolicyBasedChaosMonkey.selectRandomItem(
-      regions.toArray(new HRegionInfo[regions.size()]));
-    LOG.debug("Flushing region " + region.getRegionNameAsString());
+    RegionInfo region = PolicyBasedChaosMonkey.selectRandomItem(
+      regions.toArray(new RegionInfo[0]));
+    getLogger().debug("Flushing region " + region.getRegionNameAsString());
     try {
       admin.flushRegion(region.getRegionName());
     } catch (Exception ex) {
-      LOG.warn("Flush failed, might be caused by other chaos: " + ex.getMessage());
+      getLogger().warn("Flush failed, might be caused by other chaos: " + ex.getMessage());
     }
     if (sleepTime > 0) {
       Thread.sleep(sleepTime);
--- hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/MoveRegionsOfTableAction.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/chaos/actions/MoveRegionsOfTableAction.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -22,12 +22,9 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-
 import org.apache.commons.lang3.RandomUtils;
-import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.chaos.factories.MonkeyConstants;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.RegionInfo;
 import org.slf4j.Logger;
@@ -37,20 +34,19 @@ import org.slf4j.LoggerFactory;
 * Action that tries to move every region of a table.
 */
 public class MoveRegionsOfTableAction extends Action {
-  private static final Logger LOG =
-      LoggerFactory.getLogger(MoveRegionsOfTableAction.class);
+  private static final Logger LOG = LoggerFactory.getLogger(MoveRegionsOfTableAction.class);
   private final long sleepTime;
   private final TableName tableName;
   private final long maxTime;
-
-  public MoveRegionsOfTableAction(TableName tableName) {
-    this(-1, MonkeyConstants.DEFAULT_MOVE_REGIONS_MAX_TIME, tableName);
-  }
 
   public MoveRegionsOfTableAction(long sleepTime, long maxSleepTime, TableName tableName) {
     this.sleepTime = sleepTime;
     this.tableName = tableName;
     this.maxTime = maxSleepTime;
+  }
+
+  @Override protected Logger getLogger() {
+    return LOG;
   }
 
   @Override
@@ -62,24 +58,24 @@ public class MoveRegionsOfTableAction extends Action {
     Admin admin = this.context.getHBaseIntegrationTestingUtility().getAdmin();
     ServerName[] servers = getServers(admin);
 
-    LOG.info("Performing action: Move regions of table {}", tableName);
-    List<HRegionInfo> regions = admin.getTableRegions(tableName);
+    getLogger().info("Performing action: Move regions of table {}", tableName);
+    List<RegionInfo> regions = admin.getRegions(tableName);
     if (regions == null || regions.isEmpty()) {
-      LOG.info("Table {} doesn't have regions to move", tableName);
+      getLogger().info("Table {} doesn't have regions to move", tableName);
       return;
     }
 
     Collections.shuffle(regions);
 
     long start = System.currentTimeMillis();
-    for (HRegionInfo regionInfo:regions) {
+    for (RegionInfo regionInfo:regions) {
 
       // Don't try the move if we're stopping
       if (context.isStopping()) {
         return;
       }
 
-      moveRegion(admin, servers, regionInfo);
+      moveRegion(admin, servers, regionInfo, getLogger());
       if (sleepTime > 0) {
         Thread.sleep(sleepTime);
       }
@@ -94,16 +90,16 @@ public class MoveRegionsOfTableAction extends Action {
 
   static ServerName [] getServers(Admin admin) throws IOException {
     Collection<ServerName> serversList = admin.getRegionServers();
-    return serversList.toArray(new ServerName[serversList.size()]);
+    return serversList.toArray(new ServerName[0]);
   }
 
-  static void moveRegion(Admin admin, ServerName [] servers, RegionInfo regionInfo) {
+  static void moveRegion(Admin admin, ServerName [] servers, RegionInfo regionInfo, Logger logger) {
     try {
       ServerName destServerName = servers[RandomUtils.nextInt(0, servers.length)];
-      LOG.debug("Moving {} to {}", regionInfo.getRegionNameAsString(), destServerName);
+      logger.debug("Moving {} to {}", regionInfo.getRegionNameAsString(), destServerName);
       admin.move(regionInfo.getEncodedNameAsBytes(), destServerName);
     } catch (Exception ex) {
-      LOG.warn("Move failed, might be caused by other chaos: {}", ex.getMessage());
+      logger.warn("Move failed, might be caused by other chaos: {}", ex.getMessage());
     }
   }
 }
