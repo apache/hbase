47f42f5ac5f85b1dc55004160581adae747925dc
pustota2009 <61382543+pustota2009@users.noreply.github.com>
2020-05-04 19:06:18 +0300

    Update TestLruBlockCache.java

--- hbase-server/src/test/java/org/apache/hadoop/hbase/io/hfile/TestLruBlockCache.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/io/hfile/TestLruBlockCache.java
@@ -1067,7 +1067,6 @@ public class TestLruBlockCache {
     // It means some of blocka were not put into BlockCache
     for (BlockCacheKey key : cache.getMapForTests().keySet())
       Assert.assertTrue(key.getOffset() % 100 < percentOfCachedBlocks);
-
   }
 
   @Test
