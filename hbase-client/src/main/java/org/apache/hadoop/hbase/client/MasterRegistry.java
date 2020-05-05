deb1557539d3fff169e5aaa18b55b050d882341e
Duo Zhang <zhangduo@apache.org>
2020-05-05 11:00:54 +0800

    address review comments, and tests for hedged reads

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/MasterRegistry.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/MasterRegistry.java
@@ -24,11 +24,13 @@ import static org.apache.hadoop.hbase.util.FutureUtils.addListener;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import org.apache.hadoop.conf.Configuration;
@@ -48,7 +50,7 @@ import org.apache.yetus.audience.InterfaceAudience;
 import org.apache.hbase.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.hbase.thirdparty.com.google.common.base.Preconditions;
 import org.apache.hbase.thirdparty.com.google.common.base.Strings;
-import org.apache.hbase.thirdparty.com.google.common.collect.ImmutableList;
+import org.apache.hbase.thirdparty.com.google.common.collect.ImmutableMap;
 import org.apache.hbase.thirdparty.com.google.common.net.HostAndPort;
 import org.apache.hbase.thirdparty.com.google.protobuf.Message;
 import org.apache.hbase.thirdparty.com.google.protobuf.RpcCallback;
@@ -66,10 +68,9 @@ import org.apache.hadoop.hbase.shaded.protobuf.generated.MasterProtos.GetMetaReg
  * Master based registry implementation. Makes RPCs to the configured master addresses from config
  * {@value org.apache.hadoop.hbase.HConstants#MASTER_ADDRS_KEY}.
  * <p/>
- * It supports hedged reads, which can be enabled by setting
- * {@value org.apache.hadoop.hbase.HConstants#MASTER_REGISTRY_ENABLE_HEDGED_READS_KEY} to True. Fan
- * out the requests batch is controlled by
- * {@value org.apache.hadoop.hbase.HConstants#HBASE_RPCS_HEDGED_REQS_FANOUT_KEY}.
+ * It supports hedged reads, set the fan out of the requests batch by
+ * {@link #MASTER_REGISTRY_HEDGED_REQS_FANOUT_KEY} to a value greater than {@code 1} will enable
+ * it(the default value is {@link #MASTER_REGISTRY_HEDGED_REQS_FANOUT_DEFAULT}).
  * <p/>
  * TODO: Handle changes to the configuration dynamically without having to restart the client.
  */
@@ -88,9 +89,7 @@ public class MasterRegistry implements ConnectionRegistry {
   private final int hedgedReadFanOut;
 
   // Configured list of masters to probe the meta information from.
-  private final Set<ServerName> masterAddrs;
-
-  private final List<ClientMetaService.Interface> masterStubs;
+  private final ImmutableMap<ServerName, ClientMetaService.Interface> masterAddr2Stub;
 
   // RPC client used to talk to the masters.
   private final RpcClient rpcClient;
@@ -114,8 +113,8 @@ public class MasterRegistry implements ConnectionRegistry {
   }
 
   MasterRegistry(Configuration conf) throws IOException {
-    this.hedgedReadFanOut = conf.getInt(MASTER_REGISTRY_HEDGED_REQS_FANOUT_KEY,
-      MASTER_REGISTRY_HEDGED_REQS_FANOUT_DEFAULT);
+    this.hedgedReadFanOut = Math.max(1, conf.getInt(MASTER_REGISTRY_HEDGED_REQS_FANOUT_KEY,
+      MASTER_REGISTRY_HEDGED_REQS_FANOUT_DEFAULT));
     int rpcTimeoutMs = (int) Math.min(Integer.MAX_VALUE,
       conf.getLong(HConstants.HBASE_RPC_TIMEOUT_KEY, HConstants.DEFAULT_HBASE_RPC_TIMEOUT));
     // XXX: we pass cluster id as null here since we do not have a cluster id yet, we have to fetch
@@ -123,20 +122,23 @@ public class MasterRegistry implements ConnectionRegistry {
     // This is a problem as we will use the cluster id to determine the authentication method
     rpcClient = RpcClientFactory.createClient(conf, null);
     rpcControllerFactory = RpcControllerFactory.instantiate(conf);
-    masterAddrs = parseMasterAddrs(conf);
-    ImmutableList.Builder<ClientMetaService.Interface> builder =
-      ImmutableList.builderWithExpectedSize(masterAddrs.size());
+    Set<ServerName> masterAddrs = parseMasterAddrs(conf);
+    ImmutableMap.Builder<ServerName, ClientMetaService.Interface> builder =
+      ImmutableMap.builderWithExpectedSize(masterAddrs.size());
     User user = User.getCurrent();
     for (ServerName masterAddr : masterAddrs) {
-      builder
-        .add(ClientMetaService.newStub(rpcClient.createRpcChannel(masterAddr, user, rpcTimeoutMs)));
+      builder.put(masterAddr,
+        ClientMetaService.newStub(rpcClient.createRpcChannel(masterAddr, user, rpcTimeoutMs)));
     }
-    masterStubs = builder.build();
+    masterAddr2Stub = builder.build();
   }
 
   /**
    * Builds the default master address end point if it is not specified in the configuration.
+   * <p/>
+   * Will be called in {@code HBaseTestingUtility}.
    */
+  @VisibleForTesting
   public static String getMasterAddr(Configuration conf) throws UnknownHostException {
     String masterAddrFromConf = conf.get(MASTER_ADDRS_KEY);
     if (!Strings.isNullOrEmpty(masterAddrFromConf)) {
@@ -147,6 +149,15 @@ public class MasterRegistry implements ConnectionRegistry {
     return String.format("%s:%d", hostname, port);
   }
 
+  /**
+   * For describing the actual asynchronous rpc call.
+   * <p/>
+   * Typically, you can use lambda expression to implement this interface as
+   * 
+   * <pre>
+   * (c, s, d) -> s.xxx(c, <your request here>, d)
+   * </pre>
+   */
   @FunctionalInterface
   private interface Callable<T> {
     void call(HBaseRpcController controller, ClientMetaService.Interface stub, RpcCallback<T> done);
@@ -170,10 +181,13 @@ public class MasterRegistry implements ConnectionRegistry {
     return new IOException(String.format("Invalid result for request %s. Will be retried", debug));
   }
 
-  // send requests concurrently to hedgedReadsFanout masters
-  private <T extends Message> void groupCall(CompletableFuture<T> future, int startIndexInclusive,
-    Callable<T> callable, Predicate<T> isValidResp, String debug,
-    ConcurrentLinkedQueue<Throwable> errors) {
+  // send requests concurrently to hedgedReadsFanout masters. If any of the request is succeeded, we
+  // will complete the future and quit. If all the requests in one round are failed, we will start
+  // another round to send requests concurrently tohedgedReadsFanout masters. If all masters have
+  // been tried and all of them are failed, we will fail the future.
+  private <T extends Message> void groupCall(CompletableFuture<T> future,
+    List<ClientMetaService.Interface> masterStubs, int startIndexInclusive, Callable<T> callable,
+    Predicate<T> isValidResp, String debug, ConcurrentLinkedQueue<Throwable> errors) {
     int endIndexExclusive = Math.min(startIndexInclusive + hedgedReadFanOut, masterStubs.size());
     AtomicInteger remaining = new AtomicInteger(endIndexExclusive - startIndexInclusive);
     for (int i = startIndexInclusive; i < endIndexExclusive; i++) {
@@ -193,9 +207,11 @@ public class MasterRegistry implements ConnectionRegistry {
               // we are done, complete the future with exception
               RetriesExhaustedException ex = new RetriesExhaustedException("masters",
                 masterStubs.size(), new ArrayList<>(errors));
-              future.completeExceptionally(new MasterRegistryFetchException(masterAddrs, ex));
+              future.completeExceptionally(
+                new MasterRegistryFetchException(masterAddr2Stub.keySet(), ex));
             } else {
-              groupCall(future, endIndexExclusive, callable, isValidResp, debug, errors);
+              groupCall(future, masterStubs, endIndexExclusive, callable, isValidResp, debug,
+                errors);
             }
           }
         } else {
@@ -208,9 +224,10 @@ public class MasterRegistry implements ConnectionRegistry {
 
   private <T extends Message> CompletableFuture<T> call(Callable<T> callable,
     Predicate<T> isValidResp, String debug) {
+    List<ClientMetaService.Interface> masterStubs = new ArrayList<>(masterAddr2Stub.values());
+    Collections.shuffle(masterStubs, ThreadLocalRandom.current());
     CompletableFuture<T> future = new CompletableFuture<>();
-    ConcurrentLinkedQueue<Throwable> errors = new ConcurrentLinkedQueue<>();
-    groupCall(future, 0, callable, isValidResp, debug, errors);
+    groupCall(future, masterStubs, 0, callable, isValidResp, debug, new ConcurrentLinkedQueue<>());
     return future;
   }
 
@@ -255,7 +272,7 @@ public class MasterRegistry implements ConnectionRegistry {
 
   @VisibleForTesting
   Set<ServerName> getParsedMasterServers() {
-    return masterAddrs;
+    return masterAddr2Stub.keySet();
   }
 
   @Override
