f41c9038e72d4e85e9a3c87d11b5cb872fb85bbf
Duo Zhang <zhangduo@apache.org>
2020-05-09 12:00:45 +0800

    HBASE-24333 Backport HBASE-24304 "Separate a hbase-asyncfs module" to branch-2.x (#1664)
    Signed-off-by: stack <stack@apache.org>

--- hbase-asyncfs/src/main/java/org/apache/hadoop/hbase/util/CancelableProgressable.java
+++ hbase-asyncfs/src/main/java/org/apache/hadoop/hbase/util/CancelableProgressable.java
@@ -23,7 +23,7 @@ import org.apache.yetus.audience.InterfaceAudience;
 /**
  * Similar interface as {@link org.apache.hadoop.util.Progressable} but returns
  * a boolean to support canceling the operation.
- * <p>
+ * <p/>
  * Used for doing updating of OPENING znode during log replay on region open.
  */
 @InterfaceAudience.Private
--- hbase-asyncfs/src/test/java/org/apache/hadoop/hbase/io/asyncfs/TestLocalAsyncOutput.java
+++ hbase-asyncfs/src/test/java/org/apache/hadoop/hbase/io/asyncfs/TestLocalAsyncOutput.java
@@ -22,7 +22,7 @@ import java.util.concurrent.ExecutionException;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
-import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.HBaseCommonTestingUtility;
 import org.apache.hadoop.hbase.testclassification.MiscTests;
 import org.apache.hadoop.hbase.testclassification.SmallTests;
 import org.apache.hadoop.hbase.util.CommonFSUtils;
@@ -47,7 +47,7 @@ public class TestLocalAsyncOutput {
 
   private static Class<? extends Channel> CHANNEL_CLASS = NioSocketChannel.class;
 
-  private static final HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
+  private static final HBaseCommonTestingUtility TEST_UTIL = new HBaseCommonTestingUtility();
 
   @AfterClass
   public static void tearDownAfterClass() throws IOException {
