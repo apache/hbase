2fba96db09a2875389187618e837581ec120aa31
WenFeiYi <wenfeiyi666@gmail.com>
2020-05-25 20:58:12 +0800

    HBASE-24427 HStore.add log format error (#1773)
    Signed-off-by: Viraj Jasani <vjasani@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
@@ -751,8 +751,8 @@ public class HStore implements Store, HeapSize, StoreConfigInformation, Propagat
     lock.readLock().lock();
     try {
       if (this.currentParallelPutCount.getAndIncrement() > this.parallelPutCountPrintThreshold) {
-        LOG.trace(this.getTableName() + "tableName={}, encodedName={}, columnFamilyName={} is " +
-          "too busy!", this.getRegionInfo().getEncodedName(), this .getColumnFamilyName());
+        LOG.trace("tableName={}, encodedName={}, columnFamilyName={} is too busy!",
+            this.getTableName(), this.getRegionInfo().getEncodedName(), this.getColumnFamilyName());
       }
       this.memstore.add(cell, memstoreSizing);
     } finally {
@@ -768,8 +768,8 @@ public class HStore implements Store, HeapSize, StoreConfigInformation, Propagat
     lock.readLock().lock();
     try {
       if (this.currentParallelPutCount.getAndIncrement() > this.parallelPutCountPrintThreshold) {
-        LOG.trace(this.getTableName() + "tableName={}, encodedName={}, columnFamilyName={} is " +
-            "too busy!", this.getRegionInfo().getEncodedName(), this .getColumnFamilyName());
+        LOG.trace("tableName={}, encodedName={}, columnFamilyName={} is too busy!",
+            this.getTableName(), this.getRegionInfo().getEncodedName(), this.getColumnFamilyName());
       }
       memstore.add(cells, memstoreSizing);
     } finally {
