604d7bdfb82834f63f2f06ff30702bd6a59f7b0b
xincunSong <365724453@qq.com>
2020-05-29 21:28:57 +0800

    HBASE-24470 : Add store file info when log meessages in HStore. (#1804)
    Signed-off-by: binlijin <binlijin@gmail.com>
    Signed-off-by: Viraj Jasani <vjasani@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
@@ -615,7 +615,7 @@ public class HStore implements Store, HeapSize, StoreConfigInformation, Propagat
             file.closeStoreFile(evictOnClose);
           }
         } catch (IOException e) {
-          LOG.warn("Could not close store file", e);
+          LOG.warn("Could not close store file {}", file, e);
         }
       }
       throw ioe;
@@ -2754,13 +2754,15 @@ public class HStore implements Store, HeapSize, StoreConfigInformation, Propagat
       file.initReader();
       length = file.getReader().length();
     } catch (IOException e) {
-      LOG.trace("Failed to open reader when trying to compute store file size, ignoring", e);
+      LOG.trace("Failed to open reader when trying to compute store file size for {}, ignoring",
+        file, e);
     } finally {
       try {
         file.closeStoreFile(
             file.getCacheConf() != null ? file.getCacheConf().shouldEvictOnClose() : true);
       } catch (IOException e) {
-        LOG.trace("Failed to close reader after computing store file size, ignoring", e);
+        LOG.trace("Failed to close reader after computing store file size for {}, ignoring",
+          file, e);
       }
     }
     return length;
