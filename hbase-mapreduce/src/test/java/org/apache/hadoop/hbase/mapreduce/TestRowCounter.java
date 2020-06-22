605b92c2c983bcb53c8857e66a4bdf39b653becf
Wellington Chevreuil <wchevreuil@apache.org>
2020-06-22 19:14:22 +0100

    HBASE-21773 - Addendum - Bring back "public static Job createSubmittableJob(Configuration conf, String[] args)" for compatibility reasons.

--- hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/mapreduce/TestRowCounter.java
+++ hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/mapreduce/TestRowCounter.java
@@ -36,6 +36,8 @@ import org.apache.hadoop.hbase.testclassification.LargeTests;
 import org.apache.hadoop.hbase.testclassification.MapReduceTests;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.LauncherSecurityManager;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Job;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -305,6 +307,224 @@ public class TestRowCounter {
     long duration = System.currentTimeMillis() - start;
     LOG.debug("row count duration (ms): " + duration);
     assertTrue(result==0);
+  }
+
+  /**
+   * Run the RowCounter map reduce job and verify the row count.
+   *
+   * @param args the command line arguments to be used for rowcounter job.
+   * @param expectedCount the expected row count (result of map reduce job).
+   * @throws Exception
+   */
+  private void runCreateSubmittableJobWithArgs(String[] args, int expectedCount) throws Exception {
+    Job job = RowCounter.createSubmittableJob(TEST_UTIL.getConfiguration(), args);
+    long start = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    long duration = System.currentTimeMillis() - start;
+    LOG.debug("row count duration (ms): " + duration);
+    assertTrue(job.isSuccessful());
+    Counter counter = job.getCounters().findCounter(RowCounter.RowCounterMapper.Counters.ROWS);
+    assertEquals(expectedCount, counter.getValue());
+  }
+
+  @Test
+  public void testCreateSubmittableJobWithArgsNoColumn() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME
+    };
+    runCreateSubmittableJobWithArgs(args, 10);
+  }
+
+  /**
+   * Test a case when the column specified in command line arguments is
+   * exclusive for few rows.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsExclusiveColumn() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, COL_FAM + ":" + COL1
+    };
+    runCreateSubmittableJobWithArgs(args, 8);
+  }
+
+  /**
+   * Test a case when the column specified in command line arguments is
+   * one for which the qualifier contains colons.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsColumnWithColonInQualifier() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, COL_FAM + ":" + COMPOSITE_COLUMN
+    };
+    runCreateSubmittableJobWithArgs(args, 8);
+  }
+
+  /**
+   * Test a case when the column specified in command line arguments is not part
+   * of first KV for a row.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsHiddenColumn() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, COL_FAM + ":" + COL2
+    };
+    runCreateSubmittableJobWithArgs(args, 10);
+  }
+
+
+  /**
+   * Test a case when the column specified in command line arguments is
+   * exclusive for few rows and also a row range filter is specified
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsColumnAndRowRange() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, "--range=\\x00rov,\\x00rox", COL_FAM + ":" + COL1
+    };
+    runCreateSubmittableJobWithArgs(args, 8);
+  }
+
+  /**
+   * Test a case when a range is specified with single range of start-end keys
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsRowSingleRange() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, "--range=\\x00row1,\\x00row3"
+    };
+    runCreateSubmittableJobWithArgs(args, 2);
+  }
+
+  /**
+   * Test a case when a range is specified with single range with end key only
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsRowSingleRangeUpperBound() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, "--range=,\\x00row3"
+    };
+    runCreateSubmittableJobWithArgs(args, 3);
+  }
+
+  /**
+   * Test a case when a range is specified with two ranges where one range is with end key only
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsRowMultiRangeUpperBound() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, "--range=,\\x00row3;\\x00row5,\\x00row7"
+    };
+    runCreateSubmittableJobWithArgs(args, 5);
+  }
+
+  /**
+   * Test a case when a range is specified with multiple ranges of start-end keys
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsRowMultiRange() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, "--range=\\x00row1,\\x00row3;\\x00row5,\\x00row8"
+    };
+    runCreateSubmittableJobWithArgs(args, 5);
+  }
+
+  /**
+   * Test a case when a range is specified with multiple ranges of start-end keys;
+   * one range is filled, another two are not
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsRowMultiEmptyRange() throws Exception {
+    String[] args = new String[] {
+      TABLE_NAME, "--range=\\x00row1,\\x00row3;;"
+    };
+    runCreateSubmittableJobWithArgs(args, 2);
+  }
+
+  @Test
+  public void testCreateSubmittableJobWithArgs10kRowRange() throws Exception {
+    String tableName = TABLE_NAME + "CreateSubmittableJobWithArgs10kRowRange";
+
+    try (Table table = TEST_UTIL.createTable(
+      TableName.valueOf(tableName), Bytes.toBytes(COL_FAM))) {
+      writeRows(table, 10000, 0);
+    }
+    String[] args = new String[] {
+      tableName, "--range=\\x00row9872,\\x00row9875"
+    };
+    runCreateSubmittableJobWithArgs(args, 3);
+  }
+
+  /**
+   * Test a case when the timerange is specified with --starttime and --endtime options
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testCreateSubmittableJobWithArgsTimeRange() throws Exception {
+    final byte[] family = Bytes.toBytes(COL_FAM);
+    final byte[] col1 = Bytes.toBytes(COL1);
+    Put put1 = new Put(Bytes.toBytes("row_timerange_" + 1));
+    Put put2 = new Put(Bytes.toBytes("row_timerange_" + 2));
+    Put put3 = new Put(Bytes.toBytes("row_timerange_" + 3));
+
+    long ts;
+
+    String tableName = TABLE_NAME_TS_RANGE+"CreateSubmittableJobWithArgs";
+    // clean up content of TABLE_NAME
+    Table table = TEST_UTIL.createTable(TableName.valueOf(tableName), Bytes.toBytes(COL_FAM));
+
+    ts = System.currentTimeMillis();
+    put1.addColumn(family, col1, ts, Bytes.toBytes("val1"));
+    table.put(put1);
+    Thread.sleep(100);
+
+    ts = System.currentTimeMillis();
+    put2.addColumn(family, col1, ts, Bytes.toBytes("val2"));
+    put3.addColumn(family, col1, ts, Bytes.toBytes("val3"));
+    table.put(put2);
+    table.put(put3);
+    table.close();
+
+    String[] args = new String[] {
+      tableName, COL_FAM + ":" + COL1,
+      "--starttime=" + 0,
+      "--endtime=" + ts
+    };
+    runCreateSubmittableJobWithArgs(args, 1);
+
+    args = new String[] {
+      tableName, COL_FAM + ":" + COL1,
+      "--starttime=" + 0,
+      "--endtime=" + (ts - 10)
+    };
+    runCreateSubmittableJobWithArgs(args, 1);
+
+    args = new String[] {
+      tableName, COL_FAM + ":" + COL1,
+      "--starttime=" + ts,
+      "--endtime=" + (ts + 1000)
+    };
+    runCreateSubmittableJobWithArgs(args, 2);
+
+    args = new String[] {
+      tableName, COL_FAM + ":" + COL1,
+      "--starttime=" + (ts - 30 * 1000),
+      "--endtime=" + (ts + 30 * 1000),
+    };
+    runCreateSubmittableJobWithArgs(args, 3);
   }
 
   /**
