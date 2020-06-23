d5667720e7de3a412faa2586dcdc06f531c0e338
Elliot Miller <elliot_miller@apple.com>
2020-06-23 17:39:39 -0400

    Resolve whitespace issues

--- hbase-shell/src/main/ruby/hbase/admin.rb
+++ hbase-shell/src/main/ruby/hbase/admin.rb
@@ -765,8 +765,8 @@ module Hbase
           v = String.new(value)
           v.strip!
           # TODO: We should not require user to config the coprocessor with our inner format.
-	  # This is a roundabout approach, but will be replaced shortly since
-	  # the setCoprocessorWithSpec method is marked for deprecation.
+          # This is a roundabout approach, but will be replaced shortly since
+          # the setCoprocessorWithSpec method is marked for deprecation.
           coprocessor_descriptors = tdb.build.setCoprocessorWithSpec(v).getCoprocessorDescriptors
           tdb.setCoprocessors(coprocessor_descriptors)
           valid_coproc_keys << key
