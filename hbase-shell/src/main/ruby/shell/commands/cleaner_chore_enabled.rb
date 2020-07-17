4b3ef815ebe4f01e85b3534e439f0e3f91e40f9a
Elliot <elliot_miller@apple.com>
2020-07-17 14:44:30 -0400

    HBASE-24722 Update commands with unintentional return values (#2058)
    - Prior to this commit, there were 13 commands that unintentionally return the
      number of lines they print (usually one). This commit ensures that they
      return the value documented by the help text, or nil if there is not a simple
      logical value to return.
    - Fixes 6 hbase-shell commands that return String rather than TrueClass or
      FalseClass
    - Use double-bang to cast truthy values to TrueClass and FalseClass so that
      ruby's to_s can reliably print true or false without using ternary operators
    - Updates tests for is_disabled, is_enabled, disable_rpc_throttle,
      enable_rpc_throttle, disable_exceed_throttle_quota,
      enable_exceed_throttle_quota, clear_deadservers, snapshot_cleanup_switch,
      snapshot_cleanup_enabled, and balancer to check return values
    - Adds new tests for balance_switch, balancer_enabled, normalizer_switch,
      normalizer_enabled, catalog_janitor_switch, catalogjanitor_enabled,
      cleaner_chore_switch, cleaner_chore_enabled, splitormerge_switch, and
      splitormerge_enabled
    
    signed-off-by: stack <stack@apache.org>

--- hbase-shell/src/main/ruby/shell/commands/cleaner_chore_enabled.rb
+++ hbase-shell/src/main/ruby/shell/commands/cleaner_chore_enabled.rb
@@ -29,7 +29,9 @@ EOF
       end
 
       def command
-        formatter.row([admin.cleaner_chore_enabled ? 'true' : 'false'])
+        current_state = !!admin.cleaner_chore_enabled
+        formatter.row([current_state.to_s])
+        current_state
       end
     end
   end
