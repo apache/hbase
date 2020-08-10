79ccc9b916f619e3ebe65d78b9dd5394e9dbad58
bsglz <18031031@qq.com>
2020-08-10 17:36:13 +0800

    HBASE-24694 Support flush a single column family of table (#2218)
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/Admin.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/Admin.java
@@ -883,6 +883,16 @@ public interface Admin extends Abortable, Closeable {
   void flush(TableName tableName) throws IOException;
 
   /**
+   * Flush the specified column family stores on all regions of the passed table.
+   * This runs as a synchronous operation.
+   *
+   * @param tableName table to flush
+   * @param columnFamily column family within a table
+   * @throws IOException if a remote or network exception occurs
+   */
+  void flush(TableName tableName, byte[] columnFamily) throws IOException;
+
+  /**
    * Flush an individual region. Synchronous operation.
    *
    * @param regionName region to flush
--- hbase-shell/src/main/ruby/shell/commands/flush.rb
+++ hbase-shell/src/main/ruby/shell/commands/flush.rb
@@ -25,10 +25,12 @@ module Shell
 Flush all regions in passed table or pass a region row to
 flush an individual region or a region server name whose format
 is 'host,port,startcode', to flush all its regions.
-You can also flush a single column family within a region.
+You can also flush a single column family for all regions within a table,
+or for an specific region only.
 For example:
 
   hbase> flush 'TABLENAME'
+  hbase> flush 'TABLENAME','FAMILYNAME'
   hbase> flush 'REGIONNAME'
   hbase> flush 'REGIONNAME','FAMILYNAME'
   hbase> flush 'ENCODED_REGIONNAME'
