c74e6083b8cec74254e8a34d6b7ed49a7a108935
Zheng Wang <18031031@qq.com>
2020-08-13 15:31:58 +0530

    HBASE-24854 Correct the help content of assign and unassign commands in hbase shell
    Closes #2241
    
    Signed-off-by: Viraj Jasani <vjasani@apache.org>
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>

--- hbase-shell/src/main/ruby/shell/commands/unassign.rb
+++ hbase-shell/src/main/ruby/shell/commands/unassign.rb
@@ -22,11 +22,14 @@ module Shell
     class Unassign < Command
       def help
         <<-EOF
-Unassign a region. Unassign will close region in current location and then
-reopen it again.  Pass 'true' to force the unassignment ('force' will clear
-all in-memory state in master before the reassign. If results in
-double assignment use hbck -fix to resolve. To be used by experts).
-Use with caution.  For expert use only.  Examples:
+Unassign a region. It could be executed only when region in expected state(OPEN).
+Pass 'true' to force the unassignment ('force' will clear all in-memory state in
+master before the reassign. If results in double assignment use hbck -fix to resolve.
+To be used by experts).
+In addition, you can use "unassigns" command available on HBCK2 tool to skip the state check.
+(For more info on HBCK2: https://github.com/apache/hbase-operator-tools/blob/master/hbase-hbck2/README.md)
+Use with caution. For experts only.
+Examples:
 
   hbase> unassign 'REGIONNAME'
   hbase> unassign 'REGIONNAME', true
