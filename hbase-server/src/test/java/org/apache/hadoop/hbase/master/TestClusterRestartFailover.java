f94a91fe6e277804f3a960412dde54c7db10424d
Guanghao Zhang <zghao@apache.org>
2020-08-25 08:30:08 +0800

    HBASE-24946 Remove the metrics assert in TestClusterRestartFailover

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestClusterRestartFailover.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestClusterRestartFailover.java
@@ -136,11 +136,6 @@ public class TestClusterRestartFailover extends AbstractTestRestartCluster {
     serverNode = UTIL.getHBaseCluster().getMaster().getAssignmentManager().getRegionStates()
         .getServerNode(SERVER_FOR_TEST);
     assertNull("serverNode should be deleted after SCP finished", serverNode);
-
-    MetricsMasterSource masterSource = UTIL.getHBaseCluster().getMaster().getMasterMetrics()
-      .getMetricsSource();
-    metricsHelper.assertCounter(MetricsMasterSource.SERVER_CRASH_METRIC_PREFIX+"SubmittedCount",
-      4, masterSource);
   }
 
   private void setupCluster() throws Exception {
