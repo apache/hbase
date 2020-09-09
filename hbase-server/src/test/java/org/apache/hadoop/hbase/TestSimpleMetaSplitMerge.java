b38dda4c30cfe6c13552637e9e3df2f44a0360cb
Duo Zhang <zhangduo@apache.org>
2020-09-09 12:38:32 +0800

    HBASE-24606 Implement meta merge (#2311)
    Signed-off-by: Guanghao Zhang <zghao@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/TestSimpleMetaSplitMerge.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/TestSimpleMetaSplitMerge.java
@@ -20,17 +20,23 @@ package org.apache.hadoop.hbase;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.List;
+import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptorBuilder;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.RegionInfo;
 import org.apache.hadoop.hbase.client.RegionLocator;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.apache.hadoop.hbase.client.TableDescriptorBuilder;
+import org.apache.hadoop.hbase.regionserver.HRegion;
+import org.apache.hadoop.hbase.regionserver.HStore;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.testclassification.MiscTests;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.JVMClusterUtil;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -38,11 +44,11 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category({ MiscTests.class, MediumTests.class })
-public class TestSimpleMetaSplit {
+public class TestSimpleMetaSplitMerge {
 
   @ClassRule
   public static final HBaseClassTestRule CLASS_RULE =
-    HBaseClassTestRule.forClass(TestSimpleMetaSplit.class);
+    HBaseClassTestRule.forClass(TestSimpleMetaSplitMerge.class);
 
   private static final HBaseTestingUtility UTIL = new HBaseTestingUtility();
 
@@ -70,29 +76,66 @@ public class TestSimpleMetaSplit {
     UTIL.shutdownMiniCluster();
   }
 
+  private void assertMetaRegionCount(int count) {
+    // do not count it from client as it will reset the location cache for meta table
+    assertEquals(count, UTIL.getMiniHBaseCluster().getMaster().getAssignmentManager()
+      .getRegionStates().getRegionsOfTable(TableName.META_TABLE_NAME).size());
+  }
+
+  private void clearCache(TableName tableName) throws IOException {
+    try (RegionLocator locator = UTIL.getConnection().getRegionLocator(tableName)) {
+      locator.clearRegionLocationCache();
+    }
+  }
+
+  private void assertValue(TableName tableName, String row) throws IOException {
+    try (Table table = UTIL.getConnection().getTable(tableName)) {
+      Result result = table.get(new Get(Bytes.toBytes(row)));
+      assertEquals(row, Bytes.toString(result.getValue(CF, CQ)));
+    }
+  }
+
   @Test
-  public void test() throws IOException {
+  public void test() throws Exception {
     try (Table table = UTIL.getConnection().getTable(TD1.getTableName())) {
       table.put(new Put(Bytes.toBytes("row1")).addColumn(CF, CQ, Bytes.toBytes("row1")));
     }
     try (Table table = UTIL.getConnection().getTable(TD2.getTableName())) {
       table.put(new Put(Bytes.toBytes("row2")).addColumn(CF, CQ, Bytes.toBytes("row2")));
     }
+    Admin admin = UTIL.getAdmin();
     // split meta
-    UTIL.getAdmin().split(TableName.META_TABLE_NAME, Bytes.toBytes("b"));
-    // do not count it from client as it will reset the location cache for meta table
-    assertEquals(2, UTIL.getMiniHBaseCluster().getMaster().getAssignmentManager().getRegionStates()
-      .getRegionsOfTable(TableName.META_TABLE_NAME).size());
+    admin.split(TableName.META_TABLE_NAME, Bytes.toBytes("b"));
+    assertMetaRegionCount(2);
     // clear the cache for table 'b'
-    try (RegionLocator locator = UTIL.getConnection().getRegionLocator(TD2.getTableName())) {
-      locator.clearRegionLocationCache();
-    }
+    clearCache(TD2.getTableName());
     // make sure that we could get the location of the TD2 from the second meta region
-    try (Table table = UTIL.getConnection().getTable(TD2.getTableName())) {
-      Result result = table.get(new Get(Bytes.toBytes("row2")));
-      assertEquals("row2", Bytes.toString(result.getValue(CF, CQ)));
-    }
+    assertValue(TD2.getTableName(), "row2");
     // assert from client side
-    assertEquals(2, UTIL.getAdmin().getRegions(TableName.META_TABLE_NAME).size());
+    List<RegionInfo> regions = admin.getRegions(TableName.META_TABLE_NAME);
+    assertEquals(2, regions.size());
+    // compact to make sure we can merge
+    for (JVMClusterUtil.RegionServerThread t : UTIL.getMiniHBaseCluster()
+      .getRegionServerThreads()) {
+      for (HRegion r : t.getRegionServer().getOnlineRegionsLocalContext()) {
+        if (TableName.isMetaTableName(r.getRegionInfo().getTable())) {
+          r.compact(true);
+          for (HStore store : r.getStores()) {
+            store.closeAndArchiveCompactedFiles();
+          }
+        }
+      }
+    }
+    // merge the 2 regions back to 1
+    admin.mergeRegionsAsync(regions.stream().map(RegionInfo::getRegionName).toArray(byte[][]::new),
+      false).get();
+    assertMetaRegionCount(1);
+    // clear the cache for table 'a' and 'b'
+    clearCache(TD1.getTableName());
+    clearCache(TD2.getTableName());
+
+    // make sure that we could still get the locations from the new meta region
+    assertValue(TD2.getTableName(), "row2");
+    assertValue(TD1.getTableName(), "row1");
   }
 }
