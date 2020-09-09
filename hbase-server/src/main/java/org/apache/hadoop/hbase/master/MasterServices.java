a84cabfa51dd886f2448b9477e89db5f3e6ee5fd
XinSun <ddupgs@gmail.com>
2020-09-09 15:00:37 +0800

    HBASE-24999 Master manages ReplicationServers

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterServices.java
@@ -102,6 +102,11 @@ public interface MasterServices extends Server {
   ServerManager getServerManager();
 
   /**
+   * @return Master's {@link ReplicationServerManager} instance.
+   */
+  ReplicationServerManager getReplicationServerManager();
+
+  /**
    * @return Master's instance of {@link ExecutorService}
    */
   ExecutorService getExecutorService();
