f0c2b3749c5f08b3f4d9db91eb705a80dd9b7f64
shahrs87 <shahrs87@gmail.com>
2020-09-17 09:26:35 -0700

    HBASE-24956 : ConnectionManager#locateRegionInMeta waits for user region lock indefinitely. (#2322)
    Closes #2427
    
    Signed-off-by: Bharath Vissapragada <bharathv@apache.org>
    Signed-off-by: Andrew Purtell <apurtell@apache.org>
    Signed-off-by: Viraj Jasani <vjasani@apache.org>
    Signed-off-by: Michael Stack <stack@apache.org>
    Signed-off-by: Guanghao Zhang <zghao@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionConfiguration.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionConfiguration.java
@@ -63,7 +63,7 @@ public class ConnectionConfiguration {
   // toggle for async/sync prefetch
   private final boolean clientScannerAsyncPrefetch;
 
-    /**
+  /**
    * Constructor
    * @param conf Configuration object
    */
@@ -208,5 +208,4 @@ public class ConnectionConfiguration {
   public int getRpcTimeout() {
     return rpcTimeout;
   }
-
 }
