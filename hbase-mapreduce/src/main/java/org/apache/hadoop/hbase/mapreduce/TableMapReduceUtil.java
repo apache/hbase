3aec1989ebe05884645be052b9217967da500f70
Duo Zhang <zhangduo@apache.org>
2020-09-17 09:51:08 +0800

    HBASE-25054 Do not add non shaded protobuf as dependency in TableMapReduceUtil for master

--- hbase-mapreduce/src/main/java/org/apache/hadoop/hbase/mapreduce/TableMapReduceUtil.java
+++ hbase-mapreduce/src/main/java/org/apache/hadoop/hbase/mapreduce/TableMapReduceUtil.java
@@ -819,7 +819,6 @@ public class TableMapReduceUtil {
       org.apache.hbase.thirdparty.com.google.protobuf.UnsafeByteOperations.class, // hb-sh-protobuf
       org.apache.hbase.thirdparty.io.netty.channel.Channel.class,    // hbase-shaded-netty
       org.apache.zookeeper.ZooKeeper.class,                          // zookeeper
-      com.google.protobuf.Message.class,                             // protobuf
       org.apache.htrace.core.Tracer.class,                           // htrace
       com.codahale.metrics.MetricRegistry.class,                     // metrics-core
       org.apache.commons.lang3.ArrayUtils.class);                    // commons-lang
