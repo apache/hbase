7aa36ae10a8dee622ac152be6623da68cf045a76
KevinSmile <kevinwang013@hotmail.com>
2020-09-19 19:45:10 +0530

    HBASE-24481 REST - Fix incorrect response code of get-regions in rest api
    Closes #2425
    
    Signed-off-by: Viraj Jasani <vjasani@apache.org>

--- hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RegionsResource.java
+++ hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RegionsResource.java
@@ -75,6 +75,9 @@ public class RegionsResource extends ResourceBase {
     servlet.getMetrics().incrementRequests(1);
     try {
       TableName tableName = TableName.valueOf(tableResource.getName());
+      if (!tableResource.exists()) {
+        throw new TableNotFoundException(tableName);
+      }
       TableInfoModel model = new TableInfoModel(tableName.getNameAsString());
 
       List<HRegionLocation> locs;
