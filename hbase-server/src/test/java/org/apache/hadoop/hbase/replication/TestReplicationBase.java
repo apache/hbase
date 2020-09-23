7a1fda667e30c2a4788200daa29344c156f34621
stack <stack@apache.org>
2020-09-22 20:51:13 -0700

    Revert "HBASE-25068 Pass WALFactory to Replication so it knows of all WALProviders, not just default/user-space"
    This reverts commit b5a242f42a919e85a72406964db95d430dd6e881.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/replication/TestReplicationBase.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/replication/TestReplicationBase.java
@@ -1,4 +1,5 @@
 /*
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -20,6 +21,7 @@ package org.apache.hadoop.hbase.replication;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
