38ac7e26ae0e3dacdeba23e1e9d2215ee2649849
Duo Zhang <zhangduo@apache.org>
2020-10-15 10:22:14 +0800

    HBASE-25186 TestMasterRegionOnTwoFileSystems is failing after HBASE-25065 (#2544)
    Signed-off-by: Ramkrishna <ramkrishna@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/wal/AbstractWALRoller.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/wal/AbstractWALRoller.java
@@ -28,6 +28,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.Abortable;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.regionserver.wal.AbstractFSWAL;
@@ -85,6 +86,11 @@ public abstract class AbstractWALRoller<T extends Abortable> extends Thread
               controller.requestRoll();
               AbstractWALRoller.this.notifyAll();
             }
+          }
+
+          @Override
+          public void postLogArchive(Path oldPath, Path newPath) throws IOException {
+            afterWALArchive(oldPath, newPath);
           }
         });
       }
@@ -190,7 +196,6 @@ public abstract class AbstractWALRoller<T extends Abortable> extends Thread
             LOG.warn("WAL has been closed. Skipping rolling of writer and just remove it", e);
             iter.remove();
           }
-          afterRoll(wal);
         }
       } catch (FailedLogCloseException | ConnectException e) {
         abort("Failed log close in log roller", e);
@@ -206,10 +211,7 @@ public abstract class AbstractWALRoller<T extends Abortable> extends Thread
     LOG.info("LogRoller exiting.");
   }
 
-  /**
-   * Called after we finish rolling the give {@code wal}.
-   */
-  protected void afterRoll(WAL wal) {
+  protected void afterWALArchive(Path oldPath, Path newPath) {
   }
 
   /**
