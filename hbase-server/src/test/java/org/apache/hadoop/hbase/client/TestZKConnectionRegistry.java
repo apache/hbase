f3f3dd9a52c68ae92aebe9bdf500009e64febd82
stack <stack@apache.org>
2020-11-16 08:59:12 -0800

    Revert "HBASE-25280 [meta replicas] ArrayIndexOutOfBoundsException in ZKConnectionRegistry (#2652)"
    This reverts commit 6210dafc47a04306c1d90dd3e7395015f442a81e.
    
    Applied to master when should have been applied to branch. Revert.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestZKConnectionRegistry.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestZKConnectionRegistry.java
@@ -24,39 +24,26 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.fail;
+
 import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.IntStream;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Abortable;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HRegionLocation;
 import org.apache.hadoop.hbase.RegionLocations;
-import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.master.RegionState;
-import org.apache.hadoop.hbase.shaded.protobuf.ProtobufUtil;
-import org.apache.hadoop.hbase.shaded.protobuf.generated.ZooKeeperProtos;
 import org.apache.hadoop.hbase.testclassification.ClientTests;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.zookeeper.MiniZooKeeperCluster;
 import org.apache.hadoop.hbase.zookeeper.ReadOnlyZKClient;
-import org.apache.hadoop.hbase.zookeeper.ZKUtil;
-import org.apache.hadoop.hbase.zookeeper.ZKWatcher;
-import org.apache.zookeeper.KeeperException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -68,9 +55,6 @@ public class TestZKConnectionRegistry {
   @ClassRule
   public static final HBaseClassTestRule CLASS_RULE =
     HBaseClassTestRule.forClass(TestZKConnectionRegistry.class);
-
-  @Rule
-  public final TestName name = new TestName();
 
   static final Logger LOG = LoggerFactory.getLogger(TestZKConnectionRegistry.class);
   static final HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
@@ -99,7 +83,8 @@ public class TestZKConnectionRegistry {
       clusterId);
     assertEquals(TEST_UTIL.getHBaseCluster().getMaster().getServerName(),
       REGISTRY.getActiveMaster().get());
-    RegionReplicaTestHelper.waitUntilAllMetaReplicasAreReady(TEST_UTIL, REGISTRY);
+    RegionReplicaTestHelper
+      .waitUntilAllMetaReplicasAreReady(TEST_UTIL, REGISTRY);
     RegionLocations locs = REGISTRY.getMetaRegionLocations().get();
     assertEquals(3, locs.getRegionLocations().length);
     IntStream.range(0, 3).forEach(i -> {
@@ -117,8 +102,8 @@ public class TestZKConnectionRegistry {
       otherConf.set(HConstants.ZOOKEEPER_QUORUM, MiniZooKeeperCluster.HOST);
       try (ZKConnectionRegistry otherRegistry = new ZKConnectionRegistry(otherConf)) {
         ReadOnlyZKClient zk2 = otherRegistry.getZKClient();
-        assertNotSame("Using a different configuration / quorum should result in " +
-            "different backing zk connection.", zk1, zk2);
+        assertNotSame("Using a different configuration / quorum should result in different " +
+          "backing zk connection.", zk1, zk2);
         assertNotEquals(
           "Using a different configrution / quorum should be reflected in the zk connection.",
           zk1.getConnectString(), zk2.getConnectString());
@@ -138,42 +123,6 @@ public class TestZKConnectionRegistry {
         fail("Should have failed since we set an incorrect meta znode prefix");
       } catch (ExecutionException e) {
         assertThat(e.getCause(), instanceOf(IOException.class));
-      }
-    }
-  }
-
-  /**
-   * Pass discontinuous list of znodes to registry getMetaRegionLocation. Should work fine.
-   * It used to throw ArrayOutOfBoundsException. See HBASE-25280.
-   */
-  @Test
-  public void testDiscontinuousLocations()
-    throws ExecutionException, InterruptedException, IOException, KeeperException,
-    TimeoutException {
-    // Write discontinuous meta replica locations to a zk namespace particular to this test to
-    // avoid polluting other tests.
-    Configuration conf = new Configuration(TEST_UTIL.getConfiguration());
-    conf.set(HConstants.ZOOKEEPER_ZNODE_PARENT, "/" + this.name.getMethodName());
-    ZooKeeperProtos.MetaRegionServer pbrsr = ZooKeeperProtos.MetaRegionServer.newBuilder()
-      .setServer(ProtobufUtil.toServerName(ServerName.valueOf("example.org,1,1")))
-      .setRpcVersion(HConstants.RPC_CURRENT_VERSION)
-      .setState(RegionState.State.OPEN.convert()).build();
-    byte[] data = ProtobufUtil.prependPBMagic(pbrsr.toByteArray());
-    try (ZKWatcher zkw = new ZKWatcher(conf, this.name.getMethodName(), new Abortable() {
-      @Override public void abort(String why, Throwable e) {}
-      @Override public boolean isAborted() {
-        return false;
-      }
-    })) {
-      // Write default replica and then a replica for replicaId #3.
-      ZKUtil.createSetData(zkw, zkw.getZNodePaths().getZNodeForReplica(0), data);
-      ZKUtil.createSetData(zkw, zkw.getZNodePaths().getZNodeForReplica(3), data);
-      List<String> znodes = zkw.getMetaReplicaNodes();
-      assertEquals(2, znodes.size());
-      try (ZKConnectionRegistry registry = new ZKConnectionRegistry(conf)) {
-        CompletableFuture<RegionLocations> cf = registry.getMetaRegionLocations();
-        RegionLocations locations = cf.get(60, TimeUnit.SECONDS);
-        assertEquals(2, locations.numNonNullElements());
       }
     }
   }
