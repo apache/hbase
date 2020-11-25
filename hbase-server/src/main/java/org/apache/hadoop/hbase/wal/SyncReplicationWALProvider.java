4e433c16478f81617d16bc33c645106c32585a84
Andrew Purtell <apurtell@apache.org>
2020-11-25 14:10:12 -0800

    HBASE-24640 Purge use of VisibleForTesting (#2695)
    Signed-off-by: Reid Chan <reidchan@apache.org>
    Signed-off-by: Nick Dimiduk <ndimiduk@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/wal/SyncReplicationWALProvider.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/wal/SyncReplicationWALProvider.java
@@ -35,7 +35,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Abortable;
 import org.apache.hadoop.hbase.TableName;
