094aadcddaf96d078c768e2f9d922418c6a07ddc
Balazs Meszaros <meszibalu@apache.org>
2020-11-25 11:10:27 +0100

    HBASE-25307 ThreadLocal pooling leads to NullPointerException (#2685)
    * PoolMap does not discard any elements anymore. If an element is put,
    it always stores it. The reason: it stores expensive resources (rpc
    connections) which would lead to resource leak if we simple discard it.
    RpcClients can reference netty ByteBufs which are reference counted.
    Resource cleanup is done by AbstractRpcClient.cleanupIdleConnections().
    * PoolMap does not implement Map interface anymore, so ensuring
    thread-safety has become easier. Put method is replaced with getOrCreate().
    * ThreadLocalPool doesn't use ThreadLocal class anymore. It stores
    resources on thread basis, but it doesn't remove values when a thread
    exits. Again, proper cleanup is done by cleanupIdleConnections().
    
    Signed-off-by: Sean Busbey <busbey@apache.org>
    Signed-off-by: Wellington Chevreuil <wellington.chevreuil@gmail.com>

--- hbase-client/src/test/java/org/apache/hadoop/hbase/util/TestThreadLocalPoolMap.java
+++ hbase-client/src/test/java/org/apache/hadoop/hbase/util/TestThreadLocalPoolMap.java
@@ -19,9 +19,13 @@ package org.apache.hadoop.hbase.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.testclassification.MiscTests;
 import org.apache.hadoop.hbase.testclassification.SmallTests;
@@ -43,42 +47,71 @@ public class TestThreadLocalPoolMap extends PoolMapTestBase {
   }
 
   @Test
-  public void testSingleThreadedClient() throws InterruptedException, ExecutionException {
-    Random rand = ThreadLocalRandom.current();
-    String randomKey = String.valueOf(rand.nextInt());
-    String randomValue = String.valueOf(rand.nextInt());
-    // As long as the pool is not full, we should get back what we put
-    runThread(randomKey, randomValue, randomValue);
-    assertEquals(1, poolMap.size(randomKey));
+  public void testGetOrCreate() throws IOException {
+    String key = "key";
+    String value = "value";
+    String result = poolMap.getOrCreate(key, () -> value);
+
+    assertEquals(value, result);
+    assertEquals(1, poolMap.values().size());
   }
 
   @Test
-  public void testMultiThreadedClients() throws InterruptedException, ExecutionException {
-    Random rand = ThreadLocalRandom.current();
-    // As long as the pool is not full, we should get back what we put
-    for (int i = 0; i < POOL_SIZE; i++) {
-      String randomKey = String.valueOf(rand.nextInt());
-      String randomValue = String.valueOf(rand.nextInt());
-      runThread(randomKey, randomValue, randomValue);
-      assertEquals(1, poolMap.size(randomKey));
+  public void testMultipleKeys() throws IOException {
+    for (int i = 0; i < KEY_COUNT; i++) {
+      String key = Integer.toString(i);
+      String value = Integer.toString(2 * i);
+      String result = poolMap.getOrCreate(key, () -> value);
+
+      assertEquals(value, result);
     }
-    String randomKey = String.valueOf(rand.nextInt());
-    for (int i = 0; i < POOL_SIZE; i++) {
-      String randomValue = String.valueOf(rand.nextInt());
-      runThread(randomKey, randomValue, randomValue);
-      assertEquals(i + 1, poolMap.size(randomKey));
-    }
+
+    assertEquals(KEY_COUNT, poolMap.values().size());
   }
 
   @Test
-  public void testPoolCap() throws InterruptedException, ExecutionException {
-    Random rand = ThreadLocalRandom.current();
-    String randomKey = String.valueOf(rand.nextInt());
-    for (int i = 0; i < POOL_SIZE * 2; i++) {
-      String randomValue = String.valueOf(rand.nextInt());
-      // as of HBASE-4150, pool limit is no longer used with ThreadLocalPool
-      runThread(randomKey, randomValue, randomValue);
-    }
-    assertEquals(POOL_SIZE * 2, poolMap.size(randomKey));
+  public void testFull() throws IOException {
+    String key = "key";
+    String value = "value";
+
+    String result = poolMap.getOrCreate(key, () -> value);
+    assertEquals(value, result);
+
+    String result2 = poolMap.getOrCreate(key, () -> {
+      throw new IOException("must not call me");
+    });
+
+    assertEquals(value, result2);
+    assertEquals(1, poolMap.values().size());
+  }
+
+  @Test
+  public void testLocality() throws ExecutionException, InterruptedException {
+    String key = "key";
+    AtomicInteger id = new AtomicInteger();
+
+    Runnable runnable = () -> {
+      try {
+        String myId = Integer.toString(id.getAndIncrement());
+
+        for (int i = 0; i < 3; i++) {
+          String result = poolMap.getOrCreate(key, () -> myId);
+          assertEquals(myId, result);
+
+          Thread.yield();
+        }
+      } catch (IOException e) {
+        throw new CompletionException(e);
+      }
+    };
+
+    CompletableFuture<Void> future1 = CompletableFuture.runAsync(runnable);
+    CompletableFuture<Void> future2 = CompletableFuture.runAsync(runnable);
+
+    /* test for successful completion */
+    future1.get();
+    future2.get();
+
+    assertEquals(2, poolMap.values().size());
   }
 }
