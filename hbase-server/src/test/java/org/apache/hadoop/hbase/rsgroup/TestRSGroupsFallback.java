8475af7a0d82af8ceeffb5054d6295e8567678c6
haxiaolin <linkaline11@gmail.com>
2020-11-26 18:55:36 +0800

    HBASE-25334 TestRSGroupsFallback.testFallback is flaky

--- hbase-server/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsFallback.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRSGroupsFallback.java
@@ -25,12 +25,14 @@ import java.util.Collections;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptorBuilder;
 import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.apache.hadoop.hbase.client.TableDescriptorBuilder;
 import org.apache.hadoop.hbase.master.assignment.AssignmentTestingUtil;
 import org.apache.hadoop.hbase.net.Address;
+import org.apache.hadoop.hbase.procedure2.ProcedureTestingUtility;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.testclassification.RSGroupTests;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -61,6 +63,7 @@ public class TestRSGroupsFallback extends TestRSGroupsBase {
   public static void setUp() throws Exception {
     Configuration conf = TEST_UTIL.getConfiguration();
     conf.setBoolean(RSGroupBasedLoadBalancer.FALLBACK_GROUP_ENABLE_KEY, true);
+    conf.setInt(HConstants.HBASE_BALANCER_MAX_BALANCING, 0);
     setUpTestBeforeClass();
     MASTER.balanceSwitch(true);
   }
@@ -118,7 +121,8 @@ public class TestRSGroupsFallback extends TestRSGroupsBase {
   }
 
   private void assertRegionsInGroup(TableName table, String group) throws IOException {
-    TEST_UTIL.waitUntilAllRegionsAssigned(table);
+    ProcedureTestingUtility.waitAllProcedures(
+      TEST_UTIL.getMiniHBaseCluster().getMaster().getMasterProcedureExecutor());
     RSGroupInfo rsGroup = ADMIN.getRSGroup(group);
     MASTER.getAssignmentManager().getRegionStates().getRegionsOfTable(table).forEach(region -> {
       Address regionOnServer = MASTER.getAssignmentManager().getRegionStates()
