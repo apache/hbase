6f911eb333d453d518960978527f06205f2c7d2e
Michael Stack <saintstack@users.noreply.github.com>
2020-11-30 21:02:45 -0800

    HBASE-25345 [Flakey Tests] branch-2 TestReadReplicas#testVerifySecondaryAbilityToReadWithOnFiles (#2727)
    Check TEST_SKIP_REPORTING_TRANSITION and if true, skip trying to talk to
    update master on state transition -- i.e. reportFileArchivalForQuotas --
    as we allow for reportRegionStateTransition (For tests only)
    
    Signed-off-by: Huaxiang Sun <huaxiangsun@apache.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -3790,6 +3790,9 @@ public class HRegionServer extends Thread implements
   @Override
   public boolean reportFileArchivalForQuotas(TableName tableName,
       Collection<Entry<String, Long>> archivedFiles) {
+    if (TEST_SKIP_REPORTING_TRANSITION) {
+      return false;
+    }
     RegionServerStatusService.BlockingInterface rss = rssStub;
     if (rss == null || rsSpaceQuotaManager == null) {
       // the current server could be stopping.
