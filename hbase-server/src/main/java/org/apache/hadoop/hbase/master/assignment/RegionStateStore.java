f5b962087e4659e6d9bddfb87ac49246f0cd14f9
Bo Cui <cuibo0108@163.com>
2021-01-08 21:57:23 +0800

    HBASE-25483 set the loadMeta log level to debug (#2859)
    Signed-off-by: Pankaj Kumar<pankajkumar@apache.org>
    (cherry picked from commit a348204d4a77c0e15ddfd459cecfc4d3427d0a63)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStateStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/RegionStateStore.java
@@ -120,8 +120,7 @@ public class RegionStateStore {
       final ServerName regionLocation = getRegionServer(result, replicaId);
       final long openSeqNum = hrl.getSeqNum();
 
-      // TODO: move under trace, now is visible for debugging
-      LOG.info(
+      LOG.debug(
         "Load hbase:meta entry region={}, regionState={}, lastHost={}, " +
           "regionLocation={}, openSeqNum={}",
         regionInfo.getEncodedName(), state, lastHost, regionLocation, openSeqNum);
