6af38612915bed6a476bf63beb7f63351d3231dd
Mohammad Arshad <arshad@apache.org>
2021-01-14 00:07:02 +0530

    HBASE-25492: Create table with rsgroup

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/TableDescriptor.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/TableDescriptor.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -360,4 +361,11 @@ public interface TableDescriptor {
     }
     return !enabled;
   }
+
+  /**
+   * Get the region server group this table belongs to. The regions of this table will be placed
+   * only on the region servers within this group. If not present, will be placed on
+   * {@link org.apache.hadoop.hbase.rsgroup.RSGroupInfo#DEFAULT_GROUP}.
+   */
+  Optional<String> getRegionServerGroup();
 }
