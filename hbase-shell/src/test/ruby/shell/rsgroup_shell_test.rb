df6607aa85b3389678c139c4b38c09be0a7eefda
xijiawen <15891721997@163.com>
2021-01-18 18:57:02 +0800

    HBASE-25497 move_namespaces_rsgroup should change hbase.rsgroup.name … (#2875)
    * HBASE-25497 move_namespaces_rsgroup should change hbase.rsgroup.name config in NamespaceDescriptor
    
    Signed-off-by: Zheng Wang <wangzheng@apache.org>
    (cherry picked from commit 9e9bec24d2c6e527aaeb0ffe5510e5edb12de40d)

--- hbase-shell/src/test/ruby/shell/rsgroup_shell_test.rb
+++ hbase-shell/src/test/ruby/shell/rsgroup_shell_test.rb
@@ -115,8 +115,16 @@ module Hbase
       assert_not_nil(group)
       assert_equal(ns_table_name, group.getTables.iterator.next.toString)
 
+      ns_table_name2 = 'test_namespace:test_ns_table2'
+      @shell.command(:create, ns_table_name2, 'f')
+
+      assert_true(@admin.listTablesInRSGroup(group_name).contains(org.apache.hadoop.hbase.TableName.valueOf(ns_table_name2)))
+      assert_equal(2, @admin.listTablesInRSGroup(group_name).count)
+
       @shell.command(:disable, ns_table_name)
       @shell.command(:drop, ns_table_name)
+      @shell.command(:disable, ns_table_name2)
+      @shell.command(:drop, ns_table_name2)
       @shell.command(:drop_namespace, namespace_name)
       remove_rsgroup(group_name)
     end
