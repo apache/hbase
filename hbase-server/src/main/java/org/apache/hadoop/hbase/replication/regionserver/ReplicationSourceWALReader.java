07fb533c7d3d2f53e107ae113ad3a7a36ca32176
Rushabh <rushabh.shah@salesforce.com>
2021-01-28 12:34:36 -0800

    [HBASE-25536] Adding test case

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSourceWALReader.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSourceWALReader.java
@@ -247,6 +247,8 @@ class ReplicationSourceWALReader extends Thread {
   // (highly likely we've closed the current log), we've hit the max retries, and autorecovery is
   // enabled, then dump the log
   private void handleEofException(IOException e) {
+    // Dump the log even if logQueue size is 1 if the source is from recovered Source
+    // since we don't add current log to recovered source queue so it is safe to remove.
     if ((e instanceof EOFException || e.getCause() instanceof EOFException) &&
       (source.isRecovered() || logQueue.size() > 1) && this.eofAutoRecovery) {
       try {
