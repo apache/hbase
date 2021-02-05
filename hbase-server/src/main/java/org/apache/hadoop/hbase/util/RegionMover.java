38a8b16c5e2655f27e2deeb9dd3ba7f20db29654
Baiqiang Zhao <zbq.dean@gmail.com>
2021-02-05 16:31:53 +0800

    HBASE-25554 NPE when init RegionMover (#2927)
    Signed-off-by: Viraj Jasani <vjasani@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/util/RegionMover.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/util/RegionMover.java
@@ -87,7 +87,6 @@ public class RegionMover extends AbstractHBaseTool implements Closeable {
   public static final int DEFAULT_MOVE_RETRIES_MAX = 5;
   public static final int DEFAULT_MOVE_WAIT_MAX = 60;
   public static final int DEFAULT_SERVERSTART_WAIT_MAX = 180;
-  private final RackManager rackManager;
 
   private static final Logger LOG = LoggerFactory.getLogger(RegionMover.class);
 
@@ -126,7 +125,6 @@ public class RegionMover extends AbstractHBaseTool implements Closeable {
   }
 
   private RegionMover() {
-    rackManager = new RackManager(conf);
   }
 
   @Override
