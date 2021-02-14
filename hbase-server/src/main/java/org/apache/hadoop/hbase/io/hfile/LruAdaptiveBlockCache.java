1ec7fc965491c4667b4b2e82acd3f4b4557d46c5
pustota2009 <61382543+pustota2009@users.noreply.github.com>
2021-02-14 16:42:24 +0300

    Update LruAdaptiveBlockCache.java
    fix codestile

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/LruAdaptiveBlockCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/LruAdaptiveBlockCache.java
@@ -1,5 +1,4 @@
-* @param memoryFactor        percentage of total size for in-memory blocks
-  /**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
