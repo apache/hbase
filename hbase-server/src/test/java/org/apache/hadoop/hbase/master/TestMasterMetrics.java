aba2579f68036fd51997368646a7e9150933d504
rahul.kumar <rahul.kumar@salesforce.com>
2021-02-26 12:50:31 +0530

    HBASE-25460 : Expose drainingServers as cluster metric

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestMasterMetrics.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestMasterMetrics.java
@@ -130,9 +130,10 @@ public class TestMasterMetrics {
   public void testDefaultMasterMetrics() throws Exception {
     MetricsMasterSource masterSource = master.getMasterMetrics().getMetricsSource();
     boolean tablesOnMaster = LoadBalancer.isTablesOnMaster(TEST_UTIL.getConfiguration());
-    metricsHelper.assertGauge("numRegionServers", 1 + (tablesOnMaster ? 1 : 0), masterSource);
-    metricsHelper.assertGauge("averageLoad", 1, masterSource);
-    metricsHelper.assertGauge("numDeadRegionServers", 0, masterSource);
+    metricsHelper.assertGauge( "numRegionServers",1 + (tablesOnMaster? 1: 0), masterSource);
+    metricsHelper.assertGauge( "averageLoad", 1 + (tablesOnMaster? 0: 1), masterSource);
+    metricsHelper.assertGauge( "numDeadRegionServers", 0, masterSource);
+    metricsHelper.assertGauge( "numDrainingRegionServers", 0, masterSource);
 
     metricsHelper.assertGauge("masterStartTime", master.getMasterStartTime(), masterSource);
     metricsHelper.assertGauge("masterActiveTime", master.getMasterActiveTime(), masterSource);
