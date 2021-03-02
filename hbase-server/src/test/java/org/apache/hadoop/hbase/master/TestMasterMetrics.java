2efa6fcc2e4b3b523880a94f499a19db923d3ca7
rahul.kumar <rahul.kumar@salesforce.com>
2021-03-02 18:19:03 +0530

    fix indent

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestMasterMetrics.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestMasterMetrics.java
@@ -139,7 +139,7 @@ public class TestMasterMetrics {
     metricsHelper.assertGauge( "numRegionServers",1 + (tablesOnMaster? 1: 0), masterSource);
     metricsHelper.assertGauge( "averageLoad", 1 + (tablesOnMaster? 0: 1), masterSource);
     metricsHelper.assertGauge( "numDeadRegionServers", 0, masterSource);
-    metricsHelper.assertGauge( "numDrainingRegionServers", 0, masterSource);
+    metricsHelper.assertGauge("numDrainingRegionServers", 0, masterSource);
 
     metricsHelper.assertGauge("masterStartTime", master.getMasterStartTime(), masterSource);
     metricsHelper.assertGauge("masterActiveTime", master.getMasterActiveTime(), masterSource);
