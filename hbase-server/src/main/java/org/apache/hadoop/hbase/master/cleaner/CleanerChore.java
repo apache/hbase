aaf09cc9b62c45603f5329ecdcdfc64a31f3ac03
haxiaolin <linkaline11@gmail.com>
2021-03-05 19:24:08 +0800

    HBASE-25635 CandidateGenerator may miss some region balance actions

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/cleaner/CleanerChore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/cleaner/CleanerChore.java
@@ -120,7 +120,7 @@ public abstract class CleanerChore<T extends FileCleanerDelegate> extends Schedu
     } else if (poolSize.matches("0.[0-9]+|1.0")) {
       // if poolSize is a double, return poolSize * availableProcessors;
       // Ensure that we always return at least one.
-      int computedThreads = (int) (AVAIL_PROCESSORS * Double.valueOf(poolSize));
+      int computedThreads = (int) (AVAIL_PROCESSORS * Double.parseDouble(poolSize));
       if (computedThreads < 1) {
         LOG.debug("Computed {} threads for CleanerChore, using 1 instead", computedThreads);
         return 1;
