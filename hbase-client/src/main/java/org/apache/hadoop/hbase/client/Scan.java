09a82b6bfa22fd8fa8d7bb3788d4b0bfbf8d423a
Anoop Sam John <anoopsamjohn@gmail.com>
2021-03-08 12:44:07 +0530

    HBASE-25644 Scan#setSmall blindly sets ReadType as PREAD
    Signed-off-by: zhangduo <zhangduo@apache.org>
    Signed-off-by: Ramkrishna <ramkrishna@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/Scan.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/Scan.java
@@ -1021,7 +1021,9 @@ public class Scan extends Query {
   @Deprecated
   public Scan setSmall(boolean small) {
     this.small = small;
-    this.readType = ReadType.PREAD;
+    if (small) {
+      this.readType = ReadType.PREAD;
+    }
     return this;
   }
 
