3c9a7e01ed79b8e8655b0b43dd0498ba9a245fbe
haxiaolin <linkaline11@gmail.com>
2021-03-16 05:25:38 +0800

    HBASE-25595 TestLruBlockCache.testBackgroundEvictionThread is flaky (#2974)
    Signed-off-by: stack <stack@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/LruBlockCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/LruBlockCache.java
@@ -633,7 +633,7 @@ public class LruBlockCache implements FirstLevelBlockCache {
    * Multi-threaded call to run the eviction process.
    */
   private void runEviction() {
-    if (evictionThread == null) {
+    if (evictionThread == null || !evictionThread.isGo()) {
       evict();
     } else {
       evictionThread.evict();
@@ -947,6 +947,7 @@ public class LruBlockCache implements FirstLevelBlockCache {
         }
         LruBlockCache cache = this.cache.get();
         if (cache == null) {
+          this.go = false;
           break;
         }
         cache.evict();
@@ -964,6 +965,10 @@ public class LruBlockCache implements FirstLevelBlockCache {
     synchronized void shutdown() {
       this.go = false;
       this.notifyAll();
+    }
+
+    public boolean isGo() {
+      return go;
     }
 
     /**
