db2e6d8c63875b6ac8715036f21ea1e622fd263a
Baiqiang Zhao <zbq.dean@gmail.com>
2021-03-16 06:49:33 +0800

    HBASE-25597 Add row info in Exception when cell size exceeds maxCellSize (#2976)
    Signed-off-by: stack <stack@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
@@ -902,7 +902,8 @@ public class RSRpcServices implements HBaseRPCErrorHandler,
       while (cells.advance()) {
         int size = PrivateCellUtil.estimatedSerializedSizeOf(cells.current());
         if (size > r.maxCellSize) {
-          String msg = "Cell with size " + size + " exceeds limit of " + r.maxCellSize + " bytes";
+          String msg = "Cell[" + cells.current() + "] with size " + size
+            + " exceeds limit of " + r.maxCellSize + " bytes";
           LOG.debug(msg);
           throw new DoNotRetryIOException(msg);
         }
