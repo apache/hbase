53e5aa76f75d075ff726452ba6e899dcee0ee38e
Pankaj <pankajkumarbit24@gmail.com>
2021-03-24 18:14:21 +0530

    HBASE-25568 Upgrade Thrift jar to fix CVE-2020-13949 (branch-2.4)

--- hbase-thrift/src/main/java/org/apache/hadoop/hbase/thrift2/client/ThriftConnection.java
+++ hbase-thrift/src/main/java/org/apache/hadoop/hbase/thrift2/client/ThriftConnection.java
@@ -58,11 +58,11 @@ import org.apache.http.protocol.HttpContext;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.THttpClient;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.transport.layered.TFramedTransport;
 import org.apache.yetus.audience.InterfaceAudience;
 
 import org.apache.hbase.thirdparty.com.google.common.base.Preconditions;
@@ -160,14 +160,16 @@ public class ThriftConnection implements Connection {
 
     @Override
     public Pair<THBaseService.Client, TTransport> getClient() throws IOException {
-      TSocket sock = new TSocket(connection.getHost(), connection.getPort());
-      sock.setSocketTimeout(connection.getOperationTimeout());
-      sock.setConnectTimeout(connection.getConnectTimeout());
-      TTransport tTransport = sock;
-      if (connection.isFramed()) {
-        tTransport = new TFramedTransport(tTransport);
-      }
+      TTransport tTransport = null;
       try {
+        TSocket sock = new TSocket(connection.getHost(), connection.getPort());
+        sock.setSocketTimeout(connection.getOperationTimeout());
+        sock.setConnectTimeout(connection.getConnectTimeout());
+        tTransport = sock;
+        if (connection.isFramed()) {
+          tTransport = new TFramedTransport(tTransport);
+        }
+
         sock.open();
       } catch (TTransportException e) {
         throw new IOException(e);
