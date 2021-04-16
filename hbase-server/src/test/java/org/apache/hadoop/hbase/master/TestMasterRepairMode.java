bf78246b4f8fc1ce37421c746a8209fe8991a0db
Duo Zhang <zhangduo@apache.org>
2021-04-16 09:50:24 +0800

    HBASE-25775 Use a special balancer to deal with maintenance mode (#3161)
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestMasterRepairMode.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestMasterRepairMode.java
@@ -18,9 +18,11 @@
 package org.apache.hadoop.hbase.master;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import org.apache.hadoop.conf.Configuration;
@@ -29,6 +31,7 @@ import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.StartMiniClusterOption;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.AsyncTable;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
@@ -48,12 +51,12 @@ import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Category({MasterTests.class, LargeTests.class})
+@Category({ MasterTests.class, LargeTests.class })
 public class TestMasterRepairMode {
 
   @ClassRule
   public static final HBaseClassTestRule CLASS_RULE =
-      HBaseClassTestRule.forClass(TestMasterRepairMode.class);
+    HBaseClassTestRule.forClass(TestMasterRepairMode.class);
 
   @Rule
   public TestName name = new TestName();
@@ -84,16 +87,14 @@ public class TestMasterRepairMode {
   public void testNewCluster() throws Exception {
     enableMaintenanceMode();
 
-    TEST_UTIL.startMiniCluster(StartMiniClusterOption.builder()
-        .numRegionServers(0)
-        .numDataNodes(3)
-        .build());
+    TEST_UTIL.startMiniCluster(
+      StartMiniClusterOption.builder().numRegionServers(0).numDataNodes(3).build());
 
     Connection conn = TEST_UTIL.getConnection();
     assertTrue(conn.getAdmin().isMasterInMaintenanceMode());
 
     try (Table table = conn.getTable(TableName.META_TABLE_NAME);
-        ResultScanner scanner = table.getScanner(new Scan())) {
+      ResultScanner scanner = table.getScanner(new Scan())) {
       assertNotNull("Could not read meta.", scanner.next());
     }
   }
@@ -113,25 +114,26 @@ public class TestMasterRepairMode {
     LOG.info("Starting master-only");
 
     enableMaintenanceMode();
-    TEST_UTIL.startMiniHBaseCluster(StartMiniClusterOption.builder()
-        .numRegionServers(0).createRootDir(false).build());
+    TEST_UTIL.startMiniHBaseCluster(
+      StartMiniClusterOption.builder().numRegionServers(0).createRootDir(false).build());
 
     Connection conn = TEST_UTIL.getConnection();
     assertTrue(conn.getAdmin().isMasterInMaintenanceMode());
 
     try (Table table = conn.getTable(TableName.META_TABLE_NAME);
-        ResultScanner scanner = table.getScanner(HConstants.TABLE_FAMILY);
-        Stream<Result> results = StreamSupport.stream(scanner.spliterator(), false)) {
+      ResultScanner scanner = table.getScanner(HConstants.TABLE_FAMILY);
+      Stream<Result> results = StreamSupport.stream(scanner.spliterator(), false)) {
       assertTrue("Did not find user table records while reading hbase:meta",
-          results.anyMatch(r -> Arrays.equals(r.getRow(), testRepairMode.getName())));
+        results.anyMatch(r -> Arrays.equals(r.getRow(), testRepairMode.getName())));
     }
-
-    try (Table table = conn.getTable(testRepairMode);
-        ResultScanner scanner = table.getScanner(new Scan())) {
-      scanner.next();
-      fail("Should not be able to access user-space tables in repair mode.");
-    } catch (Exception e) {
-      // Expected
-    }
+    // use async table so we can set the timeout and retry value to let the operation fail fast
+    AsyncTable<?> table = conn.toAsyncConnection().getTableBuilder(testRepairMode)
+      .setScanTimeout(5, TimeUnit.SECONDS).setMaxRetries(2).build();
+    assertThrows("Should not be able to access user-space tables in repair mode.", Exception.class,
+      () -> {
+        try (ResultScanner scanner = table.getScanner(new Scan())) {
+          scanner.next();
+        }
+      });
   }
 }
