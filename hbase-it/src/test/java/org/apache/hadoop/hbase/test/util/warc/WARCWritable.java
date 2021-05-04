6ad5b9e569b8f5eefc1ac21af2624afcd4625dd1
Andrew Purtell <apurtell@apache.org>
2021-05-03 17:59:00 -0700

    HBASE-25824 IntegrationTestLoadCommonCrawl (#3208)
    * HBASE-25824 IntegrationTestLoadCommonCrawl
    
    This integration test loads successful resource retrieval records from
    the Common Crawl (https://commoncrawl.org/) public dataset into an HBase
    table and writes records that can be used to later verify the presence
    and integrity of those records.
    
    Run like:
    
      ./bin/hbase org.apache.hadoop.hbase.test.IntegrationTestLoadCommonCrawl \
        -Dfs.s3n.awsAccessKeyId=<AWS access key> \
        -Dfs.s3n.awsSecretAccessKey=<AWS secret key> \
        /path/to/test-CC-MAIN-2021-10-warc.paths.gz \
        /path/to/tmp/warc-loader-output
    
    Access to the Common Crawl dataset in S3 is made available to anyone by
    Amazon AWS, but Hadoop's S3N filesystem still requires valid access
    credentials to initialize.
    
    The input path can either specify a directory or a file. The file may
    optionally be compressed with gzip. If a directory, the loader expects
    the directory to contain one or more WARC files from the Common Crawl
    dataset. If a file, the loader expects a list of Hadoop S3N URIs which
    point to S3 locations for one or more WARC files from the Common Crawl
    dataset, one URI per line. Lines should be terminated with the UNIX line
    terminator.
    
    Included in hbase-it/src/test/resources/CC-MAIN-2021-10-warc.paths.gz
    is a list of all WARC files comprising the Q1 2021 crawl archive. There
    are 64,000 WARC files in this data set, each containing ~1GB of gzipped
    data. The WARC files contain several record types, such as metadata,
    request, and response, but we only load the response record types. If
    the HBase table schema does not specify compression (by default) there
    is roughly a 10x expansion. Loading the full crawl archive results in a
    table approximately 640 TB in size.
    
    The hadoop-aws jar will be needed at runtime to instantiate the S3N
    filesystem. Use the -files ToolRunner argument to add it.
    
    You can also split the Loader and Verify stages:
    
    Load with:
    
      ./bin/hbase 'org.apache.hadoop.hbase.test.IntegrationTestLoadCommonCrawl$Loader' \
        -files /path/to/hadoop-aws.jar \
        -Dfs.s3n.awsAccessKeyId=<AWS access key> \
        -Dfs.s3n.awsSecretAccessKey=<AWS secret key> \
        /path/to/test-CC-MAIN-2021-10-warc.paths.gz \
        /path/to/tmp/warc-loader-output
    
    Verify with:
    
      ./bin/hbase 'org.apache.hadoop.hbase.test.IntegrationTestLoadCommonCrawl$Verify' \
        /path/to/tmp/warc-loader-output
    
    Signed-off-by: Michael Stack <stack@apache.org>

---
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/test/util/warc/WARCWritable.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2014 Martin Kleppmann
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.apache.hadoop.hbase.test.util.warc;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+/**
+ * A mutable wrapper around a {@link WARCRecord} implementing the Hadoop Writable interface.
+ * This allows WARC records to be used throughout Hadoop (e.g. written to sequence files
+ * when shuffling data between mappers and reducers). The record is encoded as a single
+ * record in standard WARC/1.0 format.
+ */
+public class WARCWritable implements Writable {
+
+  private WARCRecord record;
+
+  /** Creates an empty writable (with a null record). */
+  public WARCWritable() {
+    this.record = null;
+  }
+
+  /** Creates a writable wrapper around a given WARCRecord. */
+  public WARCWritable(WARCRecord record) {
+    this.record = record;
+  }
+
+  /** Returns the record currently wrapped by this writable. */
+  public WARCRecord getRecord() {
+    return record;
+  }
+
+  /** Updates the record held within this writable wrapper. */
+  public void setRecord(WARCRecord record) {
+    this.record = record;
+  }
+
+  /** Appends the current record to a {@link DataOutput} stream. */
+  @Override
+  public void write(DataOutput out) throws IOException {
+    if (record != null) {
+      record.write(out);
+    }
+  }
+
+  /**
+   * Parses a {@link WARCRecord} out of a {@link DataInput} stream, and makes it the
+   * writable's current record.
+   */
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    record = new WARCRecord(in);
+  }
+
+}
