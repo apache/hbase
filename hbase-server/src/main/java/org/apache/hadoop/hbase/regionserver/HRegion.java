dfa88e1ffee8cfe6865763cbfc93d5d5be886494
Rushabh Shah <shahrs87@gmail.com>
2021-05-21 13:26:11 -0400

    HBASE-25827 Per Cell TTL tags get duplicated with increments causing tags length overflow (#3210)
    Signed-off-by: Viraj Jasani <vjasani@apache.org>
    Signed-off-by: Aman Poonia <apoonia@salesforce.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -4106,9 +4106,9 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
       Function<Cell, byte[]> supplier) throws IOException {
       // Forward any tags found on the delta.
       List<Tag> tags = TagUtil.carryForwardTags(delta);
-      tags = TagUtil.carryForwardTTLTag(tags, mutation.getTTL());
       if (currentCell != null) {
         tags = TagUtil.carryForwardTags(tags, currentCell);
+        tags = TagUtil.carryForwardTTLTag(tags, mutation.getTTL());
         byte[] newValue = supplier.apply(currentCell);
         return ExtendedCellBuilderFactory.create(CellBuilderType.SHALLOW_COPY)
           .setRow(mutation.getRow(), 0, mutation.getRow().length)
@@ -4121,6 +4121,7 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
           .setTags(TagUtil.fromList(tags))
           .build();
       } else {
+        tags = TagUtil.carryForwardTTLTag(tags, mutation.getTTL());
         PrivateCellUtil.updateLatestStamp(delta, now);
         return CollectionUtils.isEmpty(tags) ? delta : PrivateCellUtil.createCell(delta, tags);
       }
