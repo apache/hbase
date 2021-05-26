c1862c923f6e70654795c4583f532c93acfc49a0
Victor Li <victor.li@salesforce.com>
2021-05-25 18:55:45 -0700

    Removed test code for this test case.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/TestClusterPortAssignment.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/TestClusterPortAssignment.java
@@ -22,7 +22,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import java.lang.Throwable;
 import java.net.BindException;
-import java.net.ServerSocket;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.junit.ClassRule;
@@ -61,14 +60,6 @@ public class TestClusterPortAssignment {
       TEST_UTIL.getConfiguration().setInt(HConstants.REGIONSERVER_PORT, rsPort);
       TEST_UTIL.getConfiguration().setInt(HConstants.REGIONSERVER_INFO_PORT, rsInfoPort);
       LOG.info("Ports: " + masterPort + ", " + masterInfoPort + ", " + rsPort + ", " + rsInfoPort);
-      ServerSocket sock = null;
-	  if (retryCount < 2) {
-        try {
-          LOG.info("Hijack port: " + rsInfoPort);
-          sock = new ServerSocket(rsInfoPort);
-        } catch (Exception e) {
-        }
-      }
       try {
         MiniHBaseCluster cluster = TEST_UTIL.startMiniCluster();
         assertTrue("Cluster failed to come up", cluster.waitForActiveAndReadyMaster(30000));
@@ -93,12 +84,6 @@ public class TestClusterPortAssignment {
         }
       } finally {
         TEST_UTIL.shutdownMiniCluster();
-      }
-      if (sock != null) {
-        try {
-          sock.close();
-        } catch (Exception e) {
-        }
       }
     } while (retry && retryCount < 10);
 
