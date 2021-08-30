889049eab666f99bc300c070ced5505d0a59d3c5
Duo Zhang <zhangduo@apache.org>
2021-08-30 12:14:10 +0800

    HBASE-26235 We could start RegionServerTracker before becoming active master (#3645)
    Signed-off-by: Yulin Niu <niuyulin@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -188,7 +188,6 @@ import org.apache.hadoop.hbase.zookeeper.ZKClusterId;
 import org.apache.hadoop.hbase.zookeeper.ZKNodeTracker;
 import org.apache.hadoop.hbase.zookeeper.ZKUtil;
 import org.apache.hadoop.hbase.zookeeper.ZKWatcher;
-import org.apache.hadoop.hbase.zookeeper.ZNodePaths;
 import org.apache.hadoop.ipc.RemoteException;
 import org.apache.hadoop.util.ReflectionUtils;
 import org.apache.yetus.audience.InterfaceAudience;
@@ -686,7 +685,12 @@ public class HRegionServer extends Thread implements RegionServerServices, LastS
       }
       this.rpcServices.start(zooKeeper);
       this.metaRegionLocationCache = new MetaRegionLocationCache(zooKeeper);
-      this.regionServerAddressTracker = new RegionServerAddressTracker(zooKeeper, this);
+      if (!(this instanceof HMaster)) {
+        // do not create this field for HMaster, we have another region server tracker for HMaster.
+        this.regionServerAddressTracker = new RegionServerAddressTracker(zooKeeper, this);
+      } else {
+        this.regionServerAddressTracker = null;
+      }
       // This violates 'no starting stuff in Constructor' but Master depends on the below chore
       // and executor being created and takes a different startup route. Lots of overlap between HRS
       // and M (An M IS A HRS now). Need to refactor so less duplication between M and its super
@@ -3608,7 +3612,7 @@ public class HRegionServer extends Thread implements RegionServerServices, LastS
   }
 
   private String getMyEphemeralNodePath() {
-    return ZNodePaths.joinZNode(this.zooKeeper.getZNodePaths().rsZNode, getServerName().toString());
+    return zooKeeper.getZNodePaths().getRsPath(serverName);
   }
 
   private boolean isHealthCheckerConfigured() {
@@ -3995,7 +3999,7 @@ public class HRegionServer extends Thread implements RegionServerServices, LastS
   }
 
   @Override
-  public List<ServerName> getRegionServers() {
+  public Collection<ServerName> getRegionServers() {
     return regionServerAddressTracker.getRegionServers();
   }
 
