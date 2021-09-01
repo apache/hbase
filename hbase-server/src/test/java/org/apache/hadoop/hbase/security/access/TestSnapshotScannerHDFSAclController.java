534070f5cdde974f1f2fbc1d7f837de89d943c6f
XinSun <ddupgs@gmail.com>
2021-09-01 18:43:25 +0800

    HBASE-25773 TestSnapshotScannerHDFSAclController.setupBeforeClass is flaky (#3651)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    
    (cherry picked from commit 345d7256c812dd5fbdfe9f378b2884dd945c5da2)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestSnapshotScannerHDFSAclController.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestSnapshotScannerHDFSAclController.java
@@ -98,7 +98,11 @@ public class TestSnapshotScannerHDFSAclController {
           + SnapshotScannerHDFSAclController.class.getName());
 
     TEST_UTIL.startMiniCluster();
+    SnapshotScannerHDFSAclController coprocessor = TEST_UTIL.getHBaseCluster().getMaster()
+      .getMasterCoprocessorHost().findCoprocessor(SnapshotScannerHDFSAclController.class);
+    TEST_UTIL.waitFor(30000, () -> coprocessor.checkInitialized("check initialized"));
     TEST_UTIL.waitTableAvailable(PermissionStorage.ACL_TABLE_NAME);
+
     admin = TEST_UTIL.getAdmin();
     rootDir = TEST_UTIL.getDefaultRootDirPath();
     FS = rootDir.getFileSystem(conf);
@@ -128,10 +132,6 @@ public class TestSnapshotScannerHDFSAclController {
       FS.setPermission(path, commonDirectoryPermission);
       path = path.getParent();
     }
-
-    SnapshotScannerHDFSAclController coprocessor = TEST_UTIL.getHBaseCluster().getMaster()
-        .getMasterCoprocessorHost().findCoprocessor(SnapshotScannerHDFSAclController.class);
-    TEST_UTIL.waitFor(1200000, () -> coprocessor.checkInitialized("check initialized"));
     aclTable = admin.getConnection().getTable(PermissionStorage.ACL_TABLE_NAME);
   }
 
