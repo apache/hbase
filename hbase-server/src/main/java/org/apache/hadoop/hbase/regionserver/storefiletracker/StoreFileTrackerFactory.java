727d6135063aba3ed0391afce8c3f766fe0818ef
Duo Zhang <zhangduo@apache.org>
2021-09-09 12:11:25 +0800

    HBASE-26224 Introduce a MigrationStoreFileTracker to support migrating from different store file tracker implementations (#3656)
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>
    
    Conflicts:
    	hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/storefiletracker/TestMigrationStoreFileTracker.java

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/storefiletracker/StoreFileTrackerFactory.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/storefiletracker/StoreFileTrackerFactory.java
@@ -18,17 +18,19 @@
 package org.apache.hadoop.hbase.regionserver.storefiletracker;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.CompoundConfiguration;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptor;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptorBuilder;
 import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.apache.hadoop.hbase.regionserver.HRegionFileSystem;
 import org.apache.hadoop.hbase.regionserver.StoreContext;
+import org.apache.hadoop.hbase.regionserver.StoreUtils;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.ReflectionUtils;
 import org.apache.yetus.audience.InterfaceAudience;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.apache.hbase.thirdparty.com.google.common.base.Preconditions;
 
 /**
  * Factory method for creating store file tracker.
@@ -39,7 +41,7 @@ public final class StoreFileTrackerFactory {
   private static final Logger LOG = LoggerFactory.getLogger(StoreFileTrackerFactory.class);
 
   public static StoreFileTracker create(Configuration conf, boolean isPrimaryReplica,
-      StoreContext ctx) {
+    StoreContext ctx) {
     Class<? extends StoreFileTracker> tracker =
       conf.getClass(TRACK_IMPL, DefaultStoreFileTracker.class, StoreFileTracker.class);
     LOG.info("instantiating StoreFileTracker impl {}", tracker.getName());
@@ -47,22 +49,30 @@ public final class StoreFileTrackerFactory {
   }
 
   public static StoreFileTracker create(Configuration conf, boolean isPrimaryReplica, String family,
-      HRegionFileSystem regionFs) {
+    HRegionFileSystem regionFs) {
     ColumnFamilyDescriptorBuilder fDescBuilder =
       ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(family));
-    StoreContext ctx = StoreContext.getBuilder().
-      withColumnFamilyDescriptor(fDescBuilder.build()).
-      withRegionFileSystem(regionFs).
-      build();
-    return StoreFileTrackerFactory.create(conf, isPrimaryReplica, ctx);
+    StoreContext ctx = StoreContext.getBuilder().withColumnFamilyDescriptor(fDescBuilder.build())
+      .withRegionFileSystem(regionFs).build();
+    return StoreFileTrackerFactory.create(conf, TRACK_IMPL, isPrimaryReplica, ctx);
   }
 
-  public static Configuration mergeConfigurations(Configuration global,
-    TableDescriptor table, ColumnFamilyDescriptor family) {
-    return new CompoundConfiguration()
-      .add(global)
-      .addBytesMap(table.getValues())
-      .addStringMap(family.getConfiguration())
-      .addBytesMap(family.getValues());
+  public static Configuration mergeConfigurations(Configuration global, TableDescriptor table,
+    ColumnFamilyDescriptor family) {
+    return StoreUtils.createStoreConfiguration(global, table, family);
+  }
+
+  static StoreFileTrackerBase create(Configuration conf, String configName,
+    boolean isPrimaryReplica, StoreContext ctx) {
+    String className =
+      Preconditions.checkNotNull(conf.get(configName), "config %s is not set", configName);
+    Class<? extends StoreFileTrackerBase> tracker;
+    try {
+      tracker = Class.forName(className).asSubclass(StoreFileTrackerBase.class);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+    LOG.info("instantiating StoreFileTracker impl {} as {}", tracker.getName(), configName);
+    return ReflectionUtils.newInstance(tracker, conf, isPrimaryReplica, ctx);
   }
 }
