466d2d55638f27d97dc25a9a96e21fab61e1db79
Xiaolin Ha <haxiaolin@apache.org>
2021-09-09 18:26:37 +0800

    HBASE-26229 Limit count and size of L0 files compaction in StripeCompactionPolicy (#3646)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/compactions/ExploringCompactionPolicy.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/compactions/ExploringCompactionPolicy.java
@@ -22,7 +22,6 @@ package org.apache.hadoop.hbase.regionserver.compactions;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.regionserver.HStoreFile;
 import org.apache.hadoop.hbase.regionserver.StoreConfigInformation;
@@ -120,8 +119,27 @@ public class ExploringCompactionPolicy extends RatioBasedCompactionPolicy {
     }
     LOG.debug("Exploring compaction algorithm has selected {}  files of size {} starting at " +
       "candidate #{} after considering {} permutations with {} in ratio", bestSelection.size(),
-      bestSize, bestSize, opts, optsInRatio);
+      bestSize, bestStart, opts, optsInRatio);
     return new ArrayList<>(bestSelection);
+  }
+
+  /**
+   * Select at least one file in the candidates list to compact, through choosing files
+   * from the head to the index that the accumulation length larger the max compaction size.
+   * This method is a supplementary of the selectSimpleCompaction() method, aims to make sure
+   * at least one file can be selected to compact, for compactions like L0 files, which need to
+   * compact all files and as soon as possible.
+   */
+  public List<HStoreFile> selectCompactFiles(final List<HStoreFile> candidates, int maxFiles,
+      boolean isOffpeak) {
+    long selectedSize = 0L;
+    for (int end = 0; end < Math.min(candidates.size(), maxFiles); end++) {
+      selectedSize += candidates.get(end).getReader().length();
+      if (selectedSize >= comConf.getMaxCompactSize(isOffpeak)) {
+        return candidates.subList(0, end + 1);
+      }
+    }
+    return candidates;
   }
 
   private boolean isBetterSelection(List<HStoreFile> bestSelection, long bestSize,
