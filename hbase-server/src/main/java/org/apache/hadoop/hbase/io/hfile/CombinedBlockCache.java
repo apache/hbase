d956828aaca89c396d66e8bf2e9f0a4d3e0de8bc
Tak Lon (Stephen) Wu <taklwu@apache.org>
2021-09-22 09:17:18 -0700

    HBASE-26274 Create an option to reintroduce BlockCache to mapreduce job (#3684)
    Introduce `hfile.onheap.block.cache.fixed.size`
    and default to disable. when using ClientSideRegionScanner
    it will be enabled with a fixed size for caching
    INDEX/LEAF_INDEX block when a client, e.g.
    snapshot scanner, scans the entire HFile
    and does not need to seek/reseek to index
    block multiple times.
    
    Signed-off-by: Josh Elser <elserj@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/CombinedBlockCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/CombinedBlockCache.java
@@ -22,7 +22,6 @@ import java.util.Iterator;
 
 import org.apache.yetus.audience.InterfaceAudience;
 import org.apache.hadoop.hbase.io.HeapSize;
-import org.apache.hadoop.hbase.io.hfile.BlockType.BlockCategory;
 import org.apache.hadoop.hbase.io.hfile.bucket.BucketCache;
 
 /**
@@ -69,10 +68,6 @@ public class CombinedBlockCache implements ResizableBlockCache, HeapSize {
   @Override
   public void cacheBlock(BlockCacheKey cacheKey, Cacheable buf) {
     cacheBlock(cacheKey, buf, false);
-  }
-
-  private boolean isMetaBlock(BlockType blockType) {
-    return blockType.getCategory() != BlockCategory.DATA;
   }
 
   @Override
