4f6e0cf601316e4bc96f41de0b8f4ae2cf2a9c90
meiyi <myimeiyi@gmail.com>
2021-09-22 10:23:25 +0800

    Provide getConfiguration method for Region and Store interface (#3671)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Anoop Sam John <anoopsamjohn@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/Store.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/Store.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 import java.util.OptionalDouble;
 import java.util.OptionalLong;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.hbase.CellComparator;
 import org.apache.hadoop.hbase.HBaseInterfaceAudience;
@@ -292,4 +293,10 @@ public interface Store {
    * @return the number of read requests from the files under this store.
    */
   long getMixedRowReadsCount();
+
+  /**
+   * @return a read only configuration of this store; throws {@link UnsupportedOperationException}
+   *         if you try to set a configuration.
+   */
+  Configuration getReadOnlyConfiguration();
 }
