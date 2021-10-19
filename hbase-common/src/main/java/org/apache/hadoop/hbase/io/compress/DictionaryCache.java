3b126c509c82ba2859444b0a25a13b3fa720eb91
Andrew Purtell <apurtell@apache.org>
2021-10-18 17:48:59 -0700

    Extend TestZstdCodec with a dictionary compression test

--- hbase-common/src/main/java/org/apache/hadoop/hbase/io/compress/DictionaryCache.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/io/compress/DictionaryCache.java
@@ -17,7 +17,9 @@
 package org.apache.hadoop.hbase.io.compress;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import org.apache.hadoop.conf.Configuration;
@@ -42,6 +44,7 @@ public class DictionaryCache {
 
   public static final String DICTIONARY_MAX_SIZE_KEY = "hbase.io.compress.dictionary.max.size";
   public static final int DEFAULT_DICTIONARY_MAX_SIZE = 10 * 1024 * 1024;
+  public static final String RESOURCE_SCHEME = "resource://";
 
   private static final Logger LOG = LoggerFactory.getLogger(DictionaryCache.class);
   private static volatile LoadingCache<String, byte[]> CACHE;
@@ -61,37 +64,21 @@ public class DictionaryCache {
     if (CACHE == null) {
       synchronized (DictionaryCache.class) {
         if (CACHE == null) {
+          final int maxSize = conf.getInt(DICTIONARY_MAX_SIZE_KEY, DEFAULT_DICTIONARY_MAX_SIZE);
           CACHE = CacheBuilder.newBuilder()
             .maximumSize(100)
             .expireAfterAccess(10, TimeUnit.MINUTES)
             .build(
               new CacheLoader<String, byte[]>() {
                 public byte[] load(String s) throws Exception {
-                  final Path path = new Path(s);
-                  final FileSystem fs = FileSystem.get(path.toUri(), conf);
-                  final FileStatus stat = fs.getFileStatus(path);
-                  if (!stat.isFile()) {
-                    throw new IllegalArgumentException(s + " is not a file");
+                  byte[] bytes;
+                  if (path.startsWith(RESOURCE_SCHEME)) {
+                    bytes = loadFromResource(conf, path, maxSize);
+                  } else {
+                    bytes = loadFromHadoopFs(conf, path, maxSize);
                   }
-                  final int limit = conf.getInt(DICTIONARY_MAX_SIZE_KEY,
-                    DEFAULT_DICTIONARY_MAX_SIZE);
-                  if (stat.getLen() > limit) {
-                    throw new IllegalArgumentException("Dictionary " + s + " is too large" +
-                      ", size=" + stat.getLen() + ", limit=" + limit);
-                  }
-                  final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                  final byte[] buffer = new byte[8192];
-                  try (final FSDataInputStream in = fs.open(path)) {
-                    int n;
-                    do {
-                      n = in.read(buffer);
-                      if (n > 0) {
-                        baos.write(buffer, 0, n);
-                         }
-                    } while (n > 0);
-                  }
-                  LOG.info("Loaded dictionary from {} (size {})", s, stat.getLen());
-                  return baos.toByteArray();
+                  LOG.info("Loaded dictionary from {} (size {})", s, bytes.length);
+                  return bytes;
                 }
             });
         }
@@ -104,6 +91,72 @@ public class DictionaryCache {
     } catch (ExecutionException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  // Visible for testing
+  public static byte[] loadFromResource(final Configuration conf, final String s,
+      final int maxSize) throws IOException {
+    if (!s.startsWith(RESOURCE_SCHEME)) {
+      throw new IllegalArgumentException("Path does not start with " + RESOURCE_SCHEME);
+    }
+    final String path = s.substring(RESOURCE_SCHEME.length(), s.length());
+    final InputStream in = DictionaryCache.class.getClassLoader().getResourceAsStream(path);
+    if (in == null) {
+      throw new FileNotFoundException("Resource " + path + " not found");
+    }
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    final byte[] buffer = new byte[8192];
+    try {
+      int n, len = 0;
+      do {
+        n = in.read(buffer);
+        if (n > 0) {
+          len += n;
+          if (len > maxSize) {
+            throw new IllegalArgumentException("Dictionary " + s + " is too large" +
+              ", limit=" + maxSize);
+          }
+          baos.write(buffer, 0, n);
+        }
+      } while (n > 0);
+    } finally {
+      in.close();
+    }
+    return baos.toByteArray();
+  }
+
+  private static byte[] loadFromHadoopFs(final Configuration conf, final String s,
+      final int maxSize) throws IOException {
+    final Path path = new Path(s);
+    final FileSystem fs = FileSystem.get(path.toUri(), conf);
+    final FileStatus stat = fs.getFileStatus(path);
+    if (!stat.isFile()) {
+      throw new IllegalArgumentException(s + " is not a file");
+    }
+    if (stat.getLen() > maxSize) {
+      throw new IllegalArgumentException("Dictionary " + s + " is too large" +
+        ", size=" + stat.getLen() + ", limit=" + maxSize);
+    }
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    final byte[] buffer = new byte[8192];
+    try (final FSDataInputStream in = fs.open(path)) {
+      int n;
+      do {
+        n = in.read(buffer);
+        if (n > 0) {
+          baos.write(buffer, 0, n);
+        }
+      } while (n > 0);
+    }
+    return baos.toByteArray();
+  }
+
+  // Visible for testing
+  public static boolean contains(String dictionaryPath) {
+    if (CACHE != null) {
+      return CACHE.asMap().containsKey(dictionaryPath);
+    }
+    return false;
   }
 
 }
