f84ea4af048426542734c6c1509e6cfb95a43f72
Zhuoyue Huang <huangzhuoyue@apache.org>
2021-11-05 11:07:11 +0800

    Fixed Checkstyle

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/migrate/RollingUpgradeChore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/migrate/RollingUpgradeChore.java
@@ -47,10 +47,12 @@ import org.slf4j.LoggerFactory;
 @InterfaceAudience.Private
 public class RollingUpgradeChore extends ScheduledChore {
 
-  static final String ROLLING_UPGRADE_CHORE_PERIOD_SECONDS_KEY = "hbase.master.rolling.upgrade.chore.period.secs";
+  static final String ROLLING_UPGRADE_CHORE_PERIOD_SECONDS_KEY =
+    "hbase.master.rolling.upgrade.chore.period.secs";
   static final int DFAULT_ROLLING_UPGRADE_CHORE_PERIOD_SECONDS = 10; // 10 seconds by default
 
-  static final String ROLLING_UPGRADE_CHORE_DELAY_SECONDS_KEY = "hbase.master.rolling.upgrade.chore.delay.secs";
+  static final String ROLLING_UPGRADE_CHORE_DELAY_SECONDS_KEY =
+    "hbase.master.rolling.upgrade.chore.delay.secs";
   static final long DEFAULT_ROLLING_UPGRADE_CHORE_DELAY_SECONDS = 30; // 30 seconds
 
   static final int CONCURRENT_PROCEDURES_COUNT = 5;
@@ -61,13 +63,18 @@ public class RollingUpgradeChore extends ScheduledChore {
   private List<MigrateStoreFileTrackerProcedure> processingProcs = new ArrayList<>();
 
   public RollingUpgradeChore(MasterServices masterServices) {
-    this(masterServices.getConfiguration(), masterServices.getMasterProcedureExecutor(), masterServices.getTableDescriptors(), masterServices);
+    this(masterServices.getConfiguration(), masterServices.getMasterProcedureExecutor(),
+      masterServices.getTableDescriptors(), masterServices);
   }
 
-  private RollingUpgradeChore(Configuration conf, ProcedureExecutor<MasterProcedureEnv> procedureExecutor, TableDescriptors tableDescriptors, Stoppable stopper){
-    super(RollingUpgradeChore.class.getSimpleName(), stopper,
-      conf.getInt(ROLLING_UPGRADE_CHORE_PERIOD_SECONDS_KEY, DFAULT_ROLLING_UPGRADE_CHORE_PERIOD_SECONDS),
-      conf.getLong(ROLLING_UPGRADE_CHORE_DELAY_SECONDS_KEY, DEFAULT_ROLLING_UPGRADE_CHORE_DELAY_SECONDS),
+  private RollingUpgradeChore(Configuration conf,
+    ProcedureExecutor<MasterProcedureEnv> procedureExecutor, TableDescriptors tableDescriptors,
+    Stoppable stopper) {
+    super(RollingUpgradeChore.class.getSimpleName(), stopper, conf
+        .getInt(ROLLING_UPGRADE_CHORE_PERIOD_SECONDS_KEY,
+          DFAULT_ROLLING_UPGRADE_CHORE_PERIOD_SECONDS), conf
+        .getLong(ROLLING_UPGRADE_CHORE_DELAY_SECONDS_KEY,
+          DEFAULT_ROLLING_UPGRADE_CHORE_DELAY_SECONDS),
       TimeUnit.SECONDS);
     this.procedureExecutor = procedureExecutor;
     this.tableDescriptors = tableDescriptors;
