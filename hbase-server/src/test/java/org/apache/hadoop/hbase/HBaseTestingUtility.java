0a51d2b5befc0cf538cc64ae6e7663e5a0d55ad9
Duo Zhang <zhangduo@apache.org>
2021-11-23 18:31:18 +0800

    HBASE-26475 The flush and compact methods in HTU should skip processing secondary replicas (#3868)
    Signed-off-by: Xiaolin Ha <haxiaolin@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
@@ -1467,7 +1467,6 @@ public class HBaseTestingUtility extends HBaseZKTestingUtility {
 
   /**
    * Flushes all caches in the mini hbase cluster
-   * @throws IOException
    */
   public void flush() throws IOException {
     getMiniHBaseCluster().flushcache();
@@ -1475,7 +1474,6 @@ public class HBaseTestingUtility extends HBaseZKTestingUtility {
 
   /**
    * Flushes all caches in the mini hbase cluster
-   * @throws IOException
    */
   public void flush(TableName tableName) throws IOException {
     getMiniHBaseCluster().flushcache(tableName);
@@ -1483,7 +1481,6 @@ public class HBaseTestingUtility extends HBaseZKTestingUtility {
 
   /**
    * Compact all regions in the mini hbase cluster
-   * @throws IOException
    */
   public void compact(boolean major) throws IOException {
     getMiniHBaseCluster().compact(major);
@@ -1491,7 +1488,6 @@ public class HBaseTestingUtility extends HBaseZKTestingUtility {
 
   /**
    * Compact all of a table's reagion in the mini hbase cluster
-   * @throws IOException
    */
   public void compact(TableName tableName, boolean major) throws IOException {
     getMiniHBaseCluster().compact(tableName, major);
