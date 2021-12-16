987f61f97fe1a31d4457708ba6428c9cbf34e6f7
Yutong Sean <yutongsean@gmail.com>
2021-12-16 10:47:37 +0800

    HBASE-26583 Make StatisticThread alternative in BucketCache

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
@@ -216,9 +216,7 @@ public class BucketCache implements BlockCache, HeapSize {
   });
 
   /** Statistics thread schedule pool (for heavy debugging, could remove) */
-  private transient final ScheduledExecutorService scheduleThreadPool =
-    Executors.newScheduledThreadPool(1,
-      new ThreadFactoryBuilder().setNameFormat("BucketCacheStatsExecutor").setDaemon(true).build());
+  private transient ScheduledExecutorService scheduleThreadPool;
 
   // Allocate or free space for the block
   private transient BucketAllocator bucketAllocator;
@@ -244,6 +242,8 @@ public class BucketCache implements BlockCache, HeapSize {
   private static final String FILE_VERIFY_ALGORITHM =
     "hbase.bucketcache.persistent.file.integrity.check.algorithm";
   private static final String DEFAULT_FILE_VERIFY_ALGORITHM = "MD5";
+  private static final String STAT_THREAD_ENABLE_KEY = "hbase.bucketcache.stat.enable";
+  private static final boolean STAT_THREAD_ENABLE_DEFAULT = false;
 
   /**
    * Use {@link java.security.MessageDigest} class's encryption algorithms to check
@@ -328,8 +328,12 @@ public class BucketCache implements BlockCache, HeapSize {
     // Run the statistics thread periodically to print the cache statistics log
     // TODO: Add means of turning this off.  Bit obnoxious running thread just to make a log
     // every five minutes.
-    this.scheduleThreadPool.scheduleAtFixedRate(new StatisticsThread(this),
+    if (conf.getBoolean(STAT_THREAD_ENABLE_KEY, STAT_THREAD_ENABLE_DEFAULT)) {
+      this.scheduleThreadPool = Executors.newScheduledThreadPool(1,
+        new ThreadFactoryBuilder().setNameFormat("BucketCacheStatsExecutor").setDaemon(true).build());
+      this.scheduleThreadPool.scheduleAtFixedRate(new StatisticsThread(this),
         statThreadPeriod, statThreadPeriod, TimeUnit.SECONDS);
+    }
     LOG.info("Started bucket cache; ioengine=" + ioEngineName +
         ", capacity=" + StringUtils.byteDesc(capacity) +
       ", blockSize=" + StringUtils.byteDesc(blockSize) + ", writerThreadNum=" +
@@ -1316,7 +1320,9 @@ public class BucketCache implements BlockCache, HeapSize {
     if (!cacheEnabled) return;
     cacheEnabled = false;
     ioEngine.shutdown();
-    this.scheduleThreadPool.shutdown();
+    if (this.scheduleThreadPool != null) {
+      this.scheduleThreadPool.shutdown();
+    }
     for (int i = 0; i < writerThreads.length; ++i) writerThreads[i].interrupt();
     this.ramCache.clear();
     if (!ioEngine.isPersistent() || persistencePath == null) {
