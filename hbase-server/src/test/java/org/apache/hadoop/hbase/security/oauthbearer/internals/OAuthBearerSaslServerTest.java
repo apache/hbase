f34ca998d0f20dcc81770e11e742e65427c0d2fe
Andor Molnar <andor@cloudera.com>
2022-01-10 17:52:13 +0100

    HBASE-26655. Remove more extension code, move client logic out of common project

--- hbase-server/src/test/java/org/apache/hadoop/hbase/security/oauthbearer/internals/OAuthBearerSaslServerTest.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/security/oauthbearer/internals/OAuthBearerSaslServerTest.java
@@ -20,29 +20,17 @@ package org.apache.hadoop.hbase.security.oauthbearer.internals;
 import static org.apache.hadoop.hbase.security.oauthbearer.JwtTestUtils.USER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.UnsupportedCallbackException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.exceptions.SaslAuthenticationException;
-import org.apache.hadoop.hbase.security.auth.AuthenticateCallbackHandler;
-import org.apache.hadoop.hbase.security.auth.SaslExtensions;
 import org.apache.hadoop.hbase.security.oauthbearer.JwtTestUtils;
-import org.apache.hadoop.hbase.security.oauthbearer.OAuthBearerExtensionsValidatorCallback;
 import org.apache.hadoop.hbase.security.oauthbearer.OAuthBearerToken;
-import org.apache.hadoop.hbase.security.oauthbearer.OAuthBearerTokenMock;
-import org.apache.hadoop.hbase.security.oauthbearer.OAuthBearerValidatorCallback;
 import org.apache.hadoop.hbase.security.oauthbearer.internals.knox.OAuthBearerConfigException;
 import org.apache.hadoop.hbase.security.oauthbearer.internals.knox.OAuthBearerSignedJwtValidatorCallbackHandler;
 import org.apache.hadoop.hbase.testclassification.MiscTests;
@@ -59,28 +47,8 @@ public class OAuthBearerSaslServerTest {
     HBaseClassTestRule.forClass(OAuthBearerSaslServerTest.class);
 
   private static final Configuration CONFIGS;
-  private static final AuthenticateCallbackHandler EXTENSIONS_VALIDATOR_CALLBACK_HANDLER;
   static {
     CONFIGS = new Configuration();
-    EXTENSIONS_VALIDATOR_CALLBACK_HANDLER = new OAuthBearerSignedJwtValidatorCallbackHandler() {
-      @Override
-      public void handle(Callback[] callbacks) throws UnsupportedCallbackException {
-        for (Callback callback : callbacks) {
-          if (callback instanceof OAuthBearerValidatorCallback) {
-            OAuthBearerValidatorCallback validationCallback =
-              (OAuthBearerValidatorCallback) callback;
-            validationCallback.token(new OAuthBearerTokenMock());
-          } else if (callback instanceof OAuthBearerExtensionsValidatorCallback) {
-            OAuthBearerExtensionsValidatorCallback extensionsCallback =
-              (OAuthBearerExtensionsValidatorCallback) callback;
-            extensionsCallback.storeAsValid("firstKey");
-            extensionsCallback.storeAsValid("secondKey");
-          } else {
-            throw new UnsupportedCallbackException(callback);
-          }
-        }
-      }
-    };
   }
 
   private String JWT;
@@ -117,80 +85,6 @@ public class OAuthBearerSaslServerTest {
         OAuthBearerSaslServer.CREDENTIAL_LIFETIME_MS_SASL_NEGOTIATED_PROPERTY_KEY));
   }
 
-  /**
-   * SASL Extensions that are validated by the callback handler should be accessible through
-   * the {@code #getNegotiatedProperty()} method
-   */
-  @Test
-  public void savesCustomExtensionAsNegotiatedProperty() throws Exception {
-    Map<String, String> customExtensions = new HashMap<>();
-    customExtensions.put("firstKey", "value1");
-    customExtensions.put("secondKey", "value2");
-
-    byte[] nextChallenge = saslServer
-      .evaluateResponse(clientInitialResponse(null, false, customExtensions));
-
-    assertTrue("Next challenge is not empty", nextChallenge.length == 0);
-    assertEquals("value1", saslServer.getNegotiatedProperty("firstKey"));
-    assertEquals("value2", saslServer.getNegotiatedProperty("secondKey"));
-  }
-
-  /**
-   * SASL Extensions that were not recognized (neither validated nor invalidated)
-   * by the callback handler must not be accessible through the {@code #getNegotiatedProperty()}
-   * method
-   */
-  @Test
-  public void unrecognizedExtensionsAreNotSaved() throws Exception {
-    saslServer = new OAuthBearerSaslServer(EXTENSIONS_VALIDATOR_CALLBACK_HANDLER);
-    Map<String, String> customExtensions = new HashMap<>();
-    customExtensions.put("firstKey", "value1");
-    customExtensions.put("secondKey", "value1");
-    customExtensions.put("thirdKey", "value1");
-
-    byte[] nextChallenge = saslServer
-      .evaluateResponse(clientInitialResponse(null, false, customExtensions));
-
-    assertTrue("Next challenge is not empty", nextChallenge.length == 0);
-    assertNull("Extensions not recognized by the server must be ignored",
-      saslServer.getNegotiatedProperty("thirdKey"));
-  }
-
-  /**
-   * If the callback handler handles the `OAuthBearerExtensionsValidatorCallback`
-   *  and finds an invalid extension, SaslServer should throw an authentication exception
-   */
-  @Test
-  public void throwsAuthenticationExceptionOnInvalidExtensions() {
-    OAuthBearerSignedJwtValidatorCallbackHandler invalidHandler =
-      new OAuthBearerSignedJwtValidatorCallbackHandler() {
-      @Override
-      public void handle(Callback[] callbacks) throws UnsupportedCallbackException {
-        for (Callback callback : callbacks) {
-          if (callback instanceof OAuthBearerValidatorCallback) {
-            OAuthBearerValidatorCallback validationCallback =
-              (OAuthBearerValidatorCallback) callback;
-            validationCallback.token(new OAuthBearerTokenMock());
-          } else if (callback instanceof OAuthBearerExtensionsValidatorCallback) {
-            OAuthBearerExtensionsValidatorCallback extensionsCallback =
-              (OAuthBearerExtensionsValidatorCallback) callback;
-            extensionsCallback.storeAsError("firstKey", "is not valid");
-            extensionsCallback.storeAsError("secondKey", "is not valid either");
-          } else {
-            throw new UnsupportedCallbackException(callback);
-          }
-        }
-      }
-    };
-    saslServer = new OAuthBearerSaslServer(invalidHandler);
-    Map<String, String> customExtensions = new HashMap<>();
-    customExtensions.put("firstKey", "value");
-    customExtensions.put("secondKey", "value");
-
-    assertThrows(SaslAuthenticationException.class,
-      () -> saslServer.evaluateResponse(clientInitialResponse(null, false, customExtensions)));
-  }
-
   @Test
   public void authorizatonIdEqualsAuthenticationId() throws Exception {
     byte[] nextChallenge = saslServer
@@ -206,23 +100,20 @@ public class OAuthBearerSaslServerTest {
 
   @Test
   public void illegalToken() throws Exception {
-    byte[] bytes = saslServer.evaluateResponse(clientInitialResponse(null, true,
-      Collections.emptyMap()));
+    byte[] bytes = saslServer.evaluateResponse(clientInitialResponse(null, true));
     String challenge = new String(bytes, StandardCharsets.UTF_8);
     assertEquals("{\"status\":\"invalid_token\"}", challenge);
   }
 
   private byte[] clientInitialResponse(String authorizationId)
-    throws OAuthBearerConfigException, IOException {
-    return clientInitialResponse(authorizationId, false, Collections.emptyMap());
+    throws OAuthBearerConfigException {
+    return clientInitialResponse(authorizationId, false);
   }
 
-  private byte[] clientInitialResponse(String authorizationId, boolean illegalToken,
-    Map<String, String> customExtensions)
-    throws OAuthBearerConfigException, IOException {
+  private byte[] clientInitialResponse(String authorizationId, boolean illegalToken)
+    throws OAuthBearerConfigException {
     String compactSerialization = JWT;
     String tokenValue = compactSerialization + (illegalToken ? "AB" : "");
-    return new OAuthBearerClientInitialResponse(tokenValue, authorizationId,
-      new SaslExtensions(customExtensions)).toBytes();
+    return new OAuthBearerClientInitialResponse(tokenValue, authorizationId).toBytes();
   }
 }
