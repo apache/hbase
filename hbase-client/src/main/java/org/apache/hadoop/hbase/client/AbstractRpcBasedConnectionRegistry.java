bd317d913937604e1dac9e84b64c80f7401bbe9a
Nick Dimiduk <ndimiduk@apache.org>
2022-01-12 13:34:51 -0800

    HBASE-26474 Implement connection-level attributes (addendum)
    Addressing additional comments raised in branch-2 backport PR #4014

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/AbstractRpcBasedConnectionRegistry.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/AbstractRpcBasedConnectionRegistry.java
@@ -272,11 +272,6 @@ abstract class AbstractRpcBasedConnectionRegistry implements ConnectionRegistry 
   }
 
   @Override
-  public String getConnectionString() {
-    return "unimplemented";
-  }
-
-  @Override
   public void close() {
     trace(() -> {
       if (registryEndpointRefresher != null) {
