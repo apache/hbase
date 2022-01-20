d9b4c3400657a84b2c9e588fb279b52a1ca71d45
Peter Somogyi <psomogyi@apache.org>
2022-01-20 15:04:31 +0100

    HBASE-26689 Backport HBASE-24443 Refactor TestCustomSaslAuthenticationProvider (#4049)
    Duo Zhang <zhangduo@apache.org>
    
    (cherry picked from commit a02324f202dd25dc56fd00db1e70d61722c2248a)
    Change-Id: Id027a4c624e883700af74f66b80d87a8bf674512

---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/security/provider/TestCustomSaslAuthenticationProviderSimpleRpcServer.java
@@ -0,0 +1,43 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.security.provider;
+
+import org.apache.hadoop.hbase.HBaseClassTestRule;
+import org.apache.hadoop.hbase.ipc.SimpleRpcServer;
+import org.apache.hadoop.hbase.testclassification.MediumTests;
+import org.apache.hadoop.hbase.testclassification.SecurityTests;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+@Category({ MediumTests.class, SecurityTests.class })
+public class TestCustomSaslAuthenticationProviderSimpleRpcServer
+  extends CustomSaslAuthenticationProviderTestBase {
+
+  @ClassRule
+  public static final HBaseClassTestRule CLASS_RULE =
+    HBaseClassTestRule.forClass(TestCustomSaslAuthenticationProviderSimpleRpcServer.class);
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    startCluster(SimpleRpcServer.class.getName());
+  }
+}
