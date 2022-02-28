81918a46e5bd46a514937732182750991cfdc4bb
Istvan Toth <stoty@stoty.hu>
2022-02-28 17:16:19 +0100

    HBASE-26777 BufferedDataBlockEncoder$OffheapDecodedExtendedCell.deepClone throws UnsupportedOperationException
    Change-Id: I45807821ebf4ae867b5c9c10e2700ecd3c009a4b

--- hbase-common/src/main/java/org/apache/hadoop/hbase/io/encoding/BufferedDataBlockEncoder.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/io/encoding/BufferedDataBlockEncoder.java
@@ -715,11 +715,6 @@ abstract class BufferedDataBlockEncoder extends AbstractDataBlockEncoder {
       throw new UnsupportedOperationException();
     }
 
-    @Override
-    public ExtendedCell deepClone() {
-      // This is not used in actual flow. Throwing UnsupportedOperationException
-      throw new UnsupportedOperationException();
-    }
   }
 
   protected abstract static class BufferedEncodedSeeker<STATE extends SeekerState>
