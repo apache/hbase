f57812dd316741fd2e8ae810368287fb897068bd
haxiaolin <haxiaolin@apache.org>
2022-03-07 16:30:56 +0800

    address checkstyle issue

--- hbase-server/src/main/java/org/apache/hadoop/hbase/wal/AbstractWALRoller.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/wal/AbstractWALRoller.java
@@ -62,7 +62,8 @@ public abstract class AbstractWALRoller<T extends Abortable> extends Thread
   /**
    * Configure for the timeout of log rolling retry.
    */
-  protected static final String WAL_ROLL_WAIT_TIMEOUT = "hbase.regionserver.logroll.wait.timeout.ms";
+  protected static final String WAL_ROLL_WAIT_TIMEOUT =
+    "hbase.regionserver.logroll.wait.timeout.ms";
 
   /**
    * Configure for the max count of log rolling retry.
