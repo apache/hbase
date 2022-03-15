9c8d1e4625ce427bd0bc24a7892956dda839f4e3
Duo Zhang <zhangduo@apache.org>
2022-03-15 20:13:46 +0800

    HBASE-26611 Changing SFT implementation on disabled table is dangerous (#4082)
    Signed-off-by: Xiaolin Ha <haxiaolin@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/procedure/ModifyTableProcedure.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/procedure/ModifyTableProcedure.java
@@ -321,7 +321,7 @@ public class ModifyTableProcedure
 
     // check for store file tracker configurations
     StoreFileTrackerValidationUtils.checkForModifyTable(env.getMasterConfiguration(),
-      unmodifiedTableDescriptor, modifiedTableDescriptor);
+      unmodifiedTableDescriptor, modifiedTableDescriptor, !isTableEnabled(env));
   }
 
   /**
