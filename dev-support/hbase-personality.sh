93798cc012ed675864035d188fb02672d4d38227
Duo Zhang <zhangduo@apache.org>
2022-03-28 23:06:25 +0800

    HBASE-26892 Add spotless:check in our pre commit general check

--- dev-support/hbase-personality.sh
+++ dev-support/hbase-personality.sh
@@ -865,6 +865,59 @@ function hbaseanti_patchfile
   return 0
 }
 
+######################################
+
+add_test_type spotless
+
+## @description  only run the test if java or pom changes.
+## @audience     private
+## @stability    evolving
+## @param        filename
+function spotless_filefilter
+{
+  local filename=$1
+
+  if [[ ${filename} =~ \.java$ ]] || [[ ${filename} =~ pom.xml$ ]]; then
+    add_test spotless
+  fi
+}
+
+## @description run spotless:check to check format issues
+## @audience private
+## @stability evolving
+## @param repostatus
+function spotless_rebuild
+{
+  local repostatus=$1
+  local logfile="${PATCH_DIR}/${repostatus}-spotless.txt"
+
+  if ! verify_needed_test spotless; then
+    return 0
+  fi
+
+  big_console_header "Checking spotless on ${repostatus}"
+
+  start_clock
+
+  local -a maven_args=('spotless:check')
+
+  # disabled because "maven_executor" needs to return both command and args
+  # shellcheck disable=2046
+  echo_and_redirect "${logfile}" $(maven_executor) "${maven_args[@]}"
+
+  count=$(${GREP} -c '\[ERROR\]' "${logfile}")
+  if [[ ${count} -gt 0 ]]; then
+    add_vote_table -1 spotless "${repostatus} has ${count} errors when running spotless:check, run spotless:apply to fix."
+    add_footer_table spotless "@@BASE@@/${repostatus}-spotless.txt"
+    return 1
+  fi
+
+  add_vote_table +1 spotless "${repostatus} has no errors when running spotless:check."
+  return 0
+}
+
+######################################
+
 ## @description  process the javac output for generating WARNING/ERROR
 ## @audience     private
 ## @stability    evolving
