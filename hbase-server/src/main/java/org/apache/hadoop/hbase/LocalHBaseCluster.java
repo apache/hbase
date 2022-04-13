8247b7c722f95f94afd5cb9d9b80c9ce5f61a7de
Duo Zhang <zhangduo@apache.org>
2022-04-13 10:49:40 +0800

    HBASE-26941 LocalHBaseCluster.waitOnRegionServer should quit while thread is interrupted (#4333)
    Signed-off-by: Xin Sun <ddupgs@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/LocalHBaseCluster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/LocalHBaseCluster.java
@@ -300,7 +300,7 @@ public class LocalHBaseCluster {
    * Wait for the specified region server to stop. Removes this thread from list of running threads.
    * @return Name of region server that just went down.
    */
-  public String waitOnRegionServer(int serverNumber) {
+  public String waitOnRegionServer(int serverNumber) throws InterruptedException {
     JVMClusterUtil.RegionServerThread regionServerThread = this.regionThreads.get(serverNumber);
     return waitOnRegionServer(regionServerThread);
   }
@@ -309,15 +309,11 @@ public class LocalHBaseCluster {
    * Wait for the specified region server to stop. Removes this thread from list of running threads.
    * @return Name of region server that just went down.
    */
-  public String waitOnRegionServer(JVMClusterUtil.RegionServerThread rst) {
+  public String waitOnRegionServer(JVMClusterUtil.RegionServerThread rst)
+    throws InterruptedException {
     while (rst.isAlive()) {
-      try {
-        LOG.info("Waiting on " + rst.getRegionServer().toString());
-        rst.join();
-      } catch (InterruptedException e) {
-        LOG.error("Interrupted while waiting for {} to finish. Retrying join", rst.getName(), e);
-        Thread.currentThread().interrupt();
-      }
+      LOG.info("Waiting on " + rst.getRegionServer().toString());
+      rst.join();
     }
     regionThreads.remove(rst);
     return rst.getName();
@@ -373,7 +369,7 @@ public class LocalHBaseCluster {
    * Wait for the specified master to stop. Removes this thread from list of running threads.
    * @return Name of master that just went down.
    */
-  public String waitOnMaster(int serverNumber) {
+  public String waitOnMaster(int serverNumber) throws InterruptedException {
     JVMClusterUtil.MasterThread masterThread = this.masterThreads.get(serverNumber);
     return waitOnMaster(masterThread);
   }
@@ -382,16 +378,10 @@ public class LocalHBaseCluster {
    * Wait for the specified master to stop. Removes this thread from list of running threads.
    * @return Name of master that just went down.
    */
-  public String waitOnMaster(JVMClusterUtil.MasterThread masterThread) {
+  public String waitOnMaster(JVMClusterUtil.MasterThread masterThread) throws InterruptedException {
     while (masterThread.isAlive()) {
-      try {
-        LOG.info("Waiting on " + masterThread.getMaster().getServerName().toString());
-        masterThread.join();
-      } catch (InterruptedException e) {
-        LOG.error("Interrupted while waiting for {} to finish. Retrying join",
-            masterThread.getName(), e);
-        Thread.currentThread().interrupt();
-      }
+      LOG.info("Waiting on " + masterThread.getMaster().getServerName().toString());
+      masterThread.join();
     }
     masterThreads.remove(masterThread);
     return masterThread.getName();
