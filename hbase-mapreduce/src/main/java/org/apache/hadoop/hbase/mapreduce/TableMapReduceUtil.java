d65de8b4f74b6555518bf6433864e98e30162741
Nick Dimiduk <ndimiduk@apache.org>
2022-04-21 11:00:42 +0200

    HBASE-26954 Compilation of master vs hadoop-3.3.2 fails

--- hbase-mapreduce/src/main/java/org/apache/hadoop/hbase/mapreduce/TableMapReduceUtil.java
+++ hbase-mapreduce/src/main/java/org/apache/hadoop/hbase/mapreduce/TableMapReduceUtil.java
@@ -829,7 +829,6 @@ public class TableMapReduceUtil {
       org.apache.hbase.thirdparty.com.google.protobuf.UnsafeByteOperations.class, // hb-sh-protobuf
       org.apache.hbase.thirdparty.io.netty.channel.Channel.class,    // hbase-shaded-netty
       org.apache.zookeeper.ZooKeeper.class,                          // zookeeper
-      org.apache.htrace.core.Tracer.class,                           // htrace
       com.codahale.metrics.MetricRegistry.class,                     // metrics-core
       org.apache.commons.lang3.ArrayUtils.class,                     // commons-lang
       io.opentelemetry.api.trace.Span.class,                         // opentelemetry-api
