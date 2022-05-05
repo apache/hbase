3902b6a8d813bd49c656d7a5855e20902da607fc
Wellington Chevreuil <wchevreuil@apache.org>
2022-05-05 14:05:02 +0100

    HBASE-26999 HStore should try write WAL compaction marker before replacing compacted files in StoreEngine

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
@@ -1232,14 +1232,14 @@ public class HStore
       allowedOnPath = ".*/(HStore|TestHStore).java")
   void replaceStoreFiles(Collection<HStoreFile> compactedFiles, Collection<HStoreFile> result,
     boolean writeCompactionMarker) throws IOException {
+    if (writeCompactionMarker) {
+      writeCompactionWalRecord(compactedFiles, result);
+    }
     storeEngine.replaceStoreFiles(compactedFiles, result, () -> {
       synchronized (filesCompacting) {
         filesCompacting.removeAll(compactedFiles);
       }
     });
-    if (writeCompactionMarker) {
-      writeCompactionWalRecord(compactedFiles, result);
-    }
     // These may be null when the RS is shutting down. The space quota Chores will fix the Region
     // sizes later so it's not super-critical if we miss these.
     RegionServerServices rsServices = region.getRegionServerServices();
