6db32c97f0d631f3e372d906c653a5ceb601c2eb
Rushabh Shah <shahrs87@apache.org>
2022-05-06 09:30:02 -0700

    HBASE-26963 ReplicationSource#removePeer hangs if we try to remove bad peer.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
@@ -551,9 +551,16 @@ public class ReplicationSource implements ReplicationSourceInterface {
       }
     }
 
+
     if (!this.isSourceActive()) {
-      retryStartup.set(!this.abortOnError);
       setSourceStartupStatus(false);
+      if (Thread.currentThread().isInterrupted()) {
+        // If source is not running and thread is interrupted this means someone has tried to
+        // remove this peer.
+        return;
+      }
+
+      retryStartup.set(!this.abortOnError);
       throw new IllegalStateException("Source should be active.");
     }
 
@@ -575,9 +582,14 @@ public class ReplicationSource implements ReplicationSourceInterface {
       }
     }
 
-    if (!this.isSourceActive()) {
-      retryStartup.set(!this.abortOnError);
+    if(!this.isSourceActive()) {
       setSourceStartupStatus(false);
+      if (Thread.currentThread().isInterrupted()) {
+        // If source is not running and thread is interrupted this means someone has tried to
+        // remove this peer.
+        return;
+      }
+      retryStartup.set(!this.abortOnError);
       throw new IllegalStateException("Source should be active.");
     }
     LOG.info("{} queueId={} (queues={}) is replicating from cluster={} to cluster={}", logPeerId(),
