518d3e488e34c41d9ce10412ac2f3f3738080deb
Rushabh Shah <shahrs87@apache.org>
2022-05-06 11:37:52 -0700

    spotcheck apply

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
@@ -551,7 +551,6 @@ public class ReplicationSource implements ReplicationSourceInterface {
       }
     }
 
-
     if (!this.isSourceActive()) {
       setSourceStartupStatus(false);
       if (Thread.currentThread().isInterrupted()) {
@@ -582,7 +581,7 @@ public class ReplicationSource implements ReplicationSourceInterface {
       }
     }
 
-    if(!this.isSourceActive()) {
+    if (!this.isSourceActive()) {
       setSourceStartupStatus(false);
       if (Thread.currentThread().isInterrupted()) {
         // If source is not running and thread is interrupted this means someone has tried to
