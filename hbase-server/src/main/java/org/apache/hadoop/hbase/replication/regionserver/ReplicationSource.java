2d1e3c92683421684049d18a483bc4fea010b0cf
Rushabh Shah <shahrs87@gmail.com>
2022-05-07 10:37:05 -0700

    HBASE-26963 ReplicationSource#removePeer hangs if we try to remove bad peer. (#4361)
    Signed-off-by: Andrew Purtell <apurtell@apache.org>
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/ReplicationSource.java
@@ -538,9 +538,16 @@ public class ReplicationSource implements ReplicationSourceInterface {
       }
     }
 
+
     if (!this.isSourceActive()) {
-      retryStartup.set(!this.abortOnError);
       setSourceStartupStatus(false);
+      if (Thread.currentThread().isInterrupted()) {
+        // If source is not running and thread is interrupted this means someone has tried to
+        // remove this peer.
+        return;
+      }
+
+      retryStartup.set(!this.abortOnError);
       throw new IllegalStateException("Source should be active.");
     }
 
@@ -562,9 +569,14 @@ public class ReplicationSource implements ReplicationSourceInterface {
       }
     }
 
-    if (!this.isSourceActive()) {
-      retryStartup.set(!this.abortOnError);
+    if(!this.isSourceActive()) {
       setSourceStartupStatus(false);
+      if (Thread.currentThread().isInterrupted()) {
+        // If source is not running and thread is interrupted this means someone has tried to
+        // remove this peer.
+        return;
+      }
+      retryStartup.set(!this.abortOnError);
       throw new IllegalStateException("Source should be active.");
     }
     LOG.info("{} queueId={} (queues={}) is replicating from cluster={} to cluster={}", logPeerId(),
