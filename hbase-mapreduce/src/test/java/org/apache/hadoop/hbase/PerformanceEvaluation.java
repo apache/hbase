5cfce692a5d7d33d1d28f16204747b8d56e402be
litao <tomleescut@gmail.com>
2022-05-11 13:31:44 +0800

    HBASE-27003 Optimize log format for PerformanceEvaluation (#4411)
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>
    Signed-off-by: Viraj Jasani <vjasani@apache.org>

--- hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
+++ hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
@@ -1236,8 +1236,9 @@ public class PerformanceEvaluation extends Configured implements Tool {
     }
 
     String generateStatus(final int sr, final int i, final int lr) {
-      return sr + "/" + i + "/" + lr + ", latency " + getShortLatencyReport()
-        + (!isRandomValueSize() ? "" : ", value size " + getShortValueSizeReport());
+      return "row [start=" + sr + ", current=" + i + ", last=" + lr + "], latency ["
+        + getShortLatencyReport() + "]"
+        + (!isRandomValueSize() ? "" : ", value size [" + getShortValueSizeReport() + "]");
     }
 
     boolean isRandomValueSize() {
