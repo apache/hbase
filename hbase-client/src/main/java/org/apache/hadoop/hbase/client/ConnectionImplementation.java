538bb0f90dc1d5122d6325639f29cd8be52283a8
Duo Zhang <zhangduo@apache.org>
2022-07-06 08:10:50 +0800

    HBASE-23330: Fix delegation token fetch with MasterRegistry (#1084) (#4598)
    Signed-off-by: Andrew Purtell <apurtell@apache.org>
    (cherry picked from commit d8b3f55518fcf73df54f67ab1cb2f3920088d70d)
    
    Co-authored-by: Bharath Vissapragada <bharathv@apache.org>
    (cherry picked from commit 2cd30e5f000fda9ca33fea45b0cc1d99b4373a8f)
    Change-Id: I19f257814bd6a27582d6a611c5b9ab1d2a3db463

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionImplementation.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionImplementation.java
@@ -2175,4 +2175,14 @@ class ConnectionImplementation implements ClusterConnection, Closeable {
       throw new IOException(cause);
     }
   }
+
+  @Override
+  public String getClusterId() {
+    try {
+      return registry.getClusterId().get();
+    } catch (InterruptedException | ExecutionException e) {
+      LOG.error("Error fetching cluster ID: ", e);
+    }
+    return null;
+  }
 }
