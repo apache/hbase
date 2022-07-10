c56abcc94b3462f9198d955e44ab3ce6b219254e
Bryan Beaudreault <bbeaudreault@hubspot.com>
2022-07-09 20:03:12 -0400

    HBASE-27186 Report block cache size metrics separately for L1 and L2

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/MetricsRegionServerWrapperStub.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/MetricsRegionServerWrapperStub.java
@@ -293,6 +293,11 @@ public class MetricsRegionServerWrapperStub implements MetricsRegionServerWrappe
   }
 
   @Override
+  public long getBlockCacheDataBlockCount() {
+    return 300;
+  }
+
+  @Override
   public long getBlockCacheSize() {
     return 415;
   }
@@ -343,6 +348,26 @@ public class MetricsRegionServerWrapperStub implements MetricsRegionServerWrappe
   }
 
   @Override
+  public long getL1CacheSize() {
+    return 123;
+  }
+
+  @Override
+  public long getL1CacheFreeSize() {
+    return 100;
+  }
+
+  @Override
+  public long getL1CacheCount() {
+    return 50;
+  }
+
+  @Override
+  public long getL1CacheEvictedCount() {
+    return 1000;
+  }
+
+  @Override
   public long getL1CacheHitCount() {
     return 200;
   }
@@ -360,6 +385,26 @@ public class MetricsRegionServerWrapperStub implements MetricsRegionServerWrappe
   @Override
   public double getL1CacheMissRatio() {
     return 20;
+  }
+
+  @Override
+  public long getL2CacheSize() {
+    return 456;
+  }
+
+  @Override
+  public long getL2CacheFreeSize() {
+    return 200;
+  }
+
+  @Override
+  public long getL2CacheCount() {
+    return 75;
+  }
+
+  @Override
+  public long getL2CacheEvictedCount() {
+    return 2000;
   }
 
   @Override
