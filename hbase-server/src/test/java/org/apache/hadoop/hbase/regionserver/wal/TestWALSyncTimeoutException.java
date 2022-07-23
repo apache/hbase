499ff33ab2e3432438956836abf96ef092f2e3d7
comnetwork <comnetwork@163.com>
2022-07-23 10:47:10 +0800

    fix serialId

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestWALSyncTimeoutException.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestWALSyncTimeoutException.java
@@ -17,14 +17,11 @@
  */
 package org.apache.hadoop.hbase.regionserver.wal;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -37,7 +34,6 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptorBuilder;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.RegionInfo;
-import org.apache.hadoop.hbase.client.RegionReplicaUtil;
 import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.apache.hadoop.hbase.client.TableDescriptorBuilder;
 import org.apache.hadoop.hbase.io.asyncfs.monitor.StreamSlowMonitor;
@@ -50,18 +46,15 @@ import org.apache.hadoop.hbase.testclassification.LargeTests;
 import org.apache.hadoop.hbase.testclassification.RegionServerTests;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.CommonFSUtils;
-import org.apache.hadoop.hbase.util.ServerRegionReplicaUtil;
 import org.apache.hadoop.hbase.wal.AsyncFSWALProvider;
 import org.apache.hadoop.hbase.wal.WAL;
 import org.apache.hadoop.hbase.wal.WALFactory;
-import org.apache.hadoop.hbase.wal.WALKeyImpl;
 import org.apache.hadoop.hbase.wal.WALProvider;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.mockito.Mockito;
 
 import org.apache.hbase.thirdparty.io.netty.channel.Channel;
 import org.apache.hbase.thirdparty.io.netty.channel.EventLoopGroup;
@@ -78,7 +71,6 @@ public class TestWALSyncTimeoutException {
   private static final byte[] QUAL = Bytes.toBytes("qualifier_test");
 
   private static final HBaseTestingUtil HTU = new HBaseTestingUtil();
-  private static final int NB_SERVERS = 2;
 
   private static TableName tableName = TableName.valueOf("TestWALSyncTimeoutException");
   private static volatile boolean testWALTimout = false;
@@ -88,17 +80,15 @@ public class TestWALSyncTimeoutException {
   @BeforeClass
   public static void setUp() throws Exception {
     Configuration conf = HTU.getConfiguration();
-    conf.setBoolean(ServerRegionReplicaUtil.REGION_REPLICA_REPLICATION_CONF_KEY, true);
     conf.setClass(HConstants.REGION_IMPL, HRegionForTest.class, HRegion.class);
     conf.setInt(RegionReplicationSink.RETRIES_NUMBER, 1);
     conf.setLong(RegionReplicationSink.RPC_TIMEOUT_MS, 10 * 60 * 1000);
     conf.setLong(RegionReplicationSink.OPERATION_TIMEOUT_MS, 20 * 60 * 1000);
     conf.setLong(RegionReplicationSink.META_EDIT_RPC_TIMEOUT_MS, 10 * 60 * 1000);
     conf.setLong(RegionReplicationSink.META_EDIT_OPERATION_TIMEOUT_MS, 20 * 60 * 1000);
-    conf.setBoolean(RegionReplicaUtil.REGION_REPLICA_WAIT_FOR_PRIMARY_FLUSH_CONF_KEY, false);
     conf.setClass(WALFactory.WAL_PROVIDER, SlowAsyncFSWALProvider.class, WALProvider.class);
     conf.setLong(AbstractFSWAL.WAL_SYNC_TIMEOUT_MS, timeoutMIlliseconds);
-    HTU.startMiniCluster(StartTestingClusterOption.builder().numRegionServers(NB_SERVERS).build());
+    HTU.startMiniCluster(StartTestingClusterOption.builder().numRegionServers(1).build());
 
   }
 
@@ -114,11 +104,7 @@ public class TestWALSyncTimeoutException {
    */
   @Test
   public void testWALSyncWriteException() throws Exception {
-    final HRegionForTest[] regions = this.createTable();
-    RegionReplicationSink regionReplicationSink = regions[0].getRegionReplicationSink().get();
-    assertTrue(regionReplicationSink != null);
-    final AtomicInteger replicateCounter = new AtomicInteger(0);
-    setUpSpiedRegionReplicationSink(regionReplicationSink, regions[0], replicateCounter);
+    final HRegionForTest region = this.createTable();
 
     String oldThreadName = Thread.currentThread().getName();
     Thread.currentThread().setName(USER_THREAD_NAME);
@@ -133,61 +119,25 @@ public class TestWALSyncTimeoutException {
        * {@link HRegion#doWALAppend} catches this exception it aborts the RegionServer.
        */
       try {
-        regions[0].put(new Put(rowKey1).addColumn(FAMILY, QUAL, value1));
+        region.put(new Put(rowKey1).addColumn(FAMILY, QUAL, value1));
         fail();
       } catch (WALSyncTimeoutIOException e) {
         assertTrue(e != null);
       }
-      assertTrue(regions[0].getRSServices().isAborted());
-      assertTrue(replicateCounter.get() == 0);
+      assertTrue(region.getRSServices().isAborted());
     } finally {
       Thread.currentThread().setName(oldThreadName);
       testWALTimout = false;
     }
   }
 
-  private static RegionReplicationSink setUpSpiedRegionReplicationSink(
-    final RegionReplicationSink regionReplicationSink, final HRegionForTest primaryRegion,
-    final AtomicInteger counter) {
-    RegionReplicationSink spiedRegionReplicationSink = Mockito.spy(regionReplicationSink);
-
-    Mockito.doAnswer((invocationOnMock) -> {
-      if (!testWALTimout || !USER_THREAD_NAME.equals(Thread.currentThread().getName())) {
-        invocationOnMock.callRealMethod();
-        return null;
-      }
-      WALKeyImpl walKey = invocationOnMock.getArgument(0);
-      if (!walKey.getTableName().equals(tableName)) {
-        invocationOnMock.callRealMethod();
-        return null;
-      }
-      counter.incrementAndGet();
-      invocationOnMock.callRealMethod();
-      return null;
-    }).when(spiedRegionReplicationSink).add(Mockito.any(), Mockito.any(), Mockito.any());
-    primaryRegion.setRegionReplicationSink(spiedRegionReplicationSink);
-    return spiedRegionReplicationSink;
-  }
-
-  private HRegionForTest[] createTable() throws Exception {
+  private HRegionForTest createTable() throws Exception {
     TableDescriptor tableDescriptor =
-      TableDescriptorBuilder.newBuilder(tableName).setRegionReplication(NB_SERVERS)
+      TableDescriptorBuilder.newBuilder(tableName)
         .setColumnFamily(ColumnFamilyDescriptorBuilder.of(FAMILY)).build();
     HTU.getAdmin().createTable(tableDescriptor);
-    final HRegionForTest[] regions = new HRegionForTest[NB_SERVERS];
-    for (int i = 0; i < NB_SERVERS; i++) {
-      HRegionServer rs = HTU.getMiniHBaseCluster().getRegionServer(i);
-      List<HRegion> onlineRegions = rs.getRegions(tableName);
-      for (HRegion region : onlineRegions) {
-        int replicaId = region.getRegionInfo().getReplicaId();
-        assertTrue(regions[replicaId] == null);
-        regions[region.getRegionInfo().getReplicaId()] = (HRegionForTest) region;
-      }
-    }
-    for (HRegionForTest region : regions) {
-      assertNotNull(region);
-    }
-    return regions;
+    HRegionServer rs = HTU.getMiniHBaseCluster().getRegionServer(0);
+    return (HRegionForTest) rs.getRegions(tableName).get(0);
   }
 
   public static final class HRegionForTest extends HRegion {
@@ -203,9 +153,6 @@ public class TestWALSyncTimeoutException {
       super(tableDir, wal, fs, confParam, regionInfo, htd, rsServices);
     }
 
-    public void setRegionReplicationSink(RegionReplicationSink regionReplicationSink) {
-      this.regionReplicationSink = Optional.of(regionReplicationSink);
-    }
 
     public RegionServerServices getRSServices() {
       return this.rsServices;
