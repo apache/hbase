3309108ca728cb40ad0da5be567ca0e1beabbeff
Duo Zhang <zhangduo@apache.org>
2022-08-12 12:17:29 +0800

    HBASE-27278 Improve TestTlsIPC to reuse existing IPC test code (#4682)
    Signed-off-by: Bryan Beaudreault <bbeaudreault@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/ipc/TestNettyIPC.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/ipc/TestNettyIPC.java
@@ -23,11 +23,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.DoNotRetryIOException;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
-import org.apache.hadoop.hbase.Server;
 import org.apache.hadoop.hbase.codec.Codec;
-import org.apache.hadoop.hbase.nio.ByteBuff;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.testclassification.RPCTests;
 import org.apache.hadoop.hbase.util.JVM;
@@ -40,7 +37,6 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
-import org.apache.hbase.thirdparty.io.netty.channel.Channel;
 import org.apache.hbase.thirdparty.io.netty.channel.epoll.EpollEventLoopGroup;
 import org.apache.hbase.thirdparty.io.netty.channel.epoll.EpollSocketChannel;
 import org.apache.hbase.thirdparty.io.netty.channel.nio.NioEventLoopGroup;
@@ -107,10 +103,10 @@ public class TestNettyIPC extends AbstractTestIPC {
   }
 
   @Override
-  protected RpcServer createRpcServer(Server server, String name,
+  protected RpcServer createRpcServer(String name,
     List<RpcServer.BlockingServiceAndInterface> services, InetSocketAddress bindAddress,
     Configuration conf, RpcScheduler scheduler) throws IOException {
-    return new NettyRpcServer(server, name, services, bindAddress, conf, scheduler, true);
+    return new NettyRpcServer(null, name, services, bindAddress, conf, scheduler, true);
   }
 
   @Override
@@ -119,7 +115,7 @@ public class TestNettyIPC extends AbstractTestIPC {
     return new NettyRpcClient(conf) {
 
       @Override
-      Codec getCodec() {
+      protected Codec getCodec() {
         return null;
       }
 
@@ -138,48 +134,16 @@ public class TestNettyIPC extends AbstractTestIPC {
     return new NettyRpcClient(conf) {
 
       @Override
-      boolean isTcpNoDelay() {
+      protected boolean isTcpNoDelay() {
         throw new RuntimeException("Injected fault");
       }
     };
   }
 
-  private static class TestFailingRpcServer extends NettyRpcServer {
-
-    TestFailingRpcServer(Server server, String name,
-      List<RpcServer.BlockingServiceAndInterface> services, InetSocketAddress bindAddress,
-      Configuration conf, RpcScheduler scheduler) throws IOException {
-      super(server, name, services, bindAddress, conf, scheduler, true);
-    }
-
-    static final class FailingConnection extends NettyServerRpcConnection {
-      private FailingConnection(TestFailingRpcServer rpcServer, Channel channel) {
-        super(rpcServer, channel);
-      }
-
-      @Override
-      public void processRequest(ByteBuff buf) throws IOException, InterruptedException {
-        // this will throw exception after the connection header is read, and an RPC is sent
-        // from client
-        throw new DoNotRetryIOException("Failing for test");
-      }
-    }
-
-    @Override
-    protected NettyRpcServerPreambleHandler createNettyRpcServerPreambleHandler() {
-      return new NettyRpcServerPreambleHandler(TestFailingRpcServer.this) {
-        @Override
-        protected NettyServerRpcConnection createNettyServerRpcConnection(Channel channel) {
-          return new FailingConnection(TestFailingRpcServer.this, channel);
-        }
-      };
-    }
-  }
-
   @Override
-  protected RpcServer createTestFailingRpcServer(Server server, String name,
+  protected RpcServer createTestFailingRpcServer(String name,
     List<RpcServer.BlockingServiceAndInterface> services, InetSocketAddress bindAddress,
     Configuration conf, RpcScheduler scheduler) throws IOException {
-    return new TestFailingRpcServer(server, name, services, bindAddress, conf, scheduler);
+    return new FailingNettyRpcServer(null, name, services, bindAddress, conf, scheduler);
   }
 }
