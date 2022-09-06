fde05e95a82fea7c7415edf195eb89b9986deb2b
Andor Molnár <andor@cloudera.com>
2022-09-06 21:43:41 +0200

    CDPD-54349: Backport of HBASE-27346
    HBASE-27346 Autodetect key/truststore file type from file extension (#4757)
    
    "This commit does not contain secrets"
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Bryan Beaudreault <bbeaudreault@apache.org>
    (cherry picked from commit d2cc840edf4888d090e6777a621fd976e7abccd7)
    Change-Id: I36a839bf04e93b7a937cf08525620b0ec16e953b

---
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/io/crypto/tls/FileKeyStoreLoader.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.io.crypto.tls;
+
+import java.util.Objects;
+
+/**
+ * Base class for instances of {@link KeyStoreLoader} which load the key/trust stores from files on
+ * a filesystem.
+ * <p/>
+ * This file has been copied from the Apache ZooKeeper project.
+ * @see <a href=
+ *      "https://github.com/apache/zookeeper/blob/c74658d398cdc1d207aa296cb6e20de00faec03e/zookeeper-server/src/main/java/org/apache/zookeeper/common/FileKeyStoreLoader.java">Base
+ *      revision</a>
+ */
+abstract class FileKeyStoreLoader implements KeyStoreLoader {
+  final String keyStorePath;
+  final String trustStorePath;
+  final char[] keyStorePassword;
+  final char[] trustStorePassword;
+
+  FileKeyStoreLoader(String keyStorePath, String trustStorePath, char[] keyStorePassword,
+    char[] trustStorePassword) {
+    this.keyStorePath = keyStorePath;
+    this.trustStorePath = trustStorePath;
+    this.keyStorePassword = keyStorePassword;
+    this.trustStorePassword = trustStorePassword;
+  }
+
+  /**
+   * Base class for builder pattern used by subclasses.
+   * @param <T> the subtype of FileKeyStoreLoader created by the Builder.
+   */
+  static abstract class Builder<T extends FileKeyStoreLoader> {
+    String keyStorePath;
+    String trustStorePath;
+    char[] keyStorePassword;
+    char[] trustStorePassword;
+
+    Builder() {
+    }
+
+    Builder<T> setKeyStorePath(String keyStorePath) {
+      this.keyStorePath = Objects.requireNonNull(keyStorePath);
+      return this;
+    }
+
+    Builder<T> setTrustStorePath(String trustStorePath) {
+      this.trustStorePath = Objects.requireNonNull(trustStorePath);
+      return this;
+    }
+
+    Builder<T> setKeyStorePassword(char[] keyStorePassword) {
+      this.keyStorePassword = Objects.requireNonNull(keyStorePassword);
+      return this;
+    }
+
+    Builder<T> setTrustStorePassword(char[] trustStorePassword) {
+      this.trustStorePassword = Objects.requireNonNull(trustStorePassword);
+      return this;
+    }
+
+    abstract T build();
+  }
+}
--- hbase-common/src/test/java/org/apache/hadoop/hbase/io/crypto/tls/X509TestContext.java
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/io/crypto/tls/X509TestContext.java
@@ -31,7 +31,6 @@ import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.yetus.audience.InterfaceAudience;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
@@ -64,6 +63,7 @@ public final class X509TestContext {
   private File trustStoreJksFile;
   private File trustStorePemFile;
   private File trustStorePkcs12File;
+  private File trustStoreBcfksFile;
 
   private final KeyPair keyStoreKeyPair;
   private final X509Certificate keyStoreCertificate;
@@ -71,6 +71,7 @@ public final class X509TestContext {
   private File keyStoreJksFile;
   private File keyStorePemFile;
   private File keyStorePkcs12File;
+  private File keyStoreBcfksFile;
 
   /**
    * Constructor is intentionally private, use the Builder class instead.
@@ -186,6 +187,8 @@ public final class X509TestContext {
         return getTrustStorePemFile();
       case PKCS12:
         return getTrustStorePkcs12File();
+      case BCFKS:
+        return getTrustStoreBcfksFile();
       default:
         throw new IllegalArgumentException("Invalid trust store type: " + storeFileType
           + ", must be one of: " + Arrays.toString(KeyStoreFileType.values()));
@@ -243,6 +246,25 @@ public final class X509TestContext {
     return trustStorePkcs12File;
   }
 
+  private File getTrustStoreBcfksFile() throws IOException {
+    if (trustStoreBcfksFile == null) {
+      File trustStoreBcfksFile = File.createTempFile(TRUST_STORE_PREFIX,
+        KeyStoreFileType.BCFKS.getDefaultFileExtension(), tempDir);
+      trustStoreBcfksFile.deleteOnExit();
+      try (
+        final FileOutputStream trustStoreOutputStream = new FileOutputStream(trustStoreBcfksFile)) {
+        byte[] bytes =
+          X509TestHelpers.certToBCFKSTrustStoreBytes(trustStoreCertificate, trustStorePassword);
+        trustStoreOutputStream.write(bytes);
+        trustStoreOutputStream.flush();
+      } catch (GeneralSecurityException e) {
+        throw new IOException(e);
+      }
+      this.trustStoreBcfksFile = trustStoreBcfksFile;
+    }
+    return trustStoreBcfksFile;
+  }
+
   public X509Certificate getKeyStoreCertificate() {
     return keyStoreCertificate;
   }
@@ -275,6 +297,8 @@ public final class X509TestContext {
         return getKeyStorePemFile();
       case PKCS12:
         return getKeyStorePkcs12File();
+      case BCFKS:
+        return getKeyStoreBcfksFile();
       default:
         throw new IllegalArgumentException("Invalid key store type: " + storeFileType
           + ", must be one of: " + Arrays.toString(KeyStoreFileType.values()));
@@ -333,6 +357,24 @@ public final class X509TestContext {
       this.keyStorePkcs12File = keyStorePkcs12File;
     }
     return keyStorePkcs12File;
+  }
+
+  private File getKeyStoreBcfksFile() throws IOException {
+    if (keyStoreBcfksFile == null) {
+      File keyStoreBcfksFile = File.createTempFile(KEY_STORE_PREFIX,
+        KeyStoreFileType.BCFKS.getDefaultFileExtension(), tempDir);
+      keyStoreBcfksFile.deleteOnExit();
+      try (final FileOutputStream keyStoreOutputStream = new FileOutputStream(keyStoreBcfksFile)) {
+        byte[] bytes = X509TestHelpers.certAndPrivateKeyToBCFKSBytes(keyStoreCertificate,
+          keyStoreKeyPair.getPrivate(), keyStorePassword);
+        keyStoreOutputStream.write(bytes);
+        keyStoreOutputStream.flush();
+      } catch (GeneralSecurityException e) {
+        throw new IOException(e);
+      }
+      this.keyStoreBcfksFile = keyStoreBcfksFile;
+    }
+    return keyStoreBcfksFile;
   }
 
   /**
@@ -490,14 +532,6 @@ public final class X509TestContext {
       keyStorePassword = password;
       return this;
     }
-  }
-
-  /**
-   * Returns a new default-constructed Builder.
-   * @return a new Builder.
-   */
-  public static Builder newBuilder() {
-    return newBuilder(HBaseConfiguration.create());
   }
 
   /**
