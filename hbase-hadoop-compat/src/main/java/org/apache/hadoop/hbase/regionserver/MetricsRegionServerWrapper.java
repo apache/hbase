391fd756035597b4a0feffb8ef8edb1056c1962f
David Manning <david.manning@salesforce.com>
2022-10-15 14:42:33 -0700

    HBASE-27159 add BlockCache Caching Counts

--- hbase-hadoop-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsRegionServerWrapper.java
+++ hbase-hadoop-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsRegionServerWrapper.java
@@ -300,6 +300,11 @@ public interface MetricsRegionServerWrapper {
   long getBlockCachePrimaryHitCount();
 
   /**
+   * Get the count of hits to the block cache, for cacheable requests only.
+   */
+  long getBlockCacheHitCachingCount();
+
+  /**
    * Get the count of misses to the block cache.
    */
   long getBlockCacheMissCount();
@@ -308,6 +313,11 @@ public interface MetricsRegionServerWrapper {
    * Get the count of misses to primary replica in the block cache.
    */
   long getBlockCachePrimaryMissCount();
+
+  /**
+   * Get the count of misses to the block cache, for cacheable requests only.
+   */
+  long getBlockCacheMissCachingCount();
 
   /**
    * Get the number of items evicted from the block cache.
