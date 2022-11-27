f0c857a784687140c688a90c775ec5ff982710b4
Peter Somogyi <psomogyi@apache.org>
2022-11-27 19:14:44 +0100

    HBASE-26320 Implement a separate thread pool for the LogCleaner (#4895)
    This avoids starvation when the archive directory is large and takes a long time
    to iterate through.
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Anoop Sam John <anoopsamjohn@apache.org>
    Signed-off-by: Pankaj <pankajkumar@apache.org>
    Co-authored-by: Xiaolin Ha <haxiaolin@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/cleaner/TestHFileCleaner.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/cleaner/TestHFileCleaner.java
@@ -75,7 +75,7 @@ public class TestHFileCleaner {
   public static void setupCluster() throws Exception {
     // have to use a minidfs cluster because the localfs doesn't modify file times correctly
     UTIL.startMiniDFSCluster(1);
-    POOL = new DirScanPool(UTIL.getConfiguration());
+    POOL = DirScanPool.getHFileCleanerScanPool(UTIL.getConfiguration());
   }
 
   @AfterClass
