77e44f5300682d6708b65c203e35086060abfcb2
terence.yoo <terence.yoo@kakaocorp.com>
2023-01-16 19:01:35 +0900

    fix a violation

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -7756,8 +7756,8 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
     return r.openHRegion(null);
   }
 
-  public static HRegion warmupHRegion(final RegionInfo info, final TableDescriptor htd, final WAL wal,
-    final Configuration conf, final RegionServerServices rsServices,
+  public static HRegion warmupHRegion(final RegionInfo info, final TableDescriptor htd,
+    final WAL wal, final Configuration conf, final RegionServerServices rsServices,
     final CancelableProgressable reporter) throws IOException {
 
     Objects.requireNonNull(info, "RegionInfo cannot be null");
