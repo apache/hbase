dc77cbc4b02c38679e387f5a746c7b52d4af5c02
Jain, Nihal <nihaljain.cs@gmail.com>
2023-02-09 22:23:01 +0530

    HBASE-27626 Suppress noisy logging in client.ConnectionImplementation

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionImplementation.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionImplementation.java
@@ -426,7 +426,7 @@ public class ConnectionImplementation implements ClusterConnection, Closeable {
         return false;
       }
       try {
-        LOG.info("Getting master state using rpc call");
+        LOG.trace("Getting master state using rpc call");
         return this.masterServiceState.isMasterRunning();
       } catch (UndeclaredThrowableException e) {
         // It's somehow messy, but we can receive exceptions such as
@@ -2030,7 +2030,7 @@ public class ConnectionImplementation implements ClusterConnection, Closeable {
   }
 
   private boolean isKeepAliveMasterConnectedAndRunning() {
-    LOG.info("Getting master connection state from TTL Cache");
+    LOG.trace("Getting master connection state from TTL Cache");
     return masterStateSupplier.get();
   }
 
