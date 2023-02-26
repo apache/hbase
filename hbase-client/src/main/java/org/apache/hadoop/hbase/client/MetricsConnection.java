f20efafbc1bbf29dbfa0163a989933460183a8ff
Bryan Beaudreault <bbeaudreault@apache.org>
2023-02-26 16:39:48 -0500

    HBASE-27650 Merging empty regions corrupts meta cache (#5037)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/MetricsConnection.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/MetricsConnection.java
@@ -557,6 +557,10 @@ public final class MetricsConnection implements StatisticTrackable {
     metaCacheMisses.inc();
   }
 
+  public long getMetaCacheMisses() {
+    return metaCacheMisses.getCount();
+  }
+
   /** Increment the number of meta cache drops requested for entire RegionServer. */
   public void incrMetaCacheNumClearServer() {
     metaCacheNumClearServer.inc();
