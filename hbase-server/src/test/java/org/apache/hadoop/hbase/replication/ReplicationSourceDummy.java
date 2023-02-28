3fab7b3f29e5da098b9ab7a21c079713feab715f
Yuta Imazu <airman515@gmail.com>
2023-02-28 14:13:49 +0900

    HBASE-24781 Clean up peer metrics when disabling peer (#4997)
    Co-authored-by: Yuta Imazu <yuta.imazu@linecorp.com>
    Signed-off-by: Duo Zhang <zhangduo@apache.or
    (cherry picked from commit ef6a1130d0f1a186b1d31f11be26ec95e3588ae2)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/replication/ReplicationSourceDummy.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/replication/ReplicationSourceDummy.java
@@ -92,6 +92,8 @@ public class ReplicationSourceDummy implements ReplicationSourceInterface {
   public void terminate(String reason, Exception e, boolean clearMetrics) {
     if (clearMetrics) {
       this.metrics.clear();
+    } else {
+      this.metrics.terminate();
     }
   }
 
