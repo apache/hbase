a6dad700dbdf8a3afbe9ff6e7e372f73fa833441
Kota-SH <shanmukhaharipriya@gmail.com>
2023-03-27 04:49:55 -0500

    HBASE-27750: Update the list of prefetched Hfiles upon block eviction (#5140)
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
@@ -379,6 +379,7 @@ public class BucketCache implements BlockCache, HeapSize {
   void startBucketCachePersisterThread() {
     BucketCachePersister cachePersister =
       new BucketCachePersister(this, bucketcachePersistInterval);
+    cachePersister.setDaemon(true);
     cachePersister.start();
   }
 
@@ -610,6 +611,9 @@ public class BucketCache implements BlockCache, HeapSize {
       cacheStats.evicted(bucketEntry.getCachedTime(), cacheKey.isPrimary());
     }
     if (ioEngine.isPersistent()) {
+      if (prefetchedFileListPath != null) {
+        PrefetchExecutor.removePrefetchedFileWhileEvict(cacheKey.getHfileName());
+      }
       setCacheInconsistent(true);
     }
   }
