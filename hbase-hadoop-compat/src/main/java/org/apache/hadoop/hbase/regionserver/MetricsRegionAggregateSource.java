6305c7a4f2bd87d261c4179f8afc1ac619bc1555
tianhang <tangtianhang099@gmail.com>
2023-04-08 11:02:39 +0800

    HBASE-27713 Remove numRegions in Region Metrics (#5107)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    (cherry picked from commit 251e4984776e2ee13ae4df1de4c000f35a58ad59)

--- hbase-hadoop-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsRegionAggregateSource.java
+++ hbase-hadoop-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsRegionAggregateSource.java
@@ -47,8 +47,6 @@ public interface MetricsRegionAggregateSource extends BaseSource {
    */
   String METRICS_JMX_CONTEXT = "RegionServer,sub=" + METRICS_NAME;
 
-  String NUMBER_OF_REGIONS_DESC = "Number of regions in the metrics system";
-
   /**
    * Register a MetricsRegionSource as being open.
    * @param source the source for the region being opened.
