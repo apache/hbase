fec0eda92d7e4f040f455a31711c98c704f74f67
Ray Mattingly <rmattingly@hubspot.com>
2023-04-19 19:00:27 -0400

    add TestRpcThrottlingException

---
+++ hbase-client/src/test/java/org/apache/hadoop/hbase/quotas/TestRpcThrottlingException.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.quotas;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.hadoop.hbase.HBaseClassTestRule;
+import org.apache.hadoop.hbase.testclassification.SmallTests;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category({ SmallTests.class })
+public class TestRpcThrottlingException {
+
+  @ClassRule
+  public static final HBaseClassTestRule CLASS_RULE =
+    HBaseClassTestRule.forClass(TestRpcThrottlingException.class);
+
+  @Test
+  public void itHandles1sWaitIntervalMessage() {
+    try {
+      RpcThrottlingException.throwNumReadRequestsExceeded(1000);
+    } catch (RpcThrottlingException e) {
+      assertTrue(e.getMessage().contains("wait 1sec"));
+    }
+  }
+
+  @Test
+  public void itHandles50msWaitIntervalMessage() {
+    try {
+      RpcThrottlingException.throwNumReadRequestsExceeded(50);
+    } catch (RpcThrottlingException e) {
+      assertTrue(e.getMessage().contains("wait 50ms"));
+    }
+  }
+
+  @Test
+  public void itHandles1min1sWaitIntervalMessage() {
+    try {
+      RpcThrottlingException.throwNumReadRequestsExceeded(61_000);
+    } catch (RpcThrottlingException e) {
+      assertTrue(e.getMessage().contains("wait 1mins, 1sec"));
+    }
+  }
+
+}
