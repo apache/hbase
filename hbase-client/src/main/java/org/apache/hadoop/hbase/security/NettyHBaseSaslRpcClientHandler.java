df1996443615c07604e8eb8dae1833e47398f608
chenglei <chenglei@apache.org>
2023-06-12 11:23:53 +0800

    HBASE-27923 NettyRpcServer may hange if it should skip initial sasl handshake (#5282)
    cherry-pick of the branch-2 commit.
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.rog>
    (cherry picked from commit b9345a5924136d6fa034f2312ceec14840da2e2c)
    Change-Id: I4ce0b2c94a2f2d67b4efe6c71d8afe4902475934

--- hbase-client/src/main/java/org/apache/hadoop/hbase/security/NettyHBaseSaslRpcClientHandler.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/security/NettyHBaseSaslRpcClientHandler.java
@@ -87,14 +87,18 @@ public class NettyHBaseSaslRpcClientHandler extends SimpleChannelInboundHandler<
       return;
     }
 
-    ChannelPipeline p = ctx.pipeline();
-    saslRpcClient.setupSaslHandler(p, HANDLER_NAME);
-    p.remove(SaslChallengeDecoder.class);
-    p.remove(this);
+    saslRpcClient.setupSaslHandler(ctx.pipeline(), HANDLER_NAME);
+    removeHandlers(ctx);
 
     setCryptoAESOption();
 
     saslPromise.setSuccess(true);
+  }
+
+  private void removeHandlers(ChannelHandlerContext ctx) {
+    ChannelPipeline p = ctx.pipeline();
+    p.remove(SaslChallengeDecoder.class);
+    p.remove(this);
   }
 
   private void setCryptoAESOption() {
@@ -142,6 +146,9 @@ public class NettyHBaseSaslRpcClientHandler extends SimpleChannelInboundHandler<
       } else {
         saslPromise.tryFailure(new FallbackDisallowedException());
       }
+      // When we switch to simple auth, we should also remove SaslChallengeDecoder and
+      // NettyHBaseSaslRpcClientHandler.
+      removeHandlers(ctx);
       return;
     }
     LOG.trace("Reading input token size={} for processing by initSASLContext", len);
