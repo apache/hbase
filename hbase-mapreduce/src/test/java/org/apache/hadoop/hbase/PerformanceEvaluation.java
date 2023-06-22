91fd5e72ad89a7aab03a7d82c46ead531df4cf55
Prathyusha Garre <pgarre@pgarre-ltmqft8.internal.salesforce.com>
2023-06-22 21:53:21 +0530

    HBASE-27938 - load test classes by name and support command specific properties

--- hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
+++ hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/PerformanceEvaluation.java
@@ -36,6 +36,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Properties;
 import java.util.Queue;
 import java.util.Random;
 import java.util.TreeMap;
@@ -359,13 +360,11 @@ public class PerformanceEvaluation extends Configured implements Tool {
     // recreate the table when user has requested presplit or when existing
     // {RegionSplitPolicy,replica count} does not match requested, or when the
     // number of column families does not match requested.
-    if (
-      (exists && opts.presplitRegions != DEFAULT_OPTS.presplitRegions)
-        || (!isReadCmd && desc != null
-          && !StringUtils.equals(desc.getRegionSplitPolicyClassName(), opts.splitPolicy))
+    if ((exists && opts.presplitRegions != DEFAULT_OPTS.presplitRegions
+        && opts.presplitRegions != admin.getRegions(tableName).size())
+        || (!isReadCmd && desc != null && !StringUtils.equals(desc.getRegionSplitPolicyClassName(), opts.splitPolicy))
         || (!isReadCmd && desc != null && desc.getRegionReplication() != opts.replicas)
-        || (desc != null && desc.getColumnFamilyCount() != opts.families)
-    ) {
+        || (desc != null && desc.getColumnFamilyCount() != opts.families)) {
       needsDelete = true;
       // wait, why did it delete my table?!?
       LOG.debug(MoreObjects.toStringHelper("needsDelete").add("needsDelete", needsDelete)
@@ -721,6 +720,7 @@ public class PerformanceEvaluation extends Configured implements Tool {
     boolean cacheBlocks = true;
     Scan.ReadType scanReadType = Scan.ReadType.DEFAULT;
     long bufferSize = 2l * 1024l * 1024l;
+    Properties commandProperties;
 
     public TestOptions() {
     }
@@ -777,8 +777,13 @@ public class PerformanceEvaluation extends Configured implements Tool {
       this.cacheBlocks = that.cacheBlocks;
       this.scanReadType = that.scanReadType;
       this.bufferSize = that.bufferSize;
+      this.commandProperties = that.commandProperties;
     }
-
+    
+    public Properties getCommandProperties() {
+      return commandProperties;
+    }
+    
     public int getCaching() {
       return this.caching;
     }
@@ -1142,10 +1147,10 @@ public class PerformanceEvaluation extends Configured implements Tool {
     protected final Configuration conf;
     protected final TestOptions opts;
 
-    private final Status status;
+    protected final Status status;
 
     private String testName;
-    private Histogram latencyHistogram;
+    protected Histogram latencyHistogram;
     private Histogram replicaLatencyHistogram;
     private Histogram valueSizeHistogram;
     private Histogram rpcCallsHistogram;
@@ -2345,7 +2350,7 @@ public class PerformanceEvaluation extends Configured implements Tool {
     }
 
     @Override
-    boolean testRow(final int i, final long startTime) throws IOException {
+    protected boolean testRow(final int i, final long startTime) throws IOException {
       byte[] row = generateRow(i);
       Put put = new Put(row);
       for (int family = 0; family < opts.families; family++) {
@@ -3013,6 +3018,19 @@ public class PerformanceEvaluation extends Configured implements Tool {
         continue;
       }
 
+      final String commandPropertiesFile = "--commandPropertiesFile=";
+      if (cmd.startsWith(commandPropertiesFile)) {
+        String fileName = String.valueOf(cmd.substring(commandPropertiesFile.length()));
+        Properties properties = new Properties();
+        try {
+          properties.load(PerformanceEvaluation.class.getClassLoader().getResourceAsStream(fileName));
+          opts.commandProperties = properties;
+        } catch (IOException e) {
+          LOG.error("Failed to load metricIds from properties file", e);
+        }
+        continue;
+      }
+      
       validateParsedOpts(opts);
 
       if (isCommandClass(cmd)) {
@@ -3126,9 +3144,21 @@ public class PerformanceEvaluation extends Configured implements Tool {
   }
 
   private static boolean isCommandClass(String cmd) {
-    return COMMANDS.containsKey(cmd);
+    return !COMMANDS.containsKey(cmd) ? isCustomTestClass(cmd) : true;
   }
 
+  private static boolean isCustomTestClass(String cmd) {
+    Class<? extends Test> cmdClass;
+    try {
+      cmdClass = (Class<? extends Test>) PerformanceEvaluation.class.getClassLoader().loadClass(cmd);
+      addCommandDescriptor(cmdClass, cmd, "custom command");
+      return true;
+    } catch (Throwable th) {
+      LOG.info("No class found for command: " + cmd, th);
+      return false;
+    }
+  }
+  
   private static Class<? extends TestBase> determineCommandClass(String cmd) {
     CmdDescriptor descriptor = COMMANDS.get(cmd);
     return descriptor != null ? descriptor.getCmdClass() : null;
