37d62aabbd2ae2eb8cf80b024fc4f4a9dbaec2f1
Andrew Purtell <apurtell@apache.org>
2023-06-28 12:07:55 -0700

    HBASE-27951 Use ADMIN_QOS in MasterRpcServices for regionserver operational dependencies (#5309)
    It seems not correct to have the MasterRpcServices methods for direct
    regionserver<->master communication (regionServerStartup, regionServerReport,
    and reportFatalRSError) contending with normal priority requests. They
    should be made ADMIN_QOS priority to avoid potential operational deadlocks.
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Viraj Jasani <vjasani@apache.org>
    Reviewed-by: Aman Poonia <aman.poonia.29@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterRpcServices.java
@@ -539,6 +539,7 @@ public class MasterRpcServices extends RSRpcServices implements MasterService.Bl
   }
 
   @Override
+  @QosPriority(priority = HConstants.ADMIN_QOS)
   public RegionServerReportResponse regionServerReport(RpcController controller,
     RegionServerReportRequest request) throws ServiceException {
     try {
@@ -570,6 +571,7 @@ public class MasterRpcServices extends RSRpcServices implements MasterService.Bl
   }
 
   @Override
+  @QosPriority(priority = HConstants.ADMIN_QOS)
   public RegionServerStartupResponse regionServerStartup(RpcController controller,
     RegionServerStartupRequest request) throws ServiceException {
     // Register with server manager
@@ -601,6 +603,7 @@ public class MasterRpcServices extends RSRpcServices implements MasterService.Bl
   }
 
   @Override
+  @QosPriority(priority = HConstants.ADMIN_QOS)
   public ReportRSFatalErrorResponse reportRSFatalError(RpcController controller,
     ReportRSFatalErrorRequest request) throws ServiceException {
     String errorText = request.getErrorMessage();
