546d280734b3a25ac60bded73dafebe06b77e390
Sergey Soldatov <ssoldatov@cloudera.com>
2023-09-05 16:59:28 -0700

    HBASE-28055 Performance improvement for scan over several stores. (#5379)
    Signed-off-by: Ankit Singhal <ankit@apache.org>
    Signed-off-by: Tak Lon (Stephen) Wu <taklwu@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreScanner.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreScanner.java
@@ -935,7 +935,9 @@ public class StoreScanner extends NonReversedNonLazyKeyValueScanner
     // We need this check because it may happen that the new scanner that we get
     // during heap.next() is requiring reseek due of fake KV previously generated for
     // ROWCOL bloom filter optimization. See HBASE-19863 for more details
-    if (useRowColBloom && nextCell != null && matcher.compareKeyForNextColumn(nextCell, cell) < 0) {
+    if (
+      useRowColBloom && nextCell != null && cell.getTimestamp() == PrivateConstants.OLDEST_TIMESTAMP
+    ) {
       return false;
     }
     return true;
