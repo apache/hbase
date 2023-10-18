03467a7018c2c22414d1998cf8247911f553e47e
Duo Zhang <zhangduo@apache.org>
2023-10-18 19:00:57 +0800

    HBASE-28155 RecoveredReplicationSource quit when there are still unfinished groups

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/RecoveredReplicationSource.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/RecoveredReplicationSource.java
@@ -27,6 +27,22 @@ import org.apache.yetus.audience.InterfaceAudience;
 public class RecoveredReplicationSource extends ReplicationSource {
 
   @Override
+  protected void startShippers() {
+    for (String walGroupId : logQueue.getQueues().keySet()) {
+      workerThreads.put(walGroupId, createNewShipper(walGroupId));
+    }
+    // start shippers after initializing the workerThreads, as in the below postFinish logic, if
+    // workerThreads is empty, we will mark the RecoveredReplicationSource as finished. So if we
+    // start the worker on the fly, it is possible that a shipper has already finished its work and
+    // called postFinish, and find out the workerThreads is empty and then mark the
+    // RecoveredReplicationSource as finish, while the next shipper has not been added to
+    // workerThreads yet. See HBASE-28155 for more details.
+    for (ReplicationSourceShipper shipper : workerThreads.values()) {
+      startShipper(shipper);
+    }
+  }
+
+  @Override
   protected RecoveredReplicationSourceShipper createNewShipper(String walGroupId,
     ReplicationSourceWALReader walReader) {
     return new RecoveredReplicationSourceShipper(conf, walGroupId, this, walReader, queueStorage,
