1e58021ab1b3627183f158d149eaf8a8048c43d1
Nihal Jain <nihaljain@apache.org>
2023-10-25 13:47:27 +0530

    HBASE-24179 Backport fix for "Netty SASL implementation does not wait for challenge response" to branch-2.x (#5472) - Backport HBASE-23881 "Netty SASL implementation does not wait for challenge response" - Backport HBASE-24263 "TestDelegationToken is broken" - Fix assertion to check for InvalidToken.class.getName() to ensure bug is fixed

--- hbase-examples/src/main/java/org/apache/hadoop/hbase/security/provider/example/ShadeSaslServerAuthenticationProvider.java
+++ hbase-examples/src/main/java/org/apache/hadoop/hbase/security/provider/example/ShadeSaslServerAuthenticationProvider.java
@@ -138,7 +138,6 @@ public class ShadeSaslServerAuthenticationProvider extends ShadeSaslAuthenticati
 
     @Override
     public void handle(Callback[] callbacks) throws InvalidToken, UnsupportedCallbackException {
-      LOG.info("SaslServerCallbackHandler called", new Exception());
       NameCallback nc = null;
       PasswordCallback pc = null;
       AuthorizeCallback ac = null;
