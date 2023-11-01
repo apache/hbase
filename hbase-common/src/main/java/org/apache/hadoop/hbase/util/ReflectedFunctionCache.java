b3a02c3f41f857edd12bd980857d61afa7d4f2c0
Bryan Beaudreault <bbeaudreault@hubspot.com>
2023-11-01 12:42:08 -0400

    checkstyle

--- hbase-common/src/main/java/org/apache/hadoop/hbase/util/ReflectedFunctionCache.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/util/ReflectedFunctionCache.java
@@ -42,7 +42,7 @@ import org.apache.hbase.thirdparty.com.google.common.reflect.ClassPath;
  * @param <R> the return type for the resolved functions
  */
 @InterfaceAudience.Private
-public class ReflectedFunctionCache<I, R> {
+final public class ReflectedFunctionCache<I, R> {
 
   private static final Logger LOG = LoggerFactory.getLogger(ReflectedFunctionCache.class);
 
