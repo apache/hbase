d7052a573abadb4bd2bf351f8c7f9b4b9ebb3fe5
Duo Zhang <zhangduo@apache.org>
2024-01-10 11:13:09 +0800

    HBASE-28297 IntegrationTestImportTsv fails with ArrayIndexOfOutBounds (#5612)
    Signed-off-by: Nihal Jain <nihaljain@apache.org>
    (cherry picked from commit a09305d5854fc98300426271fad3b53a69d2ae71)

--- hbase-it/src/test/java/org/apache/hadoop/hbase/mapreduce/IntegrationTestImportTsv.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/mapreduce/IntegrationTestImportTsv.java
@@ -60,6 +60,9 @@ import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.hbase.thirdparty.com.google.common.base.Splitter;
+import org.apache.hbase.thirdparty.com.google.common.base.Strings;
+
 /**
  * Validate ImportTsv + LoadIncrementalHFiles on a distributed cluster.
  */
@@ -83,7 +86,10 @@ public class IntegrationTestImportTsv extends Configured implements Tool {
       private static final long serialVersionUID = 1L;
       {
         byte[] family = Bytes.toBytes("d");
-        for (String line : simple_tsv.split("\n")) {
+        for (String line : Splitter.on('\n').split(simple_tsv)) {
+          if (Strings.isNullOrEmpty(line)) {
+            continue;
+          }
           String[] row = line.split("\t");
           byte[] key = Bytes.toBytes(row[0]);
           long ts = Long.parseLong(row[1]);
