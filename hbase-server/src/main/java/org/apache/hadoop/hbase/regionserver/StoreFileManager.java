82884434d3999a8ef927f043cab30857c005333e
Kadir Ozdemir <kozdemir@salesforce.com>
2024-01-13 14:17:21 +0300

    Java doc comment edit

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFileManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFileManager.java
@@ -120,9 +120,9 @@ public interface StoreFileManager {
 
   /**
    * Gets the store files to scan for a Scan or Get request.
-   * @param startRow Start row of the request.
-   * @param stopRow  Stop row of the request.
-   * @param onlyLatestVersion  Scan only latest live version cells.
+   * @param startRow          Start row of the request.
+   * @param stopRow           Stop row of the request.
+   * @param onlyLatestVersion Scan only latest live version cells.
    * @return The list of files that are to be read for this request.
    */
   Collection<HStoreFile> getFilesForScan(byte[] startRow, boolean includeStartRow, byte[] stopRow,
