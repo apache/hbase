69fd2c96d7c8a27791b2b22cc44e38a9a9ed9273
Jing Yu <yu.j@salesforce.com>
2024-03-10 00:43:46 -0800

    HBASE-28424 Set correct Result to RegionActionResult for successful Put/Delete mutations (#5760)
    Signed-off-by: Viraj Jasani <vjasani@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
@@ -1069,8 +1069,10 @@ public class RSRpcServices implements HBaseRPCErrorHandler, AdminService.Blockin
             break;
 
           case SUCCESS:
-            builder.addResultOrException(
-              getResultOrException(ClientProtos.Result.getDefaultInstance(), index));
+            ClientProtos.Result result = codes[i].getResult() == null
+              ? ClientProtos.Result.getDefaultInstance()
+              : ProtobufUtil.toResult(codes[i].getResult());
+            builder.addResultOrException(getResultOrException(result, index));
             break;
 
           case STORE_TOO_BUSY:
