b8c797660f2e7a42aeb045271cf85ac1da6e279d
Monani Mihir <monani.mihir@gmail.com>
2024-04-08 14:39:13 -0700

    HBASE-26192 Master UI hbck should provide a JSON formatted output option (#5780)
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

---
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/HbckRegionHoles.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase;
+
+import org.apache.yetus.audience.InterfaceAudience;
+
+/**
+ * POJO to present Region Holes from Catalog Janitor Inconsistencies Report via REST API. These
+ * inconsistencies are shown on hbck.jsp page on Active HMaster UI as part of Catalog Janitor
+ * inconsistencies.
+ */
+@InterfaceAudience.Public
+public class HbckRegionHoles {
+  private final HbckRegionDetails region1Info;
+  private final HbckRegionDetails region2Info;
+
+  public HbckRegionHoles(HbckRegionDetails region1Info, HbckRegionDetails region2Info) {
+    this.region1Info = region1Info;
+    this.region2Info = region2Info;
+  }
+
+  public HbckRegionDetails getRegion1Info() {
+    return region1Info;
+  }
+
+  public HbckRegionDetails getRegion2Info() {
+    return region2Info;
+  }
+}
