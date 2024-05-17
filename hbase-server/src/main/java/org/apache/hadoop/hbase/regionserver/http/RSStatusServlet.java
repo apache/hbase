733abf22251d69d1ebcefc814b2cce5dc4cc1ab7
Kadir Ozdemir <kozdemir@salesforce.com>
2024-05-17 10:14:40 -0700

    Changes for spotless

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/http/RSStatusServlet.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/http/RSStatusServlet.java
@@ -23,8 +23,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
-import org.apache.hadoop.hbase.tmpl.regionserver
-  .RSStatusTmpl;
+import org.apache.hadoop.hbase.tmpl.regionserver.RSStatusTmpl;
 import org.apache.yetus.audience.InterfaceAudience;
 
 @InterfaceAudience.Private
