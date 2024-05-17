b8f609908b9ebaf055b89f2d6901fe211c3655df
Nick Dimiduk <ndimiduk@apache.org>
2024-05-17 18:31:19 +0200

    HBASE-28568 Incremental backup set does not correctly shrink (addendum) (#5917)
    Import the correct shaded Guava and run spotless:apply.
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/impl/BackupAdminImpl.java
+++ hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/impl/BackupAdminImpl.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
