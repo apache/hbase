e23da545c8c493b4906c60b3e7da873df4640753
Istvan Toth <stoty@apache.org>
2024-05-28 10:21:24 +0200

    HBASE-28613 Use streaming when marshalling protobuf REST output (#5943)
    Signed-off-by: Ankit Singhal <ankit@apache.org>
    (cherry picked from commit d1d8b4d64591de6cee302d648be257087c0beb48)

--- hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/model/RowModel.java
+++ hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/model/RowModel.java
@@ -36,6 +36,8 @@ import org.apache.hadoop.hbase.rest.ProtobufMessageHandler;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.yetus.audience.InterfaceAudience;
 
+import org.apache.hbase.thirdparty.com.google.protobuf.Message;
+
 /**
  * Representation of a row. A row is a related set of cells, grouped by common row key. RowModels do
  * not appear in results by themselves. They are always encapsulated within CellSetModels.
@@ -179,7 +181,7 @@ public class RowModel implements ProtobufMessageHandler, Serializable {
   }
 
   @Override
-  public byte[] createProtobufOutput() {
+  public Message messageFromObject() {
     // there is no standalone row protobuf message
     throw new UnsupportedOperationException("no protobuf equivalent to RowModel");
   }
