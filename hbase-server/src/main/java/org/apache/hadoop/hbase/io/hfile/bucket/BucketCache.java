317d3aad115aae2e4a3c0261a932419c4a6a1bb9
Wellington Ramos Chevreuil <wchevreuil@apache.org>
2024-07-22 15:39:00 +0100

    HBASE-28724 BucketCache.notifyFileCachingCompleted may throw IllegalMonitorStateException (#6074) (#6081)
    Signed-off-by: Peter Somogyi <psomogyi@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
@@ -2092,6 +2092,7 @@ public class BucketCache implements BlockCache, HeapSize {
           for (ReentrantReadWriteLock lock : locks) {
             lock.readLock().unlock();
           }
+          locks.clear();
           LOG.debug("There are still blocks pending caching for file {}. Will sleep 100ms "
             + "and try the verification again.", fileName.getName());
           Thread.sleep(100);
