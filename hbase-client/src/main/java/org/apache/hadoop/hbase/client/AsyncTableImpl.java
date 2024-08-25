334dcdeaab6e96b79fdaabb999b470f88690cd32
Charles Connell <cconnell@hubspot.com>
2024-08-25 13:59:51 -0400

    Order onComplete() call via a Phaser

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/AsyncTableImpl.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/AsyncTableImpl.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import org.apache.hadoop.conf.Configuration;
@@ -296,9 +297,45 @@ class AsyncTableImpl implements AsyncTable<ScanResultConsumer> {
   public <S, R> CoprocessorServiceBuilder<S, R> coprocessorService(
     Function<RpcChannel, S> stubMaker, ServiceCaller<S, R> callable,
     CoprocessorCallback<R> callback) {
+    final Context context = Context.current();
+    CoprocessorCallback<R> wrappedCallback = new CoprocessorCallback<R>() {
+
+      private final Phaser regionCompletesInProgress = new Phaser(1);
+
+      @Override
+      public void onRegionComplete(RegionInfo region, R resp) {
+        regionCompletesInProgress.register();
+        pool.execute(context.wrap(() -> {
+          try {
+            callback.onRegionComplete(region, resp);
+          } finally {
+            regionCompletesInProgress.arriveAndDeregister();
+          }
+        }));
+      }
+
+      @Override
+      public void onRegionError(RegionInfo region, Throwable error) {
+        pool.execute(context.wrap(() -> callback.onRegionError(region, error)));
+      }
+
+      @Override
+      public void onComplete() {
+        pool.execute(context.wrap(() -> {
+          // Guarantee that onComplete() is called after all onRegionComplete()'s are called
+          regionCompletesInProgress.arriveAndAwaitAdvance();
+          callback.onComplete();
+        }));
+      }
+
+      @Override
+      public void onError(Throwable error) {
+        pool.execute(context.wrap(() -> callback.onError(error)));
+      }
+    };
     CoprocessorServiceBuilder<S, R> builder =
-      rawTable.coprocessorService(stubMaker, callable, callback);
-    return new CoprocessorServiceBuilder<>() {
+      rawTable.coprocessorService(stubMaker, callable, wrappedCallback);
+    return new CoprocessorServiceBuilder<S, R>() {
 
       @Override
       public CoprocessorServiceBuilder<S, R> fromRow(byte[] startKey, boolean inclusive) {
