f94051627d6ca419c35f94892248a0183c746f80
Evie Boland <eboland@hubspot.com>
2024-09-30 16:53:53 -0400

    Run spotless:apply

--- hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/row/stats/SizeBucketTracker.java
+++ hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/row/stats/SizeBucketTracker.java
@@ -1,7 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.coprocessor.example.row.stats;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.yetus.audience.InterfaceAudience;
+
 import org.apache.hbase.thirdparty.com.google.gson.JsonObject;
 
 @InterfaceAudience.Private
@@ -51,29 +70,19 @@ public class SizeBucketTracker {
 
   private SizeBucket search(long val) {
     /*
-    Performance tested a few different search implementations
-    1. Linear
-    2. Binary
-    3. Search - bucket search order changes over time as more information about the table is gained
-
-    Linear and Binary implementations had roughly similar throughput
-    - Linear performs slightly better when the sizes are small
-    - Binary performs slightly better when the sizes are irregularly distributed or skewed high
-
-    Smart implementation had the lowest throughput
-    - Reassessing the bucket search order is an expensive operation
-    - Tuning the number bucket search order reassessments is tricky, since it depended on the
-       - Write patterns to a table -- including how hot/cold the compacting data is
-       - Number of values per row
-       - Number of rows per region
-    - Small number of SizeBucket values means that there is NOT a ton of value ot be gained from reassessing the bucket search order
-
-    Landed on Linear implementation because
-    - Looping through a small array is quick, especially if most of the values end up exiting out early
-    - Many tables at HubSpot have small values
-    - Implementation is clear and requires no tuning
-
-    PR with more testing context: https://git.hubteam.com/HubSpot/HubSpotCoprocessors/pull/243
+     * Performance tested a few different search implementations 1. Linear 2. Binary 3. Search -
+     * bucket search order changes over time as more information about the table is gained Linear
+     * and Binary implementations had roughly similar throughput - Linear performs slightly better
+     * when the sizes are small - Binary performs slightly better when the sizes are irregularly
+     * distributed or skewed high Smart implementation had the lowest throughput - Reassessing the
+     * bucket search order is an expensive operation - Tuning the number bucket search order
+     * reassessments is tricky, since it depended on the - Write patterns to a table -- including
+     * how hot/cold the compacting data is - Number of values per row - Number of rows per region -
+     * Small number of SizeBucket values means that there is NOT a ton of value ot be gained from
+     * reassessing the bucket search order Landed on Linear implementation because - Looping through
+     * a small array is quick, especially if most of the values end up exiting out early - Many
+     * tables at HubSpot have small values - Implementation is clear and requires no tuning PR with
+     * more testing context: https://git.hubteam.com/HubSpot/HubSpotCoprocessors/pull/243
      */
     for (SizeBucket sizeBucket : SIZE_BUCKET_ARRAY) {
       if (val < sizeBucket.maxBytes()) {
