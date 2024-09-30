2e7181ebec84ee99346cedcdba6e804ab52feac5
Evie Boland <eboland@hubspot.com>
2024-09-30 17:20:22 -0400

    Remove HubSpot specific comment

--- hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/row/stats/SizeBucketTracker.java
+++ hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/row/stats/SizeBucketTracker.java
@@ -20,7 +20,6 @@ package org.apache.hadoop.hbase.coprocessor.example.row.stats;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.yetus.audience.InterfaceAudience;
-
 import org.apache.hbase.thirdparty.com.google.gson.JsonObject;
 
 @InterfaceAudience.Private
@@ -69,21 +68,6 @@ public class SizeBucketTracker {
   }
 
   private SizeBucket search(long val) {
-    /*
-     * Performance tested a few different search implementations 1. Linear 2. Binary 3. Search -
-     * bucket search order changes over time as more information about the table is gained Linear
-     * and Binary implementations had roughly similar throughput - Linear performs slightly better
-     * when the sizes are small - Binary performs slightly better when the sizes are irregularly
-     * distributed or skewed high Smart implementation had the lowest throughput - Reassessing the
-     * bucket search order is an expensive operation - Tuning the number bucket search order
-     * reassessments is tricky, since it depended on the - Write patterns to a table -- including
-     * how hot/cold the compacting data is - Number of values per row - Number of rows per region -
-     * Small number of SizeBucket values means that there is NOT a ton of value ot be gained from
-     * reassessing the bucket search order Landed on Linear implementation because - Looping through
-     * a small array is quick, especially if most of the values end up exiting out early - Many
-     * tables at HubSpot have small values - Implementation is clear and requires no tuning PR with
-     * more testing context: https://git.hubteam.com/HubSpot/HubSpotCoprocessors/pull/243
-     */
     for (SizeBucket sizeBucket : SIZE_BUCKET_ARRAY) {
       if (val < sizeBucket.maxBytes()) {
         return sizeBucket;
