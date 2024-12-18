cc0ff3e761b7a28be6bc94b7486c25ce68f48ed2
Evie Boland <eboland@hubspot.com>
2024-12-18 13:05:55 -0500

    Design suggestions

--- hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/row/stats/utils/RowStatisticsConfigurationUtil.java
+++ hbase-examples/src/main/java/org/apache/hadoop/hbase/coprocessor/example/row/stats/utils/RowStatisticsConfigurationUtil.java
@@ -33,12 +33,4 @@ public class RowStatisticsConfigurationUtil {
   public static long getLong(Configuration conf, String name, long defaultValue) {
     return conf.getLong(ROW_STATISTICS_PREFIX + name, defaultValue);
   }
-
-  private static void setInt(Configuration conf, String name, int defaultValue) {
-    conf.setInt(name, conf.getInt(ROW_STATISTICS_PREFIX + name, defaultValue));
-  }
-
-  private static void setLong(Configuration conf, String name, long defaultValue) {
-    conf.setLong(name, conf.getLong(ROW_STATISTICS_PREFIX + name, defaultValue));
-  }
 }
