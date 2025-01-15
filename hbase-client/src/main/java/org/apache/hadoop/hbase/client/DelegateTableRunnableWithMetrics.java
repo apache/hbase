7d585a2171fc8fff52d62ae841bc24ce2f4ee11c
Sanjeet Malhotra <sanjeetmalhotra@salesforce.com>
2025-01-15 10:15:33 +0530

    Add comments

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/DelegateTableRunnableWithMetrics.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/DelegateTableRunnableWithMetrics.java
@@ -1,12 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.client;
 
-import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.metrics.TableMetrics;
 import org.apache.hadoop.hbase.client.metrics.TableThreadMetricsHolder;
 import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
 
 import org.apache.yetus.audience.InterfaceAudience;
 
+/**
+ * Wraps a runnable to record metrics about execution of the runnable. The metrics are
+ * recorded within {@link TableThreadMetricsHolder}.
+ */
 @InterfaceAudience.Private
 public class DelegateTableRunnableWithMetrics implements Runnable {
 
@@ -40,8 +60,17 @@ public class DelegateTableRunnableWithMetrics implements Runnable {
     return tableThreadMetricsHolder;
   }
 
+  /**
+   * Wraps the runnable within {@link DelegateTableRunnableWithMetrics} and registers
+   * {@link TableThreadMetricsHolder}, created as part of wrapping runnable, in
+   * {@link TableMetrics}.
+   * @param runnable Runnable to wrap.
+   * @param tableMetrics Table level metrics.
+   * @return Wrapped runnable if table level metrics are enabled, otherwise the original runnable.
+   */
   public static Runnable wrapInDelegateTableRunnableWithMetrics(Runnable runnable, TableMetrics tableMetrics) {
     if (tableMetrics.equals(TableMetrics.EMPTY_TABLE_METRICS)) {
+      // Table level metrics are not enabled.
       return runnable;
     }
     DelegateTableRunnableWithMetrics runnableWrapper = new DelegateTableRunnableWithMetrics(runnable);
--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/metrics/TableMetrics.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/metrics/TableMetrics.java
@@ -1,12 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.client.metrics;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.yetus.audience.InterfaceAudience;
 
+/**
+ * Container for metrics related to a table. This class is thread-safe.
+ *
+ * One instance of {@link TableMetrics} is created for each instance of
+ * {@link org.apache.hadoop.hbase.client.Table}. If instance of
+ * {@link org.apache.hadoop.hbase.client.Table} is shared across multiple application threads
+ * then {@link TableMetrics} will be collection of metrics recorded for all the application threads.
+ */
 @InterfaceAudience.Public
 public class TableMetrics {
 
@@ -40,6 +64,19 @@ public class TableMetrics {
     threadMetricsHolders.add(threadMetricsHolder);
   }
 
+  /**
+   * Returns list of metric holders. The list is synchronized but make sure to
+   * synchronize on returned list object while iterating the list.
+   * <pre>
+   *   synchronized (list) {
+   *       Iterator i = list.iterator(); // Must be in synchronized block
+   *       while (i.hasNext())
+   *           foo(i.next());
+   *   }
+   * </pre>
+   * Failure to follow this advice may result in non-deterministic behavior.
+   * @return list of metric holders
+   */
   public List<TableThreadMetricsHolder> getThreadMetricsHolders() {
     return threadMetricsHolders;
   }
