28679484c4fea609463e3d0c963f9218d5ae5a69
Ray Mattingly <rmattingly@apache.org>
2025-01-16 08:52:40 -0500

    HBASE-29070 Balancer cost function epsilon is imprecise (#6597) (#6600)
    Signed-off-by: Nick Dimiduk <ndimiduk@apache.org>
    Co-authored-by: Ray Mattingly <rmattingly@hubspot.com>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestStochasticLoadBalancerSmallCluster.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestStochasticLoadBalancerSmallCluster.java
@@ -38,7 +38,8 @@ public class TestStochasticLoadBalancerSmallCluster extends BalancerTestBase {
     int numRegionsPerServer = 40; // all servers except one
     int replication = 1;
     int numTables = 10;
-    testWithCluster(numNodes, numRegions, numRegionsPerServer, replication, numTables, true, true);
+    testWithClusterWithIteration(numNodes, numRegions, numRegionsPerServer, replication, numTables,
+      true, true);
   }
 
   @Test
@@ -48,7 +49,8 @@ public class TestStochasticLoadBalancerSmallCluster extends BalancerTestBase {
     int numRegionsPerServer = 40; // all servers except one
     int replication = 1;
     int numTables = 10;
-    testWithCluster(numNodes, numRegions, numRegionsPerServer, replication, numTables, true, true);
+    testWithClusterWithIteration(numNodes, numRegions, numRegionsPerServer, replication, numTables,
+      true, true);
   }
 
   @Test
@@ -58,8 +60,8 @@ public class TestStochasticLoadBalancerSmallCluster extends BalancerTestBase {
     int numRegionsPerServer = 1; // all servers except one
     int replication = 1;
     int numTables = 10;
-    /* fails because of max moves */
-    testWithCluster(numNodes, numRegions, numRegionsPerServer, replication, numTables, false,
-      false);
+    // fails because of max moves
+    testWithClusterWithIteration(numNodes, numRegions, numRegionsPerServer, replication, numTables,
+      false, false);
   }
 }
