7bcf9ceec5378a822d4e9933219b7aaa51a19a8c
terence.yoo <terence.yoo@kakaocorp.com>
2025-02-21 16:01:57 +0900

    fix permission caches can be empty during ACL update

--- hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/AuthManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/AuthManager.java
@@ -78,12 +78,13 @@ public final class AuthManager {
       }
     }
 
-    void clear() {
+    void cleanUp(ListMultimap<String, ? extends Permission> newPermissions) {
       synchronized (mutex) {
         for (Map.Entry<String, Set<T>> entry : cache.entrySet()) {
-          entry.getValue().clear();
+          if (!newPermissions.containsKey(entry.getKey())) {
+            entry.getValue().clear();
+          }
         }
-        cache.clear();
       }
     }
   }
@@ -163,7 +164,6 @@ public final class AuthManager {
    * @param globalPerms new global permissions
    */
   private void updateGlobalCache(ListMultimap<String, Permission> globalPerms) {
-    globalCache.clear();
     for (String name : globalPerms.keySet()) {
       for (Permission permission : globalPerms.get(name)) {
         // Before 2.2, the global permission which storage in zk is not right. It was saved as a
@@ -175,6 +175,12 @@ public final class AuthManager {
         }
       }
     }
+    for (String name : globalCache.keySet()) {
+      if (!globalPerms.containsKey(name)) {
+        globalCache.remove(name);
+      }
+    }
+
     mtime.incrementAndGet();
   }
 
@@ -186,7 +192,6 @@ public final class AuthManager {
   private void updateTableCache(TableName table, ListMultimap<String, Permission> tablePerms) {
     PermissionCache<TablePermission> cacheToUpdate =
       tableCache.getOrDefault(table, new PermissionCache<>());
-    clearCache(cacheToUpdate);
     updateCache(tablePerms, cacheToUpdate);
     tableCache.put(table, cacheToUpdate);
     mtime.incrementAndGet();
@@ -200,14 +205,9 @@ public final class AuthManager {
   private void updateNamespaceCache(String namespace, ListMultimap<String, Permission> nsPerms) {
     PermissionCache<NamespacePermission> cacheToUpdate =
       namespaceCache.getOrDefault(namespace, new PermissionCache<>());
-    clearCache(cacheToUpdate);
     updateCache(nsPerms, cacheToUpdate);
     namespaceCache.put(namespace, cacheToUpdate);
     mtime.incrementAndGet();
-  }
-
-  private void clearCache(PermissionCache cacheToUpdate) {
-    cacheToUpdate.clear();
   }
 
   @SuppressWarnings("unchecked")
@@ -218,6 +218,7 @@ public final class AuthManager {
         cacheToUpdate.put(name, permission);
       }
     }
+    cacheToUpdate.cleanUp(newPermissions);
   }
 
   /**
