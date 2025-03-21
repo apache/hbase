5a303acd092f91daff22093a932994ec41d9af3d
Vinayak Hegde <vinayakph123@gmail.com>
2025-03-21 16:38:21 +0530

    add pitr command for Point In Time Recovery

--- hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/BackupRestoreConstants.java
+++ hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/BackupRestoreConstants.java
@@ -101,6 +101,16 @@ public interface BackupRestoreConstants {
   String OPTION_ENABLE_CONTINUOUS_BACKUP_DESC =
     "Flag indicating that the full backup is part of a continuous backup process.";
 
+  String OPTION_TO_DATETIME = "td";
+  String LONG_OPTION_TO_DATETIME = "to-datetime";
+  String OPTION_TO_DATETIME_DESC = "Target date and time up to which data should be restored";
+
+  String OPTION_PITR_BACKUP_PATH = "bp";
+  String LONG_OPTION_PITR_BACKUP_PATH = "backup-path";
+  String OPTION_PITR_BACKUP_PATH_DESC =
+    "Specifies a custom backup location for Point-In-Time Recovery (PITR). "
+      + "If provided, this location will be used exclusively instead of deriving the path from the system table.";
+
   String JOB_NAME_CONF_KEY = "mapreduce.job.name";
 
   String BACKUP_CONFIG_STRING =
