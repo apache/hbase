b2178e3eba54edc4abf919ade63c450377e0836a
Ray Mattingly <rmattingly@apache.org>
2025-03-23 19:28:18 -0400

    HBASE-29202 Balancer conditionals make balancer actions more likely to be approved (#6821) (#6837)
    Signed-off-by: Nick Dimiduk <ndimiduk@apache.org>
    Co-authored-by: Ray Mattingly <rmattingly@hubspot.com>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestUnattainableBalancerCostGoal.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestUnattainableBalancerCostGoal.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.ServerName;
@@ -42,21 +41,27 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.hbase.thirdparty.com.google.common.collect.ImmutableSet;
 
-@Category({ MediumTests.class, MasterTests.class })
-public class TestLargeClusterBalancingTableIsolationAndReplicaDistribution {
+/**
+ * If your minCostNeedsBalance is set too low, then the balancer should still eventually stop making
+ * moves as further cost improvements become impossible, and balancer plan calculation becomes
+ * wasteful. This test ensures that the balancer will not get stuck in a loop of continuously moving
+ * regions.
+ */
+@Category({ MasterTests.class, MediumTests.class })
+public class TestUnattainableBalancerCostGoal {
 
   @ClassRule
-  public static final HBaseClassTestRule CLASS_RULE = HBaseClassTestRule
-    .forClass(TestLargeClusterBalancingTableIsolationAndReplicaDistribution.class);
+  public static final HBaseClassTestRule CLASS_RULE =
+    HBaseClassTestRule.forClass(TestUnattainableBalancerCostGoal.class);
 
-  private static final Logger LOG =
-    LoggerFactory.getLogger(TestLargeClusterBalancingTableIsolationAndReplicaDistribution.class);
+  private static final Logger LOG = LoggerFactory.getLogger(TestUnattainableBalancerCostGoal.class);
+
   private static final TableName SYSTEM_TABLE_NAME = TableName.valueOf("hbase:system");
-  private static final TableName NON_ISOLATED_TABLE_NAME = TableName.valueOf("userTable");
+  private static final TableName NON_SYSTEM_TABLE_NAME = TableName.valueOf("userTable");
 
-  private static final int NUM_SERVERS = 500;
-  private static final int NUM_REGIONS = 2_500;
-  private static final int NUM_REPLICAS = 3;
+  private static final int NUM_SERVERS = 10;
+  private static final int NUM_REGIONS = 1000;
+  private static final float UNACHIEVABLE_COST_GOAL = 0.01f;
 
   private static final ServerName[] servers = new ServerName[NUM_SERVERS];
   private static final Map<ServerName, List<RegionInfo>> serverToRegions = new HashMap<>();
@@ -66,62 +71,38 @@ public class TestLargeClusterBalancingTableIsolationAndReplicaDistribution {
     // Initialize servers
     for (int i = 0; i < NUM_SERVERS; i++) {
       servers[i] = ServerName.valueOf("server" + i, i, System.currentTimeMillis());
-      serverToRegions.put(servers[i], new ArrayList<>());
     }
 
-    // Create primary regions and their replicas
+    // Create regions
+    List<RegionInfo> allRegions = new ArrayList<>();
     for (int i = 0; i < NUM_REGIONS; i++) {
-      TableName tableName;
-      if (i < 1) {
-        tableName = TableName.META_TABLE_NAME;
-      } else if (i < 10) {
-        tableName = SYSTEM_TABLE_NAME;
-      } else {
-        tableName = NON_ISOLATED_TABLE_NAME;
-      }
-
-      // Define startKey and endKey for the region
+      TableName tableName = i < 3 ? SYSTEM_TABLE_NAME : NON_SYSTEM_TABLE_NAME;
       byte[] startKey = new byte[1];
       startKey[0] = (byte) i;
       byte[] endKey = new byte[1];
       endKey[0] = (byte) (i + 1);
 
-      Random random = new Random();
-      // Create 3 replicas for each primary region
-      for (int replicaId = 0; replicaId < NUM_REPLICAS; replicaId++) {
-        RegionInfo regionInfo = RegionInfoBuilder.newBuilder(tableName).setStartKey(startKey)
-          .setEndKey(endKey).setReplicaId(replicaId).build();
-        // Assign region to random server
-        int randomServer = random.nextInt(servers.length);
-        serverToRegions.get(servers[randomServer]).add(regionInfo);
-      }
+      RegionInfo regionInfo =
+        RegionInfoBuilder.newBuilder(tableName).setStartKey(startKey).setEndKey(endKey).build();
+      allRegions.add(regionInfo);
+    }
+
+    // Assign all regions to the first server
+    serverToRegions.put(servers[0], new ArrayList<>(allRegions));
+    for (int i = 1; i < NUM_SERVERS; i++) {
+      serverToRegions.put(servers[i], new ArrayList<>());
     }
   }
 
   @Test
-  public void testTableIsolationAndReplicaDistribution() {
+  public void testSystemTableIsolation() {
     Configuration conf = new Configuration(false);
-    conf.setBoolean(BalancerConditionals.ISOLATE_META_TABLE_KEY, true);
     conf.setBoolean(BalancerConditionals.ISOLATE_SYSTEM_TABLES_KEY, true);
-    DistributeReplicasTestConditional.enableConditionalReplicaDistributionForTest(conf);
-    runBalancerToExhaustion(conf, serverToRegions,
-      ImmutableSet.of(this::isMetaTableIsolated, this::isSystemTableIsolated,
-        CandidateGeneratorTestUtil::areAllReplicasDistributed),
-      10.0f, 60_000, CandidateGeneratorTestUtil.ExhaustionType.COST_GOAL_ACHIEVED);
-    LOG.info("Meta table regions are successfully isolated, "
-      + "and region replicas are appropriately distributed.");
+    runBalancerToExhaustion(conf, serverToRegions, ImmutableSet.of(this::isSystemTableIsolated),
+      UNACHIEVABLE_COST_GOAL, 10_000, CandidateGeneratorTestUtil.ExhaustionType.NO_MORE_MOVES);
+    LOG.info("Meta table regions are successfully isolated.");
   }
 
-  /**
-   * Validates whether all meta table regions are isolated.
-   */
-  private boolean isMetaTableIsolated(BalancerClusterState cluster) {
-    return isTableIsolated(cluster, TableName.META_TABLE_NAME, "Meta");
-  }
-
-  /**
-   * Validates whether all meta table regions are isolated.
-   */
   private boolean isSystemTableIsolated(BalancerClusterState cluster) {
     return isTableIsolated(cluster, SYSTEM_TABLE_NAME, "System");
   }
