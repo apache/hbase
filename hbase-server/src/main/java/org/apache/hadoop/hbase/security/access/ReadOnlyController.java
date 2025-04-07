eedcb20e05b137093b4cdc888242ed1dbf11328b
Andor Molnar <andor@cloudera.com>
2025-04-07 16:11:44 -0500

    HBASE-29083. Spotless apply

--- hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/ReadOnlyController.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/security/access/ReadOnlyController.java
@@ -1,18 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.security.access;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CompareOperator;
 import org.apache.hadoop.hbase.CoprocessorEnvironment;
-import org.apache.hadoop.hbase.DoNotRetryIOException;
 import org.apache.hadoop.hbase.HBaseInterfaceAudience;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.NamespaceDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Append;
 import org.apache.hadoop.hbase.client.Delete;
-import org.apache.hadoop.hbase.client.Durability;
-import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.RegionInfo;
@@ -29,39 +45,38 @@ import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessor;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.RegionObserver;
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 import org.apache.hadoop.hbase.coprocessor.RegionServerCoprocessor;
 import org.apache.hadoop.hbase.coprocessor.RegionServerCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.RegionServerObserver;
-import org.apache.hadoop.hbase.exceptions.FailedSanityCheckException;
 import org.apache.hadoop.hbase.filter.ByteArrayComparable;
 import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.regionserver.FlushLifeCycleTracker;
 import org.apache.hadoop.hbase.regionserver.MiniBatchOperationInProgress;
-import org.apache.hadoop.hbase.shaded.protobuf.generated.AdminProtos;
 import org.apache.hadoop.hbase.util.Pair;
-import org.apache.hadoop.hbase.wal.WAL;
 import org.apache.hadoop.hbase.wal.WALEdit;
-import org.apache.hbase.thirdparty.com.google.protobuf.Message;
-import org.apache.hbase.thirdparty.com.google.protobuf.Service;
 import org.apache.yetus.audience.InterfaceAudience;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.hbase.thirdparty.com.google.protobuf.Message;
+import org.apache.hbase.thirdparty.com.google.protobuf.Service;
+
+import org.apache.hadoop.hbase.shaded.protobuf.generated.AdminProtos;
+
 @CoreCoprocessor
 @InterfaceAudience.LimitedPrivate(HBaseInterfaceAudience.CONFIG)
-public class ReadOnlyController implements MasterCoprocessor, RegionCoprocessor,
-  MasterObserver, RegionObserver, RegionServerCoprocessor, RegionServerObserver,
-  EndpointObserver, BulkLoadObserver {
+public class ReadOnlyController
+  implements MasterCoprocessor, RegionCoprocessor, MasterObserver, RegionObserver,
+  RegionServerCoprocessor, RegionServerObserver, EndpointObserver, BulkLoadObserver {
 
   private static final Logger LOG = LoggerFactory.getLogger(ReadOnlyController.class);
   private Configuration conf;
 
-  private void internalReadOnlyGuard() throws IOException{
-    if (conf.getBoolean(HConstants.HBASE_GLOBAL_READONLY_ENABLED_KEY,
-      HConstants.HBASE_GLOBAL_READONLY_ENABLED_DEFAULT)){
+  private void internalReadOnlyGuard() throws IOException {
+    if (
+      conf.getBoolean(HConstants.HBASE_GLOBAL_READONLY_ENABLED_KEY,
+        HConstants.HBASE_GLOBAL_READONLY_ENABLED_DEFAULT)
+    ) {
       // throw new FailedSanityCheckException("Operation not allowed in Read-Only Mode");
       throw new IOException("Operation not allowed in Read-Only Mode");
     }
@@ -97,7 +112,8 @@ public class ReadOnlyController implements MasterCoprocessor, RegionCoprocessor,
     internalReadOnlyGuard();
   }
 
-  @Override public void preBatchMutate(ObserverContext<? extends RegionCoprocessorEnvironment> c,
+  @Override
+  public void preBatchMutate(ObserverContext<? extends RegionCoprocessorEnvironment> c,
     MiniBatchOperationInProgress<Mutation> miniBatchOp) throws IOException {
     for (int i = 0; i < miniBatchOp.size(); i++) {
       WALEdit edit = miniBatchOp.getWalEdit(i);
@@ -164,7 +180,8 @@ public class ReadOnlyController implements MasterCoprocessor, RegionCoprocessor,
     return RegionObserver.super.preCheckAndDelete(c, row, filter, delete, result);
   }
 
-  @Override public boolean preCheckAndDeleteAfterRowLock(
+  @Override
+  public boolean preCheckAndDeleteAfterRowLock(
     ObserverContext<? extends RegionCoprocessorEnvironment> c, byte[] row, byte[] family,
     byte[] qualifier, CompareOperator op, ByteArrayComparable comparator, Delete delete,
     boolean result) throws IOException {
@@ -173,7 +190,8 @@ public class ReadOnlyController implements MasterCoprocessor, RegionCoprocessor,
       comparator, delete, result);
   }
 
-  @Override public boolean preCheckAndDeleteAfterRowLock(
+  @Override
+  public boolean preCheckAndDeleteAfterRowLock(
     ObserverContext<? extends RegionCoprocessorEnvironment> c, byte[] row, Filter filter,
     Delete delete, boolean result) throws IOException {
     internalReadOnlyGuard();
@@ -228,7 +246,8 @@ public class ReadOnlyController implements MasterCoprocessor, RegionCoprocessor,
     MasterObserver.super.preDeleteTable(ctx, tableName);
   }
 
-  @Override public void preDeleteTableAction(ObserverContext<MasterCoprocessorEnvironment> ctx,
+  @Override
+  public void preDeleteTableAction(ObserverContext<MasterCoprocessorEnvironment> ctx,
     TableName tableName) throws IOException {
     internalReadOnlyGuard();
     MasterObserver.super.preDeleteTableAction(ctx, tableName);
@@ -241,7 +260,8 @@ public class ReadOnlyController implements MasterCoprocessor, RegionCoprocessor,
     MasterObserver.super.preTruncateTable(ctx, tableName);
   }
 
-  @Override public void preTruncateTableAction(ObserverContext<MasterCoprocessorEnvironment> ctx,
+  @Override
+  public void preTruncateTableAction(ObserverContext<MasterCoprocessorEnvironment> ctx,
     TableName tableName) throws IOException {
     internalReadOnlyGuard();
     MasterObserver.super.preTruncateTableAction(ctx, tableName);
--- hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestReadOnlyController.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestReadOnlyController.java
@@ -1,5 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.security.access;
 
+import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.HBaseTestingUtil;
@@ -7,7 +25,6 @@ import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.coprocessor.CoprocessorHost;
 import org.apache.hadoop.hbase.coprocessor.RegionServerCoprocessorEnvironment;
@@ -24,10 +41,6 @@ import org.junit.rules.ExpectedException;
 import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.IOException;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 @Category({ SecurityTests.class, LargeTests.class })
 @SuppressWarnings("deprecation")
@@ -52,6 +65,7 @@ public class TestReadOnlyController {
 
   @Rule
   public ExpectedException exception = ExpectedException.none();
+
   @BeforeClass
   public static void beforeClass() throws Exception {
     conf = TEST_UTIL.getConfiguration();
