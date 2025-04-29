01430d1efcfc2a2cfccc6a2091cae60f9ff0831c
Abhishek Kothalikar <kabhishek@cloudera.com>
2025-04-29 17:21:46 +0530

    HBASE-29228 Add support to prevent running multiple active clusters

--- hbase-common/src/main/java/org/apache/hadoop/hbase/HConstants.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/HConstants.java
@@ -1663,6 +1663,9 @@ public final class HConstants {
    */
   public final static boolean HBASE_GLOBAL_READONLY_ENABLED_DEFAULT = false;
 
+  /** name of the file having active cluster suffix */
+  public static final String ACTIVE_CLUSTER_SUFFIX_FILE_NAME = "active_cluster_suffix.id";
+
   private HConstants() {
     // Can't be instantiated with this ctor.
   }
