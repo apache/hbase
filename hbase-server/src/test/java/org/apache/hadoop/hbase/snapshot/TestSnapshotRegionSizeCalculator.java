c9825db00e025720b57ebc2f1e69ec38bb35f3d7
terrytlu <terrytlu@tencent.com>
2025-05-07 16:20:19 +0800

    HBASE-29272 When Spark reads an HBase snapshot, it always read empty data.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/snapshot/TestSnapshotRegionSizeCalculator.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/snapshot/TestSnapshotRegionSizeCalculator.java
@@ -1,16 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.snapshot;
 
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Map;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.HBaseTestingUtil;
-import org.apache.hadoop.hbase.shaded.protobuf.generated.SnapshotProtos;
 import org.apache.hadoop.hbase.testclassification.SmallTests;
 import org.apache.hadoop.hbase.util.CommonFSUtils;
 import org.junit.AfterClass;
@@ -18,6 +33,8 @@ import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
+import org.apache.hadoop.hbase.shaded.protobuf.generated.SnapshotProtos;
 
 @Category(SmallTests.class)
 public class TestSnapshotRegionSizeCalculator {
@@ -67,9 +84,9 @@ public class TestSnapshotRegionSizeCalculator {
     Map<String, Long> regionSizes = calculator.calculateRegionSizes();
 
     // Verify that the region sizes are calculated correctly
-    assertTrue("No regions found in the snapshot", !regionSizes.isEmpty());
+    assertTrue("No regions found in the snapshot", regionSizes.size() == 4);
     for (Map.Entry<String, Long> entry : regionSizes.entrySet()) {
-      assertTrue("Region size should be non-negative", entry.getValue() == 0);
+      assertTrue("Region size should be non-negative", entry.getValue() > 0);
     }
   }
 }
