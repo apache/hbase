ef505d511fe38ec5b452903891d1036a0ba78e6d
terrytlu <terrytlu@tencent.com>
2025-05-07 16:20:19 +0800

    HBASE-29272 When Spark reads an HBase snapshot, it always read empty data.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/snapshot/SnapshotRegionSizeCalculator.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/snapshot/SnapshotRegionSizeCalculator.java
@@ -1,22 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.snapshot;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hbase.HConstants;
-import org.apache.hadoop.hbase.client.RegionInfo;
-import org.apache.hadoop.hbase.io.HFileLink;
-import org.apache.hadoop.hbase.regionserver.StoreFileInfo;
-import org.apache.hadoop.hbase.shaded.protobuf.generated.SnapshotProtos.SnapshotDescription;
-import org.apache.hadoop.hbase.shaded.protobuf.generated.SnapshotProtos.SnapshotRegionManifest;
-import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.hbase.util.CommonFSUtils;
 import org.apache.hadoop.hbase.snapshot.SnapshotInfo.SnapshotStats;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,7 +33,6 @@ public class SnapshotRegionSizeCalculator {
   private final Configuration conf;
   private final Map<String, Long> regionSizes;
 
-
   public SnapshotRegionSizeCalculator(Configuration conf, SnapshotManifest manifest)
     throws IOException {
     this.conf = conf;
@@ -43,13 +45,11 @@ public class SnapshotRegionSizeCalculator {
    * @return A map of region encoded names to their total size in bytes.
    */
   public Map<String, Long> calculateRegionSizes() throws IOException {
-    SnapshotStats stats =  SnapshotInfo.getSnapshotStats(conf, manifest, null);
+    SnapshotStats stats = SnapshotInfo.getSnapshotStats(conf, manifest, null);
     return stats.getRegionSizeMap();
   }
 
-
-
-  public long getRegionSize(String encodedRegionName)  {
+  public long getRegionSize(String encodedRegionName) {
     Long size = regionSizes.get(encodedRegionName);
     if (size == null) {
       LOG.debug("Unknown region:" + encodedRegionName);
