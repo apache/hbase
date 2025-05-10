2cc896854f33ec2eba19f53e342e741ee63fb68d
Chandra Sekhar K <chandrasekhar188k@gmail.com>
2025-05-10 13:12:25 +0530

    HBASE-29284 Add Region name to OutOfOrderScannerNextException message (#6960)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
@@ -3239,7 +3239,8 @@ public class RSRpcServices extends HBaseRpcServicesBase<HRegionServer>
       if (!rsh.incNextCallSeq(callSeq)) {
         throw new OutOfOrderScannerNextException(
           "Expected nextCallSeq: " + rsh.getNextCallSeq() + " But the nextCallSeq got from client: "
-            + request.getNextCallSeq() + "; request=" + TextFormat.shortDebugString(request));
+            + request.getNextCallSeq() + "; request=" + TextFormat.shortDebugString(request)
+            + "; region=" + rsh.r.getRegionInfo().getRegionNameAsString());
       }
     }
   }
