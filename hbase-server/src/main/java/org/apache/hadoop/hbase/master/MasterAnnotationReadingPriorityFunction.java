a8c338b55014044f62d124252c34c1c7ecea427a
ukumawat <ukumawat@salesforce.com>
2025-05-17 22:55:43 +0530

    HBASE-29323 Handling NPE if no enclosing class present

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterAnnotationReadingPriorityFunction.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/MasterAnnotationReadingPriorityFunction.java
@@ -93,7 +93,7 @@ public class MasterAnnotationReadingPriorityFunction
       return HConstants.HIGH_QOS;
     }
     // also use HIGH_QOS for all rest methods in RegionServerStatusProtos
-    if (param.getClass().getEnclosingClass().equals(RegionServerStatusProtos.class)) {
+    if (RegionServerStatusProtos.class.equals(param.getClass().getEnclosingClass())) {
       return HConstants.HIGH_QOS;
     }
     // Trust the client-set priorities if set
