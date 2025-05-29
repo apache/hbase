af90b7672f9069a35d6a58058932b9dd22fee7b4
Kevin Geiszler <kgeiszler@cloudera.com>
2025-05-29 05:40:52 -0700

    HBASE-29236: Add Support for Dynamic Configuration at the Coprocessor Level (#6931)
    Co-authored-by: Andor Molnar <andor@cloudera.com>
    Co-authored-by: Anuj Sharma <sharma.anuj1991@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -617,6 +617,12 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
   private void registerConfigurationObservers() {
     configurationManager.registerObserver(this.rpcServices);
     configurationManager.registerObserver(this);
+    if (cpHost != null) {
+      cpHost.registerConfigurationObservers(configurationManager);
+    } else {
+      LOG.warn("Could not register HMaster coprocessors to the ConfigurationManager because "
+        + "MasterCoprocessorHost is null");
+    }
   }
 
   // Main run loop. Calls through to the regionserver run loop AFTER becoming active Master; will
@@ -625,7 +631,6 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
   public void run() {
     try {
       installShutdownHook();
-      registerConfigurationObservers();
       Threads.setDaemonThreadRunning(new Thread(TraceUtil.tracedRunnable(() -> {
         try {
           int infoPort = putUpJettyServer();
@@ -1086,7 +1091,7 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
     if (!maintenanceMode) {
       startupTaskGroup.addTask("Initializing master coprocessors");
       setQuotasObserver(conf);
-      this.cpHost = new MasterCoprocessorHost(this, conf);
+      initializeCoprocessorHost(conf);
     } else {
       // start an in process region server for carrying system regions
       maintenanceRegionServer =
@@ -4495,7 +4500,7 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
         CoprocessorHost.MASTER_COPROCESSOR_CONF_KEY) && !maintenanceMode
     ) {
       LOG.info("Update the master coprocessor(s) because the configuration has changed");
-      this.cpHost = new MasterCoprocessorHost(this, newConf);
+      initializeCoprocessorHost(newConf);
     }
   }
 
@@ -4592,6 +4597,12 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
     if (QuotaUtil.isQuotaEnabled(conf)) {
       updateConfigurationForQuotasObserver(conf);
     }
+  }
+
+  private void initializeCoprocessorHost(Configuration conf) {
+    // initialize master side coprocessors before we start handling requests
+    this.cpHost = new MasterCoprocessorHost(this, conf);
+    registerConfigurationObservers();
   }
 
   @Override
