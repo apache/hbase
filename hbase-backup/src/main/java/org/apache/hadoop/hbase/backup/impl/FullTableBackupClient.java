a1e6bfb321c6a76e6c9ac1d2fce9a8abd0edf81f
Vinayak Hegde <vinayakph123@gmail.com>
2025-06-22 19:19:20 +0530

    only enable the replication peer if it is disabled

--- hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/impl/FullTableBackupClient.java
+++ hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/impl/FullTableBackupClient.java
@@ -305,7 +305,9 @@ public class FullTableBackupClient extends TableBackupClient {
       .collect(Collectors.toMap(tableName -> tableName, tableName -> new ArrayList<>()));
 
     try {
-      admin.enableReplicationPeer(CONTINUOUS_BACKUP_REPLICATION_PEER);
+      if (!admin.isReplicationPeerEnabled(CONTINUOUS_BACKUP_REPLICATION_PEER)) {
+        admin.removeReplicationPeer(CONTINUOUS_BACKUP_REPLICATION_PEER);
+      }
       admin.appendReplicationPeerTableCFs(CONTINUOUS_BACKUP_REPLICATION_PEER, tableMap);
       LOG.info("Updated replication peer {} with table and column family map.",
         CONTINUOUS_BACKUP_REPLICATION_PEER);
