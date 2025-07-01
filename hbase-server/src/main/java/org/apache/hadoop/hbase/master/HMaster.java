46c4ac3006a62be0ad62fd63a7c2dc3b44225fc3
Wellington Ramos Chevreuil <wchevreuil@apache.org>
2025-07-01 04:03:11 +0100

    [ADDENDUM] Revert "HBASE-28962 Meta replication is inconsistent after startup when reusing hbase storage location (#6448)" (#7125)
    As discussed further on https://github.com/apache/hbase/pull/7046, the described issue may not apply to branches containing
    the changes from HBASE-26193 (present since 2.5.0).
    
    This reverts commit 08621f94d4bd9b6a180e7dcf5d4a530cf643258d.
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -1172,33 +1172,30 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
       int replicasNumInConf =
         conf.getInt(HConstants.META_REPLICAS_NUM, HConstants.DEFAULT_META_REPLICA_NUM);
       TableDescriptor metaDesc = tableDescriptors.get(TableName.META_TABLE_NAME);
-      int existingReplicasCount =
-        assignmentManager.getRegionStates().getRegionsOfTable(TableName.META_TABLE_NAME).size();
-
       if (metaDesc.getRegionReplication() != replicasNumInConf) {
         // it is possible that we already have some replicas before upgrading, so we must set the
         // region replication number in meta TableDescriptor directly first, without creating a
         // ModifyTableProcedure, otherwise it may cause a double assign for the meta replicas.
-        LOG.info("Update replica count of hbase:meta from {}(in TableDescriptor)"
-          + " to {}(existing ZNodes)", metaDesc.getRegionReplication(), existingReplicasCount);
-        metaDesc = TableDescriptorBuilder.newBuilder(metaDesc)
-          .setRegionReplication(existingReplicasCount).build();
-        tableDescriptors.update(metaDesc);
-      }
-      // check again, and issue a ModifyTableProcedure if needed
-      if (
-        metaDesc.getRegionReplication() != replicasNumInConf
-          || existingReplicasCount != metaDesc.getRegionReplication()
-      ) {
-        LOG.info(
-          "The {} config is {} while the replica count in TableDescriptor is {},"
-            + " The number of replicas seen on ZK {} for hbase:meta, altering...",
-          HConstants.META_REPLICAS_NUM, replicasNumInConf, metaDesc.getRegionReplication(),
-          existingReplicasCount);
-        procedureExecutor.submitProcedure(new ModifyTableProcedure(
-          procedureExecutor.getEnvironment(), TableDescriptorBuilder.newBuilder(metaDesc)
-            .setRegionReplication(replicasNumInConf).build(),
-          null, metaDesc, false, true));
+        int existingReplicasCount =
+          assignmentManager.getRegionStates().getRegionsOfTable(TableName.META_TABLE_NAME).size();
+        if (existingReplicasCount > metaDesc.getRegionReplication()) {
+          LOG.info("Update replica count of hbase:meta from {}(in TableDescriptor)"
+            + " to {}(existing ZNodes)", metaDesc.getRegionReplication(), existingReplicasCount);
+          metaDesc = TableDescriptorBuilder.newBuilder(metaDesc)
+            .setRegionReplication(existingReplicasCount).build();
+          tableDescriptors.update(metaDesc);
+        }
+        // check again, and issue a ModifyTableProcedure if needed
+        if (metaDesc.getRegionReplication() != replicasNumInConf) {
+          LOG.info(
+            "The {} config is {} while the replica count in TableDescriptor is {}"
+              + " for hbase:meta, altering...",
+            HConstants.META_REPLICAS_NUM, replicasNumInConf, metaDesc.getRegionReplication());
+          procedureExecutor.submitProcedure(new ModifyTableProcedure(
+            procedureExecutor.getEnvironment(), TableDescriptorBuilder.newBuilder(metaDesc)
+              .setRegionReplication(replicasNumInConf).build(),
+            null, metaDesc, false, true));
+        }
       }
     }
     // Initialize after meta is up as below scans meta
