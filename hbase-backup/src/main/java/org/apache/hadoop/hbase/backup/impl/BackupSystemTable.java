3555f216f3da6c1736b5d0c4407814ed84cc5162
asolomon <ankitsolomon@gmail.com>
2025-07-23 21:49:35 +0530

    HBASE-29310 Handle Bulk Load Operations in Continuous Backup (#7150)
    Signed-off-by: Tak Lon (Stephen) Wu <taklwu@apache.org>
    Reviewed by: Kevin Geiszler <kevin.j.geiszler@gmail.com>

--- hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/impl/BackupSystemTable.java
+++ hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/impl/BackupSystemTable.java
@@ -444,8 +444,8 @@ public final class BackupSystemTable implements Closeable {
             path = Bytes.toString(CellUtil.cloneValue(cell));
           }
         }
-        result.add(new BulkLoad(table, region, fam, path, row));
-        LOG.debug("Found bulk load entry for table {}, family {}: {}", table, fam, path);
+        LOG.debug("Found orig path {} for family {} of table {} and region {} with timestamp {}",
+          path, fam, table, region, timestamp);
         if (timestamp <= endTimestamp) {
           result.add(new BulkLoad(table, region, fam, path, row, timestamp));
         }
