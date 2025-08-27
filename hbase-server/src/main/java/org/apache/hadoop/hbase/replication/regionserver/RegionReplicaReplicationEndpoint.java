96160c3cfa373398f14ee6a9b05070a0dea3ecb9
Charles Connell <charles@charlesconnell.com>
2025-08-27 13:48:06 -0400

    Add retry limit

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/RegionReplicaReplicationEndpoint.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/regionserver/RegionReplicaReplicationEndpoint.java
@@ -427,6 +427,7 @@ public class RegionReplicaReplicationEndpoint extends HBaseReplicationEndpoint {
       // invalidate the cache and check from meta
       RegionLocations locations = null;
       boolean useCache = true;
+      int retries = 0;
       while (true) {
         // get the replicas of the primary region
         try {
@@ -440,11 +441,12 @@ public class RegionReplicaReplicationEndpoint extends HBaseReplicationEndpoint {
           // keep going to the cache, we will not learn of the replicas and their locations after
           // they come online.
           if (useCache && locations.size() == 1) {
-            if (tableDescriptors.get(tableName).getRegionReplication() > 1) {
+            if (tableDescriptors.get(tableName).getRegionReplication() > 1 && retries <= 3) {
               // Make an obnoxious log here. See how bad this issue is. Add a timer if happening
               // too much.
               LOG.info("Skipping location cache; only one location found for {}", tableName);
               useCache = false;
+              retries++;
               continue;
             }
           }
