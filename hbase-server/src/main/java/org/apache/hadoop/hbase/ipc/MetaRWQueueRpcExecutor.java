12c05d5040f58af099b754f81a39ae72f8d05a53
Ruanhui <32773751+frostruan@users.noreply.github.com>
2025-09-15 09:47:30 +0800

    HBASE-27355 Separate meta read requests from master and client (#7261)
    Co-authored-by: huiruan <huiruan@tencent.com>
    
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Reviewed-by: Aman Poonia <aman.poonia.29@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/MetaRWQueueRpcExecutor.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/MetaRWQueueRpcExecutor.java
@@ -19,6 +19,7 @@ package org.apache.hadoop.hbase.ipc;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Abortable;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.yetus.audience.InterfaceAudience;
 import org.apache.yetus.audience.InterfaceStability;
 
@@ -32,7 +33,10 @@ public class MetaRWQueueRpcExecutor extends RWQueueRpcExecutor {
     "hbase.ipc.server.metacallqueue.read.ratio";
   public static final String META_CALL_QUEUE_SCAN_SHARE_CONF_KEY =
     "hbase.ipc.server.metacallqueue.scan.ratio";
-  public static final float DEFAULT_META_CALL_QUEUE_READ_SHARE = 0.9f;
+  public static final String META_CALL_QUEUE_HANDLER_FACTOR_CONF_KEY =
+    "hbase.ipc.server.metacallqueue.handler.factor";
+  public static final float DEFAULT_META_CALL_QUEUE_READ_SHARE = 0.8f;
+  private static final float DEFAULT_META_CALL_QUEUE_SCAN_SHARE = 0.2f;
 
   public MetaRWQueueRpcExecutor(final String name, final int handlerCount, final int maxQueueLength,
     final PriorityFunction priority, final Configuration conf, final Abortable abortable) {
@@ -46,6 +50,23 @@ public class MetaRWQueueRpcExecutor extends RWQueueRpcExecutor {
 
   @Override
   protected float getScanShare(final Configuration conf) {
-    return conf.getFloat(META_CALL_QUEUE_SCAN_SHARE_CONF_KEY, 0);
+    return conf.getFloat(META_CALL_QUEUE_SCAN_SHARE_CONF_KEY, DEFAULT_META_CALL_QUEUE_SCAN_SHARE);
+  }
+
+  @Override
+  public boolean dispatch(CallRunner callTask) {
+    RpcCall call = callTask.getRpcCall();
+    int level = call.getHeader().getPriority();
+    final boolean toWriteQueue = isWriteRequest(call.getHeader(), call.getParam());
+    // dispatch client system read request to read handlers
+    // dispatch internal system read request to scan handlers
+    final boolean toScanQueue =
+      getNumScanQueues() > 0 && level == HConstants.INTERNAL_READ_QOS;
+    return dispatchTo(toWriteQueue, toScanQueue, callTask);
+  }
+
+  @Override
+  protected float getCallQueueHandlerFactor(Configuration conf) {
+    return conf.getFloat(META_CALL_QUEUE_HANDLER_FACTOR_CONF_KEY, 0.5f);
   }
 }
