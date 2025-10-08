08cf765141fa36013680277497b53744422f5814
Siddharth Khillon <sidkhillon24@gmail.com>
2025-10-08 10:56:16 -0700

    HubSpot Backport HBASE-29629 Record the quota user name value on metrics for RpcThrottlingExceptions (#7345) (#208)
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/RegionServerRpcQuotaManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/RegionServerRpcQuotaManager.java
@@ -199,7 +199,7 @@ public class RegionServerRpcQuotaManager implements RpcQuotaManager, Configurati
       LOG.debug("Throttling exception for user=" + ugi.getUserName() + " table=" + table + " scan="
         + scanRequest.getScannerId() + ": " + e.getMessage());
 
-      rsServices.getMetrics().recordThrottleException(e.getType(), ugi.getShortUserName(),
+      rsServices.getMetrics().recordThrottleException(e.getType(), quotaCache.getQuotaUserName(ugi),
         table.getNameAsString());
 
       throw e;
@@ -276,7 +276,7 @@ public class RegionServerRpcQuotaManager implements RpcQuotaManager, Configurati
       LOG.debug("Throttling exception for user=" + ugi.getUserName() + " table=" + table
         + " numWrites=" + numWrites + " numReads=" + numReads + ": " + e.getMessage());
 
-      rsServices.getMetrics().recordThrottleException(e.getType(), ugi.getShortUserName(),
+      rsServices.getMetrics().recordThrottleException(e.getType(), quotaCache.getQuotaUserName(ugi),
         table.getNameAsString());
 
       throw e;
