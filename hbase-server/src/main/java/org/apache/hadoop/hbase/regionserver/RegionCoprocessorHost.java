361a563addd1dfc1ff866c2fdcb4e15233886e54
Istvan Toth <stoty@apache.org>
2025-10-09 06:21:05 +0200

    HBASE-29649 Un-deprecate preWALRestore and postWALRestore in RegionCoprocessorHost (#7370)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionCoprocessorHost.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionCoprocessorHost.java
@@ -1437,10 +1437,7 @@ public class RegionCoprocessorHost
   /**
    * Supports Coprocessor 'bypass'.
    * @return true if default behavior should be bypassed, false otherwise
-   * @deprecated Since hbase-2.0.0. No replacement. To be removed in hbase-3.0.0 and replaced with
-   *             something that doesn't expose IntefaceAudience.Private classes.
    */
-  @Deprecated
   public boolean preWALRestore(final RegionInfo info, final WALKey logKey, final WALEdit logEdit)
     throws IOException {
     return execOperation(
@@ -1452,11 +1449,6 @@ public class RegionCoprocessorHost
       });
   }
 
-  /**
-   * @deprecated Since hbase-2.0.0. No replacement. To be removed in hbase-3.0.0 and replaced with
-   *             something that doesn't expose IntefaceAudience.Private classes.
-   */
-  @Deprecated
   public void postWALRestore(final RegionInfo info, final WALKey logKey, final WALEdit logEdit)
     throws IOException {
     execOperation(coprocEnvironments.isEmpty() ? null : new RegionObserverOperationWithoutResult() {
