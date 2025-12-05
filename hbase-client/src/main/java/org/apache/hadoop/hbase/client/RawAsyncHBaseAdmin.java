0b6b1de25a5c8bf01bee9e08d663f1310625c0c2
Peng Lu <lupeng@apache.org>
2025-12-05 22:56:58 +0800

    HBASE-29709 NPE may occur when compacting table via RawAsyncHBaseAdmin (#7495)
    Signed-off-by: Pankaj Kumar <pankajkumar@apache.org>

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/RawAsyncHBaseAdmin.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/RawAsyncHBaseAdmin.java
@@ -1186,6 +1186,7 @@ class RawAsyncHBaseAdmin implements AsyncAdmin {
           }
           if (locations == null || locations.isEmpty()) {
             future.completeExceptionally(new TableNotFoundException(tableName));
+            return;
           }
           CompletableFuture<?>[] compactFutures =
             locations.stream().filter(l -> l.getRegion() != null)
