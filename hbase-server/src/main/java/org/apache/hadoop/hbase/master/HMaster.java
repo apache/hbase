35e5cbac228b0842d46ec8436bee41d7f100f34e
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2025-12-10 16:14:30 -0800

    Use HMaster.listTableDescriptors() to take advantage of internal filter
    Change-Id: I2001fd4f8289ee6c74621bd6d78f642e77e78129

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -3116,14 +3116,9 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
           if (isActiveMaster() && isInitialized() && assignmentManager != null) {
             try {
               Map<TableName, RegionStatesCount> tableRegionStatesCountMap = new HashMap<>();
-              Map<String, TableDescriptor> tableDescriptorMap = getTableDescriptors().getAll();
-              List<TableName> tableNames = listTableNames(null, null, true);
-              for (TableDescriptor tableDescriptor : tableDescriptorMap.values()) {
+              List<TableDescriptor> tableDescriptors = listTableDescriptors(null, null, null, true);
+              for (TableDescriptor tableDescriptor : tableDescriptors) {
                 TableName tableName = tableDescriptor.getTableName();
-                if (!tableNames.contains(tableName)) {
-                  LOG.info("Skipping foreign table {} in cluster metrics", tableName);
-                  continue;
-                }
                 RegionStatesCount regionStatesCount =
                   assignmentManager.getRegionStatesCount(tableName);
                 tableRegionStatesCountMap.put(tableName, regionStatesCount);
@@ -3826,7 +3821,7 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
   }
 
   /**
-   * Return a list of table table descriptors after applying any provided filter parameters. Note
+   * Return a list of table descriptors after applying any provided filter parameters. Note
    * that the user-facing description of this filter logic is presented on the class-level javadoc
    * of {@link NormalizeTableFilterParams}.
    */
@@ -3837,7 +3832,7 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
       // request for all TableDescriptors
       Collection<TableDescriptor> allHtds;
       if (namespace != null && namespace.length() > 0) {
-        // Do a check on the namespace existence. Will fail if does not exist.
+        // Do a check on the namespace existence. Will fail if it does not exist.
         this.clusterSchemaService.getNamespace(namespace);
         allHtds = tableDescriptors.getByNamespace(namespace).values();
       } else {
