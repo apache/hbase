bb40a419939bec0c5bd040f89f54f41a7849d0ad
Duo Zhang <zhangduo@apache.org>
2025-12-28 22:22:55 +0800

    HBASE-29795 RegionServer abort because of NPE when closing compacted store file (#7576)
    Signed-off-by: Wellington Ramos Chevreuil <wchevreuil@apache.org>
    (cherry picked from commit 1a3e371ca0a9c5cc3a884cb6121a4c8769f25a70)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketCache.java
@@ -410,7 +410,6 @@ public class BucketCache implements BlockCache, HeapSize {
         LOG.warn("Can't restore from file[{}]. The bucket cache will be reset and rebuilt."
           + " Exception seen: ", persistencePath, ex);
         backingMap.clear();
-        FilePathStringPool.getInstance().clear();
         fullyCachedFiles.clear();
         backingMapValidated.set(true);
         regionCachedSize.clear();
@@ -793,7 +792,6 @@ public class BucketCache implements BlockCache, HeapSize {
         // remove the entry for that region from regionCachedSize map.
         if (regionCachedSize.get(regionName) <= 0) {
           regionCachedSize.remove(regionName);
-          FilePathStringPool.getInstance().remove(regionName);
         }
       }
     }
@@ -1013,7 +1011,6 @@ public class BucketCache implements BlockCache, HeapSize {
       + cacheStats.getEvictedCount() + ", " + "evictedPerRun=" + cacheStats.evictedPerEviction()
       + ", " + "allocationFailCount=" + cacheStats.getAllocationFailCount() + ", blocksCount="
       + backingMap.size());
-    LOG.info(FilePathStringPool.getInstance().getPoolStats());
     cacheStats.reset();
 
     bucketAllocator.logDebugStatistics();
@@ -1757,7 +1754,6 @@ public class BucketCache implements BlockCache, HeapSize {
   }
 
   private void retrieveChunkedBackingMap(FileInputStream in) throws IOException {
-
     // Read the first chunk that has all the details.
     BucketCacheProtos.BucketCacheEntry cacheEntry =
       BucketCacheProtos.BucketCacheEntry.parseDelimitedFrom(in);
@@ -1767,7 +1763,6 @@ public class BucketCache implements BlockCache, HeapSize {
 
     backingMap.clear();
     blocksByHFile.clear();
-    FilePathStringPool.getInstance().clear();
 
     // Read the backing map entries in batches.
     int numChunks = 0;
@@ -1823,7 +1818,6 @@ public class BucketCache implements BlockCache, HeapSize {
       this.blocksByHFile.clear();
       this.fullyCachedFiles.clear();
       this.regionCachedSize.clear();
-      FilePathStringPool.getInstance().clear();
     }
     if (cacheStats.getMetricsRollerScheduler() != null) {
       cacheStats.getMetricsRollerScheduler().shutdownNow();
@@ -1852,7 +1846,6 @@ public class BucketCache implements BlockCache, HeapSize {
             }
           }
           persistToFile();
-          FilePathStringPool.getInstance().clear();
         } catch (IOException ex) {
           LOG.error("Unable to persist data on exit: " + ex.toString(), ex);
         } catch (InterruptedException e) {
@@ -1943,20 +1936,12 @@ public class BucketCache implements BlockCache, HeapSize {
     Set<BlockCacheKey> keySet = getAllCacheKeysForFile(hfileName, initOffset, endOffset);
     // We need to make sure whether we are evicting all blocks for this given file. In case of
     // split references, we might be evicting just half of the blocks
-    int totalFileKeys = (endOffset == Long.MAX_VALUE)
-      ? keySet.size()
-      : getAllCacheKeysForFile(hfileName, 0, Long.MAX_VALUE).size();
     LOG.debug("found {} blocks for file {}, starting offset: {}, end offset: {}", keySet.size(),
       hfileName, initOffset, endOffset);
     int numEvicted = 0;
     for (BlockCacheKey key : keySet) {
       if (evictBlock(key)) {
         ++numEvicted;
-      }
-    }
-    if (numEvicted > 0) {
-      if (totalFileKeys == numEvicted) {
-        FilePathStringPool.getInstance().remove(hfileName);
       }
     }
     return numEvicted;
