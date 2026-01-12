8a0e71d8b4a240b3a3e06bd84f7236336a30e646
Duo Zhang <zhangduo@apache.org>
2026-01-12 22:59:08 +0800

    HBASE-29802 NPE when shutting down mini cluster cause tests hang (#7604) (#7612)
    Remove the 'shutdownHook' in SingleProcessHBaseCluster where we close
    the FileSystem instance of a region server.
    Set DistributedFileSystem cache to false when creating MiniMRCluster so
    when shutting down MiniMRCluster it will not close the shared FileSystem
    instance.
    Also re-enable TestExportSnapshot.
    
    (cherry picked from commit d11ee6a4586bd2afd7201f525a68f76de98e279d)
    
    Signed-off-by: Nick Dimiduk <ndimiduk@apache.org>
    (cherry picked from commit 8841debe872ccc3054e7a80dcfb471f28b3af34d)

--- hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
@@ -2832,10 +2832,14 @@ public class HBaseTestingUtility extends HBaseZKTestingUtility {
         jvmOpts + " --add-opens java.base/java.lang=ALL-UNNAMED");
     }
 
+    // Disable fs cache for DistributedFileSystem, to avoid YARN RM close the shared FileSystem
+    // instance and cause trouble in HBase. See HBASE-29802 for more details.
+    JobConf mrClusterConf = new JobConf(conf);
+    mrClusterConf.setBoolean("fs.hdfs.impl.disable.cache", true);
     // Allow the user to override FS URI for this map-reduce cluster to use.
     mrCluster =
       new MiniMRCluster(servers, FS_URI != null ? FS_URI : FileSystem.get(conf).getUri().toString(),
-        1, null, null, new JobConf(this.conf));
+        1, null, null, mrClusterConf);
     JobConf jobConf = MapreduceTestingShim.getJobConf(mrCluster);
     if (jobConf == null) {
       jobConf = mrCluster.createJobConf();
