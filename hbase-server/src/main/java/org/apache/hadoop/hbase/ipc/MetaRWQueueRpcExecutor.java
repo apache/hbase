43dbd893c74044d7f0ee9beddbce1239c0830231
Umesh <9414umeshkumar@gmail.com>
2026-01-27 03:37:54 +0530

    HBASE-29141 Calculate default maxQueueLength call queues correctly (#7490)
    Co-authored-by: ukumawat <ukumawat@salesforce.com>
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/MetaRWQueueRpcExecutor.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/MetaRWQueueRpcExecutor.java
@@ -35,6 +35,7 @@ public class MetaRWQueueRpcExecutor extends RWQueueRpcExecutor {
     "hbase.ipc.server.metacallqueue.scan.ratio";
   public static final String META_CALL_QUEUE_HANDLER_FACTOR_CONF_KEY =
     "hbase.ipc.server.metacallqueue.handler.factor";
+  public static final float DEFAULT_META_CALL_QUEUE_HANDLER_FACTOR = 0.5f;
   public static final float DEFAULT_META_CALL_QUEUE_READ_SHARE = 0.8f;
   private static final float DEFAULT_META_CALL_QUEUE_SCAN_SHARE = 0.2f;
 
@@ -66,6 +67,7 @@ public class MetaRWQueueRpcExecutor extends RWQueueRpcExecutor {
 
   @Override
   protected float getCallQueueHandlerFactor(Configuration conf) {
-    return conf.getFloat(META_CALL_QUEUE_HANDLER_FACTOR_CONF_KEY, 0.5f);
+    return conf.getFloat(META_CALL_QUEUE_HANDLER_FACTOR_CONF_KEY,
+      DEFAULT_META_CALL_QUEUE_HANDLER_FACTOR);
   }
 }
