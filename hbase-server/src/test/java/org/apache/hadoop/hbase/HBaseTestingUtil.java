1220fb3ad6fbe2479b386357357e28bc023a471d
Duo Zhang <zhangduo@apache.org>
2026-02-05 15:56:42 +0800

    HBASE-29868 Can not start mini dfs cluster when building against hadoop 3.3.x
    Just disable metrics logger for NameNode and DataNode

--- hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtil.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/HBaseTestingUtil.java
@@ -145,6 +145,7 @@ import org.apache.hadoop.hbase.zookeeper.EmptyWatcher;
 import org.apache.hadoop.hbase.zookeeper.ZKConfig;
 import org.apache.hadoop.hbase.zookeeper.ZKWatcher;
 import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.hadoop.hdfs.DFSConfigKeys;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.hdfs.server.datanode.DataNode;
@@ -729,6 +730,11 @@ public class HBaseTestingUtil extends HBaseZKTestingUtil {
     createDirAndSetProperty("dfs.journalnode.edits.dir");
     createDirAndSetProperty("dfs.provided.aliasmap.inmemory.leveldb.dir");
     createDirAndSetProperty("fs.s3a.committer.staging.tmp.path");
+
+    // disable metrics logger since it depend on commons-logging internal classes and we do not want
+    // commons-logging on our classpath
+    conf.setInt(DFSConfigKeys.DFS_NAMENODE_METRICS_LOGGER_PERIOD_SECONDS_KEY, 0);
+    conf.setInt(DFSConfigKeys.DFS_DATANODE_METRICS_LOGGER_PERIOD_SECONDS_KEY, 0);
   }
 
   /**
