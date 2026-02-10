059a9e36190dabd88423071d34108d1b52dce29f
Liu Xiao <42756849+liuxiaocs7@users.noreply.github.com>
2026-02-10 10:20:03 +0800

    HBASE-29869 Upgrade hbase-balancer to use junit5 (#7716)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    (cherry picked from commit 4ba1c366a3e23b6a511178fbeda41fb32c703621)

--- hbase-balancer/src/test/java/org/apache/hadoop/hbase/master/balancer/TestLargeClusterBalancingSystemTableIsolation.java
+++ hbase-balancer/src/test/java/org/apache/hadoop/hbase/master/balancer/TestLargeClusterBalancingSystemTableIsolation.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.RegionInfo;
@@ -35,19 +34,15 @@ import org.apache.hadoop.hbase.master.balancer.BalancerTestBase.MockMapping;
 import org.apache.hadoop.hbase.testclassification.MasterTests;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.net.DNSToSwitchMapping;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Category({ MediumTests.class, MasterTests.class })
+@Tag(MediumTests.TAG)
+@Tag(MasterTests.TAG)
 public class TestLargeClusterBalancingSystemTableIsolation {
-
-  @ClassRule
-  public static final HBaseClassTestRule CLASS_RULE =
-    HBaseClassTestRule.forClass(TestLargeClusterBalancingSystemTableIsolation.class);
 
   private static final Logger LOG =
     LoggerFactory.getLogger(TestLargeClusterBalancingSystemTableIsolation.class);
@@ -61,7 +56,7 @@ public class TestLargeClusterBalancingSystemTableIsolation {
   private static final ServerName[] servers = new ServerName[NUM_SERVERS];
   private static final Map<ServerName, List<RegionInfo>> serverToRegions = new HashMap<>();
 
-  @BeforeClass
+  @BeforeAll
   public static void setup() {
     // Initialize servers
     for (int i = 0; i < NUM_SERVERS; i++) {
@@ -101,5 +96,4 @@ public class TestLargeClusterBalancingSystemTableIsolation {
   private boolean isSystemTableIsolated(BalancerClusterState cluster) {
     return isTableIsolated(cluster, SYSTEM_TABLE_NAME, "System");
   }
-
 }
