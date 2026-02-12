2c035402a4636a1a04e3cded9a2feeef9057f6c7
Liu Xiao <42756849+liuxiaocs7@users.noreply.github.com>
2026-02-12 23:39:29 +0800

    HBASE-29876 Upgrade hbase-shell to use junit5 (#7729)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Dávid Paksy <paksyd@apache.org>
    (cherry picked from commit a0a4bb8e3f1b0096ce66eb02fc79d8c67c9c3816)

--- hbase-shell/src/test/java/org/apache/hadoop/hbase/client/RubyShellTest.java
+++ hbase-shell/src/test/java/org/apache/hadoop/hbase/client/RubyShellTest.java
@@ -34,7 +34,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public interface RubyShellTest {
-  static Logger LOG = LoggerFactory.getLogger(RubyShellTest.class);
+  Logger LOG = LoggerFactory.getLogger(RubyShellTest.class);
 
   HBaseTestingUtility getTEST_UTIL();
 
