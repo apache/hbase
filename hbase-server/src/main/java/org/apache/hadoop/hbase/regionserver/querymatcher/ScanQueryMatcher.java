e46411b32d57851479641756df8b710ad6410e26
Liu Xiao <42756849+liuxiaocs7@users.noreply.github.com>
2026-02-25 22:17:18 +0800

    HBASE-29874 Removing the useless parameters from ScanQueryMatcher.isCellTTLExpired (#7720)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    Signed-off-by: Junegunn Choi <junegunn@apache.org>
    (cherry picked from commit e2cfd2c8cfa7689360b161c6f4a7fa3829ed5685)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/querymatcher/ScanQueryMatcher.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/querymatcher/ScanQueryMatcher.java
@@ -138,8 +138,7 @@ public abstract class ScanQueryMatcher implements ShipperListener {
   }
 
   /** Returns true if the cell is expired */
-  private static boolean isCellTTLExpired(final Cell cell, final long oldestTimestamp,
-    final long now) {
+  private static boolean isCellTTLExpired(final Cell cell, final long now) {
     // Look for a TTL tag first. Use it instead of the family setting if
     // found. If a cell has multiple TTLs, resolve the conflict by using the
     // first tag encountered.
@@ -174,7 +173,7 @@ public abstract class ScanQueryMatcher implements ShipperListener {
     }
 
     // check if the cell is expired by cell TTL
-    if (isCellTTLExpired(cell, this.oldestUnexpiredTS, this.now)) {
+    if (isCellTTLExpired(cell, this.now)) {
       return MatchCode.SKIP;
     }
 
