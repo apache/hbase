5a7bd5a6aa46c508c84d4104db3debb54df1212a
Xiao Liu <liuxiaocs@apache.org>
2026-04-25 01:06:24 +0800

    HBASE-30067 Upgrade hbase-server to use junit5 Part10 (#8121)
    Signed-off-by: Duo Zhang <zhangduo@apache.org>
    
    (cherry picked from commit 73f8114c1aff64023a2cf7fc85c23b0d3f0b27ee)

---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/ipc/AbstractTestRpcServer.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.ipc;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.TableDescriptor;
+import org.apache.hadoop.hbase.client.TableDescriptorBuilder;
+import org.apache.hadoop.hbase.regionserver.DisabledRegionSplitPolicy;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.LoadTestKVGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+
+public abstract class AbstractTestRpcServer {
+
+  private static final byte[] FAMILY = Bytes.toBytes("f");
+  private static final byte[] QUALIFIER = Bytes.toBytes("q");
+  private static final int NUM_ROWS = 100;
+  private static final int MIN_LEN = 1000;
+  private static final int MAX_LEN = 1000000;
+  protected static final LoadTestKVGenerator GENERATOR = new LoadTestKVGenerator(MIN_LEN, MAX_LEN);
+  protected static HBaseTestingUtility TEST_UTIL;
+  protected TableName tableName;
+
+  @BeforeEach
+  public void setUpTest(TestInfo testInfo) {
+    tableName = TableName.valueOf(testInfo.getTestMethod().get().getName());
+  }
+
+  protected void doTest(TableName tableName) throws Exception {
+    // Splitting just complicates the test scenario, disable it
+    final TableDescriptor desc = TableDescriptorBuilder.newBuilder(tableName)
+      .setRegionSplitPolicyClassName(DisabledRegionSplitPolicy.class.getName()).build();
+    try (Table table =
+      TEST_UTIL.createTable(desc, new byte[][] { FAMILY }, TEST_UTIL.getConfiguration())) {
+      // put some test data
+      for (int i = 0; i < NUM_ROWS; i++) {
+        final byte[] rowKey = Bytes.toBytes(LoadTestKVGenerator.md5PrefixedKey(i));
+        final byte[] v = GENERATOR.generateRandomSizeValue(rowKey, QUALIFIER);
+        table.put(new Put(rowKey).addColumn(FAMILY, QUALIFIER, v));
+      }
+      // read to verify it.
+      for (int i = 0; i < NUM_ROWS; i++) {
+        final byte[] rowKey = Bytes.toBytes(LoadTestKVGenerator.md5PrefixedKey(i));
+        final Result r = table.get(new Get(rowKey).addColumn(FAMILY, QUALIFIER));
+        assertNotNull(r, "Result was empty");
+        final byte[] v = r.getValue(FAMILY, QUALIFIER);
+        assertNotNull(v, "Result did not contain expected value");
+        assertTrue(LoadTestKVGenerator.verify(v, rowKey, QUALIFIER), "Value was not verified");
+      }
+    }
+  }
+}
