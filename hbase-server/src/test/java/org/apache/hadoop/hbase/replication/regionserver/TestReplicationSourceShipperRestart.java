5a4d9d1a4527f9a05d4646ac9a038ceb58dc3d82
Ankit Solomon <asolomon@cloudera.com>
2026-05-05 21:27:36 +0530

    Spotless fix

--- hbase-server/src/test/java/org/apache/hadoop/hbase/replication/regionserver/TestReplicationSourceShipperRestart.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/replication/regionserver/TestReplicationSourceShipperRestart.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.replication.regionserver;
 
 import static org.mockito.ArgumentMatchers.*;
@@ -5,7 +22,6 @@ import static org.mockito.Mockito.*;
 
 import java.io.IOException;
 import java.util.Collections;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.TableName;
@@ -95,7 +111,8 @@ public class TestReplicationSourceShipperRestart {
       }
 
       @Override
-      protected void cleanUpHFileRefs(WALEdit edit) {}
+      protected void cleanUpHFileRefs(WALEdit edit) {
+      }
 
       @Override
       void persistLogPosition() {
@@ -117,7 +134,8 @@ public class TestReplicationSourceShipperRestart {
       }
 
       @Override
-      protected void cleanUpHFileRefs(WALEdit edit) {}
+      protected void cleanUpHFileRefs(WALEdit edit) {
+      }
     };
   }
 
@@ -247,9 +265,7 @@ public class TestReplicationSourceShipperRestart {
 
     WALEntryBatch batch = emptyBatch();
 
-    when(reader.poll(anyLong()))
-      .thenReturn(batch)
-      .thenThrow(new InterruptedException());
+    when(reader.poll(anyLong())).thenReturn(batch).thenThrow(new InterruptedException());
 
     doThrow(new IOException()).when(endpoint).beforePersistingReplicationOffset();
 
@@ -273,9 +289,7 @@ public class TestReplicationSourceShipperRestart {
 
     WALEntryBatch batch = emptyBatch();
 
-    when(reader.poll(anyLong()))
-      .thenReturn(batch)
-      .thenThrow(new InterruptedException());
+    when(reader.poll(anyLong())).thenReturn(batch).thenThrow(new InterruptedException());
 
     ReplicationSourceShipper s = lightShipper(new Configuration());
 
