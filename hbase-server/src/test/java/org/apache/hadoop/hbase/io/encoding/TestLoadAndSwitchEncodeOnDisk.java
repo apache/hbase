3e03fe7e298a45df307575069178c1c56852745a
Duo Zhang <zhangduo@apache.org>
2026-05-18 18:51:03 +0800

    HBASE-30123 Addendum upgrade missing tests to junit5

--- hbase-server/src/test/java/org/apache/hadoop/hbase/io/encoding/TestLoadAndSwitchEncodeOnDisk.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/io/encoding/TestLoadAndSwitchEncodeOnDisk.java
@@ -17,14 +17,13 @@
  */
 package org.apache.hadoop.hbase.io.encoding;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import org.apache.hadoop.hbase.HBaseClassTestRule;
-import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HRegionLocation;
 import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.ColumnFamilyDescriptor;
 import org.apache.hadoop.hbase.client.RegionLocator;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
@@ -34,32 +33,21 @@ import org.apache.hadoop.hbase.io.compress.Compression;
 import org.apache.hadoop.hbase.io.hfile.CacheConfig;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
 import org.apache.hadoop.hbase.testclassification.IOTests;
-import org.apache.hadoop.hbase.testclassification.MediumTests;
+import org.apache.hadoop.hbase.testclassification.LargeTests;
 import org.apache.hadoop.hbase.util.TestMiniClusterLoadSequential;
 import org.apache.hadoop.hbase.util.Threads;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestTemplate;
 
 /**
  * Uses the load tester
  */
-@Category({ IOTests.class, MediumTests.class })
+@Tag(IOTests.TAG)
+@Tag(LargeTests.TAG)
 public class TestLoadAndSwitchEncodeOnDisk extends TestMiniClusterLoadSequential {
-
-  @ClassRule
-  public static final HBaseClassTestRule CLASS_RULE =
-    HBaseClassTestRule.forClass(TestLoadAndSwitchEncodeOnDisk.class);
 
   /** We do not alternate the multi-put flag in this test. */
   private static final boolean USE_MULTI_PUT = true;
-
-  /** Un-parameterize the test */
-  @Parameters
-  public static Collection<Object[]> parameters() {
-    return Arrays.asList(new Object[][] { new Object[0] });
-  }
 
   public TestLoadAndSwitchEncodeOnDisk() {
     super(USE_MULTI_PUT, DataBlockEncoding.PREFIX);
@@ -72,14 +60,14 @@ public class TestLoadAndSwitchEncodeOnDisk extends TestMiniClusterLoadSequential
   }
 
   @Override
-  @Test
+  @TestTemplate
   public void loadTest() throws Exception {
     Admin admin = TEST_UTIL.getAdmin();
 
     compression = Compression.Algorithm.GZ; // used for table setup
     super.loadTest();
 
-    HColumnDescriptor hcd = getColumnDesc(admin);
+    ColumnFamilyDescriptor hcd = getColumnDesc(admin);
     System.err.println("\nDisabling encode-on-disk. Old column descriptor: " + hcd + "\n");
     Table t = TEST_UTIL.getConnection().getTable(TABLE);
     assertAllOnLine(t);
@@ -114,11 +102,11 @@ public class TestLoadAndSwitchEncodeOnDisk extends TestMiniClusterLoadSequential
       regions = rl.getAllRegionLocations();
     }
     for (HRegionLocation e : regions) {
-      byte[] startkey = e.getRegionInfo().getStartKey();
-      Scan s = new Scan(startkey);
+      byte[] startkey = e.getRegion().getStartKey();
+      Scan s = new Scan().withStartRow(startkey);
       ResultScanner scanner = t.getScanner(s);
       Result r = scanner.next();
-      org.junit.Assert.assertTrue(r != null && r.size() > 0);
+      assertTrue(r != null && r.size() > 0);
       scanner.close();
     }
   }
