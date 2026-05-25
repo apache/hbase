87919728b731bc5107d30c63af969cda09a7f6ef
Umeshkumar9414 <9414umeshkumar@gmail.com>
2026-05-26 00:55:53 +0530

    HBASE-30142 review comments

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/ServerManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/ServerManager.java
@@ -978,10 +978,10 @@ public class ServerManager implements ConfigurationObserver {
   /**
    * Check if a server is unknown. A server can be online, or known to be dead, or unknown to this
    * manager (i.e, not online, not known to be dead either; it is simply not tracked by the master
-   * any more, for example, a very old previous instance). A null serverName should not be
-   * considered unknown. We set regionLocation null before finding the assign candidate (in-between
-   * region transition) or while marking it OFFLINE/FAILED_OPEN For more info regarding null-check
-   * in this refer HBASE-30142
+   * anymore, for example, a very old previous instance). A serverName with value null should not be
+   * considered unknown. We set value of regionLocation to null just before finding the assign
+   * candidate (in-between region transition) or while marking it OFFLINE/FAILED_OPEN. Refer
+   * HBASE-30142.
    */
   public boolean isServerUnknown(ServerName serverName) {
     return serverName != null
