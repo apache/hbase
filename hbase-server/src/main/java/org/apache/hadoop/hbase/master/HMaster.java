ea5f4c500e2ca3d114e6d835e3d7be87614e2777
Xiao Liu <liuxiaocs@apache.org>
2026-06-11 20:39:17 +0800

    HBASE-30209 MobFileCleanerChore is not registered for online configur… (#8324)
    Signed-off-by: Junegunn Choi <junegunn@apache.org>
    Reviewed-by: Vladimir Rodionov <vladrodionov@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -1567,6 +1567,7 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
 
   private void initMobCleaner() {
     this.mobFileCleanerChore = new MobFileCleanerChore(this);
+    configurationManager.registerObserver(this.mobFileCleanerChore);
     getChoreService().scheduleChore(mobFileCleanerChore);
     this.mobFileCompactionChore = new MobFileCompactionChore(this);
     getChoreService().scheduleChore(mobFileCompactionChore);
