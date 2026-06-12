73fd39a9061e132b224f90a4ea1a1b25c14b5692
Duo Zhang <zhangduo@apache.org>
2026-06-12 22:38:51 +0800

    fix

--- hbase-server/src/test/java/org/apache/hadoop/hbase/util/TestRegionMover2.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/util/TestRegionMover2.java
@@ -17,9 +17,9 @@
  */
 package org.apache.hadoop.hbase.util;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
