fa0bc15c6c21228d44a3fa0274b449ef552442ee
Ankit Solomon <asolomon@cloudera.com>
2026-06-17 03:29:28 +0530

    [HBASE-30170] Jmx metrics for missing table in destination during replication

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -183,6 +183,7 @@ import org.apache.hadoop.hbase.master.replication.DisablePeerProcedure;
 import org.apache.hadoop.hbase.master.replication.EnablePeerProcedure;
 import org.apache.hadoop.hbase.master.replication.MigrateReplicationQueueFromZkToTableProcedure;
 import org.apache.hadoop.hbase.master.replication.RemovePeerProcedure;
+import org.apache.hadoop.hbase.master.replication.ReplicationMissingTargetTablesChore;
 import org.apache.hadoop.hbase.master.replication.ReplicationPeerManager;
 import org.apache.hadoop.hbase.master.replication.ReplicationPeerModificationStateStore;
 import org.apache.hadoop.hbase.master.replication.SyncReplicationReplayWALManager;
@@ -429,6 +430,7 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
   // The exclusive hfile cleaner pool for scanning the archive directory
   private DirScanPool exclusiveHFileCleanerPool;
   private ReplicationBarrierCleaner replicationBarrierCleaner;
+  private ReplicationMissingTargetTablesChore replicationMissingTargetTablesChore;
   private MobFileCleanerChore mobFileCleanerChore;
   private MobFileCompactionChore mobFileCompactionChore;
   private RollingUpgradeChore rollingUpgradeChore;
@@ -1778,6 +1780,13 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
       new ReplicationBarrierCleaner(conf, this, getConnection(), replicationPeerManager);
     getChoreService().scheduleChore(replicationBarrierCleaner);
 
+    if (conf.getBoolean(ReplicationMissingTargetTablesChore.ENABLED_KEY,
+      ReplicationMissingTargetTablesChore.DEFAULT_ENABLED)) {
+      replicationMissingTargetTablesChore =
+        new ReplicationMissingTargetTablesChore(this, this, conf);
+      getChoreService().scheduleChore(replicationMissingTargetTablesChore);
+    }
+
     final boolean isSnapshotChoreEnabled = this.snapshotCleanupStateStore.get();
     this.snapshotCleanerChore = new SnapshotCleanerChore(this, conf, getSnapshotManager());
     if (isSnapshotChoreEnabled) {
@@ -1964,6 +1973,7 @@ public class HMaster extends HBaseServerBase<MasterRpcServices> implements Maste
       hfileCleaners = null;
     }
     shutdownChore(replicationBarrierCleaner);
+    shutdownChore(replicationMissingTargetTablesChore);
     shutdownChore(snapshotCleanerChore);
     shutdownChore(hbckChore);
     shutdownChore(regionsRecoveryChore);
