557d9f27ba0002eabb14e446121a2f23cdf8570f
Peng Lu <lupeng@apache.org>
2026-06-17 00:29:34 +0800

    HBASE-30221 ADDENDUM use junit5 import in TestSnapshotInfo (#8361)
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>
    Signed-off-by: Xiao Liu <liuxiaocs@apache.org>
    Signed-off-by: Duo Zhang <zhangduo@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/snapshot/TestSnapshotInfo.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/snapshot/TestSnapshotInfo.java
@@ -17,8 +17,8 @@
  */
 package org.apache.hadoop.hbase.master.snapshot;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
