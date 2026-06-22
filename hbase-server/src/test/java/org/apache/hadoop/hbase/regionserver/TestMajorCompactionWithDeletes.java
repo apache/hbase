249957e8312707189df9fb96637d92edb9463a70
Duo Zhang <zhangduo@apache.org>
2026-06-22 22:55:24 +0800

    HBASE-30243 Split TestMajorCompaction (#8390)
    Signed-off-by: Xiao Liu <liuxiaocs@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestMajorCompactionWithDeletes.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestMajorCompactionWithDeletes.java
@@ -18,7 +18,6 @@
 package org.apache.hadoop.hbase.regionserver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,6 +25,7 @@ import java.util.List;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseParameterizedTestTemplate;
+import org.apache.hadoop.hbase.KeepDeletedCells;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.testclassification.LargeTests;
@@ -36,62 +36,67 @@ import org.junit.jupiter.api.TestTemplate;
 @Tag(RegionServerTests.TAG)
 @Tag(LargeTests.TAG)
 @HBaseParameterizedTestTemplate(name = "{index}: compType={0}")
-public class TestMajorCompaction extends MajorCompactionTestBase {
+public class TestMajorCompactionWithDeletes extends MajorCompactionTestBase {
 
-  public TestMajorCompaction(String compType) {
+  public TestMajorCompactionWithDeletes(String compType) {
     super(compType);
-  }
-
-  /**
-   * Run compaction and flushing memstore Assert deletes get cleaned up.
-   */
-  @TestTemplate
-  public void testMajorCompaction() throws Exception {
-    majorCompaction();
   }
 
   /**
    * Test that on a major compaction, if all cells are expired or deleted, then we'll end up with no
    * product. Make sure scanner over region returns right answer in this case - and that it just
    * basically works.
+   * @throws IOException exception encountered
    */
   @TestTemplate
-  public void testMajorCompactingToNoOutputWithReverseScan() throws IOException {
+  public void testMajorCompactingToNoOutput() throws IOException {
+    testMajorCompactingWithDeletes(KeepDeletedCells.FALSE);
+  }
+
+  /**
+   * Test that on a major compaction,Deleted cells are retained if keep deleted cells is set to true
+   * @throws IOException exception encountered
+   */
+  @TestTemplate
+  public void testMajorCompactingWithKeepDeletedCells() throws IOException {
+    testMajorCompactingWithDeletes(KeepDeletedCells.TRUE);
+  }
+
+  private void testMajorCompactingWithDeletes(KeepDeletedCells keepDeletedCells)
+    throws IOException {
     createStoreFile(r);
     for (int i = 0; i < compactionThreshold; i++) {
       createStoreFile(r);
     }
     // Now delete everything.
-    Scan scan = new Scan();
-    scan.setReversed(true);
-    try (InternalScanner s = r.getScanner(scan)) {
-      do {
-        List<Cell> results = new ArrayList<>();
-        boolean result = s.next(results);
-        assertTrue(!results.isEmpty());
-        r.delete(new Delete(CellUtil.cloneRow(results.get(0))));
-        if (!result) {
-          break;
-        }
-      } while (true);
-    }
+    InternalScanner s = r.getScanner(new Scan());
+    int originalCount = 0;
+    do {
+      List<Cell> results = new ArrayList<>();
+      boolean result = s.next(results);
+      r.delete(new Delete(CellUtil.cloneRow(results.get(0))));
+      if (!result) break;
+      originalCount++;
+    } while (true);
+    s.close();
     // Flush
     r.flush(true);
+
+    for (HStore store : this.r.stores.values()) {
+      ScanInfo old = store.getScanInfo();
+      ScanInfo si = old.customize(old.getMaxVersions(), old.getTtl(), keepDeletedCells);
+      store.setScanInfo(si);
+    }
     // Major compact.
     r.compact(true);
-    scan = new Scan();
-    scan.setReversed(true);
+    s = r.getScanner(new Scan().setRaw(true));
     int counter = 0;
-    try (InternalScanner s = r.getScanner(scan)) {
-      do {
-        List<Cell> results = new ArrayList<>();
-        boolean result = s.next(results);
-        if (!result) {
-          break;
-        }
-        counter++;
-      } while (true);
-    }
-    assertEquals(0, counter);
+    do {
+      List<Cell> results = new ArrayList<>();
+      boolean result = s.next(results);
+      if (!result) break;
+      counter++;
+    } while (true);
+    assertEquals(keepDeletedCells == KeepDeletedCells.TRUE ? originalCount : 0, counter);
   }
 }
