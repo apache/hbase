cd25a5a2194036550549288f3260d98ab860696d
Peng Lu <lupeng@apache.org>
2026-08-26 18:20:24 +0800

    HBASE-30336 An extra empty MOB region dir is created under the table dir when restoring a MOB table from snapshot (#8567)
    Signed-off-by: Xiao Liu <liuxiaocs@apache.org>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/snapshot/TestMobRestoreSnapshotHelper.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/snapshot/TestMobRestoreSnapshotHelper.java
@@ -17,15 +17,24 @@
  */
 package org.apache.hadoop.hbase.snapshot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseTestingUtil;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.mob.MobConstants;
+import org.apache.hadoop.hbase.mob.MobUtils;
 import org.apache.hadoop.hbase.snapshot.MobSnapshotTestingUtils.SnapshotMock;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.CommonFSUtils;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -54,5 +63,48 @@ public class TestMobRestoreSnapshotHelper extends TestRestoreSnapshotHelper {
     Table table = MobSnapshotTestingUtils.createMobTable(TEST_UTIL, tableName, column);
     TEST_UTIL.loadTable(table, column);
     TEST_UTIL.getAdmin().snapshot(snapshotName, tableName);
+  }
+
+  @Test
+  public void testRestoreMobTableFromSnapshot() throws IOException {
+    TableName mobTableName = TableName.valueOf("testRestoreMobTable");
+    String snapshotName = "testRestoreMobTable_snapshot";
+    createTableAndSnapshot(mobTableName, snapshotName);
+    assertTrue(TEST_UTIL.getAdmin().tableExists(mobTableName));
+    assertTrue(MobUtils.hasMobColumns(TEST_UTIL.getAdmin().getDescriptor(mobTableName)));
+    assertTrue(TEST_UTIL.getAdmin().listSnapshots().stream()
+      .anyMatch(snapshotDescription -> snapshotName.equals(snapshotDescription.getName())));
+
+    // Clone the snapshot to a new MOB table
+    TableName newMobTableName = TableName.valueOf("newTestRestoreMobTable");
+    TEST_UTIL.getAdmin().cloneSnapshot(snapshotName, newMobTableName);
+    assertTrue(TEST_UTIL.getAdmin().tableExists(newMobTableName));
+    assertTrue(MobUtils.hasMobColumns(TEST_UTIL.getAdmin().getDescriptor(newMobTableName)));
+
+    Path hbaseRootDir = TEST_UTIL.getDefaultRootDirPath();
+    Path mobRegionPath = MobUtils.getMobTableDir(hbaseRootDir, newMobTableName);
+    assertTrue(fs.exists(mobRegionPath));
+    Path errorMobRegionPathInTableDir =
+      new Path(CommonFSUtils.getTableDir(hbaseRootDir, newMobTableName),
+        MobUtils.getMobRegionInfo(newMobTableName).getEncodedName());
+    assertFalse(fs.exists(errorMobRegionPathInTableDir));
+
+    try (Table originMobTable = TEST_UTIL.getConnection().getTable(mobTableName);
+      Table clonedMobTable = TEST_UTIL.getConnection().getTable(newMobTableName)) {
+      assertEquals(HBaseTestingUtil.countRows(originMobTable),
+        HBaseTestingUtil.countRows(clonedMobTable));
+    }
+
+    // Delete the original MOB table and restore it from the snapshot
+    TEST_UTIL.deleteTable(mobTableName);
+    assertFalse(TEST_UTIL.getAdmin().tableExists(mobTableName));
+    TEST_UTIL.getAdmin().cloneSnapshot(snapshotName, mobTableName);
+    assertTrue(TEST_UTIL.getAdmin().tableExists(mobTableName));
+    assertTrue(MobUtils.hasMobColumns(TEST_UTIL.getAdmin().getDescriptor(mobTableName)));
+    mobRegionPath = MobUtils.getMobTableDir(hbaseRootDir, mobTableName);
+    assertTrue(fs.exists(mobRegionPath));
+    errorMobRegionPathInTableDir = new Path(CommonFSUtils.getTableDir(hbaseRootDir, mobTableName),
+      MobUtils.getMobRegionInfo(mobTableName).getEncodedName());
+    assertFalse(fs.exists(errorMobRegionPathInTableDir));
   }
 }
