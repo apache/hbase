4969c2d5ea2ebfe23e3f2dcd9fb0589b0d97f73e
Ray Mattingly <rmattingly@apache.org>
2026-09-02 10:16:16 -0400

    Revert "HBASE-30348 Throttles should work for system tables (excluding quotas, meta) (#8585) (#8590) (#8595)"
    This reverts commit 2849b8b5be9cd9e6440db15ddd7c6b0cba5407e6.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/RegionServerRpcQuotaManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/quotas/RegionServerRpcQuotaManager.java
@@ -137,7 +137,7 @@ public class RegionServerRpcQuotaManager implements RpcQuotaManager, Configurati
    */
   public OperationQuota getQuota(final UserGroupInformation ugi, final TableName table,
     final int blockSizeBytes) {
-    if (isQuotaEnabled() && !QuotaUtil.isThrottleExempt(table) && isRpcThrottleEnabled()) {
+    if (isQuotaEnabled() && !table.isSystemTable() && isRpcThrottleEnabled()) {
       UserQuotaState userQuotaState = quotaCache.getUserQuotaState(ugi);
       QuotaLimiter userLimiter = userQuotaState.getTableLimiter(table);
 
