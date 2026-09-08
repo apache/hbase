56ac75c4b62e84b0336255859c505418add9839c
liuxiaocs7 <liuxiao2103@qq.com>
2026-09-09 04:50:57 +0800

    update

--- hbase-mapreduce/src/main/java/org/apache/hadoop/hbase/mapreduce/TableSnapshotInputFormatImpl.java
+++ hbase-mapreduce/src/main/java/org/apache/hadoop/hbase/mapreduce/TableSnapshotInputFormatImpl.java
@@ -363,7 +363,7 @@ public class TableSnapshotInputFormatImpl {
         continue;
       }
       // The mob region is a dummy region used only to organise mob files under mobdir. It has no
-      // region directory under the table dir to open. See HBASE-30365 and HBASE-30368.
+      // region directory under the table dir to open. See HBASE-30365.
       if (MobUtils.isMobRegionInfo(hri)) {
         continue;
       }
