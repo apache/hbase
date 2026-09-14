096beca14c4dcb8f566ea26194aa6d8558720f3b
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-09-14 10:05:41 -0400

    Move test_*.py to test dir; Add test_read_replica_feature.py; Use pytest to run test scripts
    Change-Id: I559c1c95a708620848e2360fb957671210c5cf64

--- dev-support/read-replica/python/test/test_create_drop_behavior.py
+++ dev-support/read-replica/python/test/test_create_drop_behavior.py
@@ -26,8 +26,8 @@ tables cannot be created/dropped on the replica cluster.
 import argparse
 
 from python.src.logger_config import get_logger
-from python.src.utils import (add_common_skip_table_cleanup_arg, clean_up_tables, load_env_and_set_up_clients,
-                              log_script_start, log_script_end)
+from python.src.utils import (add_common_drop_existing_tables_arg, add_common_new_containers_arg, clean_up_tables,
+                              log_script_start, log_script_end, reset_docker_container_environment)
 
 logger = get_logger(__name__)
 
@@ -73,26 +73,35 @@ def test_table_creation_behavior(active_cluster, replica_cluster, table_name, co
     replica_cluster.assert_table_does_not_exist(table_name)
 
 
-def main():
+def run_test(drop_existing_tables: bool = False, new_containers: bool = False):
     start_time = log_script_start(__file__, logger)
 
-    parser = argparse.ArgumentParser()
-    parser = add_common_skip_table_cleanup_arg(parser)
-    args = parser.parse_args()
+    active_cluster, replica_cluster = reset_docker_container_environment(
+        new_containers=new_containers,
+        cluster1_name="Active Cluster",
+        cluster2_name="Replica Cluster"
+    )
 
-    active_cluster, replica_cluster = load_env_and_set_up_clients(cluster1_name="Active Cluster",
-                                                                  cluster2_name="Read-Replica Cluster")
     table_name = "t1"
     column_family = "cf"
-    if not args.skip_table_cleanup_on_start:
-        # Delete any lingering tables
-        logger.info(f"Checking if table '{table_name}' already exists on {active_cluster.name} "
-                    f"and dropping it if necessary")
+    if drop_existing_tables:
         clean_up_tables(active_cluster, replica_cluster)
 
     test_table_creation_behavior(active_cluster, replica_cluster, table_name, column_family)
 
     log_script_end(__file__, logger, start_time)
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser()
+    parser = add_common_drop_existing_tables_arg(parser)
+    parser = add_common_new_containers_arg(parser)
+    parsed_args = parser.parse_args(args)
+
+    run_test(
+        drop_existing_tables=parsed_args.drop_existing_tables,
+        new_containers=parsed_args.new_containers
+    )
 
 
 if __name__ == "__main__":
