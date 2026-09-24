313c168ed1139c4df7895bfd6b24c8dfda3423ad
Ma Zhengxuan <49856528+Sigma-Ma@users.noreply.github.com>
2026-09-24 13:36:38 +0800

    HBASE-29640 Add metric for oldest procedure age (#8689) (#8563)
    Signed-off-by: Viraj Jasani <vjasani@apache.org>
    Signed-off-by: Umesh <9414umeshkumar@gmail.com>
    Signed-off-by: Aman Poonia <apoonia@salesforce.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/MetricsMasterWrapperImpl.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/MetricsMasterWrapperImpl.java
@@ -25,8 +25,12 @@ import java.util.Map.Entry;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.master.procedure.MasterProcedureEnv;
+import org.apache.hadoop.hbase.procedure2.Procedure;
+import org.apache.hadoop.hbase.procedure2.ProcedureExecutor;
 import org.apache.hadoop.hbase.quotas.QuotaObserverChore;
 import org.apache.hadoop.hbase.quotas.SpaceQuotaSnapshot;
+import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
 import org.apache.hadoop.hbase.zookeeper.ZKWatcher;
 import org.apache.yetus.audience.InterfaceAudience;
 
@@ -173,6 +177,23 @@ public class MetricsMasterWrapperImpl implements MetricsMasterWrapper {
   @Override
   public long getNumWALFiles() {
     return master.getNumWALFiles();
+  }
+
+  @Override
+  public Map<String, Long> getOldestProcedureAgeByType() {
+    ProcedureExecutor<MasterProcedureEnv> procedureExecutor = master.getMasterProcedureExecutor();
+    if (procedureExecutor == null) {
+      return Collections.emptyMap();
+    }
+    long now = EnvironmentEdgeManager.currentTime();
+    Map<String, Long> oldestProcedureAgeByType = new HashMap<>();
+    for (Procedure<MasterProcedureEnv> procedure : procedureExecutor.getProcedures()) {
+      if (!procedure.isFinished()) {
+        long age = Math.max(0L, now - procedure.getSubmittedTime());
+        oldestProcedureAgeByType.merge(procedure.getClass().getSimpleName(), age, Math::max);
+      }
+    }
+    return oldestProcedureAgeByType;
   }
 
   @Override
